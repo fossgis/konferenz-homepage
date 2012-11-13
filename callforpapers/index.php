6dcb6b4a34a17fd4cf8bec8ab1d72431600ff9e3
Oliver Roick <oliver.roick@gmail.com>
2012-11-13 08:52:29 +0100

    Initial Commit

---
+++ callforpapers/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2013 - Call for Papers</title>
+		<meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. GIScience, Heidelberg University - http://giscience.uni-hd.de/">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
+        <meta name="date" content="2008-04-15T20:00:01+01:00">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="call">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+            <div id="content">
+                <?php include('../inc/nav.inc'); ?>
+				<div id="mainContent">
+					<h2>Call for Papers</h2>
+					
+					<p>Der Call for Papers wird am 19. November 2012 veröffentlicht.</p>
+				</div>
+			</div>
+    	</div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2013</title>
+        <meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. GIScience, Heidelberg University - http://giscience.uni-hd.de/">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
+        <meta name="date" content="2008-04-15T20:00:01+01:00">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="home">
+        <div id="wrap">
+            <?php include('inc/head.inc'); ?>
+            <div id="content">
+                <?php include('inc/nav.inc'); ?>
+				
+                <div id="mainContent">
+                    <h2>Willkommen bei der FOSSGIS-Konferenz</h2>
+                    <p>
+                        FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.
+                    </p>
+                    <p>
+                        Die FOSSGIS Konferenz 2013 wird vom gemeinnützigen Verein <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a>
+                        und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a>
+                        in Zusammenarbeit mit der <a href="http://www.hsr.ch/index.php">HSR Hochschule für Technik</a> in Rapperswil in der Schweiz organisiert.
+                    </p>
+                    <p class="logos">
+                        <a href="http://www.fossgis.de/"><img src="img/Fossgis_neu.png" width="168" height="50"></a><a href="http://www.osgeo.org/"><img src="img/Osgeo_neu.png" width="150" height="67"></a><a href="http://www.openstreetmap.de/"><img src="img/osm_2012.png" width="258	" height="60"></a>
+                    </p>
+                    <h2>Sponsoren</h2>
+					<p>
+                        Sie möchten die FOSSGIS-Konferenz als Sponsor unterstützen? Lesen Sie bitte die <a href="http://www.fossgis.de/konferenz/wiki/2012/Sponsoren_und_Aussteller">Informationen zu Sponsoren und Ausstellern</a>
+                        oder wenden sie sich direkt an <a href="mailto:katja.haferkorn@fossgis.de">Frau Haferkorn</a>.
+                    </p>
+                </div>
+            </div>
+			
+        </div>
+    </body>
+</html>
