a896787b4f13c2ea01b66a24a75142e3104170de
Oliver Roick <oliver.roick@gmail.com>
2012-11-19 18:01:35 +0100

    Added CfP

--- callforpapers/index.php
+++ callforpapers/index.php
@@ -17,7 +17,27 @@
 				<div id="mainContent">
 					<h2>Call for Papers</h2>
 					
-					<p>Der Call for Papers wird am 19. November 2012 veröffentlicht.</p>
+					<p>Du forschst, du entwickelst, du lehrst, du benutzt Open-Source-Geo-Software oder freie Geodaten? Du hast neue Ideen in diesem Themenbereich oder bist bereits dabei, freie Software und freie Geodaten zu nutzen oder zu entwickeln? Dann bist du auf unserer Konferenz - der FOSSGIS-Konferenz 2013 - genau richtig.</p>
+
+                    <p><strong>WIR BIETEN:</strong> Eine Plattform für deine Idee, dein Projekt, deinen Erfahrungsbericht auf der größten deutschsprachigen Anwenderkonferenz für freie Geoinformationssysteme und freie Geodaten. Im Jahr 2013 findet die FOSSGIS vom 12. bis 14. Juni auf dem Gelände der HSR Hochschule für Technik in Rapperswil in der Schweiz statt. An der Konferenz 2012 nahmen über 500 Besucher teil.</p>
+
+                    <p><strong>WIR SUCHEN:</strong> Deine Idee. Dein Projekt. Deinen Erfahrungsbericht. Dein Thema. Genauer gesagt, suchen wir anspruchsvolle Vorträge, spannende Diskussionen, praxisorientierte Workshops für unser Programm. Vorträge zum Thema freie Geodaten, zum Beispiel OpenStreetMap, sind ebenso möglich wie beispielsweise Beiträge zu Softwarelösungen aus dem Bereich WebGIS, Desktop GIS, Geodatenbanken oder Location-Based-Services. Bewirb dich jetzt mit deinem Vortrag, deinem Lightning Talk oder deinem Workshop beim Call for Papers.</p>
+
+                    <p><strong>ABSTRACT:</strong> Die Einreichung deines Abstracts für die FOSSGIS 2013 ist ab sofort bis zum 31. Januar 2013 möglich. Dein Abstract sollte ca. 500 Zeichen umfassen. Ob deine Einreichung angenommen wurde, erfährst du ca. im Februar 2013. Neben einem kompletten Vortrag besteht auch die Möglichkeit einen Lightning Talk - also einen 5 Minuten-Vortrag - zu halten. Zudem können Posters gestaltet werden.</p>
+
+                    <p><strong>WER WIR SIND:</strong> Veranstaltet wird die Konferenz vom FOSSGIS e.V., von der OSGeo Foundation und der HSR Hochschule für Technik Rapperswil (Schweiz).</p>
+
+                    <p>Unsere Konferenz lebt von ihren deinen Beiträgen und von deinem Besuch. Werde Teil der Konferenz - wir freuen uns auf dich!</p>
+
+                    <p>Hast du noch Fragen? Wir haben versucht, in unseren <a href="http://www.fossgis.de/wiki/2013/KonferenzHomepage#Call_for_Papers_-_FAQ">FAQs</a> alle wichtigen Fragen zu beantworten. Falls du noch weitere Fragen hast, schreib uns einfach eine <a href="mailto:Konferenz liste <konferenz-liste@fossgis.de>">E-Mail</a>.</p>
+
+                    <h3>Note to English speakers</h3>
+
+                    <p>The FOSSGIS-conference is the largest german-language conference for Free and Open Source Software for GIS and Free Geodata hosting about 500 participants.</p>
+                    <p>Today, the Conference Committee announced the "Call for papers" for 2013. Because the conference-language is german, the CfP is either.</p>
+                    <p>The program committee will, however, also consider applications for talks or workshops held in English if they are deeemed to add to the quality of the conference. So if you don't speak German, but are a FOSS/Open Data celebrity, or have a story that only you can tell, please do submit your talk. We are unlikely to be able to provide interpreters, but we'll make sure you don't get lost in Germany.</p>
+                    <p>Please be aware that you can submit paper until January, 31. 2013.</p>
+                    <p>We are looking forward to see you in Rapperwill (Switzerland) June 12-14.</p>
 				</div>
 			</div>
     	</div>
