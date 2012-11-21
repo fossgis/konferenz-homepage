60b1418d2ddede039bc73e25c6dbc233b7f86260
Oliver Roick <oliver.roick@gmail.com>
2012-11-21 11:56:25 +0100

    Updates to CfP

--- callforpapers/faq.php
+++ callforpapers/faq.php
@@ -18,49 +18,49 @@
 					<h2>Call for Papers - FAQ</h2>
 					<dl>
                         <dt>Ich möchte als eine Vortrag auf der FOSSGIS-Konfernez halten, was muss ich dafür tun?</dt>
-                        <dd>Reiche einen Abstract, also eine Kurz-Zusammenfassung mit max. 1500 Zeichen ein. Dieser wird dann nach der Einreichungsfrist vom Programmkommitee begutachtet und bewertet.</dd>
+                        <dd>Reiche einen Abstract, also eine Kurz-Zusammenfassung mit max. 1500 Zeichen ein. Dieser wird dann nach der Einreichungsfrist vom Programmkomitee begutachtet und bewertet.</dd>
 
                         <dt>Welche Informationen benötigt ihr über mich?</dt>
-                        <dd>Wir benötigen deinen Namen und deine E-Mail-Adresse. Das alles kannst du bequem im Pentabarf eintragen.</dd>
+                        <dd>Wir benötigen deinen Namen und deine E-Mail-Adresse. Das alles kannst du bequem im <a href="http://pb.fossgis.de/submission/FOSSGIS2013/">Pantabarf</a> eintragen.</dd>
 
                         <dt>Bekomme ich eine E-Mail dass mein Vortrag korrekt eingereicht ist?</dt>
-                        <dd>Nein der Vortrag landet direkt nach dem Speichern in unserem System. So kannst du auch noch bis zur Einreichungsfrist Änderungen oder Verbesserungen duchführen.</dd>
+                        <dd>Nein der Vortrag landet direkt nach dem Speichern in unserem System. So kannst du auch noch bis zur Einreichungsfrist Änderungen oder Verbesserungen durchführen.</dd>
 
                         <dt>Wie viel Zeit habe ich für mein Vortrag?</dt>
-                        <dd>Ein Vortrag dauert 20 Minuten. Imm Anschluss stehen dann noch 5 Minuten Zeit für Fragen zur Verfügung. 3 Vorträge werden jeweils zu einer Session zusammengefasst.</dd>
+                        <dd>Ein Vortrag dauert 20 Minuten. Im Anschluss stehen dann noch 5 Minuten Zeit für Fragen zur Verfügung. 3 Vorträge werden jeweils zu einer Session zusammengefasst.</dd>
 
-                        <dt>Nach welchen Kriterien werden die Voträge ausgewählt?</dt>
-                        <dd>Das Programmkommitee entscheidet über verschiedene Auswahlkriterien. So sind Qualität und Vollständigkeit und auch die Relevanz des Themas für das Gesamtprogramm wichtig. Zudem versuchen wir die Wünschen der Besucher aus dem letzten Jahr mit in die Auswahl einfließen zu lassen.</dd>
+                        <dt>Nach welchen Kriterien werden die Vorträge ausgewählt?</dt>
+                        <dd>Das Programmkomitee entscheidet über verschiedene Auswahlkriterien. So sind Qualität und Vollständigkeit und auch die Relevanz des Themas für das Gesamtprogramm wichtig. Zudem versuchen wir die Wünsche der Besucher aus dem letzten Jahr mit in die Auswahl einfließen zu lassen. Eine große Rolle spielt auch der Open-Source-Schwerpunkt der FOSSGIS - im Idealfall kann der Zuhörer sich nach einem interessanten Vortrag die besprochene Software im Quelltext herunterladen und selber ausprobieren. Vorträge, die proprietäre Software behandeln, werden nur in Ausnahmefällen angenommen.</dd>
 
                         <dt>Bis wann kann ich mich mit meinem Vortrag bewerben?</dt>
-                        <dd>Dein Vortrag kannst du bist zum 31.01.2013 einreichen.</dd>
+                        <dd>Deinen Vortrag kannst du bis zum 31.01.2013 einreichen über das <a href="http://pb.fossgis.de/submission/FOSSGIS2013/">Pantabarf</a> einreichen.</dd>
 
                         <dt>Wie lang oder kurz muss die Einreichung für meinen Vortrag sein?</dt>
-                        <dd>Die Beschreibung für deinen Vortrag sollte max. 1500 Zeichen haben. Nicht mehr und nicht weniger. Das Programmkommitee muss sich einen Eindruck machen können worum es geht.</dd>
+                        <dd>Die Beschreibung für deinen Vortrag sollte max. 1500 Zeichen haben. Nicht mehr und nicht weniger. Das Programmkomitee muss sich einen Eindruck machen können worum es geht.</dd>
 
                         <dt>Wie erfahre ich, ob mein Beitrag angenommen wurde?</dt>
                         <dd>Wir melden uns per E-Mail bei dir bis Mitte Februar.</dd>
 
                         <dt>Wann wird das finale Programm veröffentlicht?</dt>
-                        <dd>Das finale Programm wird Mitte Februar veröffentlicht. So kannst du früh genug deine Anreise planen.    </dd>
+                        <dd>Das finale Programm wird Mitte Februar veröffentlicht. So kannst du früh genug deine Anreise planen. </dd>
 
                         <dt>Wann muss mein Vortrag dann fertig sein?</dt>
-                        <dd>Theoretisch nur eine Sekunde vor Beginn deines Vortrages. Den ausgearbeiteten Vortrag benötigen wir vorher nicht. Deine Kurzbeschreibung wird jedoch auf der Website stehen und sollte daher schon bei Einreichung möglichst informativ formuliert sein. Zudem solltest du einen langen Beitrag für den Tagungsband vorher reinreichen.</dd>
+                        <dd>Spätestens zu Beginn deiner Session. Den ausgearbeiteten Vortrag benötigen wir vorher nicht. Deine Kurzbeschreibung wird jedoch auf der Website veröffentlicht und sollte daher schon bei Einreichung möglichst informativ sein. Zudem solltest du einen langen Beitrag für den Tagungsband vorher reinreichen.</dd>
 
-                        <dt>Wieso muss ich mein Tagungsband Beitrag vorher einrichen?</dt>
+                        <dt>Wieso muss ich meinen Tagungsband-Beitrag vorher einreichen?</dt>
                         <dd>Der Tagungsband wird gedruckt und allen Teilnehmer zur Verfügung gestellt. Hierfür ist ein gewisser Vorlauf nötig.</dd>
 
                         <dt>Erhalte ich als Vortragende/r freien Eintritt?</dt>
                         <dd>Natürlich.</dd>
 
                         <dt>Bekomme ich ein Honorar für meinen Votrag?</dt>
-                        <dd>Nein.   </dd>
+                        <dd>Nein.  </dd>
 
                         <dt>Übernehmt ihr Hotel oder Fahrtkosten?</dt>
                         <dd>Nein, auch das können wir leider nicht tun.</dd>
 
                         <dt>Kann ich als Vertreter eines Unternehmens mit einem Vortrag teilnehmen?</dt>
-                        <dd>Natürlich kannst du als Vertreter eines Unternehmens am CfP teilnehmen. Reine Unternehmens- oder Produktpräsentationen lehnen wir jedoch in der Regel ebenso wie PR-Vorträge ab. Falls dein Unternehmen aber Interesse an einer Zusammenarbeit hat z.B. durch Sponsoring, setzt dich einfach mit uns Verbindung setzten.</dd>
+                        <dd>Natürlich kannst du als Vertreter eines Unternehmens am CfP teilnehmen. Reine Unternehmens- oder Produktpräsentationen lehnen wir jedoch in der Regel ebenso wie PR-Vorträge ab. Falls dein Unternehmen aber Interesse an einer Zusammenarbeit hat z.B. durch Sponsoring, setzt dich einfach mit uns Verbindung.</dd>
 
                     </dl>
 				</div>
--- callforpapers/index.php
+++ callforpapers/index.php
@@ -23,20 +23,23 @@
 
                     <p><strong>WIR SUCHEN:</strong> Deine Idee. Dein Projekt. Deinen Erfahrungsbericht. Dein Thema. Genauer gesagt, suchen wir anspruchsvolle Vorträge, spannende Diskussionen, praxisorientierte Workshops für unser Programm. Vorträge zum Thema freie Geodaten, zum Beispiel OpenStreetMap, sind ebenso möglich wie beispielsweise Beiträge zu Softwarelösungen aus dem Bereich WebGIS, Desktop GIS, Geodatenbanken oder Location-Based-Services. Bewirb dich jetzt mit deinem Vortrag, deinem Lightning Talk oder deinem Workshop beim Call for Papers.</p>
 
-                    <p><strong>ABSTRACT:</strong> Die Einreichung deines Abstracts für die FOSSGIS 2013 ist ab sofort bis zum 31. Januar 2013 möglich. Dein Abstract sollte ca. 500 Zeichen umfassen. Ob deine Einreichung angenommen wurde, erfährst du ca. im Februar 2013. Neben einem kompletten Vortrag besteht auch die Möglichkeit einen Lightning Talk - also einen 5 Minuten-Vortrag - zu halten. Zudem können Posters gestaltet werden.</p>
+                    <p><strong>ABSTRACT:</strong> Die Einreichung deines Abstracts für die FOSSGIS 2013 ist ab sofort bis zum 31. Januar 2013 über <a href="http://pb.fossgis.de/submission/FOSSGIS2013/">Pantabarf</a> möglich. Dein Abstract sollte ca. 1500 Zeichen umfassen. Ob deine Einreichung angenommen wurde, erfährst du ca. im Februar 2013. Neben einem kompletten Vortrag besteht auch die Möglichkeit einen Lightning Talk zu halten. Lightning-Talks sind Kurzpräsentationen mit einer zeitlichen Dauer von nicht mehr als 5 Minuten. Sie dienen der Auseinandersetzung mit einem Thema, dem (gerne auch humorvoll gemeinten) Hinweis auf eine Problemstellung und der Anregung zu anschließenden Diskussionen. Zudem können Posters gestaltet werden.</p>
 
                     <p><strong>WER WIR SIND:</strong> Veranstaltet wird die Konferenz vom FOSSGIS e.V., von der OSGeo Foundation und der HSR Hochschule für Technik Rapperswil (Schweiz).</p>
 
                     <p>Unsere Konferenz lebt von ihren deinen Beiträgen und von deinem Besuch. Werde Teil der Konferenz - wir freuen uns auf dich!</p>
 
-                    <p>Hast du noch Fragen? Wir haben versucht, in unseren <a href="faq.php">FAQs</a> alle wichtigen Fragen zu beantworten. Falls du noch weitere Fragen hast, schreib uns einfach eine <a href="mailto:Konferenz liste <konferenz-liste@fossgis.de>">E-Mail</a>.</p>
+                    <p>Hast du noch Fragen? Wir haben versucht, in unseren <a href="faq.php">FAQs</a> alle wichtigen Fragen zu beantworten. Falls du noch weitere Fragen hast, schreib uns einfach eine <a href="mailto:konferenz-liste@fossgis.de">E-Mail</a>.</p>
 
                     <h3>Note to English speakers</h3>
 
                     <p>The FOSSGIS-conference is the largest german-language conference for Free and Open Source Software for GIS and Free Geodata hosting about 500 participants.</p>
+
                     <p>Today, the Conference Committee announced the "Call for papers" for 2013. Because the conference-language is german, the CfP is either.</p>
-                    <p>The program committee will, however, also consider applications for talks or workshops held in English if they are deeemed to add to the quality of the conference. So if you don't speak German, but are a FOSS/Open Data celebrity, or have a story that only you can tell, please do submit your talk. We are unlikely to be able to provide interpreters, but we'll make sure you don't get lost in Germany.</p>
-                    <p>Please be aware that you can submit paper until January, 31. 2013.</p>
+
+                    <p>The program committee will, however, also consider applications for talks or workshops held in English if they are deeemed to add to the quality of the conference. So if you don't speak German, but are a FOSS/Open Data celebrity, or have a story that only you can tell, please do submit your talk. We are unlikely to be able to provide interpreters, but we'll make sure you don't get lost in Switzerland.</p>
+
+                    <p>Please be aware that you can submit paper until January, 31. 2013. You can submit your abstract using <a href="http://pb.fossgis.de/submission/FOSSGIS2013/">Pantabarf</a>.</p>
                     <p>We are looking forward to see you in Rapperwill (Switzerland) June 12-14.</p>
 				</div>
 			</div>
