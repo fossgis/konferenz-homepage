a64451579cb66124a21e0b8fa49fab5809630a3f
Oliver Roick <oliver.roick@gmail.com>
2013-07-07 22:10:12 +0200

    Added social media stuff

--- 2014/index.php
+++ 2014/index.php
@@ -9,6 +9,9 @@
 
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
         <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
+		
+		<script type="text/javascript" src="js/twitterwall/jquery-1.8.2.js"></script>
+		<script type="text/javascript" src="js/twitterwall/twitter.js"></script>
     </head>
     <body id="home">
         <div id="wrap">
@@ -23,15 +26,29 @@
                 <h2>Willkommen zur FOSSGIS-Konferenz 2014</h2>
 				<p>FOSSGIS steht für Freie und Open Source Software für Geoinformationssysteme. Die FOSSGIS-Konferenz ist im deutschsprachigenRaum die führende Konferenz zu diesem Thema. Sie behandelt neben FOSSGIS auch die Themen Open Data und OpenStreetMap.</p>
 				
-				<p>Die FOSSGIS Konferenz 2013 wird vom gemeinnützigen Verein <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a> in Zusammenarbeit mit der <a href="http://www.beuth-hochschule.de/">Beuth Hochschule für Technik Berlin</a> organisiert.</p>
-				
 				<div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/Fossgis_neu.png" valign="middle"></a></div>
 				<div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/Osgeo_neu.png" valign="middle"></a></div>
 				<div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm_2012.png" valign="middle"></a></div>
 				<div class="sponsor organizer"><a href="http://www.beuth-hochschule.de/"><img src="img/beuth.png" valign="middle"></a></div>
+				
+				<p>Die FOSSGIS Konferenz 2014 wird vom gemeinnützigen Verein <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a> in Zusammenarbeit mit der <a href="http://www.beuth-hochschule.de/">Beuth Hochschule für Technik Berlin</a> organisiert.</p>
+				    
                     
-                    
-                <h2>Sponsoren</h2>
+                <h2>Folgen sie uns</h2>
+				<div id="twitter">
+					<a href="http://twitter.com/fossgis2013"><img src="img/twitter.svg" width="55" height="55"></a>
+					<div id="twitterwall">
+						<div class="arrow"></div>
+					</div>
+				</div>
+				<div id="facebook">
+					<a href="http://twitter.com/fossgis2013"><img src="img/facebook2.svg" width="55" height="55"></a>
+					<div id="facebooklink">
+						<div class="arrow"></div>
+					</div>
+				</div>
+				
+				<h2>Sponsoren</h2>
 				<p>Sie möchten die FOSSGIS-Konferenz als Silber, Bronze oder Mediasponsor unterstützen? Lesen Sie bitte die <a href="http://www.fossgis.de/wiki/2013/Sponsoren_und_Aussteller">Informationen zu Sponsoren und Ausstellern</a>
                        		 oder wenden sie sich direkt an <a href="mailto:katja.haferkorn@fossgis.de">Frau Haferkorn</a>.</p>
                 </div>
