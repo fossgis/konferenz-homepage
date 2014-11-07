06026d4a50bd84e5b66bd5d0031ea37b16bf6136
Oliver Roick <oliver.roick@gmail.com>
2014-11-07 17:35:54 +0000

    Fixed Twitter for 2015

--- 2015/index.php
+++ 2015/index.php
@@ -35,36 +35,38 @@
                     <div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm_2012.png" valign="middle"></a></div>
                     <div class="sponsor organizer"><a href="http://www.uni-muenster.de/Geoinformatics/"><img src="img/ifgi.jpg" valign="middle"></a></div>
                     <div class="sponsor organizer"><a href="http://www.uni-muenster.de/de/"><img src="img/wwu.jpg" valign="middle"></a></div>
-                    <!--<div class="sponsor organizer"><a href="http://52north.org/"><img src="img/52n.png" valign="middle"></a></div>
-                </div>-->
+                    <!--<div class="sponsor organizer"><a href="http://52north.org/"><img src="img/52n.png" valign="middle"></a></div>-->
+                </div>
                 <p class="clear"></p>
                 <p>Ziel der jährlich stattfindenden Konferenz mit mehr als 400 Teilnehmern ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie Open Street Map.</p>
 
-                <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
-
-
-                <!-- <h2>FOSSGIS 2015 auf Twitter</h2>
+                <h2>FOSSGIS 2015 auf Twitter</h2>
                 <div id="twitter">
-                    <a href="http://twitter.com/fossgis2014"><img src="img/twitter.png" width="55" height="45"></a>
+                    <a href="http://twitter.com/fossgis2015"><img src="img/twitter.png" width="55" height="45"></a>
                     <div id="twitterwall">
                         <div class="arrow"></div>
                     </div>
-                </div> -->
+                </div>
+
+ 		  <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+
 
                 <h2>Sponsoren</h2>
 
-                <!-- <h3>Goldsponsor</h3>
-                <div class="sponsor gold"><a href="http://www.omniscale.de/" target="_blank"><img src="img/s/omniscale.png" valign="middle"></a></div> -->
+                <h3>Goldsponsor</h3>
+                <div class="sponsor gold"><a href="http://www.wheregroup.com/" target="_blank"><img src="img/s/001_wheregroup.jpg" valign="middle" width="330" height="88"></a></div>
 
                 <!-- ACHTUNG: Logos für Silbersponsoren sollten nicht mehr als 220px breit und 75px hoch sein. -->
 
-                <!-- <h3>Silbersponsoren</h3>
-                <div class="sponsor silber" id="omniscale"><a href="http://www.omniscale.de/" target="_blank"><img src="img/s/omniscale.png" valign="middle" width="220" height="45"></a></div> -->
+                <h3>Silbersponsoren</h3>
+                <div class="sponsor silber" id="52north"><a href="http://52north.org/" target="_blank"><img src="img/s/101_52n.PNG" valign="middle" width="200" height="72"></a></div>
+ 		  <div class="sponsor silber" id="camptocamp"><a href="www.camptocamp.com" target="_blank"><img src="img/s/102_Camp2Camp.PNG" valign="middle" width="200" height="97"></a></div>
+
 
                 <!-- ACHTUNG: Logos für Bronzesponsoren sollten nicht mehr als 150px breit und 75px hoch sein. -->
 
-                <!-- <h3>Bronzesponsoren</h3>
-                <div class="sponsor bronze" id="csgis"><a href="http://www.csgis.de/" target="_blank"><img valign="middle" src="img/s/CSGIS.PNG" width="133" height="60"></a></div> -->
+                <h3>Bronzesponsoren</h3>
+                <div class="sponsor gold" id="mapmedia"><a href="http://www.mapmedia.de/" target="_blank"><img valign="middle" src="img/s/201_MapMedia.png" width="150" height="26"></a></div>
 
 				<p class="clear">Sie möchten die FOSSGIS-Konferenz als Gold, Silber, Bronze oder Mediasponsor unterstützen? Lesen Sie bitte die <a href="http://www.fossgis.de/wiki/Sponsoren_und_Aussteller">Informationen zu Sponsoren und Ausstellern</a> oder wenden sie sich direkt an <a href="mailto:katja.haferkorn@fossgis.de">Frau Haferkorn</a>.
 				</p>
