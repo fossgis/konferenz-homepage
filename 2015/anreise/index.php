f4b1360fde55d4e1eb5f561f3f23d753659ba386
Oliver Roick <oliver.roick@gmail.com>
2014-11-27 14:07:58 +0000

    Karte Anreise

--- 2015/anreise/index.php
+++ 2015/anreise/index.php
@@ -10,6 +10,10 @@
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
         <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
 
+        <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css">
+        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+        <script src="script/Leaflet.MakiMarkers.js"></script>
+
     </head>
     <body id="anreise">
         <div id="wrap">
@@ -26,8 +30,10 @@
 
                 <h2>Anreise und Unterkunft</h2>
 
+                <div id="mapContainer"><div id="map"></div></div>
+
                 <h3>Allgemeines</h3>
-                <p>Die FOSSGIS-Konferenz 2015 wird vom 11.-13. März an der Uiversität Münster stattfinden.</p>
+                <p>Die FOSSGIS-Konferenz 2015 wird vom 11.-13. März an der Universität Münster stattfinden.</p>
 
                 <p><strong>Adresse:</strong><br>Schlossplatz 2<br>48149 Münster</p>
 
@@ -44,7 +50,7 @@
                     <li>"Neutor" mit den Buslinien 1 (bzw. "Schlossplatz"), 5 und 6.</li>
                 </ul>
                 <p>Hilfreiche Infos liefert auch der <a href="http://www.bahn.de/">Planer der Deutschen Bahn</a> und der <a href="http://www.netzplan-muenster.de/">interaktive Netzplan der Stadtwerke Münster</a></p>
-
+		  <p>Am Hauptabhnhof Münster befindet sich die Fahrradverleihstation.</p> <!--src="img/Prinzipalmarkt_Muenster.png"-->
                 <h4>Anreise mit dem Flugzeug</h4>
                 <p>Der lokale Flughafen Münster-Osnabrück (FMO) ist relativ klein, aber einige Gesellschaften bieten evtl. günstige Anschlussflüge.</p>
                 <p>Größere/alternative Flughäfen sind:</p>
@@ -65,10 +71,26 @@
                 <p><strong><a href="http://www.agora-muenster.de/index.php">Agora - das Hotel am Aasee</a></strong><br>(1,2 km 15 min)<br>inkl. Frühstück, Bettwäsche, Handtücher<br>verschiedene Einzelzimmer: ab 69,00 €/Person/Nacht<br>verschiedene Doppelzimmer: ab 54,50 €/Person/Nacht (in Einzelbelegung: ab 79,00 €/Person/Nacht)</p>
                 <p><strong><a href="http://www.djh-wl.de/de/jugendherbergen/muenster">Jugend Gästehaus Aasee</a></strong><br>(1,4 km, 17 min)<br>inkl. Frühstück, Bettwäsche, Handtücher, Badezimmer mit Dusche und WC je Zimmer:<br>Doppelzimmer: 35,70 €/Person/Nacht (in Einzelbelegung: 52,80 €/Person/Nacht)<br>Vierbettzimmer: 30,40 €/Person/Nacht</p>
 
+		  <h4>Münster</h4>
+		  <p>Münster ist bekannt für seine Architektur und <a href="http://www.muenster.de/stadt/medien/pdf/schloss.mp3"> Geräusche.</a></p>
+		  <div class="sponsor silber" id="Prizipalmarkt"><img src="img/Prinzipalmarkt_Muenster.png" valign="middle" width="*" height="170"></a></div>
+ 		  <div class="sponsor silber" id="Dom"><img src="img/Dom.png" valign="middle" width="*" height="170"></a></div>
+ 		  <div class="sponsor silber" id="Unterwasserkirche"><img src="img/Unterwasserkirche.png" valign="middle" width="*" height="170"></a></div>
+
+		  <p>Das traditionelle Volksfest der Stadt Münster wird auch während der FOSSGIS-Konferenz seine Pforten öffnen.</p>
+
+          <div class="sponsor silber" id="Send"><img src="img/Send.png" valign="middle" width="250" height="*"></a></div>
+ 		  <div class="sponsor silber" id="Send"><img src="img/Send_.png" valign="middle" width="250" height="*"></a></div>
+
+ 		  <p>Münster hat einen <a href="http://www.qmediaservices.de/audioguide/index.htm">Audioguide</a>, der als App für Smartphones verfügbar ist.</p>
+
+ </div>
+
             <?php include('../inc/footer.inc'); ?>
         </div>
     </body>
 
+    <script src="script/map.js"></script>
     <script type='text/javascript'>
         window.onload = init;
     </script>
