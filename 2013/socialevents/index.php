3716891aa0fa0042fb2693190948b3c5fd75a600
Dominik Helle <helle@omniscale.de>
2015-06-22 22:23:25 +0200

    update fossgis conference page 2013 from the server

--- 2013/socialevents/index.php
+++ 2013/socialevents/index.php
@@ -18,8 +18,8 @@
 					<h2>Social Events</h2>
 					
 					<h3>Dienstag, 11. Juni 2013 - Inoffizieller Start</h3>
-					<p>Am Dienstag Abend findet die inoffizielle Konferenzeröffnung mit den Organisatoren und dem Lokalteam in einem nahegelegenen Restaurant in Rapperswil und Umgebung statt.</p>
-					<p>Beginn ist 18 Uhr.</p>
+					<p>Am Dienstag Abend findet die inoffizielle Konferenzeröffnung mit den Organisatoren und dem Lokalteam im Restaurant <a href="http://www.de-medici.ch/willkommen/"> De Medici</a> (Fischmarktstrasse <a href="http://www.openstreetmap.org/browse/node/1355269727"> 8</a>, 8640 Rapperswil, Altstadt) statt.</p>
+					<p>Beginn ist 18.15 Uhr.</p>
 					
 					<h3>Mittwoch, 12. Juni 2013 - Social Event</h3>
 					<p>Das Social Event am Mittwoch unter dem Motto 'Ein gemütlicher Abend auf dem Bauernhof' statt. Das Social Event soll den Teilnehmerinnen und Teilnehmern die Möglichkeit geben, sich im abendlichen Ambiente mit anderen Teilnehmern auszutauschen.</p>
@@ -31,15 +31,25 @@
 					<p><em>Dessert:</em> Appenzellerbiberli, Schockoladestengel, Waffeln und frische hofeigene Erdbeeren mit Rahm.</p>
 					<p><em>Getränke:</em> Kaffee, Mineral, Most und natürlich Bier. </p>
 
-					<p>Beginn: ab 17:30 Uhr  Ende: 2:00 Uhr<br>
+					<p>Beginn: ab 18:30 Uhr  Ende: 2:00 Uhr<br>
 					Ort: Bächlihof Jona (SG), Blaubrunnenstrasse 70, 8645 Jona <br>
 					<a href="http://bit.ly/WVKgKM">Map</a></p>
 					
 					<h3>Donnerstag, 13. Juni 2013 - Möglichkeiten der Abendgestaltung</h3>
-					<p>siehe <a href="http://giswiki.hsr.ch/Rappi_Info">Rappi Info</a></p>
+					<p>Der <a href="http://www.fossgis.de/">FOSSGIS e.V.</a>, Veranstalter der Konferenz wird am Donnerstag ab 17 Uhr am Stand einen Sektempfang geben. Mitglieder des Vereins und Interessierte sind herzlich eingeladen mit anderen Mitgliedern ins Gespräch zu kommen. </p>
+					<p>Einige Bars, Schnellimbiss-Ecken und Restaurants siehe hier auf <a href="http://giswiki.hsr.ch/Rappi_Info">Rappi Info.</a></p>
 					
 					<h3>Freitag, 14. Juni 2013 - Angebote </h3>
-					<p>Es besteht die Möglichkeit einen geführten Stadtrundgang oder eine Schifffahrt zur Insel Ufenau (mit Verpflegung) über die Touristinformation Rapperswil zu buchen. (<a href="http://www.vvrj.ch/">http://www.vvrj.ch/</a>)</p>
+					<b>Rahmenprogramm</b>
+					<p>Die Tourismusstelle Rapperswil-Jona organisiert ein (kostenpflichtiges) Rahmenprogramm am Freitagnachmittag ab ca. 15 Uhr. Die Möglichkeiten sind: <p>	
+						<ul>
+						  <li>Geführter Stadtrundgang </li>
+  						  <li>Schifffahrt zur Insel Ufenau (mit Verpflegung)</li>
+  						</ul>	
+					<p>Das Rahmenprogramm ist kostenpflichtig und die Teilnehmenden müssen sich mind. 2 Wochen vorher einschreiben.</p>
+					<b>Ausstellung der Bachelorarbeiten Informatik </b>
+					<p>Ab 16 Uhr gibt es eine Ausstellung der HSR-Bachelorarbeiten Informatik im Gebäude 1 oberer Stock, darunter u.a. <a href="http://neomap.hsr.ch/">NeoMap App</a> und das <a href="http://www.kort.ch/"> Kort Game</a>. 
+					
 				</div>
 			</div>
     	</div>
