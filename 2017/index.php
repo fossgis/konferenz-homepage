61faf31295855e10b9f3c943358fd259643e6386
Dominik Helle <helle@omniscale.de>
2015-10-02 09:04:10 +0200

    Add first version of 2017 page using for call for location

---
+++ 2017/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+    <head>
+        <title>FOSSGIS 2017</title>
+        <meta name="description" content="Die FOSSGIS-Konferenz ist im D-A-CH Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme OpenStreetMap.">
+
+        <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, 2017, FOSSGIS-Konferenz 2017, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
+        <meta charset="utf-8">
+
+        <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+        <script type="text/javascript" src="js/twitterwall/jquery-1.8.2.js"></script>
+        <script type="text/javascript" src="js/twitterwall/twitter.js"></script>
+    </head>
+    <body id="home">
+        <div id="wrap">
+            <h1>FOSSGIS Konferenz 2017 – <span>Call for Location</span></h1>
+
+            <div id="content">
+                <h2>Call for Location zur FOSSGIS-Konferenz 2017</h2>
+
+		        <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
+
+                <p>Die FOSSGIS-Konferenz 2017 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a>organisiert.</p>
+
+                <div class="organisers">
+                    <div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
+                    <div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/osgeo.png" valign="middle"></a></div>
+                    <div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
+                </div>
+                <p class="clear"></p>
+                <p>Ziel der jährlich stattfindenden Konferenz mit mehr als 400 Teilnehmern ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>
+
+ 		         <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+
+            </div>
+				<?php include('inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
