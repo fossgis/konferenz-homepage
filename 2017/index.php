21e06b79eb64fa1c2d1be83749c7b184bfe16f1e
Dominik Helle <helle@omniscale.de>
2015-10-13 21:25:11 +0200

    update text on cfl 2017

--- 2017/index.php
+++ 2017/index.php
@@ -23,6 +23,8 @@
 
             <p>Die FOSSGIS-Konferenz 2017 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a>organisiert.</p>
 
+            <p>Der Veranstalter sucht auf diesem Wege eine Austragungsort für die Konferenz im Jahre 2017 und ruft zur Einreichung von Bewerbungen auf.</p>
+
             <div class="organisers">
                 <div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
                 <div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/osgeo.png" valign="middle"></a></div>
@@ -31,16 +33,17 @@
             <p class="clear"></p>
 
             <h3>Was ist die FOSSGIS Konferenz?</h3>
+
             <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Web-Kartenanwendungen, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS.</p>
 
-            <p>Zusätzlich zum Software-Bereich ist auch das Thema freie Geodaten ein Schwerpunkt der Konferenz. Spezielles Augenmerk liegt hier auf dem Projekt OpenStreetMap.</p>
+            <p>Zusätzlich ist sind auch freie Geodaten ein Schwerpunkt der Konferenz. Spezielles Augenmerk liegt hier auf dem Projekt OpenStreetMap.</p>
 
             <h3>An welchen Orten wird die Konferenz ausgetragen?</h3>
             <p>Typischerweise wird die Konferenz an Hochschulen oder Bildungseinrichtungen durchgeführt.</p>
 
-            <p>In den letzten Jahren war die Konferenz zu Gast bei der Hochschule Anhalt in Dessau (2012), bei der HSR Hochschule für Technik in Rapperswil (2013) und bei der  Beuth Hochschule für Technik in Berlin.</p>
+            <p>In den letzten Jahren war die Konferenz zu Gast bei der Hochschule Anhalt in Dessau (2012), bei der HSR Hochschule für Technik in Rapperswil (2013) und bei der Beuth Hochschule für Technik in Berlin.</p>
 
-            <p>Zuletzt gastierte die Konferenz an der Westfälische Wilhelms-Universität in Münster. Die nächste Konferenz im Jahr 2016 findet die Konferenz erstmalig in Salzburg statt.</p>
+            <p>Zuletzt gastierte die Konferenz an der Westfälische Wilhelms-Universität in Münster. Die nächste Konferenz findet im Jahr 2016 erstmalig in Salzburg statt.</p>
 
             <h3>Wie finanziert sich die Konferenz?</h3>
 
