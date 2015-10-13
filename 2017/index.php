99838f7c8439fa30368f8cdf49bf339a0d3e8926
Dominik Helle <helle@omniscale.de>
2015-10-13 21:18:34 +0200

    update text for cfl 2017

--- 2017/index.php
+++ 2017/index.php
@@ -1,40 +1,57 @@
 <!doctype html>
 <html>
-    <head>
-        <title>FOSSGIS 2017</title>
-        <meta name="description" content="Die FOSSGIS-Konferenz ist im D-A-CH Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme OpenStreetMap.">
+<head>
+    <title>FOSSGIS 2017</title>
+    <meta name="description" content="Die FOSSGIS-Konferenz ist im D-A-CH Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme OpenStreetMap.">
 
-        <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, 2017, FOSSGIS-Konferenz 2017, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
-        <meta charset="utf-8">
+    <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, 2017, FOSSGIS-Konferenz 2017, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
+    <meta charset="utf-8">
 
-        <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
 
-        <script type="text/javascript" src="js/twitterwall/jquery-1.8.2.js"></script>
-        <script type="text/javascript" src="js/twitterwall/twitter.js"></script>
-    </head>
-    <body id="home">
-        <div id="wrap">
-            <h1>FOSSGIS Konferenz 2017 – <span>Call for Location</span></h1>
+    <script type="text/javascript" src="js/twitterwall/jquery-1.8.2.js"></script>
+    <script type="text/javascript" src="js/twitterwall/twitter.js"></script>
+</head>
+<body id="home">
+    <div id="wrap">
+        <h1>FOSSGIS Konferenz <span>Call for Location</span> <br>2017</h1>
 
-            <div id="content">
-                <h2>Call for Location zur FOSSGIS-Konferenz 2017</h2>
+        <div id="content">
+            <h2>Call for Location zur FOSSGIS-Konferenz 2017</h2>
 
-		        <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
+            <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
 
-                <p>Die FOSSGIS-Konferenz 2017 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a>organisiert.</p>
+            <p>Die FOSSGIS-Konferenz 2017 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="http://www.osgeo.org/">Open Source Geospatial Foundation (OSGeo)</a>organisiert.</p>
 
-                <div class="organisers">
-                    <div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
-                    <div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/osgeo.png" valign="middle"></a></div>
-                    <div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
-                </div>
-                <p class="clear"></p>
-                <p>Ziel der jährlich stattfindenden Konferenz mit mehr als 400 Teilnehmern ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>
-
- 		         <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
-
+            <div class="organisers">
+                <div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
+                <div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/osgeo.png" valign="middle"></a></div>
+                <div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
             </div>
-				<?php include('inc/footer.inc'); ?>
+            <p class="clear"></p>
+
+            <h3>Was ist die FOSSGIS Konferenz?</h3>
+            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Web-Kartenanwendungen, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS.</p>
+
+            <p>Zusätzlich zum Software-Bereich ist auch das Thema freie Geodaten ein Schwerpunkt der Konferenz. Spezielles Augenmerk liegt hier auf dem Projekt OpenStreetMap.</p>
+
+            <h3>An welchen Orten wird die Konferenz ausgetragen?</h3>
+            <p>Typischerweise wird die Konferenz an Hochschulen oder Bildungseinrichtungen durchgeführt.</p>
+
+            <p>In den letzten Jahren war die Konferenz zu Gast bei der Hochschule Anhalt in Dessau (2012), bei der HSR Hochschule für Technik in Rapperswil (2013) und bei der  Beuth Hochschule für Technik in Berlin.</p>
+
+            <p>Zuletzt gastierte die Konferenz an der Westfälische Wilhelms-Universität in Münster. Die nächste Konferenz im Jahr 2016 findet die Konferenz erstmalig in Salzburg statt.</p>
+
+            <h3>Wie finanziert sich die Konferenz?</h3>
+
+            <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+
+            <h3>Interesse?</h3>
+            <p>Sie möchten die Konferenz bei Ihnen auszurichten? Laden Sie sich <a href="data/cfl-fossgis-2017.pdf">hier das PDF mit weiteren Details</a> herunter.</p>
+
+            <p>Wir freuen uns auf Ihre kurze Bewerbung. Bei Fragen können Sie sich gerne an info[aet]fossgis.de wenden.</p>
         </div>
-    </body>
+        <?php include('inc/footer.inc'); ?>
+    </div>
+</body>
 </html>
