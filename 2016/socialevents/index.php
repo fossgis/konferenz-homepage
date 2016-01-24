bb40bf4007571fa4bbe1c0a87fa65c68a2a8f5a0
katjahafi <katjahafi@gmx.de>
2016-01-24 19:11:20 +0100

    OSM Hackday hinzugefügt

--- 2016/socialevents/index.php
+++ 2016/socialevents/index.php
@@ -23,18 +23,21 @@
                 <h2>Social Events</h2>
 
                 <h3>Sonntag, 03. Juli 2016</h3>
+				<h4>OSM-Hackday</h4>
+				<p>Am Vortrag der FOSSGIS-Konferenz findet der OSM-Hackday statt. Geplant ist ein Tag für OSM-Mapper und Interessierte, an welchen der Teilnehmer sich an Vorträgen und Workshops beteiligen kann. Weitere Informationen dazu <a href="http://www.fossgis.de/wiki/Konferenz_2016/Programm/OSM-Sonntag"> hier</a>, die Planungen dazu finden im <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2016/Hackday_t-1">OSM-Wiki</a> statt. Wer sich einbringen möchte, gibt es auf der Planungsseite bekannt. Fragen dazu gerne an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.
+
 				<h4>Inoffizieller Start</h4>
                 <p>Ab 18 Uhr gemeinsames Abendessen für alle schon anwesenden FOSSGISler im <a href="http://taste-gassner.com/stieglkeller/">Stieglkeller Salzburg</a> (Festungsgasse 10, 5020 Salzburg). Wir treffen uns bei schönem Wetter auf der Terasse des Stieglkellers, für die Schlechtwettervariante reserviert das Lokal Team einen Raum. Jeder bezahlt seine Rechnung selbst.</p>
 
 				<h3>Montag, 04. Juli 2016</h3>
-				<h4>Eröffnung FOSSGIS-Konferenz>
-				<p>um 13:00 wird die Konferenz im Audimx eröffnet.</p>
+				<h4>Eröffnung FOSSGIS-Konferenz</h4>
+				<p>Um 13:00 wird die Konferenz im Audimax eröffnet.</p>
 				<h4>Anwendertreffen</h4>
-                <p>Von 17 bis 18:30 Uhr können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen zur Verfügung. Bitte per E-Mail an konferenz-orga@fossgis.de anfragen oder das Team vor Ort ansprechen.</p>
+                <p>Von 17 bis 18:30 Uhr können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen zur Verfügung. Bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a> anfragen oder das Team vor Ort ansprechen.</p>
                                 
 				<h3>Dienstag, 05. Juli 2016</h3>
 				<h4>Abendveranstaltung "Dialoge am Amphitheater"</h4>
-				<p>Geselliges Beisammensein auf der Terasse des Gebäudes Nawi in der Universität Salzburg. Die "Dialoge am Amphitheater" finden von 18 bis 23 Uhr zusammen mit dem "Icebreaker" der AGIT statt. Die Veranstaltung wird sich durch ein offenes Ambiente auszeichnen. Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Bei der Teilnahmeanmeldung bitten wir um Angabe der Teilnahme (ja|nein) zum Social Event, dies dient der guten Planung der Veranstaltung.
+				<p>Geselliges Beisammensein auf der Terasse des Gebäudes Naturwissenschaften (Nawi) in der Universität Salzburg. Die "Dialoge am Amphitheater" finden von 18 bis 23 Uhr zusammen mit dem "Icebreaker" der AGIT statt. Die Veranstaltung wird sich durch ein offenes Ambiente auszeichnen. Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Bei der Teilnahmeanmeldung bitten wir um Angabe der Teilnahme (ja|nein) zum Social Event, dies dient der guten Planung der Veranstaltung.
 Als Schlechtwettervariante steht das Foyer der Universität zur Verfügung.</p>
 
                 <h3>Mittwoch, 06. Juli 2016</h3>
