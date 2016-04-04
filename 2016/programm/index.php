5c249467c79c8940bb92491d48910d76ddf50f09
Dominik Helle <helle@omniscale.de>
2016-04-04 22:14:53 +0200

    add index php for programm

---
+++ 2016/programm/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2016 Programm</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Salzburg, Hohensalzburg, Z_GIS">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+    </head>
+    <body id="anmeldung">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+
+                <h2>Programm zur Konferenz 2016</h2>
+                <p>Wählen Sie den gewünschten Konferenztag aus.</p>
+                <ul>
+                    <li>
+                        <a href="http://frab.fossgis-konferenz.de/de/2016/public/schedule/0">Tag 1 – Montag, 04.07.2016</a>
+                    </li>
+
+                    <li>
+                        <a href="http://frab.fossgis-konferenz.de/de/2016/public/schedule/1">Tag 2 – Dienstag, 05.07.2016</a>
+                    </li>
+
+                    <li>
+                        <a href="http://frab.fossgis-konferenz.de/de/2016/public/schedule/2">Tag 3 – Mittwoch, 06.07.2016</a>
+                    </li>
+                </ul>
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
