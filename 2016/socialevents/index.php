2ad896d9960049e414006927e7cdc40ecf11cba0
Dominik Helle <helle@omniscale.de>
2016-04-27 10:44:51 +0200

    update text for osm sonntag

--- 2016/socialevents/index.php
+++ 2016/socialevents/index.php
@@ -23,13 +23,14 @@
                 <h2>Social Events</h2>
 
                 <h3>Sonntag, 03. Juli 2016</h3>
-				<h4>OSM-Hackday</h4>
-				<p>Am Vortag der FOSSGIS-Konferenz findet der OSM-Sonntag stattund ist als eine Mischung aus Konferenz, Hackweekend und Mappertreffen gedacht. Es richtet sich an Mapper, Entwickler und OSM-Interessierte im näheren und weiteren Umkreis von Salzburg.</p>
+				<h4>OSM-Sonntag.</h4>
+				<p>Am Vortag der FOSSGIS-Konferenz findet der OSM-Sonntag statt und ist als eine Mischung aus Konferenz, Hackweekend und Mappertreffen gedacht. Es richtet sich an Mapper, Entwickler und OSM-Interessierte.</p>
 
-<p>Es gibt eine Schiene mit Vorträgen und Workshops, jeweils 60 Minuten lang. So kann man etwas tiefer in die Thematik einsteigen, als es in einem 25-Minuten-Vortrag wie auf der FOSSGIS üblich wäre, und auch über ein kontroverses Thema etwas länger diskutieren.</p>
+                <p>Es gibt Vorträge und Workshops die jeweils eine länger von 60 Minuten eingeplant sind. So möchten wir ermöglichen tiefer in die Thematik einzusteigen, wie z.B. auf einer FOSSGIS-Konferenz üblich. Auch kann man so länger über ein kontroverses Thema diskutieren.</p>
 
-<p>Parallel zu den Vorträgen/Workshops stehen Räumlichkeiten zur Verfügung, in denen ihr euch mit anderen Teilnehmern austauschen und an euren Projekten (seien es Entwicklerprojekte oder normales Editieren mit JOSM/iD/…) arbeiten könnt.
-Weitere Informationen dazu <a href="http://ruep.github.io/osm-sonntag/#schedule"> hier</a>, die Planungen dazu finden im <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2016/Hackday_t-1">OSM-Wiki</a> statt. Wer sich einbringen möchte, gibt es auf der Planungsseite bekannt. Fragen dazu gerne an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.
+                <p>Parallel zu den Vorträgen/Workshops stehen Räumlichkeiten zur Verfügung in denen ihr euch mit anderen Teilnehmern austauschen und an euren Projekten (z.B. Entwicklerprojekte oder Editieren mit JOSM/iD/…) arbeiten könnt. </p>
+
+                <p>Weitere Informationen dazu <a href="http://ruep.github.io/osm-sonntag/#schedule"> hier</a>, die Planungen dazu finden im <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2016/Hackday_t-1">OSM-Wiki</a> statt. Wer sich einbringen möchte, tträgt sich bitte auf der Planungsseite ein. Fragen dazu gerne an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.
 
 				<h4>Inoffizieller Start</h4>
                 <p>Ab 18 Uhr gemeinsames Abendessen für alle schon anwesenden FOSSGISler im <a href="http://taste-gassner.com/stieglkeller/">Stieglkeller Salzburg</a> (Festungsgasse 10, 5020 Salzburg). Wir treffen uns bei schönem Wetter auf der Terasse des Stieglkellers, für die Schlechtwettervariante reserviert das Lokal Team einen Raum. Jeder bezahlt seine Rechnung selbst.</p>
@@ -39,7 +40,7 @@ Weitere Informationen dazu <a href="http://ruep.github.io/osm-sonntag/#schedule"
 				<p>um 13:00 Uhr im Hörsaal Audimax.</p>
 				<h4>Anwendertreffen</h4>
                 <p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder das Team vor Ort ansprechen.</p>
-                                
+
 				<h3>Dienstag, 05. Juli 2016</h3>
 				<h4>Abendveranstaltung "Dialoge am Amphitheater"</h4>
 				<p>das gesellige Beisammensein findet von 18 - 23 Uhr zusammen mit dem "Icebreaker" der AGIT auf der Terasse des Gebäudes Naturwissenschaften (Nawi) in der Universität Salzburg statt. Die "Dialoge am Amphitheater" werden sich durch ein offenes Ambiente auszeichnen. Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Wir bitten darum bei der Teilnahmeanmeldung ja oder nein anzugeben, dies dient der guten Planung der Veranstaltung. Als Schlechtwettervariante steht das Foyer der Universität zur Verfügung.</p>
@@ -47,7 +48,7 @@ Weitere Informationen dazu <a href="http://ruep.github.io/osm-sonntag/#schedule"
                 <h3>Mittwoch, 06. Juli 2016</h3>
                 <h4>Sektempfang am FOSSGIS-Stand</h4>
                 <p>Alle Mitglieder, Freunde und Interessierte des FOSSGIS-Vereins sind nach der Abschlussveranstaltung herzlich zum Sektausklang am FOSSGIS-Stand eingeladen.</p>
-                 
+
             </div>
             <?php include('../inc/footer.inc'); ?>
         </div>
