cbc5943dce57167300cecc8863bdd1a0a371abdd
katjahafi <katjahafi@gmx.de>
2016-11-30 21:56:28 +0100

    Anreise und Unterkunft erstellt

---
+++ 2017/anreise/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2016 Anreise + Unterkunft</title>
+        <meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+        <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, 2017, FOSSGIS-Konferenz 2017, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, Universität Passau">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+       <!-- <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css">
+        <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+        <script src="script/Leaflet.MakiMarkers.js"></script>-->
+
+    </head>
+    <body id="anreise">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+				 <figure>
+                    <img src="img/passau.jpg" width="880" height="241" alt="Passau">
+                    <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+                </figure>
+
+                    <h2>Anreise und Unterkunft</h2>
+
+                    <!--<div id="mapContainer">
+					<div id="map"></div></div>-->
+             
+					<h3>Allgemeines</h3>
+                <p>Die FOSSGIS-Konferenz findet vom 22.-25. März 2017 an der Universität Passau statt.</p>
+
+                <p><strong>Adresse:</strong><br>FORWISS - Universität Passau<br>Innstraße 43<br>
+D-94032 Passau</p>
+
+                <h3>Anreise</h3>
+				<p>Der <a href="http://osm.org/go/0JODBwBPV?m="> Konferenzstandort </a> ist gut an das ÖV Netz angebunden und schnell zu erreichen.</p>
+				<ul class="list">
+				<li>1 km in die Altstadt von Passau</li>
+				<li>1,5 km zum Bahnhof Passau</li>
+				<li>gute Erreichbarkeit vieler Hotels sowie der Altstadt zu Fuß und per Fahrrad.		</li></ul></p>
+
+				<h4>Anreise mit öffentlichen Verkehrsmitteln</h4>
+                <p>Passau besitzt einen ICE-Bahnhof und ist somit über den Fernverkehr gut erreichbar. Vom Hauptbahnhof Passau sind es nur wenige hundert Meter zur Universität sowie zu vielen stadtgelegenen Unterkünften.<br>
+Die Buslinien 8 und 9 verkehren zwischen Hauptbahnhof und Universität und passieren den ZOB (Zentraler Omnibus Bahnhof). Die Haltestellen Universität und Wörthstraße sind in unmittelbarer Nähe zum Veranstaltungsort.<br>
+Weitere Informationen finden sich auf den Seiten der <a href="https://www.stadtwerke-passau.de/bus-parken/busfahrplaene.html">Stadtwerke Passau</a>.<br></p>
+
+
+            <h4>Anreise mit dem Auto</h4>
+            <p>Passau ist direkt an der Autobahn A3 gelegen. Auch wenn Navigationssysteme oft anderes behaupten empfiehlt sich im Normalfall immer die Abfahrt an der Anschlussstelle Passau Mitte.<br>
+Für Reisende aus der Richtung München empfehlen wir dringend die Anreise über die A92/A3 über Deggendorf und nicht die A94 zu verwenden.<br>
+Während der Konferenzzeit wird die Tiefgarage unter der Zentralbibliothek kostenfrei für alle Teilnehmer zur Verfügung stehen. Zu beachten ist, dass die Tiefgarage Nachts abgesperrt wird. <br><br>
+				<b>    Eingabe ins Navigationsgerät</b>
+				<ul class="list"><li> Straße: Innstraße 41</li>
+				<li> Ort: Passau</li>
+				<li> Postleitzahl: 94032</li>
+				</ul></p>
+				
+			
+			<p>Haben Sie schon einmal an die Möglichkeit von Carsharing oder Mitfahrgelegenheit bei<a href="https://www.blablacar.de/" class="external text" title="https://www.blablacar.de/" rel="nofollow"> BlaBlaCar</a> gedacht? Die Umwelt und Ihr Geldbeutel werden sich freuen.</p>
+
+                
+                <h4>Anreise mit dem Flugzeug</h4>
+				<p>Eine Anreise per Flugzeug ist entweder über den Flughafen München <a href="http://www.flughafen-muenchen.de/" rel="nofollow">MUC</a> oder über den  Flughafen Linz <a href="http://www.linz-airport.com/de"> LNZ</a> möglich. Von dort aus kann z.B. mit Regionalexpress oder ICE nach Passau weitergefahren werden. Der Shuttle-Bus des Münchner Flughafens bringt Sie bis nach Freising, weiter geht es mit der Bahn von Freising über Landshut nach Passau, Transferzeit ca. 2,5 Stunden.</p>
+			<p><b>Taxi</b></p>
+			<ul class="list">
+			<li> Taxi-Zentrale Passau eG: 0851 57373</li>
+			<li>Taxi Karl Heinz Hubinger: 0170 2468118</li></ul>
+
+            <h3>Unterkunft</h3>
+            <p>Diese Hotels befinden sich in der Nähe der Universität und Altstadt:
+			<ul class="list">
+			<li> Hotel Spitzberg Garni, Neuburger Str. 29, 94032 Passau</li>
+			<li> IBB Hotel Passau City Centre, Bahnhofstrasse 24, 94032 Passau</li> 
+			<li> Hotel Garni Herdegen, Bahnhofstr. 5, 94032 Passau</li> 
+			<li> Hotel Weisser Hase, Heiliggeistgasse 1, 94032 Passau</li>
+			<li> StadtHotel Passau, Große Klingergasse 17, 94032 Passau</li> 	
+			<li> Hotel Passauer Wolf, Untere Donaulände 4, 94032 Passau</li> 	
+			<li> Rotel Inn, Haissengasse 10, 94032 Passau </li>	
+			<li> IBB Hotel Passau Sued, Neuburger Str. 79, 94036 Passau</li>	
+			</ul></p>
+				
+			<p><b>Mit folgenden Unterkünften sind Frühbucherkontingente in Verhandlung:</b>
+			<ul class="list">
+			<li>Jugendherberge Passau</li>
+			<ul><li> Web: <a href="http://passau.jugendherberge.de/de-DE/Portraet" rel="nofollow">http://passau.jugendherberge.de/de-DE/Portraet/</a></li></ul>
+			<ul><li> 3 km, 30 min </li></ul>
+				<ul><li> Adresse: Oberhaus 125, 94034 Passau</li></ul>
+				<ul><li> Telefon: +49 851 49378-0</li></ul>
+				<ul><li> Ab ca. 25€ im Mehrbettzimmer pro Person pro Nacht, ab 56€ im Zweibettzimmer für 2 Personen pro Nacht</li></ul>
+				<ul><li>inkl. Frühstück, Bettwäsche, geeignet für Fahrradfahrer, super Aussicht</li></ul></ul>
+				<ul class="list">
+			<li>Hotel Weisser Hase</li>
+			<ul><li> Web: <a href="http://www.hotel-weisser-hase.de/" rel="nofollow">http://www.hotel-weisser-hase.de/</a></li></ul>
+			<ul><li> 1 km, 10 min </li></ul>
+				<ul><li> Adresse: Heiliggeistgasse 1, 94032 Passau</li></ul>
+				<ul><li> Telefon: +49 851 9211-0</li></ul>
+				<ul><li> Einzelzimmer: ab 56€/Person/Nacht, Doppelzimmer: ab 69€/2 Personen/Nacht, Junior Suite: bis zu 4 Erwachsene ab 96€/Nacht.</li></ul></ul>
+				<ul class="list">
+				<li>IBB Hotel</li>
+			<ul><li> Web: <a href="http://www.ibbhotelpassau.de/sued/home" rel="nofollow">http://www.ibbhotelpassau.de/sued/home</a></li></ul>
+			<ul><li> 1,6 km, 20 min</li></ul>
+				<ul><li> Adresse: Neuburger Str. 79, 94036 Passau</li></ul>
+				<ul><li> Telefon: +49 851 95180</li></ul>
+				<ul><li> Einzelzimmer: ab 56€/Person/Nacht, Doppelzimmer: ab 75€ für 2 Personen pro Nacht, 4 Personenzimmer ab 120€/Nacht, inkl. Frühstück und Internet.</li></ul></ul>
+			</p>
+			
+			<p>Weitere Informationen finden sie auf der Website der Dreiflüssestadt <a href="https://www.passau.de/">Passau</a>.</p>
+		   </div>
+
+                
+
+             <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+   <!-- <script src="script/hotels.js"></script>
+    <script src="script/restaurants.js"></script>
+    <script src="script/ausflug.js"></script>
+
+    <script src="script/map.js"></script>
+    <script type='text/javascript'>
+        window.onload = init;
+    </script>-->
+	
+</html>
