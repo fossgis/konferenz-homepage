6c5e84ef490d52ee04a8419df816a359c9e05b13
Peter Barth (Peda) <osm-peda@won2.de>
2016-12-10 21:02:30 +0100

    Add files for map and map data for Passau

---
+++ 2017/anreise/script/hotels.js
@@ -0,0 +1,134 @@
+var hotels = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+	    13.45601,
+	    48.57379
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.hotel-herdegen.de/",
+        "icon": "warehouse",
+        "title": "Hotel Garni Herdegen",
+        "description": "Ein Hotel in der neuen Mitte Passau<br>Entfernung: 1,1km, unter 10 Minuten vom Veranstaltungsort entfernt<br/>Adresse: Bahnhofstraße 5, 94032 Passau<br/>Telefon: +49 851 955 160<br/>Einzelzimmer: 60€, Doppelzimmer: 90€ (inkl. Frühstück)"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.43565,
+          48.56525
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.ibbhotelpassau.de/sued/home",
+        "icon": "warehouse",
+        "title": "IBB Hotel",
+        "description": "Entfernung: 1,6km, ca 20 min<br/>Adresse: Neuburger Straße 79, 94036 Passau<br/>Telefon: +49 851 95180<br/>Einzelzimmer: ab 56€/Person/Nacht, Doppelzimmer: ab 75€ für 2 Personen pro Nacht, 4 Personenzimmer ab 120€/Nacht, inkl. Frühstück und Internet."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.46210,
+          48.57586
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.hotel-passauer-wolf.de/",
+        "icon": "warehouse",
+        "title": "Hotel Passauer Wolf",
+        "description": "Entfernung: 1,6 km, 20 min<br/>Adresse: Untere Donaulände 4, 94032 Passau<br/>Telefon: +49 851 93 15 10<br/>Einzelzimmer: 55€, Doppelzimmer: 75€"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.46252,
+          48.57591
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.ampaulusbogen.de/",
+        "icon": "warehouse",
+        "title": "Am Paulusbogen",
+        "description": "Entfernung: 1,5km, 20 min<br/>Adresse: Rindermarkt 2, 94032 Passau<br/>Telefon: +49 851 931060<br/>Einzelzimmer: ab 59€, Doppelzimmer: 79€, inkl. Frühstück"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.46815,
+          48.57513
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.wilder-mann.com/",
+        "icon": "warehouse",
+        "title": "Hotel Wilder Mann",
+        "description": "Entfernung: 1,6 km, 20 min<br/>Adresse: Am Rathausplatz, 94032 Passau<br/>Telefon: +49 851 3 50 71<br/>Einzelzimmer: 60€, Doppelzimmer: 70€"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.45897,
+          48.57388
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.hotel-weisser-hase.de/",
+        "icon": "warehouse",
+        "title": "Hotel Weisser Hase",
+        "description": "Entfernung: 1 km, 10 min<br/>Adresse: Heiliggeistgasse 1, 94032 Passau<br/>Telefon: +49 851 9211-0<br/>Einzelzimmer: 56€, Doppelzimmer: 69€, Junior Suite: 4 Erwachsene ab 96€."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+            13.47141,
+            48.57840
+        ]
+      },
+      "properties": {
+        "homepage": "http://passau.jugendherberge.de/de-DE/Portraet",
+        "icon": "warehouse",
+        "title": "Jugendherberge Passau",
+        "description": "Entfernung: 3 km, 30 min<br/>Adresse: Oberhaus 125, 94034 Passau<br/>Telefon: +49 851 49378-0<br/>Ab ca. 25€ im Mehrbettzimmer pro Person pro Nacht, ab 56€ im Zweibettzimmer für 2 Personen pro Nacht<br/>inkl. Frühstück, Bettwäsche, geeignet für Fahrradfahrer, super Aussicht"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          13.45010,
+          48.57516
+        ]
+      },
+      "properties": {
+        "homepage": "http://www.rotel-inn.de/",
+        "icon": "warehouse",
+        "title": "Rotel Inn in Passau",
+        "description": "Entfernung: 2km, 20 min<br/>Adresse: Haissengasse 10, 94032 Passau<br/>Telefon: +49 851 951 60<br/>Einzelzimmer: 35€, Doppelzimmer: 25€, Frühstück: 6€"
+      }
+    },
+     
+  ]
+}
---
+++ 2017/anreise/script/map.js
@@ -0,0 +1,113 @@
+var colours = {
+    "bus": "#2C3E50",
+    "warehouse": "#96281B",
+    "restaurant": "1E824C",
+    "bar": "1E824C",
+    "theatre": "#D35400",
+    "basketball": "#D35400",
+    "religious-christian": "#111c97",
+    "town-hall": "#7c5397",
+    "museum": "#7c5397",
+    "parking": "#003bff",
+    "rail": "#003bff",
+    "bus": "#003bff",  
+};
+
+function init () {
+
+    var hotelGroup = L.layerGroup();
+    L.geoJson(hotels, {
+        pointToLayer: function(feature, latlng) {
+            var iconString = feature.properties.icon;
+            var icon = L.MakiMarkers.icon({icon: iconString, size: "m", color: colours[iconString]});
+            var layer = L.marker(latlng, {icon: icon});
+	    hotelGroup.addLayer(layer);
+	    return layer;
+        },
+        onEachFeature: function(feature, layer) {
+            var html = '<b>' + feature.properties.title + '</b><br/>'+ feature.properties.description +'<br/><br/><a href="' + feature.properties.homepage +'">Zur Homepage</a>';
+            layer.bindPopup(html);
+        }
+    });
+
+    var verkehrGroup = L.layerGroup();
+    L.geoJson(verkehr, {
+        pointToLayer: function(feature, latlng) {
+            var iconString = feature.properties.icon;
+            var icon = L.MakiMarkers.icon({icon: iconString, size: "m", color: colours[iconString]});
+            var layer = L.marker(latlng, {icon: icon});
+	    verkehrGroup.addLayer(layer);
+	    return layer;
+        },
+        onEachFeature: function(feature, layer) {
+            var html = '<b>' + feature.properties.title + '</b><br/>'+ feature.properties.description +'<br/><br/><a href="' + feature.properties.homepage +'">Zur Homepage</a>';
+            layer.bindPopup(html);
+        }
+    });    
+
+    var restaurantsGroup = L.layerGroup();
+    L.geoJson(restaurants, {
+        pointToLayer: function(feature, latlng) {
+            var iconString = feature.properties.icon;
+            var icon = L.MakiMarkers.icon({icon: iconString, size: "m", color: colours[iconString]});
+            var layer = L.marker(latlng, {icon: icon});
+	    restaurantsGroup.addLayer(layer);
+	    return layer;
+        },
+        onEachFeature: function(feature, layer) {
+            var html = '<b>' + feature.properties.title + '</b></br>' + feature.properties.description + '<br/><br/><a href="' + feature.properties.homepage +'">Zur Homepage</a>';
+            layer.bindPopup(html);
+        }
+    })
+
+    var ausflugGroup = L.layerGroup();
+    L.geoJson(ausflug, {
+        pointToLayer: function(feature, latlng) {
+            var iconString = feature.properties.icon;
+            var icon = L.MakiMarkers.icon({icon: iconString, size: "m", color: colours[iconString]});
+            var layer = L.marker(latlng, {icon: icon});
+	    ausflugGroup.addLayer(layer);
+	    return layer;
+        },
+        onEachFeature: function(feature, layer) {
+            var html = '<b>' + feature.properties.title + '</b><br>' + feature.properties.description + '<br/><br/><a href="' + feature.properties.homepage +'">Weitere Informationen</a>';
+            layer.bindPopup(html);
+        }
+    })
+
+    var fossgisGroup = L.layerGroup();
+    L.geoJson(fossgis, {
+        onEachFeature: function(feature, layer) {
+            var html = '<b>' + feature.properties.title + '</b><br>' + feature.properties.description + '<br/><br/><a href="' + feature.properties.homepage +'">Weitere Informationen</a>';
+            layer.bindPopup(html);
+	    fossgisGroup.addLayer(layer);
+	}
+    })
+
+    
+    var mapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
+	maxZoom: 20,
+	maxNativeZoom: 19
+    })
+
+    var map = L.map('map', {
+	center: [48.56705, 13.45208],
+	zoom: 15,
+	layers: [mapLayer, hotelGroup, verkehrGroup, restaurantsGroup, ausflugGroup, fossgisGroup]
+    });
+    
+    var baseMaps = {
+	"OpenStreetMap" : mapLayer
+    };
+
+    var overlay = {
+	"Unterkunft": hotelGroup,
+	"Verkehr": verkehrGroup,	
+	"Verpflegung": restaurantsGroup,
+	"Sehenswürdigkeiten": ausflugGroup,
+	"Veranstaltungsort": fossgisGroup
+    };
+
+    L.control.layers(baseMaps, overlay).addTo(map);
+}
