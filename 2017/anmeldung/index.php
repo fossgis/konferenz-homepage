c9871fc2b06fa1cf1e8f2453599d72663bb6a711
katjahafi <katjahafi@gmx.de>
2017-01-20 18:09:11 +0100

    Preise, Social Events und Green hinzugefügt

---
+++ 2017/anmeldung/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2017 Anmeldung</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Passau, Inn, Donau">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+    </head>
+    <body id="anmeldung">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+
+                 <figure>
+                    <img src="img/passau.jpg" width="880" height="241" alt="Passau">
+                    <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+                </figure>
+
+                <h2>Anmeldung zur Konferenz</h2>
+                <p>Wir laden Sie ein Teil der FOSSGIS-Konferenz 2017 zu werden.  Denn die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie!</p>
+                <p>In Erwartung von &uuml;ber 300 Teilnehmenden ist eine Registrierung Ihrerseits notwendig und hilfreich f&uuml;r die Organisation. Nach erfolgreicher Anmeldung erhalten Sie eine Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages sind Sie registriert. Hier gelangen Sie direkt <a href="https://pretix.eu/fossgis/2017/"> zur Anmeldung</a>. Im Rahmen der Anmeldung können Sie bis zum 16.03.2017 eine Reihe von kostenpflichtigen FOSSGIS Workshops auswählen. Informationen zu den einzelnen Workshops finden Sie im <a href="http://frab.fossgis-konferenz.de/de/2016/public/schedule">Programm</a>. </p>
+				<p>Eine spontane Teilnahme ist m&ouml;glich, bitte melden Sie sich bei Ankunft am Welcome Desk.</p>
+
+				<h3>Preise FOSSGIS-Konferenz 2017</h3>
+				<ul class="list">
+				<li> Teilnehmer_innen (Normalpreis): € 190</li>
+				<li> Teilnehmer_innen (Frühbucherrabatt bis 13.02.2017, 21 Uhr): € 150</li>
+				<li> Autor_innen: € 120</li>
+				<li> Studierende: € 100</li>
+				<li> FOSSGIS Community*: € 0*</li>
+				<li> Workshop: € 100</li>
+				<li> Workshops (Frühbucherrabatt bis 13.02.2017, 21 Uhr): € 90</li>
+				</ul>
+                <p><b>Im Konferenzticket enthalten:</b> FOSSGIS-Konferenz-Teilnahme, Pausensnack, Tagungsband, Tasche, Dialoge am Inn.<br>
+                * Regelungen für das Communityticket gelten für Aktive in der FOSS/OSM-Communiy, bitte bei Anmeldung im Textfenster darstellen.</p>
+
+                <h3>Helfer</h3>
+                <p>Freiwillige Helfer sind eingeladen und willkommen während der Konferenz bei den Videoaufnahmen zu unterstützen. Bei Interesse bitte bei per E-Mail unter konferenz-orga@fossgis.de melden und Zeitraumpräferenz angeben.</p>
+
+
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
---
+++ 2017/socialevents/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2017 Social Events</title>
+        <meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+        <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, Passau, Universität Passau, Dialoge am Inn, Donau, Inn">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="socialEvents">
+        <div id="wrap">
+        	<?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+                <?php include('../inc/nav.inc'); ?>
+                <figure>
+                    <img src="img/passau.jpg" width="880" height="241" alt="Passau">
+                    <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+                </figure>
+
+                <h2>Social Events</h2>
+				
+				 <h3>Dienstag, 21. März 2017 ab 19:30 Uhr: Inoffizieller Start</h3>
+                <p>Gemeinsames Abendessen für alle schon anwesenden FOSSGISler in der <a href="http://www.padu-innstrasse.de/">Pizzeria Padu</a> (Innstraße 46, 94032 Passau) im Nebenraum. Warme Küche gibt's bis 22:30. Jeder bezahlt seine Rechnung selbst. </p>
+
+                <h3>Mittwoch, 22. März 2017</h3>
+				<h4>Eröffnung FOSSGIS-Konferenz</h4>
+				<p>um 13:00 Uhr im Hörsaal Audimax.</p>
+				<h4>Anwendertreffen</h4>
+                <p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen bis 21 Uhr zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder das Team vor Ort ansprechen.</p>
+				<h4>Abendveranstaltung "Dialoge am Inn"</h4>
+				<p>Das gesellige Beisammensein am Inn findet von 19 - 23 Uhr in der Mensa der Universität Passau statt. Die "Dialoge am Inn" werden sich durch ein offenes Ambiente auszeichnen. Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Wir bitten darum bei der Teilnahmeanmeldung ja oder nein anzugeben, dies dient der guten Planung der Veranstaltung.</p>
+				
+				<h3>Donnerstag, 23. März 2017</h3>
+				<h4>FOSSGIS e.V. Mitgliederversammlung</h4>
+				<p>um 18:00 Uhr im Seminarraum IM 008. Es wird für alle Pizza bestellt.</p>
+				
+				<h4>Anwendertreffen</h4>
+                <p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen bis 21 Uhr zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder das Team vor Ort ansprechen.</p>
+				
+				<h3>Freitag, 24. März 2017</h3>
+				<h4>Sektempfang am FOSSGIS-Stand </h4>
+				<p>Alle Mitglieder des FOSSGIS-Vereins, Freunde und Interessierte sind herzlich ab 14:30 Uhr zum Sektausklang eingeladen.</p>
+				
+				<h4>OSM-Mapping-Event</h4>
+				<p>Unter der Bezeichnung Qualitätsoffensive OSM Mapping Passau werden sich Mapper der Altstadt Passau widmen und Themen wie Geschäfte/Öffnungszeiten/Ärzte/Rechtsanwälte, 3DTagging, Barierrefreiheit, Briefkästen, Indoor (Aufzüge) bearbeiten. Falls Sie soetwas noch nie gemacht haben, ist das die Gelegenheit für den Einstieg. Das Mapping-Event soll auch für Anfänger geeignet sein, deshalb findet um 16 Uhr ein kurzer Workshop statt, in dem das How-To-Do besprochen wird. Anmeldung zur Veranstaltung entweder vor Ort oder im <a href="http://wiki.openstreetmap.org/wiki/FOSSGIS_2017/OSM-Events"> OSM-Wiki </a>. Das Mapping-Event ist bis ca. 20 Uhr geplant.</p>
+
+                <h3>Samstag, 25. März 2017</h3>
+				<h4>OSM-Samstag </h4>
+				<p>Von 9.30 bis 18 Uhr findet eine OSM-Unconference statt. Interessierte sind eingeladen daran teilzunehmen. Die Anmeldung und Themensammlung erfolgt im <a href="http://wiki.openstreetmap.org/wiki/FOSSGIS_2017/OSM-Events"> OSM-Wiki </a> sowie an der Pinnwand am FOSSGIS-Stand. Um 10 Uhr werden die Themen in Zeiten und Räume sortiert. Zu Mittag gibt es Pizza.</p>
+
+                </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
