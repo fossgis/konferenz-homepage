176c6b11d99a50a1d051281e6fe216575589abb6
Peter Barth (Peda) <osm-peda@won2.de>
2017-02-01 00:16:37 +0100

    add a php scraper script to generate an inline program

---
+++ 2017/programm/event.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<?php require_once 'simple_html_dom.php'; ?>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2017 Programm</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Passau">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+	<link type="text/css" href="programm.css" media="screen,print" rel="Stylesheet"/>
+	<link type="text/css" href="extra.css" media="screen,print" rel="Stylesheet"/>	
+    </head>
+    
+    <body id="programm">
+      <div id="wrap">
+        <?php include('../inc/head.inc'); ?>
+	
+        <div id="content">
+	  
+          <?php
+	     include('../inc/nav.inc');
+	     include('prog_nav.inc');
+          ?>
+	  
+	  <figure>
+            <img src="../img/passau.jpg" width="880" height="241" alt="Passau">
+            <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+          </figure>
+	  
+          <h2>Event-Details</h2>
+	  <?php
+
+	     if (isset($_GET['id'])) {
+	        $html = file_get_html("https://frab.fossgis-konferenz.de/de/2017/public/events/" . $_GET['id']);
+	        $referent = $html->find('div[id=sidebar]', 0);
+                $text = $html->find('div[id=basic]', 0);
+	  
+                $referent = str_replace("de/2017/public/speakers/", "2017/programm/speaker.php?id=", $referent);
+                $referent = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $referent);
+                $referent = str_replace("/system/", "https://frab.fossgis-konferenz.de/system/", $referent);
+                $text = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $text);
+	  
+                echo $referent;
+	        echo $text;
+	     }
+          ?>
+	  
+	</div>
+	<?php include('../inc/footer.inc'); ?>
+      </div>
+    </body>
+</html>
---
+++ 2017/programm/speaker.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<?php require_once 'simple_html_dom.php'; ?>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2017 Programm</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Passau">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+	<link type="text/css" href="programm.css" media="screen,print" rel="Stylesheet"/>
+	<link type="text/css" href="extra.css" media="screen,print" rel="Stylesheet"/>	
+    </head>
+    
+    <body id="programm">
+      <div id="wrap">
+        <?php include('../inc/head.inc'); ?>
+	
+        <div id="content">
+	  
+          <?php
+	     include('../inc/nav.inc');
+	     include('prog_nav.inc');
+          ?>
+	  
+	  <figure>
+            <img src="../img/passau.jpg" width="880" height="241" alt="Passau">
+            <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+          </figure>
+	  
+	  <?php
+
+	     if (isset($_GET['id'])) {
+	        $html = file_get_html("https://frab.fossgis-konferenz.de/de/2017/public/speakers/" . $_GET['id']);
+	        $referent = $html->find('h2[class=title]', 0)->innertext;
+          $text1 = $html->find('div[id=basic]', 0);
+	  $text2 = $html->find('div[id=details]', 0);
+	  $text3 = $html->find('div[id=sidebar]', 0);
+
+	  $text3 = str_replace("de/2017/public/events/", "2017/programm/event.php?id=", $text3);
+          $text3 = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $text3);
+	  
+          $text1 = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $text1);
+          $text1 = str_replace("/system/", "https://frab.fossgis-konferenz.de/system/", $text1);
+
+	  echo "<h2>" . $referent . "</h2>";
+          echo $text1;
+	  echo $text2;
+	  echo $text3;
+	     }
+          ?>
+	  
+	</div>
+	<?php include('../inc/footer.inc'); ?>
+      </div>
+    </body>
+</html>
