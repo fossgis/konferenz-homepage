322575e69a7b89c43177cec6b3e1b55685972016
Peter Barth (Peda) <osm-peda@won2.de>
2017-02-20 22:43:06 +0100

    added Infobox for single events

--- 2017/programm/event.php
+++ 2017/programm/event.php
@@ -36,16 +36,23 @@
 	        $html = file_get_html("https://frab.fossgis-konferenz.de/de/2017/public/events/" . $_GET['id']);
 	        $referent = $html->find('div[id=sidebar]', 0);
                 $text = $html->find('div[id=basic]', 0);
-	        $topic = $html->find('div[id=main-content]', 0)->find('h2', 0)->innertext;
-	  
+                $info = $html->find('div[id=details]', 0);
+	        $info->find('h3', 1)->outertext = '';
+	        $info->find('ul', 0)->outertext = '';
+	        $info->find('div[id=concurrent_events]', 0)->outertext = '';
+                $topic = $html->find('div[id=main-content]', 0)->find('h2', 0)->innertext;
+
+	        $info = str_replace("de/2017/public/schedule/", "2017/programm/programm.php?day=", $info);
                 $referent = str_replace("de/2017/public/speakers/", "2017/programm/speaker.php?id=", $referent);
                 $referent = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $referent);
                 $referent = str_replace("/system/", "https://frab.fossgis-konferenz.de/system/", $referent);
                 $text = str_replace("/assets/", "https://frab.fossgis-konferenz.de/assets/", $text);
 
-	        echo "<h3>" . $topic . "</h3>"; 
+	        
+                echo "<h3>" . $topic . "</h3>";
                 echo $referent;
-	        echo $text;
+	  echo $text;
+          echo $info;	  
 	     }
           ?>
 	  
