3b20acc7ba7620bc3472f8d95998b56c435969f5
Peter Barth (Peda) <osm-peda@won2.de>
2017-03-30 23:07:15 +0200

    hinzufügen einer leeren Bildergalerie

---
+++ 2017/gallery/FOSSGIS_e_V/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2017 Bildergalerie</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Passau">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+	<link rel="stylesheet" href="../static/styles.min.css">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+	<script src="../static/photoswipe.min.js"></script>
+	<script src="../static/photoswipe-ui-default.min.js"></script>
+	<script src="../static/echo/echo.min.js"></script>
+    </head>
+    
+    <body id="gallery">
+      <div id="wrap">
+        <?php include('../../inc/head.inc'); ?>
+	
+        <div id="content">
+	  
+          <?php
+	     include('../../inc/nav.inc');
+          ?>
+	  
+	  <figure>
+            <img src="../../img/passau.jpg" width="880" height="241" alt="Passau">
+            <figcaption>Photo: Tobias Hobmeier (CC-BY-SA)</figcaption>
+          </figure>
+	  
+          <h2>Bildergalerie - FOSSGIS e.V.</h2>
+
+		<?php include('imgs.html'); ?>
+
+	</div>
+	<?php include('../../inc/footer.inc'); ?>
+      </div>
+    </body>
+</html>
