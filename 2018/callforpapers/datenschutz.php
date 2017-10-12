98ae93fb90f3e2b539e55fa2d104ff121e393b73
Dominik Helle <helle@omniscale.de>
2017-10-12 19:14:42 +0200

    add datenschutz for 2018

---
+++ 2018/callforpapers/datenschutz.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+    <head>
+        <title>FOSSGIS 2018</title>
+        <meta name="description" content="Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme und OpenStreetMap.">
+        <meta charset="utf-8">
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="call">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+                <?php include('../inc/nav.inc'); ?>
+
+                <figure>
+                    <img src="../img/bonn.jpeg" width="880" height="241" alt="Bonn">
+                    <figcaption>Photo: Stadt Bonn</figcaption>
+                </figure>
+
+                <h2>Datenschutz und Lizenzvereinbarung</h2>
+                <h3>Lizenzvereinbarung Veröffentlichung</h3>
+
+                <p>Ich stelle meinen Konferenzbeitrag unter der <a href="https://creativecommons.org/licenses/by-sa/3.0/de/">Lizenz CC BY 3.0 DE</a> zur Verfügung. Der Konferenzbeitrag umfasst die Kurzbeschreibung des Beitrags, den Aufsatz im Konferenzband, die Vortragsfolien sowie die Videoaufzeichnung und Liveübertragung des Vortrags.</p>
+
+                <p>Soweit ein Konferenzbeitrag Material enthält, das unter einer abweichenden freien oder offenen Lizenz steht, sind diese Inhalte zu kennzeichnen. Entsprechend gekennzeichnete Inhalte werden unter dieser abweichenden Lizenz bereitgestellt.</p>
+
+                <p>Ich erkläre, dass mein Konferenzbeitrag keine Rechte Dritter (Urheberrechte, Persönlichkeitsrechte, etc.) verletzt. Insbesondere ist Sichergestellt, dass bei der Verwendung von geschützten Inhalten Dritter im Konferenzbeitrag eine Lizenzierung unter CC BY 3.0 DE erfolgen kann.</p>
+
+                <h3>Datenschutz</h3>
+
+                <p>Ich willige in die Erfassung, Speicherung, Verarbeitung und Weitergabe meiner personenbezogenen Daten (Name, Unternehmen, Postanschrift, E-Mailadresse, Telefonnummer) im Rahmen der FOSSGIS-Konferenz 2018 ein. Eine Weitergabe der erfassten Informationen an Dritte erfolgt nur, soweit dies für die bibliographische Aufbereitung des Konferenzbeitras notwendig ist.</p>
+
+                <p>Die Erhebung, Verarbeitung, Nutzung und Weitergabe meiner Daten erfolgt auf freiwilliger Basis. Die Einwilligung kann jederzeit mit Wirkung für die Zukunft per E-Mail an den FOSSGIS e.V. [info@fossgis.de] widerrufen werden. Davon ausgenommen sind Informationen, für die ein gesetzlicher Erlaubnistatbestand besteht.</p>
+
+
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
