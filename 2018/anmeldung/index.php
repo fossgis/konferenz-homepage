a2083e66497e453cb8ceccd5f5e251b3f6b924dc
katjahafi <katjahafi@gmx.de>
2017-12-10 19:00:11 +0100

    Anmelde-Seite und Preisinformationen

---
+++ 2018/anmeldung/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2018 Anmeldung</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, OSGeo, CodeSprint, Universität Bonn, Rhein, Beethovenstadt, UN-Stadt">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:regular,bold" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+    </head>
+    <body id="anmeldung">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+
+                  <figure>
+                    <img src="img/bonn.jpeg" width="880" height="241" alt="Bonn">
+                    <figcaption>Photo: Stadt Bonn</figcaption>
+                </figure>
+
+                <h2>Anmeldung zur Konferenz</h2>
+				<!-- <p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/konf-2018/">Direkt zur Anmeldung &rarr;</a></p>-->
+                <p>Wir laden Sie ein Teil der FOSSGIS-Konferenz 2018 zu werden. Die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie!</p>
+                <p>In Erwartung von &uuml;ber 400 Teilnehmenden ist eine Registrierung Ihrerseits notwendig und hilfreich f&uuml;r die Organisation. Nach erfolgreicher Anmeldung erhalten Sie eine Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages sind Sie registriert. 
+<!--Hier gelangen Sie <a href="https://pretix.eu/fossgis/konf-2018/"> zur Anmeldung</a>. Im Rahmen der Anmeldung können Sie bis zum 19.03.2017 eine Reihe von FOSSGIS-Workshops (kostenpflichtig) auswählen. Informationen zu den einzelnen Workshops finden Sie im <a href="https://www.fossgis-konferenz.de/2018/programm/">Programm</a>. </p>-->
+				<p>Eine spontane Teilnahme ist m&ouml;glich, bitte melden Sie sich bei Ankunft am Welcome Desk. Vor Ort kann nur in Bar bezahlt werden.</p>
+
+				<!--<p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/2017/">Direkt zur Anmeldung &rarr;</a></p>-->
+				<h3>Preise FOSSGIS-Konferenz 2018</h3> 
+				<ul class="list">
+				<li> Standardticket (Normalpreis): € 190</li>
+				<li> Standardticket (Frühbucherrabatt bis 06.02.2018, 21 Uhr): € 150</li>
+				<li> Studierendenticket: € 80</li>
+				<li> Communityticket*: € 0*</li>
+				<li> Workshop: € 100</li>
+				<li> Workshop (Frühbucherrabatt bis 06.02.2017, 21 Uhr): € 90</li>
+				</ul>
+                <p><b>Im Konferenzticket enthalten:</b> FOSSGIS-Konferenz-Teilnahme, Pausensnack, Tagungsband, Tasche, Campus-Dialoge.<br>
+* Regelungen für das Communityticket gelten für FOSSGIS-, Openstreetmap- und OSGEO-Aktive, bitte bei Anmeldung im "Freitext für FOSS/OSM-Community Aktive" darstellen.</p>
+
+                <h3>Helfer</h3>
+                <p>Freiwillige Helfer sind eingeladen und willkommen während der Konferenz bei den Videoaufnahmen zu unterstützen. Bei Interesse bitte bei per E-Mail unter konferenz-orga@fossgis.de melden und Zeitraumpräferenz angeben. Es ist möglich dafür ein kostenfreies Ticket zu erhalten.</p>
+
+
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
