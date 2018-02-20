02b78964943784d0a0f13f13932e79c44b8630fe
katjahafi <katjahafi@gmx.de>
2018-02-20 18:20:34 +0100

    Vor-Ort-Anmeldung

--- 2018/anmeldung/index.php
+++ 2018/anmeldung/index.php
@@ -31,7 +31,7 @@
                 <p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz 2018 zu werden. Die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie!</p>
                 <p>In Erwartung von &uuml;ber 400 Teilnehmenden ist eine Registrierung Ihrerseits notwendig und hilfreich f&uuml;r die Organisation. Nach erfolgreicher Anmeldung erhalten Sie eine Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages erhalten Sie den Link zu Ihrem Ticket.</p>
 <!--Hier gelangen Sie <a href="https://pretix.eu/fossgis/konf-2018/"> zur Anmeldung</a>. Im Rahmen der Anmeldung können Sie bis zum 19.03.2017 eine Reihe von FOSSGIS-Workshops (kostenpflichtig) auswählen. Informationen zu den einzelnen Workshops finden Sie im <a href="https://www.fossgis-konferenz.de/2018/programm/">Programm</a>. </p>-->
-				<p>Wir empfehlen Ihnen sich online anzumelden. Eine spontane Teilnahme kann aufgrund der räumlichen Begrenzung nicht gewährleistet werden. Im Falle einer Ort-Ort-Anmeldung kann nur in bar bezahlt werden. </p>
+				<p>Wir empfehlen Ihnen sich online anzumelden. Eine spontane Teilnahme kann aufgrund der räumlichen Begrenzung nicht gewährleistet werden. Im Falle einer Vor-Ort-Anmeldung kann nur in bar bezahlt werden. </p>
 
 				<!--<p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/2017/">Direkt zur Anmeldung &rarr;</a></p>-->
 				<h3>Preise FOSSGIS-Konferenz 2018</h3> 
