8f4645e07868c283223fababbbd10dc60e0e1f6a
Astrid Emde <astrid.emde@wheregroup.com>
2018-03-05 08:55:58 +0100

    Social Event erweitert. Acts + Buchung
    * Hinweis auf die Künstler
    * Anpassung Zeit auf 0:30
    * Link zur Buchung separat

--- 2018/socialevents/index.php
+++ 2018/socialevents/index.php
@@ -23,7 +23,7 @@
 
       <h2>Social Events</h2>
         <h3>Zusammentreffen mit den OSGeo Code Sprintern 2018</h3>
-		<p>Zeitgleich zur FOSSGIS findet der internationale <a href="https://wiki.osgeo.org/wiki/OSGeo_Code_Sprint_2018">OSGeo Code Sprint</a> vom 19. bis 25. März 2018 im Bonner BaseCamp statt. Der In- und Outdoor Retro-Park mit ausrangierten Wohnwagen und Zugschlafwagen hat sich mit seinem nostalgischen Flair schon bei der FOSS4G 2016 bewährt.
+		<p>Zeitgleich zur FOSSGIS findet der internationale <a href="https://wiki.osgeo.org/wiki/OSGeo_Code_Sprint_2018">OSGeo Code Sprint</a> vom 19. bis 25. März 2018 im Bonner BaseCamp statt. Der In- und Outdoor Retro-Park mit ausgefallenen Wohnwagen und Zugschlafwagen hat sich mit seinem nostalgischen Flair schon bei der FOSS4G 2016 bewährt.
 		<br> Wer Lust hat internationale Entwickler unserer OSGeo-Projekte zu treffen, beteiligt sich am "Pubrace". Beim seit der FOSS4G 2016 beliebten Pubrace treffen sich Leute Abends ab 20 Uhr bei einem Getränk im <a href="http://www.openstreetmap.org/node/1581262116">Namenlos Bonn</a> am Tor zur Bonner Altstadt oder im <a href="http://www.openstreetmap.org/node/225642197">Pawlow</a> im Herzen derselbigen.
 		    <br>Wer Lust hat direkt auch am OSGeo Code Sprint teilzunehmen ist ebenso herzlich eingeladen. Anmeldung unter <a href="https://wiki.osgeo.org/wiki/OSGeo_Code_Sprint_2018">OSGeo Code Sprint</a>.
 		    </p>
@@ -41,7 +41,15 @@
           <p>Es können an allen Tagen Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen bis 19:30 Uhr zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder mit dem Team vor Ort ansprechen.</p>
 
 		<h4>Abendveranstaltung "Campus-Dialoge"</h4>
-		 <p> Das gesellige Beisammensein findet von 19:15 - 24:00 Uhr in der CAMPO Campusmensa Poppelsdorf der Universität Bonn statt. Die "Campus-Dialoge" werden sich durch ein offenes Ambiente auszeichnen. Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Wir bitten darum bei der Anmeldung die Teilnahme an der Abendveranstaltung anzugeben, dies dient der guten Planung der Veranstaltung.</p>
+		 <p>Das gesellige Beisammensein findet von 19:15 - 0:30 Uhr in der CAMPO Campusmensa Poppelsdorf der Universität Bonn statt. Die "Campus-Dialoge" werden sich durch ein offenes Ambiente auszeichnen.
+Neben einem reichhaltigen Rheinischen und Mediterranen Buffet und Getränken bieten wir Raum zur Diskussion und zur späteren Stunde auch ein kulturelles Programm. 
+<br>
+<a href="https://de-de.facebook.com/dadsphonkey/" target="_blank" title="Dad's Phonkey">Dad's Phonkey - Christian Padberg</a> singt mit sich selbst und entfacht mit seiner Loop-Station ein musikalisches Feuerwerk. Anschließend wird das <a href="http://luchaamada.blogsport.de/" target="_blank" title="DJ Kollektiv Lucha Amada">DJ Kollektiv Lucha Amada</a> für Stimmung sorgen.
+<br>
+Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Wir bitten darum bei der Anmeldung die Teilnahme an der Abendveranstaltung anzugeben, dies dient der guten Planung der Veranstaltung. 
+<br>
+Darüber hinaus kann das Social Event auch separat ohne Konferenzteilnahme gebucht werden - <a href="https://pretix.eu/fossgis/konf-2018/w/Tg5PUTaoTJOIAaoY/redeem?voucher=CAMPUS_DIALOG_3QMZX9X7MPWYDF8K&subevent=&locale=de" target="_blank" title="Buchung Social Event">zur Buchung</a>.
+</p>
 
 		<h3>Donnerstag, 22. März 2018</h3>
 		 <h4>FOSSGIS e.V. Mitgliederversammlung</h4>
