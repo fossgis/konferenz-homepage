0182441e53bb126ace58711b523349612472ca44
katjahafi <katjahafi@gmx.de>
2018-03-10 22:08:01 +0100

    FOSSIS ausgebucht

--- 2018/anmeldung/index.php
+++ 2018/anmeldung/index.php
@@ -29,9 +29,11 @@
                 <h2>Anmeldung zur Konferenz</h2>
 				<!-- <p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/konf-2018/">Direkt zur Anmeldung &rarr;</a></p>-->
                 <p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz 2018 zu werden. Die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie!</p>
-                <p>In Erwartung von &uuml;ber 400 Teilnehmenden ist eine Registrierung Ihrerseits notwendig und hilfreich f&uuml;r die Organisation. Nach erfolgreicher Anmeldung erhalten Sie eine Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages erhalten Sie den Link zu Ihrem Ticket.</p>
+                <p><b>Die Konferenz 2018 ist erstmalig ausgebucht.</b> Frei werdende Tickets (über Stornierung) werden über eine Warteliste angeboten.  
+Der Besuch eines Workshop setzt ein Konferenzticket voraus.</p>
+<!--In Erwartung von &uuml;ber 400 Teilnehmenden ist eine Registrierung Ihrerseits notwendig und hilfreich f&uuml;r die Organisation. Nach erfolgreicher Anmeldung erhalten Sie eine Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages erhalten Sie den Link zu Ihrem Ticket.</p>
 <!--Hier gelangen Sie <a href="https://pretix.eu/fossgis/konf-2018/"> zur Anmeldung</a>. Im Rahmen der Anmeldung können Sie bis zum 19.03.2017 eine Reihe von FOSSGIS-Workshops (kostenpflichtig) auswählen. Informationen zu den einzelnen Workshops finden Sie im <a href="https://www.fossgis-konferenz.de/2018/programm/">Programm</a>. </p>-->
-				<p>Wir empfehlen Ihnen sich online anzumelden. Eine spontane Teilnahme kann aufgrund der räumlichen Begrenzung nicht gewährleistet werden. Im Falle einer Vor-Ort-Anmeldung kann nur in bar bezahlt werden. </p>
+				<!--<p>Wir empfehlen Ihnen sich online anzumelden. Eine spontane Teilnahme kann aufgrund der räumlichen Begrenzung nicht gewährleistet werden. Im Falle einer Vor-Ort-Anmeldung kann nur in bar bezahlt werden. </p>
 
 				<!--<p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/2017/">Direkt zur Anmeldung &rarr;</a></p>-->
 				<h3>Preise FOSSGIS-Konferenz 2018</h3> 
@@ -46,11 +48,12 @@
                 <p><b>Im Konferenzticket enthalten: </b>FOSSGIS-Konferenz-Teilnahme, Pausensnack, Tasche und wenn bestellt Tagungsband, T-Shirt und Abendveranstaltung Campus-Dialoge.<br>
 * Regelungen für das Communityticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie Helfer der Konferenz. Bitte bei der Anmeldung im "Freitext zum Communityticket" angeben.</p>
 
-                <h3>Helfer</h3>
-                <p>Freiwillige Helfer sind eingeladen und willkommen während der Konferenz bei den Videoaufnahmen, als Sessionleiter oder beim Catering zu unterstützen. Bei Interesse bitte direkt per E-Mail an konferenz-orga@fossgis.de melden oder die entsprechenden Fragen in der Onlineanmeldung beantworten. Es ist möglich dafür das kostenfreie "Community-Ticket" zu erhalten.</p>
+            <!--    <h3>Helfer</h3>
+                <p>Freiwillige Helfer sind eingeladen und willkommen während der Konferenz bei den Videoaufnahmen, als Sessionleiter oder beim Catering zu unterstützen. Bei Interesse bitte direkt per E-Mail an konferenz-orga@fossgis.de melden oder die entsprechenden Fragen in der Onlineanmeldung beantworten. Es ist möglich dafür das kostenfreie "Community-Ticket" zu erhalten.</p>-->
 
 				<h3>Anmeldeformular</h3>
 				<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, finden Sie hier den direkten Link zum <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/konf-2018/">Ticketsystem</a>.
+Bei erfolgter Buchung erhalten Sie nach der bestätigung der Überweisung des Rechnungsbetrages Sie den Link zu Ihrem Ticket.
 				<pretix-widget event="https://pretix.eu/fossgis/konf-2018/"></pretix-widget>
 				<noscript>
   				 <div class="pretix-widget">
