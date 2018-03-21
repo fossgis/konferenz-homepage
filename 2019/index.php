f7ef8a91bf1c067420a31a8267339157633140ac
Dominik Helle <helle@omniscale.de>
2018-03-21 14:38:39 +0100

    update 2019er conference

--- 2019/index.php
+++ 2019/index.php
@@ -11,44 +11,36 @@
 </head>
 <body id="home">
     <div id="wrap">
-        <h1>FOSSGIS Konferenz <span>Call for Location</span> <br>2019</h1>
+        <h1>FOSSGIS Konferenz 2019</h1>
 
-        <div id="content">
-            <h2>Call for Location zur FOSSGIS-Konferenz 2019</h2>
+      <div id="content">
+                <?php include('inc/nav.inc'); ?>
 
-            <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
+<!--            <figure>
+                    <img src="img/dres.jpeg" width="880" height="241" alt="Bonn">
+                    <figcaption>Photo: Stadt Bonn</figcaption>
+                </figure>
+ -->
+                <h2>Willkommen zur FOSSGIS-Konferenz 2019</h2>
 
-            <p>Die FOSSGIS-Konferenz 2019 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> organisiert.</p>
+                <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
 
-            <p>Der Veranstalter sucht auf diesem Wege einen Austragungsort für die Konferenz im Jahre 2019 und ruft zur Einreichung von Bewerbungen auf.</p>
+                <p>Die FOSSGIS-Konferenz 2019 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> mit Unterstützung der <a href="https://www.htw-dresden.de">Hochschule für Technik und Wirtschaft Dresden</a> veranstaltet.</p>
 
-            <div class="organisers">
-                <div class="sponsor organizer"><a href="http://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
-                    <div class="sponsor organizer"><a href="http://www.osgeo.org/"><img src="img/osgeo.png" width="180px" valign="middle"></a></div>
-                <div class="sponsor organizer"><a href="http://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
+                <div class="organisers">
+                    <div class="sponsor organizer"><a href="https://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
+                    <div class="sponsor organizer"><a href="https://www.osgeo.org/"><img src="img/osgeo.png" width="180px" valign="middle"></a></div>
+                    <div class="sponsor organizer"><a href="https://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
+                </div>
+                <p class="clear"></p>
+                <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>
+
+                 <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+
+                <h3>Werden Sie Sponsor!</h3>          
+                <p> Sie möchten die FOSSGIS-Konferenz als Sponsor unterstützen? Weitere Informationen für Sponsoren und Aussteller finden Sie <a href="http://www.fossgis.de/wiki/Sponsoren_und_Aussteller"> hier </a> oder wenden sie sich direkt <a href="mailto:katja.haferkorn@fossgis.de">Frau Haferkorn</a>.
+                </div>
             </div>
-            <p class="clear"></p>
-
-            <h3>Was ist die FOSSGIS Konferenz?</h3>
-
-            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Web-Kartenanwendungen, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS.</p>
-
-            <p>Zusätzlich sind auch freie Geodaten ein Schwerpunkt der Konferenz. Spezielles Augenmerk liegt hier auf dem Projekt OpenStreetMap.</p>
-
-            <h3>An welchen Orten wird die Konferenz ausgetragen?</h3>
-            <p>Typischerweise wird die Konferenz an Hochschulen oder Bildungseinrichtungen durchgeführt.</p>
-
-            <p>In den letzten Jahren war die Konferenz zu Gast an der Beuth Hochschule für Technik in Berlin (2014) und an der Westfälische Wilhelms-Universität in Münster (2015). Im Jahr 2017 gastierte die FOSSGIS-Konferenz in Passau. In diesem Jahr findet die Konferenz in Bonn statt.</p>
-
-            <h3>Wie finanziert sich die Konferenz?</h3>
-
-            <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen.</p>
-
-            <h3>Interesse?</h3>
-            <p>Sie möchten die Konferenz bei Ihnen auszurichten? Detaillierte Informationen finden Sie im <a href="data/CfL-FOSSGIS-2019.pdf">Bewerbungs-PDF</a>.</p>
-
-            <p>Wir freuen uns auf Ihre kurze Bewerbung. Bei Fragen können Sie sich gerne an info[aet]fossgis.de wenden.</p>
-        </div>
         <?php include('inc/footer.inc'); ?>
     </div>
 </body>
