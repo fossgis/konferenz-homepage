9bf8491ae178bac9296e65615150fc8b4b5470d8
Dominik Helle <helle@omniscale.de>
2018-03-22 15:07:38 +0100

    update 2019 page

--- 2019/index.php
+++ 2019/index.php
@@ -16,23 +16,40 @@
       <div id="content">
                 <?php include('inc/nav.inc'); ?>
 
-<!--            <figure>
-                    <img src="img/dres.jpeg" width="880" height="241" alt="Bonn">
-                    <figcaption>Photo: Stadt Bonn</figcaption>
+                <figure>
+                    <img src="img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                    <figcaption>Photo: </figcaption>
                 </figure>
- -->
+
                 <h2>Willkommen zur FOSSGIS-Konferenz 2019</h2>
 
                 <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
 
-                <p>Die FOSSGIS-Konferenz 2019 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> mit Unterstützung der <a href="https://www.htw-dresden.de">Hochschule für Technik und Wirtschaft Dresden</a> veranstaltet.</p>
+                <p>Die FOSSGIS-Konferenz 2019 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> und der <a href="https://www.htw-dresden.de">Hochschule für Technik und Wirtschaft Dresden</a> veranstaltet.</p>
 
                 <div class="organisers">
-                    <div class="sponsor organizer"><a href="https://www.fossgis.de/"><img src="img/fossgis.png" valign="middle"></a></div>
-                    <div class="sponsor organizer"><a href="https://www.osgeo.org/"><img src="img/osgeo.png" width="180px" valign="middle"></a></div>
-                    <div class="sponsor organizer"><a href="https://www.openstreetmap.de/"><img src="img/osm.png" valign="middle"></a></div>
+                    <div class="sponsor organizer">
+                        <a href="https://www.fossgis.de/">
+                            <img src="img/fossgis.png" valign="middle">
+                        </a>
+                    </div>
+                    <div class="sponsor organizer">
+                        <a href="https://www.htw-dresden.de/">
+                            <img src="img/htw.png" valign="middle">
+                        </a>
+                    </div>     
+                    <div class="sponsor organizer">
+                        <a href="https://www.openstreetmap.de/">
+                            <img src="img/osm.png" valign="middle">
+                        </a>
+                    </div>                                   
+                    <div class="sponsor organizer">
+                        <a href="https://www.osgeo.org/">
+                            <img src="img/osgeo.png" width="180px" valign="middle">
+                        </a>
+                    </div>                    
                 </div>
-                <p class="clear"></p>
+
                 <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>
 
                  <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
