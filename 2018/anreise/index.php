e93002b1ffeb4e5da45ef31fb5a3a146e44be09e
Michael Reichert <nakaner@gmx.net>
2018-05-26 20:31:02 +0200

    remove unnecessary duplication of Leaflet

--- 2018/anreise/index.php
+++ 2018/anreise/index.php
@@ -10,8 +10,8 @@
         <link rel="stylesheet" type="text/css" href="/static/droidsans/droidsans.css" />
         <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
 
-        <link rel="stylesheet" href="../css/leaflet.css">
-        <script src="../js/leaflet/leaflet.js"></script>
+        <link rel="stylesheet" href="/static/leaflet/0.7.3/leaflet.css">
+        <script src="/static/leaflet/0.7.3/leaflet.js"></script>
         <script src="script/Leaflet.MakiMarkers.js"></script>
 
     </head>
