760cf0d3e956c0554829ba1dd881f0e909a86ee7
katjahafi <katjahafi@gmx.de>
2018-08-31 13:40:13 +0200

    Call for Papers erstellt

---
+++ 2019/callforpapers/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html>
+    <head>
+        <title>FOSSGIS 2019</title>
+        <meta name="description" content="Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme und OpenStreetMap.">
+         <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, 2019, FOSSGIS-Konferenz 2019, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, HTW Dresden">
+		 <meta charset="utf-8">
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="call">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+                <?php include('../inc/nav.inc'); ?>
+
+               <figure>
+                    <img src="img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                    <figcaption>Photo: Kolossos</figcaption>
+                </figure>
+
+                <h2>Call for Papers</h2>
+                <p class="text-right"><a class="btn btn-primary" href="https://pretalx.com/fossgis2019/cfp">Zum Konferenzsystem und Beitrag einreichen &rarr;</a></p>
+
+                <p>Forscher, Entwickler und Anwender sowie Mitwirkende des OpenStreetMap-Projekts können einen Vortrag oder Workshop einreichen. Es werden Vorträge mit Neuigkeitswert sowie solche gesucht, die spannende Themen behandeln oder anregende Diskussionen auslösen. Thema der Konferenz sind freie Geodaten (insbesondere, aber nicht nur, OpenStreetMap), Open Data und freie GIS-Software. </p>
+
+                <dl>
+                <dt>Wer wir sind:</dt>
+                <dd>Die Konferenz wird vom <a href="http://fossgis.de">FOSSGIS e.V.</a> und der <a href="http://www.openstreetmap.de">OpenStreetMap Community</a> veranstaltet.</dd>
+
+                <dt>Wir bieten:</dt>
+                <dd>Eine Plattform für Ihre Idee, Ihr Projekt, Ihren Erfahrungsbericht auf der größten deutschsprachigen Anwenderkonferenz für freie Geoinformationssysteme und freie Geodaten. Im Jahr 2019 findet die FOSSGIS vom 13. bis 16. März an der Hochschule für Technik und Wirtschaft in Dresden statt. Die Konferenz ist die größte deutschsprachige Anwender-Konferenz für Open-Source-Geo-Software und offene Geodaten. Der Call for Papers ist bis zum <strong>08.11.2018</strong> geöffnet.</dd>
+
+                <dt>Wir suchen:</dt>
+                <dd>Ihre Idee, Ihr Projekt, Ihr Erfahrungsbericht, Ihr Thema. Genauer gesagt, suchen wir Vorträge für Einsteiger und Fortgeschrittene, die spannende Themen behandeln und anregende Diskussionen auslösen. Vorträge zum Thema freie Geodaten, zum Beispiel OpenStreetMap oder Open Data sind ebenso möglich wie Beiträge zu Lösungen mit freier Software aus dem Bereich WebGIS, Desktop GIS, Geodatenbanken, Location-Based Services, etc.
+                </dd>
+                </dl>
+
+                <p>Werden Sie Teil der Konferenz und bewerben Sie sich jetzt mit einem <strong>Vortrag</strong>, <strong>Lightning Talk</strong> oder <strong>Workshop</strong>.</p>
+
+                <p><a class="btn btn-primary" href="https://pretalx.com/fossgis2019/cfp">Zum Konferenzsystem und Beitrag einreichen &rarr;</a></p>
+
+                <p>Die FOSSGIS-Konferenz 2019 ermöglicht folgende Veranstaltungstypen:</p>
+                <dl>
+                    <dd>
+                    <strong>Vorträge und Workshops:</strong> 20 Minuten lange Vorträge machen den Hauptteil aus. Diese finden am Mittwochnachmittag, Donnerstag und Freitagvormittag statt. Parallel zu den Vorträgen finden 90 Minuten lange Workshops statt. Die Teilnahme am Workshop ist kostenpflichtig, die Teilnehmer*innenanzahl begrenzt. Workshops mit zwei Leitern sind möglich, insbesondere, wenn der Workshop stark nachgefragt ist. Diese finden am Mittwochnachmittag, Donnerstag und Freitagvormittag statt.
+                    </dd>
+                    <dd>
+                        <strong>Lightning Talk:</strong> 5 Minuten lange Lightning Talks sind die Gelegenheit neue Ideen und neue Software in Kürze zu präsentieren. Diese Vorträge haben keine Fragerunde.
+                    </dd>
+
+                    <dd><strong>Neu im Programm sind die beiden folgenden Veranstaltungstypen:</strong> 60 Minuten lange „Demo-Sessions“ sind für die intensive Vorführung freier GIS-Software vorgesehen, in der Redner die Benutzung einer Software zeigen. Die Teilnahme ist im Konferenzticket enthalten. Die Demo-Sessions zeichnen sich durch eine höhere inhaltliche Tiefe gegenüber Workshops und 20-Minuten-Vorträgen aus, weil sie in Hörsälen statt in Computerpools stattfinden und die Teilnehmer*innen die Schritte nicht in Echtzeit an ihren Computern nachvollziehen können.<br>40 Minuten lange Vorträge zu OpenStreetMap-Themen, an die sich jeweils eine 20 Minuten lange Fragerunde anschließt. Sie sind für kontroverse Themen der OpenStreetMap-Community vorgesehen.</dd>
+                </dl>
+
+                <p>Unserer Befragung zufolge interessieren sich unsere Teilnehmer*innen aus dem GIS-Sektor insbesondere für folgende Themen: Docker, Angular2, 1. Schritte in Python, GeoPackage, QGIS für Fortgeschrittene, QGIS-3-Plugins, GeoNetwork, GeoServer Einführung, GeoTools und Java Topology Suite (JTS), pgRouting, INSPIRE Dienste, ATOM-Feeds, Big-Data Geo-Frameworks, Big-Data Analyse in R, SpatiaLite. Sehen Sie diese Auflistung bitte als Anregung. Werden Sie Teil der Konferenz und bewerben Sie sich jetzt mit einem Vortrag oder Workshop. Fragen zum Call for Papers beantworten FOSSGIS-Mitglieder gerne per E-Mail an konferenz-orga@fossgis.de.</p>
+
+<p>Die Programmplanung findet beim 11. FOSSGIS Hacking vom 23. bis 25. November im Linuxhotel in Essen statt. Daher wird es keine Verlängerung des Call for Papers geben. Das detaillierte Programm wird wenige Wochen später bekannt gegeben werden, gleichzeitig wird die Anmeldung eröffnet werden.</p>
+<p>Als Redner sind Sie berechtigt, ein kostenloses Communityticket als Eintrittskarte zu lösen. Workshopleiter erhalten einen Rabatt auf den Eintritt, falls sie nicht anderweitig berechtigt sind, ein kostenloses Communityticket zu lösen.</p>
+<p>Sie haben die Möglichkeit, bei der Einreichung des Vortrags anzugeben, an welchen Tagen der Konferenz Sie Zeit haben. Scheuen Sie sich nicht davor, einen Vortrag einzureichen, auch wenn Sie während der Konferenz nur eingeschränkt verfügbar sind.</p>
+<p>Bitte reichen Sie Ihren Abstract für die FOSSGIS 2019 ab sofort bis zum 08. November 2018 ein. Eine Fristverlängerung ist wegen der ohnehin straffen Zeitplanung nicht vorgesehen.</p>
+
+                <p ><a class="btn btn-primary" href="https://pretalx.com/fossgis2019/cfp">Zum Konferenzsystem und Beitrag einreichen &rarr;</a></p>
+
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
