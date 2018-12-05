55d000ef17e86f2d60721c3e69fdeda5d2bc60a9
Benjamin Thurm <bentrm@posteo.de>
2018-12-05 10:38:38 +0100

    Add social events page

---
+++ 2019/socialevents/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>FOSSGIS 2019 Social Events</title>
+    <meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+    <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+    <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, Bonn, Universität Bonn, Campus-Dialoge, Rhein">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+    <link rel="stylesheet" type="text/css" href="/fonts/Droid_Sans_regular_bold.css" />
+    <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+<body id="socialEvents">
+    <div id="wrap">
+        <?php include('../inc/head.inc'); ?>
+
+        <div id="content">
+            <?php include('../inc/nav.inc'); ?>
+            <figure>
+                <img src="../img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                <figcaption>Photo: Kolossos</figcaption>
+            </figure>
+
+            <h2>Social Events</h2>
+
+            <h3>
+                Innoffizieller Start<br>
+                <small>Dienstag, 12. März 2019 ab 18:30 Uhr</small>
+            </h3>
+            <p>Noch vor der eigentlichen Konferenz besteht die Möglichkeit alte Gesichter wiederzusehen und neue Bekanntschaften zu machen.</p>
+            <p>Für dieses inoffizielle Treffen wurde am <b>12. März 2019 ab 18:30 Uhr</b> eine offene Reservierung im Altmarktkeller Dresden eingerichtet:</p>
+            <p>
+                <b>Adresse:</b><br>
+                <i>Altmarktkeller Dresden</i><br>
+                Altmarkt 4<br>
+                01067 Dresden
+            </p>
+            <p>Bis ca. 23 Uhr bietet die Küche warme Speisen. Jeder zahlt seine Rechnung selbst.</p>
+
+            <h3>
+                Abendveranstaltung "Dialoge im Bärenzwinger"<br>
+                <small>Mittwoch, 13. März 2019 ab 19:00 Uhr</small>
+            </h3>
+            <p>
+                Die diesjährige Abendveranstaltung zur Konferenz "Dialoge im Bärenzwinger" findet am <b>13. März 2019 ab circa 19:00 Uhr</b> im <a href="https://www.baerenzwinger.de" target="_blank" rel="nofollow">Studentenklub "Bärenzwinger"</a> statt.
+                Für das Buffet steht außerdem das direkt daran angrenzende historische Kassemattengewölbe zur Verfügung.
+                Der Studentenclub liegt direkt an der historischen Altstadt unterhalb der Brühlschen Terrassen. Er ist per Straßenbahnlinie 3, 7, 8 und 9 erreichbar.
+                Zu Fuß ist die Veranstaltung ab dem HTW-Zentralgebäude etwa 30 Minuten entlang der Einkaufsstraße "Prager Straße" entfernt.
+            </p>
+            <p>
+                <b>Adresse:</b><br>
+                <i>Studentenclub Bärenzwinger</i><br>
+                Brühlscher Garten 1<br>
+                01067 Dresden
+            </p>
+            <p>
+                Der Eintritt zur Veranstaltung ist im FOSSGIS-Konferenz-Ticket enthalten, <b>die Teilnehmerzahl ist allerdings streng begrenzt</b>.
+                Eine verbindliche Voranmeldung zum Event bei der Ticketbuchung ist deshalb ratsam.
+            </p>
+            <p>
+                Zur Veranstaltung werden jedem Gast Getränkegutscheine gereicht, die an der Bar des Bärenzwingers eingelöst werden können.
+            </p>
+
+            <h3>
+                FOSSGIS e.V. Mitgliederversammlung<br>
+                <small>Donnerstag, 14. März 2019, 18:00</small>
+            </h3>
+            <p>Die FOSSGIS e.V. Mitgliederversammlung findet im Anschluss des zweiten Konferenztages im Zentralgebäude der Hochschule statt.</p>
+            <p>Treffpunkt ist der Mathe-Hörsaal Z211.</p>
+
+            <h3>
+                Sektempfang<br>
+                <small>Freitag, 15. März 2019 ab 14:30 Uhr</small>
+            </h3>
+            <p>Am Freitag gegen 14:30 Uhr laden die Veranstalter zum Sektempfang am FOSSGIS-Stand.</p>
+            <p>Alle Konferenzteilnehmerinnen & Teilnehmer sowie Vereinsmitglieder, Freunde und Interessierte sind herzlich eingeladen.</p>
+
+            <h3>
+                Exkursion zum Mathematisch-Physikalischen Salon im Zwinger<br>
+                <small>Freitag, 15. März 2019 ab 15:30 Uhr</small>
+            </h3>
+            <p>Nach dem freitäglichen Sektempfang ist gegen 15:30 Uhr eine Exkursion zum <a href="https://mathematisch-physikalischer-salon.skd.museum" target="_blank" rel="nofollow">Mathematisch-Physikalischen Salon im Zwinger</a> geplant.</p>
+            <p>Es wird eine auf die Konferenzteilnehmer zugeschnittene Führung durch das Museum geben.</p>
+            <p>Interessierte sind herzlich eingeladen sich anzuschließen. Der Eintritt zum Salon ist durch jeden Teilnehmer selbst zu zahlen.</p>
+
+            <h3>
+                Anwendertreffen
+            </h3>
+            <p><u>Es können Anwendertreffen, Sprints und Birds-of-a-Feather-Treffen selbst organisiert werden.</u></p>
+            <p>Räume stehen bis Samstag 18 Uhr zur Verfügung, Termine bitte beim Konferenzteam per E-Mail (konferenz-orga@fossgis.de ) anfragen oder das Team vor Ort ansprechen.</p>
+        </div>
+
+        <?php include('../inc/footer.inc'); ?>
+    </div>
+</body>
+</html>
