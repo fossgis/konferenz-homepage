3a72b9ae8a6cac33cc014ad931f2b1f82c921e70
katjahafi <katjahafi@gmx.de>
2018-12-05 22:33:51 +0100

    Ergänzungen Social Events und Anmeldung

---
+++ 2019/anmeldung/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2019 Anmeldung</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="author" content="Oliver Roick. http://oliverroick.de/">
+        <meta name="keywords" content="FOSSGIS, FOSSGIS-KOnferenz 2019, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, OSGeo, CodeSprint, HTW Dresden, Elbe, Dresden, FOSSGIS-Konferenz, Dialoge im Bärenzwinger">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="/static/droidsans/droidsans.css" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/konf-2019/widget/v1.css">
+		<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+
+    </head>
+    <body id="anmeldung">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+
+                  <figure>
+                    <img src="../img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                    <figcaption>Photo: Kolossos</figcaption>
+                </figure>
+
+                <h2>Anmeldung zur Konferenz</h2>
+				<!-- <p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/konf-2018/">Direkt zur Anmeldung &rarr;</a></p>-->
+                <p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz 2019 zu werden. Die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie!</p>
+                <p>Das Konferenz-Team empfielt Ihnen sich online anzumelden. Eine spontane Teilnahme kann aufgrund der räumlichen Begrenzung auf 400 Pesonen nicht gewährleistet werden. Eine Registrierung Ihrerseits ist notwendig, um Ihre Teilnahme zu sichern und hilfreich f&uuml;r die Organisation. </p>
+<!--Hier gelangen Sie <a href="https://pretix.eu/fossgis/2019/"> zur Anmeldung</a>. Im Rahmen der Anmeldung können Sie bis zum 11.03.2019 eine Reihe von FOSSGIS-Workshops (kostenpflichtig) auswählen. Informationen zu den einzelnen Workshops finden Sie im <a href="https://www.fossgis-konferenz.de/2019/programm/">Programm</a>. </p>-->
+				<!--
+				<!--<p class="text-right"><a class="btn btn-primary" target="_blank" href="https://pretix.eu/fossgis/2017/">Direkt zur Anmeldung &rarr;</a></p>-->
+				<p><b>Der Ticketverkauf startet in wenigen Tagen</b></p>
+				<h3>Preise FOSSGIS-Konferenz 2019</h3> 
+				<ul class="list">
+				<li> Standardticket (Normalpreis): € 190</li>
+				<li> Standardticket (Frühbucherrabatt bis 31.01.2019): € 150</li>
+				<li> Studierendenticket: € 40</li>
+				<li> Communityticket*: € 0*</li>
+				<li> Workshop: € 100</li>
+				<li> Workshop (Frühbucherrabatt bis 31.01.2019): € 90</li>
+				</ul>
+                <p><b>Im Konferenzticket enthalten: </b>FOSSGIS-Konferenz-Teilnahme, Pausensnack, Tasche und wenn bestellt Tagungsband, T-Shirt und Abendveranstaltung Dialoge im Bärenzwinger. <small>* Regelungen für das Communityticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie aktive Helfer der Konferenz.</small></p>
+
+            	<h3>Helfer</h3>
+                <p>Freiwillige Helfer sind eingeladen und willkommen während der Konferenz bei den Videoaufnahmen, als Sessionleiter oder beim Catering zu unterstützen. Bei Interesse bitte im <!--<a href="https://helfer-2019.fossgis.de/">Helfersystem</a>--> Helfersystem anmelden und eine E-Mail an konferenz-orga@fossgis.de senden. Helfer dürfen das kostenfreie "Community-Ticket" buchen.</p>
+
+			<!--	<h3>Anmeldeformular</h3>
+				<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktioniert, nutzen Sie den direkten Link zum <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2019/">Ticketsystem</a>.
+Nach erfolgreicher Buchung erhalten Sie eine Anmeldebestätigung mit der Rechnung. Nach &Uuml;berweisung des Rechnungsbetrages erhalten Sie den Link zu Ihrem Ticket. Der Abgleich der Kontoeingänge mit den Buchungen im Ticket-System erfolgt ca. 1x pro Woche. 
+Im Falle einer Vor-Ort-Anmeldung kann nur in bar bezahlt werden. <br><br>
+<u>Ab hier beginnt das FOSSGIS-Konferenz-Ticket-Buchungsformular. </u></p>
+<p>
+				<pretix-widget event="https://pretix.eu/fossgis/2019/"></pretix-widget>
+				<noscript>
+  				 <div class="pretix-widget">
+       			 <div class="pretix-widget-info-message">
+            JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2019/">hier</a>.
+        		 </div>
+    			 </div>
+				</noscript></p>-->
+				
+
+
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
--- 2019/socialevents/index.php
+++ 2019/socialevents/index.php
@@ -4,63 +4,84 @@
     <title>FOSSGIS 2019 Social Events</title>
     <meta name="description" content="FOSSGIS ist die Abkürzung für Freie und Open Source Software für Geoinformationssysteme und ist die führende Konferenz zu diesem Thema im deutschsprachigen Raum.">
     <meta name="author" content="Oliver Roick. http://oliverroick.de/">
-    <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, Bonn, Universität Bonn, Campus-Dialoge, Rhein">
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+     <meta name="keywords" content="FOSSGIS, FOSSGIS-KOnferenz 2019, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, OSGeo, HTW Dresden, Elbe, Dresden, FOSSGIS-Konferenz, Dialoge im Bärenzwinger">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
-    <link rel="stylesheet" type="text/css" href="/fonts/Droid_Sans_regular_bold.css" />
-    <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
-</head>
-<body id="socialEvents">
-    <div id="wrap">
-        <?php include('../inc/head.inc'); ?>
+        <link rel="stylesheet" type="text/css" href="/fonts/Droid_Sans_regular_bold.css" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+    </head>
+    <body id="socialEvents">
+        <div id="wrap">
+        	<?php include('../inc/head.inc'); ?>
 
-        <div id="content">
-            <?php include('../inc/nav.inc'); ?>
-            <figure>
-                <img src="../img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
-                <figcaption>Photo: Kolossos</figcaption>
-            </figure>
+            <div id="content">
+                <?php include('../inc/nav.inc'); ?>
+                 <figure>
+                    <img src="../img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                    <figcaption>Photo: Kolossos</figcaption>
+                </figure>
 
             <h2>Social Events</h2>
 
-            <h3>
-                Innoffizieller Start<br>
-                <small>Dienstag, 12. März 2019 ab 18:30 Uhr</small>
-            </h3>
-            <p>Noch vor der eigentlichen Konferenz besteht die Möglichkeit alte Gesichter wiederzusehen und neue Bekanntschaften zu machen.</p>
-            <p>Für dieses inoffizielle Treffen wurde am <b>12. März 2019 ab 18:30 Uhr</b> eine offene Reservierung im Altmarktkeller Dresden eingerichtet:</p>
-            <p>
-                <b>Adresse:</b><br>
-                <i>Altmarktkeller Dresden</i><br>
-                Altmarkt 4<br>
-                01067 Dresden
-            </p>
-            <p>Bis ca. 23 Uhr bietet die Küche warme Speisen. Jeder zahlt seine Rechnung selbst.</p>
+            <h3>Dienstag, 12. März 2019</h3>
+                
+            <h4>Innoffizieller Start</h4>
+           
+            <p>Am Abend vor der FOSSGIS-Konferenz besteht die Möglichkeit bei einem gemeinsamen Abendessen alte Gesichter wiederzusehen und neue Bekanntschaften zu machen. Für den inoffiziellen Start wurde am 12. März 2019 ab 18:30 Uhr eine offene Reservierung im <a href="https://www.altmarktkeller.de/speisen-getraenke/">Altmarktkeller Dresden</a>, Adresse: <a href="https://www.openstreetmap.org/export#map=18/51.04962/13.73932">Altmarkt 4</a>, 01067 Dresden eingerichtet.<br>Die Küche bietet bis ca. 23 Uhr warme Speisen. Jeder bezahlt seine Rechnung selbst.</p>
 
-            <h3>
-                Abendveranstaltung "Dialoge im Bärenzwinger"<br>
-                <small>Mittwoch, 13. März 2019 ab 19:00 Uhr</small>
-            </h3>
-            <p>
-                Die diesjährige Abendveranstaltung zur Konferenz "Dialoge im Bärenzwinger" findet am <b>13. März 2019 ab circa 19:00 Uhr</b> im <a href="https://www.baerenzwinger.de" target="_blank" rel="nofollow">Studentenklub "Bärenzwinger"</a> statt.
-                Für das Buffet steht außerdem das direkt daran angrenzende historische Kassemattengewölbe zur Verfügung.
-                Der Studentenclub liegt direkt an der historischen Altstadt unterhalb der Brühlschen Terrassen. Er ist per Straßenbahnlinie 3, 7, 8 und 9 erreichbar.
-                Zu Fuß ist die Veranstaltung ab dem HTW-Zentralgebäude etwa 30 Minuten entlang der Einkaufsstraße "Prager Straße" entfernt.
-            </p>
-            <p>
-                <b>Adresse:</b><br>
-                <i>Studentenclub Bärenzwinger</i><br>
-                Brühlscher Garten 1<br>
-                01067 Dresden
-            </p>
-            <p>
-                Der Eintritt zur Veranstaltung ist im FOSSGIS-Konferenz-Ticket enthalten, <b>die Teilnehmerzahl ist allerdings streng begrenzt</b>.
-                Eine verbindliche Voranmeldung zum Event bei der Ticketbuchung ist deshalb ratsam.
-            </p>
-            <p>
-                Zur Veranstaltung werden jedem Gast Getränkegutscheine gereicht, die an der Bar des Bärenzwingers eingelöst werden können.
-            </p>
+<p> Wer dann noch weitergehen möchte, hat die Möglichkeit die umliegende <a href="https://www.openstreetmap.org/export#map=17/51.04948/13.74082">Weiße Gasse</a>, oder aber die <a href="https://www.openstreetmap.org/export#map=15/51.0650/13.7569">Dresdner Neustadt</a> zu erkunden. </P>
 
+             <h3>Mittwoch, 13. März 2019</h3>
+		 <h4>Eröffnung FOSSGIS-Konferenz</h4>
+		 <p>um 13:00 Uhr im Audimax der HTW Dresden, Friedrich-List-Platz 1, Dresden.</p>
+
+ <h4>Anwendertreffen</h4>
+         <p>Es können an allen Tagen Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen bis 19:30 Uhr zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder mit dem Team vor Ort ansprechen.</p>
+
+<h4>Abendveranstaltung "Dialoge im Bärenzwinger"</h4>
+		 <p>Das gesellige Beisammensein in Form der Abendveranstaltung "Dialoge im Bärenzwinger" findet von 19 - 23 Uhr statt. Das Buffet wird im angrenzenden historische Kassemattengewölbe eröffnet. Der <a href="https://www.baerenzwinger.de/" target="_blank" rel="nofollow">Bärenzwinger</a> ist ein Studentenclub und liegt direkt an der historischen Altstadt, den Brühlschen Terrassen. Adresse: Studentenclub Bärenzwinger, <a href="https://www.openstreetmap.org/node/267966087">Brühlscher Garten 1</a>, 01067 Dresden. Per Straßenbahnlinie 3, 7, 8 und 9 erreichbar. Zu Fuß ist man vom Zentralgebäude der HTW entlang der Einkaufsstraße "Prager Straße" ca. 30 Minuten (ohne Einkauf) unterwegs. 
+<br>  		<p>Zur Veranstaltung werden jedem Gast Getränkegutscheine gereicht, die an der Bar des Bärenzwingers eingelöst werden können.
+            </p>
+               
+            <p>Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Die Teilnehmer*innenzahl ist auf 250 Personen begrenzt. Da ein Zutritt nur mit Ticket möglich sein wird, ist eine verbindliche Voranmeldung bei der Konferenz-Ticketbuchung ratsam. 
+            </p>
+          
+
+            <h3>Donnerstag, 14. März 2019</h3>
+		 	<h4>FOSSGIS e.V. Mitgliederversammlung</h4>
+            
+           	<p>ab 18:00 Uhr im Mathe-Hörsaal Z211. Alle Mitglieder sind herzlich eingeladen, teilzunehmen und sich zu beteiligen. Der FOSSGIS-Verein lädt ein zum Diskutieren, Kennenlernen, Abstimmen & Neuwahlen.
+Getränke werden gestellt und es wird für alle Pizza bestellt. Der Verein freut sich über zahlreiches Erscheinen.
+</p>
+
+            <h3>Freitag, 15. März 2019</h3>
+		 	<h4>Sektempfang am FOSSGIS-Stand </h4>
+			<p>Alle Mitglieder des FOSSGIS-Vereins, Freunde und Interessierte sind ab 14:30 Uhr herzlich zum Sektausklang eingeladen.</p>
+
+			<h4>OSM-Event am Freitagnachmittag</h4>
+			<p>Das Programmkomitee hat ab für Freitagnachmittag ein Vortragsprogramm mit OpenStreetMap-Themen zusammengestellt. Die beginnt um 15 Uhr mit dem OSM-Quiz.</p> 
+
+            <h4>Exkursion zum Mathematisch-Physikalischen Salon  im Zwinger</h4>
+            <p>Um 15:30 Uhr ist am Freitagnachmittag eine Exkursion zum <a href="https://mathematisch-physikalischer-salon.skd.museum" target="_blank" rel="nofollow">Mathematisch-Physikalischen Salon im Zwinger</a> geplant.<br>
+            Es wird eine auf die Konferenzteilnehmer zugeschnittene Führung durch das Museum geben. Der Eintritt zum Salon ist durch jeden Teilnehmer selbst zu zahlen.Interessierte sind herzlich eingeladen sich anzuschließen. <br> Bitte geben sie ihre Teilnahme im Anmeldesystem an, dies dient der guten Vorbereitung.</p>
+
+            <h4>Anwendertreffen</h4>
+         	<p>Es können an allen Tagen Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen bis 19:30 Uhr zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a>  an das Konferenzteam anfragen oder mit dem Team vor Ort ansprechen.</p>
+
+			<h3>Samstag, 16. März 2019</h3>
+		 	<h4>OSM-Samstag </h4>
+			<p>Von 9:30 bis 18 Uhr findet die mittlerweile beliebte OSM-Unconference statt. Interessierte sind eingeladen daran teilzunehmen. Die Veranstaltung ist kostenfrei. Die Themensammlung erfolgt im <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2019/OSM-Samstag"> OSM-Wiki </a> sowie an der Pinnwand am FOSSGIS-Stand. Um 10 Uhr werden die Themen in Zeiten und Räume sortiert. Für das leibliche Wohl wird gesorgt sein. Wir bitten um Anmeldung über das <a href="https://www.fossgis-konferenz.de/2019/anmeldung/">FOSSGIS-Konferenz-Teilnehmeranmeldesystem</a></p>
+
+		 <!--	<h4>Ausgehempfehlungen vom Lokal Team</h4>
+			<ul class="list">
+			<li>Sächsische Schweiz</li>
+    		<li>Altstadt, Frauenkirche, Museen</li>
+    		<li>Neustadt, Assi-Eck, Bermudadreieck</li>
+    		<li>Elbwiesen, Blaues Wunder, Stand- und Schwebeseilbahn</li>
+    		<li>Burgen & Schlösser um Dresden (Königsstein, ...)</li>
+    		<li>Dresdner Heide</li></ul>-->
+
+<<<<<<< 42cb8dc40551e91b7fc84bc35c9b5eb25322d70c
             <h3>
                 Fototermin<br>
                 <small>Donnerstag, 14. März 2019 ab 15:00 Uhr</small>                
@@ -97,6 +118,8 @@
             </h3>
             <p><u>Es können Anwendertreffen, Sprints und Birds-of-a-Feather-Treffen selbst organisiert werden.</u></p>
             <p>Räume stehen bis Samstag 18 Uhr zur Verfügung, Termine bitte beim Konferenzteam per E-Mail (konferenz-orga@fossgis.de ) anfragen oder das Team vor Ort ansprechen.</p>
+=======
+>>>>>>> Ergänzungen Social Events und Anmeldung
         </div>
 
         <?php include('../inc/footer.inc'); ?>
