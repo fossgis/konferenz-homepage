abd6ca92a159e5f2f966d2903e88b245a670e4d2
Dominik Helle <helle@omniscale.de>
2019-01-09 12:56:31 +0100

    add omniscale as bronze sponsor for 2019

--- 2019/index.php
+++ 2019/index.php
@@ -11,7 +11,7 @@
 </head>
 <body id="home">
     <div id="wrap">
-				<?php include('inc/head.inc'); ?>
+                <?php include('inc/head.inc'); ?>
 
       <div id="content">
                 <?php include('inc/nav.inc'); ?>
@@ -61,7 +61,7 @@
                   <h3>Goldsponsoren</h3>
                     <div class="sponsor gold" id="wheregroup"><a href="https://www.wheregroup.com/" target="_blank"><img src="img/s/001_2019-XYEW3-1-Wheregroup.png" valign="middle" width="330" height="101"></a></div>
 <!--                    
-					<div class="sponsor gold" id="geocat"><a href="https://www.geocat.net/" target="_blank"><img src="img/s/003_GeoCat.png" valign="middle" width="315" height="85"></a></div>
+                    <div class="sponsor gold" id="geocat"><a href="https://www.geocat.net/" target="_blank"><img src="img/s/003_GeoCat.png" valign="middle" width="315" height="85"></a></div>
 
                 <!-- ACHTUNG: Logos für Silbersponsoren sollten nicht mehr als 220px breit und 75px hoch sein. -->
                     <h3>Silbersponsoren</h3>
@@ -71,33 +71,31 @@
                  <!-- ACHTUNG: Logos für Bronzesponsoren sollten nicht mehr als 150px breit und 75px hoch sein. -->
                 <h3>Bronzesponsoren</h3>
 <div class="sponsor bronze" id="terrestris"><a href="https://www.terrestris.de/" target="_blank"><img src="img/s/201_2019-3VEBG-1-terrestris.png" valign="middle" width="136" height="68"></a></div>               
-
-<div class="sponsor bronze" id="gbd"><a href="https://www.gbd-consult.de" target="_blank"><img src="img/s/202_GBD.png" valign="middle" width="120" height="32"></a></div>  
-
-<div class="sponsor bronze" id="dbg"><a href="https://www.datenbankgesellschaft.de" target="_blank"><img src="img/s/203_DBG_LOGO.jpg" valign="middle" width="146" height="72"></a></div>  
-<div class="sponsor bronze" id="geops"><a href="https://geops.ch" target="_blank"><img valign="middle"><img src="img/s/204_GEOPS.png" valign="middle" width="133"></a></div>
+<div class="sponsor bronze" id="gbd"><a href="https://www.gbd-consult.de" target="_blank"><img src="img/s/202_GBD.png" valign="middle" width="136" height="36"></a></div>  
+<div class="sponsor bronze" id="dbg"><a href="https://www.datenbankgesellschaft.de" target="_blank"><img src="img/s/203_DBG_LOGO.jpg" valign="middle" width="136" height=67></a></div>  
+<div class="sponsor bronze" id="geops"><a href="https://geops.ch" target="_blank"><img valign="middle"><img src="img/s/204_GEOPS.png" valign="middle" width="136" height="39"></a></div>
+<div class="sponsor bronze" id="omniscale"><a href="https://maps.omniscale.com/de/" target="_blank"><img src="img/s/205_omniscale.jpg" valign="middle" width="136" height="42"></a></div>
   <!--<div class="sponsor bronze" id="..."><a href="http://www.fossgis.de" target="_blank"><img valign="middle"> </a></div>
-<div class="sponsor bronze" id="omniscale"><a href="https://omniscale.de/" target="_blank"><img src="img/s/201_omniscale.png" valign="middle" width="133" height="27"></a></div>
                 <div class="sponsor bronze" id="mundialis"><a href="https://www.mundialis.de/" target="_blank"><img src="img/s/202_mundialis.png" valign="middle" width="100" height="63"></a></div>
                 
                 <div class="sponsor bronze" id="sourcepole"><a href="http://qgiscloud.com" target="_blank"><img src="img/s/204_sourcepole.png" valign="middle" width="160" height="40"></a></div>
-				<div class="sponsor bronze" id="mapwebbing"><a href="http://www.lingner.eu/" target="_blank"><img valign="middle" src="img/s/205_mapwebbing.PNG" width="150" height="46"></a></div>
-				<div class="sponsor bronze" id="52n"><a href="http://52north.org" target="_blank"><img valign="middle" src="img/s/206_52n.png" width="156" height="56"></a></div>
-				
-				<div class="sponsor bronze" id="GKG Kassel - Dr.-Ing. Claas Leiner"><a href="http://www.gkg-kassel.de" target="_blank"><img valign="middle" src="img/s/208_gkg_logo.png" width="130" height="99"></a></div>
-				<div class="sponsor bronze" id="Kuestenschmiede"><a href="https://con4gis.org" target="_blank"><img valign="middle" src="img/s/209_kuestenschmiede.png" width="170" height="48"></a></div>
-				<div class="sponsor bronze" id="Lat/Lon"><a href="http://www.lat-lon.de" target="_blank"><img valign="middle" src="img/s/210_logo_latlon_web.png" width="130" height="94"></a></div>-->
+                <div class="sponsor bronze" id="mapwebbing"><a href="http://www.lingner.eu/" target="_blank"><img valign="middle" src="img/s/205_mapwebbing.PNG" width="150" height="46"></a></div>
+                <div class="sponsor bronze" id="52n"><a href="http://52north.org" target="_blank"><img valign="middle" src="img/s/206_52n.png" width="156" height="56"></a></div>
+                
+                <div class="sponsor bronze" id="GKG Kassel - Dr.-Ing. Claas Leiner"><a href="http://www.gkg-kassel.de" target="_blank"><img valign="middle" src="img/s/208_gkg_logo.png" width="130" height="99"></a></div>
+                <div class="sponsor bronze" id="Kuestenschmiede"><a href="https://con4gis.org" target="_blank"><img valign="middle" src="img/s/209_kuestenschmiede.png" width="170" height="48"></a></div>
+                <div class="sponsor bronze" id="Lat/Lon"><a href="http://www.lat-lon.de" target="_blank"><img valign="middle" src="img/s/210_logo_latlon_web.png" width="130" height="94"></a></div>-->
 
                 <h3>Medienpartner</h3>
-               <div class="sponsor media" id="OSGeoLive"><a href="https://live.osgeo.org/de/index.html" target="_blank"><img valign="middle" src="img/s/401_OSGeoLive_banner.png" width="239px"></a></div>
-<div class="sponsor media" id="nextone"></div>
- 				  <div class="sponsor media" id="nextone"></div>
+                <div class="sponsor media" id="OSGeoLive"><a href="https://live.osgeo.org/de/index.html" target="_blank"><img valign="middle" src="img/s/401_OSGeoLive_banner.png" width="239px"></a></div>
+                <div class="sponsor media" id="nextone"></div>
+                <div class="sponsor media" id="nextone"></div>
 <!-- <div class="sponsor media" id="TIB">
                   <a href="https://av.tib.eu/search?f=publisher%3Bhttp://av.tib.eu/resource/FOSSGIS_e.V."><img valign="middle" src="img/s/401_TIB.PNG" width="241" height="70"></a></div>
                   <div class="sponsor media" id="C3VOC"><a href="https://c3voc.de/" ><img valign="middle" src="img/s/402_voc_square.png" width="150" height="150"></a></div>
                   
                 <br>
-				  <div class="sponsor media" id="wichmann"><a href="https://gispoint.de" target="_blank"><img valign="middle" src="img/s/405_Wichmann_sw-116.png" width="116" height="23"></a></div>
+                  <div class="sponsor media" id="wichmann"><a href="https://gispoint.de" target="_blank"><img valign="middle" src="img/s/405_Wichmann_sw-116.png" width="116" height="23"></a></div>
                   -->
 
                 <p> Sie möchten die FOSSGIS-Konferenz als Sponsor unterstützen? Weitere Informationen für Sponsoren und Aussteller finden Sie <a href="http://www.fossgis.de/wiki/Sponsoren_und_Aussteller">hier</a>. Sie können das Sponsorenpaket bequem über unser <a href="https://pretix.eu/fossgis/2019/"> Buchungsformular</a> buchen. Bei Fragen wenden sie sich gerne an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.
