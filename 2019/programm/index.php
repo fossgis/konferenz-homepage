fe9a5446ae3ce01299cb8b79281b753f5f3efdad
katjahafi <katjahafi@gmx.de>
2019-01-12 19:48:12 +0100

    Programmtage verlinkt

---
+++ 2019/programm/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>FOSSGIS 2019 Programm</title>
+        <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Salzburg, Hohensalzburg, Z_GIS">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" type="text/css" href="/static/droidsans/droidsans.css" />
+        <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
+
+    </head>
+    <body id="programm">
+        <div id="wrap">
+            <?php include('../inc/head.inc'); ?>
+
+            <div id="content">
+
+                <?php include('../inc/nav.inc'); ?>
+				<figure>
+                    <img src="../img/dresden.jpg" width="880" height="241" alt="Dresden Panorama">
+                    <figcaption>Photo: Kolossos</figcaption>
+                </figure>
+
+                <h2>Programm</h2>
+                <p>Wählen Sie den gewünschten Konferenztag aus.</p>
+                <div style="margin: 5px 85px 7px 160px">
+                <ul>
+                <li>
+                <a href="https://pretalx.com/fossgis2019/schedule/#2019-03-13" target="_blank" rel="nofollow">Mittwoch, 13.03.2019</a>
+                </li>                    	
+                <li>
+                <a href="https://pretalx.com/fossgis2019/schedule/#2019-03-14" target="_blank" rel="nofollow">Donnerstag, 14.03.2019</a>
+                </li>
+                <li>
+                <a href="https://pretalx.com/fossgis2019/schedule/#2019-03-15" target="_blank" rel="nofollow">Freitag, 15.03.2019</a>
+                </li>
+                <li>
+                <a href="https://pretalx.com/fossgis2019/schedule/#2019-03-16" target="_blank" rel="nofollow">OSM Samstag, 16.03.2019</a>
+                </li>
+				<li>
+                <a href="https://pretalx.com/fossgis2019/speaker/" target="_blank" rel="nofollow">Referenten</a>
+                </li>
+                </ul>
+                </div>
+
+
+		
+			<?php /*
+				<p>Die FOSSGIS-Konferenz wird per Livestream übertragen:</p>
+				<div style="margin: 5px 85px 7px 160px">
+                    <ul>
+                        <li>
+                            <a href="http://streaming.media.ccc.de/fossgis19/">Livestream der FOSSGIS-Konferenz</a>.
+                        </li>
+                    </ul>
+                </div>
+*/ ?>
+			<!--	<p>Die Vorträge der FOSSGIS-Konferenz wurden aufgezeichnet, eine Übersicht finden Sie hier:</p>
+				<div style="margin: 5px 85px 7px 160px">
+                    <ul>
+                        <li>
+                            <a href="/2019/videos/">Liste der Videoaufzeichnungen</a>.
+                        </li>
+                    </ul>
+                </div>-->
+            </div>
+            <?php include('../inc/footer.inc'); ?>
+        </div>
+    </body>
+</html>
