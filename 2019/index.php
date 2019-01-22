74b9ce109aabb3499144959618cf44859b3e85cc
katjahafi <katjahafi@gmx.de>
2019-01-22 14:28:30 +0100

    Bronzesponsor Camptocamp hinzugefügt

--- 2019/index.php
+++ 2019/index.php
@@ -78,9 +78,9 @@
 <div class="sponsor bronze" id="mapwebbing"><a href="http://www.lingner.eu/" target="_blank"><img valign="middle" src="img/s/206_mapwebbing.PNG" width="150" height="46"></a></div>
                 <div class="sponsor bronze" id="graphhopper"><a href="https://www.graphhopper.com/de/" target="_blank"><img src="img/s/207_graphhopper.png" valign="middle" width="150" height="24"></a></div> 
 
-            <!--<div class="sponsor bronze" id="..."><a href="http://www.fossgis.de" target="_blank"><img valign="middle"> </a></div>      
+            <div class="sponsor bronze" id="camptocamp"><a href="www.camptocamp.com" target="_blank"><img src="img/s/208_camptocamp.png" valign="middle"width="136" height="91"></a></div>       
                 
-<div class="sponsor bronze" id="sourcepole"><a href="http://qgiscloud.com" target="_blank"><img src="img/s/204_sourcepole.png" valign="middle" width="160" height="40"></a></div>
+<!--<div class="sponsor bronze" id="sourcepole"><a href="http://qgiscloud.com" target="_blank"><img src="img/s/204_sourcepole.png" valign="middle" width="160" height="40"></a></div>
                
                 <div class="sponsor bronze" id="52n"><a href="http://52north.org" target="_blank"><img valign="middle" src="img/s/206_52n.png" width="156" height="56"></a></div>
                 
