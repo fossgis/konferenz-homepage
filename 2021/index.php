e2699c26e14917c97c38c0fbb588e49ef40f07ef
Dominik Helle <helle@omniscale.de>
2019-08-26 20:21:15 +0200

    add cfl for 2021

--- 2021/index.php
+++ 2021/index.php
@@ -1,26 +1,26 @@
 <!doctype html>
 <html>
 <head>
-    <title>FOSSGIS 2020</title>
+    <title>FOSSGIS 2021</title>
     <meta name="description" content="Die FOSSGIS-Konferenz ist im D-A-CH Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme OpenStreetMap.">
 
-    <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz 2020, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
+    <meta name="keywords" content="FOSSGIS, FOSSGIS-Konferenz 2021, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap">
     <meta charset="utf-8">
 
     <link href="css/screen.css" type="text/css" rel="stylesheet" media="screen">
 </head>
 <body id="home">
     <div id="wrap">
-        <h1>FOSSGIS Konferenz <span>Call for Location</span> <br>2020</h1>
+        <h1>FOSSGIS Konferenz <span>Call for Location</span> <br>2021</h1>
 
         <div id="content">
-            <h2>Call for Location zur FOSSGIS-Konferenz 2020</h2>
+            <h2>Call for Location zur FOSSGIS-Konferenz 2021</h2>
 
             <p>Die FOSSGIS-Konferenz ist die führende Konferenz für Freie und Open Source Software Geo-Software und OpenStreetMap.</p>
 
-            <p>Die FOSSGIS-Konferenz 2020 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> organisiert.</p>
+            <p>Die FOSSGIS-Konferenz 2021 wird vom gemeinnützigen <a href="http://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="http://www.openstreetmap.de/">OpenStreetMap Community</a> organisiert.</p>
 
-            <p>Der Veranstalter sucht auf diesem Wege einen Austragungsort für die Konferenz im Jahre 2020 und ruft zur Einreichung von Bewerbungen auf.</p>
+            <p>Der Veranstalter sucht auf diesem Wege einen Austragungsort für die Konferenz im Jahre 2021 und ruft zur Einreichung von Bewerbungen auf.</p>
 
                 <div class="organisers">
                     <div class="sponsor organizer">
@@ -50,14 +50,14 @@
             <h3>An welchen Orten wird die Konferenz ausgetragen?</h3>
             <p>Typischerweise wird die Konferenz an Hochschulen oder Bildungseinrichtungen durchgeführt.</p>
 
-            <p>In den letzten Jahren war die Konferenz u.a. zu Gast an der Beuth Hochschule für Technik in Berlin, an der Westfälische Wilhelms-Universität in Münster und an der Universität in Bonn. Im Jahr 2019 gastiert die FOSSGIS-Konferenz in Dresden.</p>
+            <p>In den letzten Jahren war die Konferenz u.a. zu Gast an der Beuth Hochschule für Technik in Berlin, an der Westfälische Wilhelms-Universität in Münster und an der Hochschule für Technik und Wirtschaft in Dresden. Im Jahr 2020 gastiert die FOSSGIS-Konferenz in Freiburg.</p>
 
             <h3>Wie finanziert sich die Konferenz?</h3>
 
             <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen.</p>
 
             <h3>Interesse?</h3>
-            <p>Sie möchten die Konferenz bei Ihnen auszurichten? Detaillierte Informationen finden Sie im <a href="data/CfL_FOSSGIS-2020.pdf">Bewerbungs-PDF</a>.</p>
+            <p>Sie möchten die Konferenz bei Ihnen auszurichten? Detaillierte Informationen finden Sie im <a href="data/CfL_FOSSGIS-2021.pdf">Bewerbungs-PDF</a>.</p>
 
             <p>Wir freuen uns auf Ihre Bewerbung. Bei Fragen können Sie sich gerne an info[aet]fossgis.de wenden.</p>
         </div>
