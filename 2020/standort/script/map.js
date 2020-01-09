88d16ad707ea3a5e27f0c15d17d4e4b0ed95c20c
Thomas Skowron <th@skowron.eu>
2020-01-09 13:53:31 +0100

    wechsel auf den schnelleren, deutschen tileserver

--- 2020/standort/script/map.js
+++ 2020/standort/script/map.js
@@ -26,7 +26,7 @@
 		radius: 12,
 		className: 'svg-feature'
 	};
-	var baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	var baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	});
 	var campusLabels = L.layerGroup();
