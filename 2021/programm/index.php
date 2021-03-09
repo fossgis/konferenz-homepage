3b040524aaf4e26ebf0cde991ff2033c917dc7ad
Katja Haferkorn <katjahafi@gmx.de>
2021-03-09 09:49:08 +0100

    Text Community Voting angepasst.

--- 2021/programm/index.php
+++ 2021/programm/index.php
@@ -17,21 +17,10 @@
 
 	<h2>Programm</h2>
 
+	<p>An der Öffentlichen Abstimmung (Community-Voting) haben sich fast 200 Teilnehmer:innen beteiligt. Vielen Dank dafür!</p>
+
 	<p>Das Konferenz-Programm wird voraussichtlich Ende März/Anfang April veröffentlicht.</p>
-
-	<h3>Öffentliche Abstimmung zu den eingereichten Beiträgen</h3>
-	<p>Erstmals findet ein Community-Voting (Öffentliche Abstimmung) zu den eingereichten Beiträgen statt. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.</p>
-		<ul><b>Dies geht ganz einfach:</b>
 		
-		<li>1. Diesen <a href="https://pretalx.com/fossgis2021/p/voting/signup/">Link</a> zur Öffentlichen Abstimmung klicken, um sich dafür anzumelden. </li> 
-		<li>2. Sie erhalten eine E-Mail mit Ihrem persönlichen Link zur Öffentlichen Abstimmung.</li> 
-		<li> 3. Einreichungen anschauen und bewerten, dies kann in Etappen erfolgen.</li> 
-		</ul>
-		<p>Das Community-Voting ist bis zum 01.03.2021 möglich.</p> 
-
-
-
-
 	<?php include "../inc/footer.inc"; ?>
 
 </body>
