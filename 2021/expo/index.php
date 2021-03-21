7ce5595b67b8efb1b1564bd420efbd90acf15131
Dominik Helle <helle@omniscale.de>
2021-03-21 20:10:41 +0100

    erste version von expo seiten

---
+++ 2021/expo/index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Standort</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="standort">
+
+	<?php include "../inc/header.inc"; ?>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="section-title">
+					    <h2>Ausstellerstand auf der FOSSGIS</h2>
+					    <p>Die bewährte Firmen- und Projektausstellung, die einen direkten Kontakt zwischen Dienstleistern, Projekten und Teilnehmer:innen bietet, wird in diesem Jahr in einem besonderen Format stattfinden. Dazu werden wir in der Netzwerkwelt einen Expobereich einrichten.</p>
+						<p>Im Expobereich können sich Firmen und Open Source Projekte präsentieren</p>
+						<p>Jeder Ausstellungsstand wird mit dem eignen Logo gekennzeichnet, bekommt einen eignen Videokonferenzraum sowie eine Infoseite, die per Mouseover angezeigt wird. In der Netzwerkwelt, die den OST Campus Rapperswil nachbilden wird, können sich die Teilnehmenden in einer virtuellen Karte/Landschaft bewegen und einander begegnen.  Zum Einstieg gibt man sich einen Namen und wählt einen Avatar aus. Nach der Landung im Eingangsbereich können Sie sich mit Hilfe der Tastatur bewegen. Sie können die Netzwerkwelt erkunden, sehen auch, ob weitere Teilnehmende unterwegs sind.
+						</p>
+						<p>Bei Begegnung von zwei Avataren öffnet sich ein Videochat, welcher sich mit einem kurzen Geräusch ankündigt. Räume können betreten werden, dort sind Informationen zu finden oder eine Videokonferenz findet statt. Das heißt Sie, als Aussteller, können die Teilnehmenden sehen und auch aktiv auf diese zugehen.</p>
+						<p>Der Ausstellerstand ist im Sponsoringpaket der FOSSGIS-Konferenz enthalten und bis zum 10.04.2021 kostenfrei buchbar. </p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/001_camptocamp_logo.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>Camptocamp Germany GmbH</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+						</div>
+						<button class="btn btn-sm btn-primary">
+							Details anschauen</button>
+					</div>
+				</div>
+			
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/002_WhereGroup-zentriert.jpg" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>Wheregroup GmbH</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<button class="btn btn-sm btn-primary">
+								Details anschauen</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/201_terrestris.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>terrestris GmbH & Co. KG</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<button class="btn btn-sm btn-primary">
+								Details anschauen</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/202_geOps.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>geops GmbH</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<button class="btn btn-sm btn-primary">
+								Details anschauen</button>						
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/203_Geoinfo_Logo.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>GeoInfo</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<button class="btn btn-sm btn-primary">
+								Details anschauen</button>						
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="icon d-flex align-items-center justify-content-center">
+							<img src="https://fossgis-konferenz.de/2021/img/s/407_sourcepole_block2.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+							<h5>Sourcepole</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<button class="btn btn-sm btn-primary">
+								Details anschauen</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
+</html>
