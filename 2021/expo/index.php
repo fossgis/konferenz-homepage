1d93b37b5ff832b3246f5082581387416fbe8cd6
Katja Haferkorn <katjahafi@gmx.de>
2021-03-22 11:01:01 +0100

    Webseiten für Darstellung Expo

--- 2021/expo/index.php
+++ 2021/expo/index.php
@@ -25,7 +25,7 @@
 					<div class="section-title">
 					    <h2>Ausstellerstand auf der FOSSGIS</h2>
 					    <p>Die bewährte Firmen- und Projektausstellung, die einen direkten Kontakt zwischen Dienstleistern, Projekten und Teilnehmer:innen bietet, wird in diesem Jahr in einem besonderen Format stattfinden. Dazu werden wir in der Netzwerkwelt einen Expobereich einrichten.</p>
-						<p>Im Expobereich können sich Firmen und Open Source Projekte präsentieren</p>
+						<p>Im Expobereich können sich Firmen und Open Source Projekte präsentieren.</p>
 						<p>Jeder Ausstellungsstand wird mit dem eignen Logo gekennzeichnet, bekommt einen eignen Videokonferenzraum sowie eine Infoseite, die per Mouseover angezeigt wird. In der Netzwerkwelt, die den OST Campus Rapperswil nachbilden wird, können sich die Teilnehmenden in einer virtuellen Karte/Landschaft bewegen und einander begegnen.  Zum Einstieg gibt man sich einen Namen und wählt einen Avatar aus. Nach der Landung im Eingangsbereich können Sie sich mit Hilfe der Tastatur bewegen. Sie können die Netzwerkwelt erkunden, sehen auch, ob weitere Teilnehmende unterwegs sind.
 						</p>
 						<p>Bei Begegnung von zwei Avataren öffnet sich ein Videochat, welcher sich mit einem kurzen Geräusch ankündigt. Räume können betreten werden, dort sind Informationen zu finden oder eine Videokonferenz findet statt. Das heißt Sie, als Aussteller, können die Teilnehmenden sehen und auch aktiv auf diese zugehen.</p>
@@ -42,8 +42,8 @@
 							<img src="https://fossgis-konferenz.de/2021/img/s/001_camptocamp_logo.png" title="" alt="">
 						</div>
 						<div class="feature-content">
-							<h5>Camptocamp Germany GmbH</h5>
-							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
+							<h5>Camptocamp GmbH</h5>
+							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores dolores dolores .... </p>
 						</div>
 						<a href="expo/camptocamp.php" 
 							class="btn btn-sm btn-primary active" role="button" aria-pressed="true">
@@ -60,7 +60,7 @@
 						<div class="feature-content">
 							<h5>Wheregroup GmbH</h5>
 							<p class="small">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores </p>
-							<a href="expo/camptocamp.php" 
+							<a href="expo/wheregroup.php" 
 								class="btn btn-sm btn-primary active" role="button" aria-pressed="true">
 								Details anschauen
 							</a>
@@ -68,7 +68,7 @@
 					</div>
 				</div>
 
-				<div class="col-sm-6 col-lg-6 mt-3 mb-3">
+			<!--	<div class="col-sm-6 col-lg-6 mt-3 mb-3">
 					<div class="expo-box">
 						<div class="icon d-flex align-items-center justify-content-center">
 							<img src="https://fossgis-konferenz.de/2021/img/s/201_terrestris.png" title="" alt="">
@@ -129,7 +129,7 @@
 								Details anschauen
 							</a>
 						</div>
-					</div>
+					</div>-->
 				</div>
 			</div>
 		</div>
--- 2021/expo/wheregroup.php
+++ 2021/expo/wheregroup.php
@@ -22,46 +22,40 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
-					<h3>Camptocamp</h3>
-					<p>Camptocamp gehört zu den führenden Dienstleistern im Bereich Open-Source-GIS und ist in vielen unterschiedlichen Open-Source-Communitys stark engagiert.
+					<h3>WhereGroup GmbH</h3>
+					<p>Die WhereGroup gehört zu den führenden Anbietern von Geoinformationssystemen mit Open-Source-Software in Deutschland.</p>
+<p>Wir bieten alle Dienstleistungen rund um die räumliche Datenverarbeitung, Geoinformationssysteme und Datenbanken mit freier Software. Angefangen bei der Beratung, Konzeption und Entwicklung bis hin zum Betrieb dynamischer Kartenanwendungen im Intra- und Internet.
+Dabei reicht das Spektrum unserer Lösungen von Geoportalen und kartenbasierter Datenverwaltung bis hin zu hochverfügbaren Anwendungen für die freie Wirtschaft und die öffentliche Verwaltung.
+Unser Schulungsinstitut, die FOSS Academy, bietet außerdem praxisorientierte Schulungen zum Thema „GIS mit Open-Source-Software“ an.
+Das WhereGroup-Team umfasst über 40 Kollegen unterschiedlicher Fachrichtungen verteilt auf die Standorte Bonn (Hauptsitz), Freiburg und Berlin.
+Grundlage unseres Schaffens ist der Open-Source-Gedanke. Als Teil einer starken Community, mit der wir in engem Austausch stehen, engagieren wir uns aus Überzeugung im FOSSGIS e. V. und bei der OSGeo.
+Mehr zur WhereGroup unter www.wheregroup.com und www.foss-academy.com.
 					</p>
-					<p>Unsere Dienstleistungen stützen sich auf über 15 Jahre Erfahrung in der
-						Umsetzung von innovativen GIS-Lösungen für Behörden und Unternehmen und
-						erlauben einen hochwertigen und individuellen Service. Das Besondere an
-						Camptocamp sind die hochqualifizierten Mitarbeiter und ihr großes Engagement im
-						"`Ökosystem"' der eingesetzten Open-Source Software-Lösungen, indem sehr enge
-					Beziehungen zu den Herstellern der jeweiligen Produkte gepflegt werden.</p>
-
-					<p>Um die oft anspruchsvollen Projekte umzusetzen, erstellt Camptocamp
-						individuelle Lösungen, die auf den am besten geeigneten und fortschrittlichsten
-						Open Source-Technologien basieren. Camptcamp ist in München, Lausanne, Olten,
-						Paris und Chambéry vertreten und bietet neben Lösungen im GIS-Bereich auch eine
-					große Expertise im den Bereichen ERP (Enterprise-Resource-Planning) und  IT-Infrastruktur-Lösungen.</p>
-
+					
 				</div>	
 
 				<div class="col-lg-3">
 					<div class="icon mt-3">
-						<img src="./img/s/001_camptocamp_logo.png" title="" alt="">
+						<img src="./img/s/002_WhereGroup-zentriert.jpg" title="" alt="">
 					</div>
 					<div class="mb-4">
 						<h6 class="mb-2">Tags</h6>
 						<div class="post-tags">
-							<span class="badge badge-info">QGIS</span>
-							<span class="badge badge-info">Python</span>
+							<span class="badge badge-info">FOSSAcadamy</span>
+							<span class="badge badge-info">Tag_Eurer_Wahl</span>
 						</div>
 					</div>
 					<div class="mb-4">
 						<h6 class="mb-2">Webseite</h6>
 						<a href="https://www.camptocamp.com">
 							<i class="fas fa-link text-dark pr-1"></i>
-							www.camptocamp.com</a>
+							https://wheregroup.com/</a>
 					</div>
 					<div class="social-icon d-flex align-items-center">
 						<h6 class="font-weight-bold mb-0 pr-3">Social</h6><br>
 						<ul>
 							<li>
-								<a href="#">https://twitter.com/camptocamp<i class="fab fa-twitter"></i></a>
+								<a href="#">https://twitter.com/WhereGroup_com<i class="fab fa-twitter"></i></a>
 							</li>
 							<li>
 								<a href="#"><i class="fab fa-linkedin-in"></i></a>
@@ -78,7 +72,7 @@
 				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
 					<div class="expo-box">
 						<div class="project-tile d-flex align-items-center justify-content-center">
-							<img src="https://fossgis-konferenz.de/2021/img/s/001_camptocamp_logo.png" title="" alt="">
+							<img src="https://fossgis-konferenz.de/2021/img/s/002_WhereGroup-zentriert.jpg" title="" alt="">
 						</div>
 						<div class="feature-content">
 						<a href="#">Zum Projekt XYZ</a>
@@ -88,7 +82,7 @@
 				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
 					<div class="expo-box">
 						<div class="project-tile d-flex align-items-center justify-content-center">
-							<img src="https://fossgis-konferenz.de/2021/img/s/001_camptocamp_logo.png" title="" alt="">
+							<img src="https://fossgis-konferenz.de/2021/img/s/002_WhereGroup-zentriert.jpg" title="" alt="">
 						</div>
 						<div class="feature-content">
 						<a href="#">Zum Video XYZ</a>
@@ -98,7 +92,7 @@
 				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
 					<div class="expo-box">
 						<div class="project-tile d-flex align-items-center justify-content-center">
-							<img src="https://fossgis-konferenz.de/2021/img/s/001_camptocamp_logo.png" title="" alt="">
+							<img src="https://fossgis-konferenz.de/2021/img/s/002_WhereGroup-zentriert.jpg" title="" alt="">
 						</div>
 						<div class="feature-content">
 						<a href="#">Zum Projekt XYZ</a>
