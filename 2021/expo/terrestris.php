0959f3744ae1091c3cbae2f9ada3072fd51fd669
Katja Haferkorn <katjahafi@gmx.de>
2021-04-12 08:49:15 +0200

    terrestris Infoseite

---
+++ 2021/expo/terrestris.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Expo</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+
+<body id="expo">
+
+	<p><a href="https://fossgis-konferenz.de/2021/expo">Zurück zur Ausstellerliste</a>.</p>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>terrestris</h3>
+					<p>Tterrestris bietet Dienstleistungen, Produkte und maßgeschneiderte Entwicklungen mit Freier und Open Source Software an. Ein Fokus liegt insbesondere auf der Verarbeitung sowie Darstellung raumbezogener Daten mit Geoinformationssystemen (GIS) in allen ihren Ausprägungen.</p>
+
+					<p>Dabei orientieren wir uns an den Anforderungen unserer Kunden und bieten auf den jeweiligen Bedarf zugeschnittene Lösungen an. Als Grundlage verwenden wir bewährte und vielfach eingesetzte Open Source Technologien in Kombination mit OGC- und anderen offenen Standards sowie freien Daten (etwa auch solche aus dem OpenStreetMap-Projekt). Darauf aufbauend entwickeln wir innovative und maßgeschneiderte Lösungen, die zudem oft nahtlos in bestehende Architekturen eingebettet sind.</p>
+
+					<p>Wir sind national wie international gut vernetzt und adaptieren frühzeitig neueste Technologien. Dank des hohen Know-hows unserer Mitarbeiter und einer lebendigen Firmenkultur folgen wir unserem eigenen Anspruch nur Technologie einzusetzen, die uns vollständig überzeugt. </p>
+
+					<p>Diese Rahmenbedingungen erlauben es uns unvoreingenommen Lösungen zu entwickeln und anzubieten, die tatsächlich alle Anforderungen unserer Kunden erfüllen. Dies ist nach unserem eigenen Verständnis auch Grundlage unserer fairen, transparenten und häufig langfristigen Kundenbeziehungen.</p>
+
+					<p>Unser Leistungsspektrum erstreckt sich auf folgende Bereiche:</p>
+					<ul>
+						<li>Geoportale auf Bundes-, Landes-, Kreis- und Kommunalebene</li>
+						<li>Stadtplandienste</li>
+						<li>Tourismusportale</li>
+						<li>Umweltdatenportale</li>
+						<li>Visualisierung von geographischen 3D-Daten im Browser</li>
+						<li>Lösungen für mobile Endgeräte</li>
+						<li>Einsatz und Verwendung freier Geodaten wie OpenStreetMap</li>
+					</ul>
+					<p>Technologisch setzen wir unter anderem ein:</p>
+					<ul>
+						<li>QGIS</li>
+						<li>OpenLayers</li>
+						<li>GeoServer</li>
+						<li>SHOGun</li>
+						<li>react / react-geo</li>
+						<li>GeoNetwork OpenSource</li>
+						<li>ExtJS / GeoExt </li>
+						<li>PostgreSQL / PostGIS</li>
+					</ul>
+					<p>Folgende Dienstleistungen bieten wir an:</p>
+					<ul>
+						<li>Beratung und Konzeption für den Aufbau von Geodateninfrastrukturen</li>
+						<li>Softwareentwicklung</li>
+						<li>Schulungen zu unseren eingesetzten und inhaltlich nahestehenden Technologien </li>
+						<li>Wartung und Support</li>
+					</ul>
+
+
+				
+				</div>	
+
+				<div class="col-lg-3">
+					<div class="icon mt-3">
+						<img src="./img/s/201_terrestris.png" title="terrestris" alt="terrestris">
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-2">Tags</h6>
+						<div class="post-tags">
+							<span class="badge badge-info">QGIS</span>
+							<span class="badge badge-info">3D</span>
+							<span class="badge badge-info">GIS</span>
+							<span class="badge badge-info">Geodaten</span>
+							<span class="badge badge-info">Cesium</span>
+							<span class="badge badge-info">GeoServer</span>
+							<span class="badge badge-info">Openlayers</span>
+							<span class="badge badge-info">PostGIS</span>
+							<span class="badge badge-info">GeoStyler</span>
+						</div>
+
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-0">Webseite</h6>
+						<a href="https://www.terrestris.de/">
+							https://www.terrestris.de/</a>
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-0">News</h6>
+						<a href="https://www.terrestris.de/de/neuigkeiten/">
+							https://www.terrestris.de/de/neuigkeiten/</a>
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-0">Demo-Anwendungen & Dienste</h6>
+						<a href="https://demo.terrestris.de/">
+							https://demo.terrestris.de/</a>
+					</div>
+					<div class="social-icon">
+						<h6 class="font-weight-bold mb-0">Social</h6>
+						<ul>
+							<li>
+								<a href="https://twitter.com/terrestrisde"><i class="fab fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com/company/18223560/"><i class="fab fa-linkedin-in"></i></a>
+							</li>
+							<li>
+								<a href="https://github.com/terrestris"><i class="fab fa-github"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		<!--	<div class="row">
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Video</p>
+						<p><a href="https://www.terrestris.de/">...</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="Link Bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Projekt </p>
+						<p><a href="https://www.terrestris.de/">Projekt</a></p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="Link Bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Projekt </p>
+						<p><a href="https://www.terrestris.de/">Projekt</a></p>
+						</div>
+					</div>
+				</div>	-->										
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
+</html>
