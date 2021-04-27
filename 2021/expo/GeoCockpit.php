c6b71ee09845324e0e888766e46e1464346dcc01
Katja Haferkorn <katjahafi@gmx.de>
2021-04-27 16:29:23 +0200

    GeoCockpit Infoseite erstellt, Inhalte fehlen noch

---
+++ 2021/expo/GeoCockpit.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Expo</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+
+<body id="expo">
+
+	<p><a href="https://fossgis-konferenz.de/2021/expo">Zurück zur Ausstellerliste</a>.</p>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>GeoCockpit UG</h3>
+					<p>Text 1.
+					</p>
+					<p>Text 2.</p>
+
+					<p><a href="https://www.">Text 3</a>.</p>
+
+				</div>	
+
+				<div class="col-lg-3">
+					<div class="icon mt-3">
+						<img src="./img/s/408_YEY-Maps_Logo.png" title="GeoCockpit" alt="GeoCockpit">
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-2">Tags</h6>
+						<div class="post-tags">
+							<span class="badge badge-info">...</span>
+							<span class="badge badge-info">...</span>
+							<span class="badge badge-info">...</span>
+						</div>
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-0">Webseite</h6>
+						<a href="http://www.yeymaps.de/">
+							http://www.yeymaps.de/</a>
+					</div>
+					<div class="social-icon">
+						<h6 class="font-weight-bold mb-0">Social</h6>
+						<ul>
+							<li>
+								<a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com/company/"><i class="fab fa-linkedin-in"></i></a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+							</li>
+							<li>
+								<a href="https://github.com/"><i class="fab fa-github"></i></a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/user/"><i class="fab fa-youtube"></i></a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Video</p>
+						<p><a href="Link">...</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="Link Bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Projekt </p>
+						<p><a href="Link">Projekt</a></p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
+					<div class="expo-box">
+						<div class="project-tile d-flex align-items-center justify-content-center">
+							<img src="Link Bild.png" title="" alt="">
+						</div>
+						<div class="feature-content">
+						<p>Zum Projekt </p>
+						<p><a href="Link">Projekt</a></p>
+						</div>
+					</div>
+				</div>											
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
+</html>
