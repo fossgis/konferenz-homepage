d39b46fda6470d0093fa82ea62979f69740355b9
Katja Haferkorn <katjahafi@gmx.de>
2021-05-07 17:33:11 +0200

    Infoseite LAT/LON GmbH

--- 2021/expo/latlon.php
+++ 2021/expo/latlon.php
@@ -21,14 +21,23 @@
 
 	<section class="section">
 		<div class="container">
+			<img src="./expo/img/latlon_bg4.png" class="img-fluid" alt="LATLON">
 			<div class="row">
 				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
-					<h3>LAT LON</h3>
-					<p>Text 1.
-					</p>
-					<p>Text 2.</p>
+					<h3>lat/lon GmbH</h3>
+					<p>lat/lon bietet Dienstleistungen auf dem Gebiet raumbezogener und moderner Internet gestützter Informationssysteme auf Basis von Open-Source Software an. Unter unserer Federführung ist gemeinsam mit der Universität Bonn das OSGeo-Projekt deegree entstanden, auf dessen Basis weltweit viele Geodateninfrastrukturen realisiert worden sind.</p>
+					<p>Als Teil der deegree Community engagieren wir uns aus Überzeugung auch im FOSSGIS e.V. und in der OSGeo Foundation. lat/lon ist auch Mitglied im Open Geospatial Consortium (OGC) und arbeitet aktiv an der Weiterentwicklung der OGC TEAM Engine im Rahmen des CITE Projekts mit.</p>
 
-					<p><a href="https://www.">Text 3</a>.</p>
+					<p>Tätigkeitsschwerpunkte:
+					<ul>
+					<li>Dienstleistungen rund um die räumliche Datenverarbeitung, Geoinformationssysteme und Datenbanken mit freier Software </li>
+					<li>Softwareentwicklung und Beratung sowie Schulungen und Workshops zu deegree</li>
+					<li>Neu- und Weiterentwicklung von Web-, Desktop- und Geodatenbank-Anwendungen für alle Plattformen auf Basis von Open-Source Software.</li></ul></p>
+
+					<p>Hauptprodukte:
+					<ul>
+					<li>deegree: Open-Source-Framework zur Bereitstellung von Geodiensten wie WFS und WMS</li>
+					<li>xPlanBox: Fachanwendung zur Bereitstellung von raumbezogenen Daten auf Basis des Standards XPlanung </li></ul></p>
 
 				</div>	
 
@@ -49,28 +58,26 @@
 						<a href="https://www.lat-lon.de/">
 							https://www.lat-lon.de/</a>
 					</div>
+					<div class="mb-4">
+						<h6 class="mb-0">Kontakt</h6>
+						Torsten Friebe: <a href="mailto:friebe@lat-lon.de">
+							E-Mail</a>
+						lat/lon GmbH<br>
+						Aennchenstrasse 19<br>
+						53177 Bonn, Germany<br>
+						T: +49 228 9477 9877<br>
+						@: info@lat-lon.de<br>
+					</div>
 					<div class="social-icon">
 						<h6 class="font-weight-bold mb-0">Social</h6>
 						<ul>
 							<li>
-								<a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+								<a href="https://twitter.com/latlon_de "><i class="fab fa-twitter"></i></a>
 							</li>
 							<li>
-								<a href="https://www.linkedin.com/company/"><i class="fab fa-linkedin-in"></i></a>
+								<a href="https://github.com/lat-lon "><i class="fab fa-github"></i></a>
 							</li>
-							<li>
-								<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-							</li>
-							<li>
-								<a href="https://github.com/"><i class="fab fa-github"></i></a>
-							</li>
-							<li>
-								<a href="https://www.youtube.com/user/"><i class="fab fa-youtube"></i></a>
-							</li>
-							<li>
-								<a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
-							</li>
-
+						
 						</ul>
 					</div>
 				</div>
@@ -80,11 +87,11 @@
 				<div class="col-sm-6 col-lg-4 mt-3 mb-3">
 					<div class="expo-box">
 						<div class="project-tile d-flex align-items-center justify-content-center">
-							<img src="bild.png" title="" alt="">
+							<img src="./expo/img/deegree_logo.png" title="" alt="">
 						</div>
 						<div class="feature-content">
-						<p>Zum Video</p>
-						<p><a href="Link">...</a></p>
+						
+						<p><a href="https://www.deegree.org/">Projektseite deegree</a></p>
 						</div>
 					</div>
 				</div>
@@ -94,8 +101,7 @@
 							<img src="Link Bild.png" title="" alt="">
 						</div>
 						<div class="feature-content">
-						<p>Zum Projekt </p>
-						<p><a href="Link">Projekt</a></p>
+						<p><a href="Link">Projekt xPlanBox</a></p>
 						</div>
 					</div>
 				</div>
@@ -106,7 +112,6 @@
 							<img src="Link Bild.png" title="" alt="">
 						</div>
 						<div class="feature-content">
-						<p>Zum Projekt </p>
 						<p><a href="Link">Projekt</a></p>
 						</div>
 					</div>
