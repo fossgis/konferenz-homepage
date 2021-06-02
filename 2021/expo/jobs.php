88bfee0c269fe9c91341da83ef42a173eb8aa814
Katja Haferkorn <katjahafi@gmx.de>
2021-06-02 18:15:50 +0200

    Jobbörse

---
+++ 2021/expo/jobs.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Expo</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+</head>
+
+<body id="expo">
+
+	<p><a href="https://fossgis-konferenz.de/2021/expo">Zurück zur Ausstellerliste</a>.</p>
+
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>Jobbörse</h3>
+					<ul>
+					<li>
+					<a href="https://www.mundialis.de/de/stellenausschreibung-projektleiterin-m-w-d-in-vollzeit-40h-woche/">Projektleitung in Bonn</a> (Firma mundialis)</li><br>
+					<li><a href="https://jobs.maxime-media.de/wheregroup-gmbh/7330103?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic">Software-Entwickler (m/w/d) in Bonn, Berlin und Freiburg oder Hamburg</a> (Firma WhereGroup)</li><br>
+
+					<li>...</li>
+
+				</div>	
+
+				<div class="col-lg-3">
+					<div class="icon mt-3">
+						<img src="./expo/img/FOSSGIS_Konferenz_Logo_2021_BGw.png" title="Vorlage" alt="Vorlage">
+					</div>
+					<div class="mb-4">
+						<h6 class="mb-2">Tags</h6>
+						<div class="post-tags">
+							<span class="badge badge-info">Jobs</span>
+							<span class="badge badge-info">GIS-Jobs</span>
+							<span class="badge badge-info">OpenSource-Firmen</span>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+
+											
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
+</html>
