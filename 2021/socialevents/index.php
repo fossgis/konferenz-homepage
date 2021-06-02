3f3057d06453c5a63abcfb0b55cb784ef16b5463
Katja Haferkorn <katjahafi@gmx.de>
2021-06-02 12:02:52 +0200

    Rahmenprogramm Jeopardy und Fototermin

--- 2021/socialevents/index.php
+++ 2021/socialevents/index.php
@@ -25,20 +25,28 @@
 
 	<h4>Inoffizieller Start</h4>
 
-	<p>Am Abend des 06. Juni laden wir Sie ein zum virtuellen "Warmup in der Netzwerkwelt". Nutzen Sie die Möglichkeit sich in der Onlineplattform umzuschauen und in der Netzwerkwelt andere Teilnehmende zu treffen. Bei Fragen finden Sie Hilfe am InfoDesk. Der Zugang wird in Ihrem Ticketbestelllink zur Verfügung gestellt. </p>
+	<p>Am Abend des 06. Juni laden wir Sie ein zum virtuellen "Warmup in der Netzwerkwelt". Nutzen Sie die Möglichkeit sich in der Onlineplattform umzuschauen und in der Netzwerkwelt andere Teilnehmende zu treffen. Bei Fragen finden Sie Hilfe am InfoDesk (Personen mit rotem Hut sind Helfer der FOSSGIS-Konferenz). Der Zugang wird in Ihrem Ticketbestelllink zur Verfügung gestellt. </p>
 	
-
 	<h4>Abendveranstaltung </h4>
 	<p>Traditionell findet am ersten Abend der FOSSGIS-Konferenz die Abendveranstaltung statt. Auch wenn die FOSSGIS im Jahr 2021 ausschließlich online stattfindet, soll es für die Teilnehmenden die Möglichkeit geben sich bei einer Abendveranstaltung zu treffen. </p>
 
-	<p>Am 07.06.2021 laden wir Sie von 20 -23 Uhr zur Abendveranstaltung "Netzwerkweltdialoge" ein. Der gesamte virtuelle Campus Rapperswil steht zum Flanieren und Treffen zu Verfügung, auch ein Partyraum wird eingerichtet.</p>
-	<p>Live-Acts oder musikalische Beiträge sind im Partyraum der Netzwerkwelt möglich. Wer etwas beitragen möchte, meldet sich beim <a href="mailto:konferenz-orga@fossgis.de"> Konferenzorgateam</a>.</p>
+	<p>Schauen Sie gerne am 07.06.2021 von 20 -23 Uhr zur Abendveranstaltung "Netzwerkweltdialoge" rein. Der gesamte virtuelle Campus Rapperswil steht zum Flanieren und Treffen zu Verfügung.</p>
+
+	<h4>Jeopardy-Quiz</h4>
+	<p>Am <a href="https://fossgis-konferenz.de/2021/sessions/DSXKQN.php">Montag</a> und <a href="https://fossgis-konferenz.de/2021/sessions/AWSQZ7.php">Dienstag</a> gibt es nach dem regulären Konferenzprogramm um 17:30 Uhr ein humorvolles Quiz in Form des FOSSGIS-Jeopardy. Zum generellen Ablauf sowie zur Anmeldung als Teilnehmerin oder Teilnehmer finden sich <a href="https://fossgis-konferenz.de/2021/sessions/DSXKQN.php">Details im Programm</a>.
+
+	<!-- <p>Live-Acts oder musikalische Beiträge sind im Partyraum der Netzwerkwelt möglich. Wer etwas beitragen möchte, meldet sich beim <a href="mailto:konferenz-orga@fossgis.de"> Konferenzorgateam</a>.</p>
 		
 	<h4>Diskussionspanel</h4>
-	<p>Am Dienstagabend wird ein Diskussionspanel stattfinden. Das Thema wird im Programm bekannt gegeben</p>
+	<p>Am Dienstagabend wird ein Diskussionspanel stattfinden. Das Thema wird im Programm bekannt gegeben</p>-->
+
+	<h4>Gruppenfoto auf der Fotowiese</h4>
+	<p>Auch in deisem Jahr wollen wir uns das Gruppenfoto nicht entgehen lassen und laden ein am Dienstag, den 08. Juni um 15.30 Uhr beim Gruppenfoto in der Netzwerkwelt auf der Fotowiese auf dem virtuellen Campus dabei zu sein. </p>
+	
+	<p><img src="./socialevents/img/Fotowiese_Netzwerkwelt.png" class="img-fluid" alt="Fotowiese Netzwerkwelt"></p>
 	
 	<h4>Anwendertreffen</h4>
-	<p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen virtuell zur Verfügung, bitte <a href="mailto:konferenz-orga@fossgis.de">per E-Mail</a> beim Konferenzteam anfragen. </p>
+	<p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen virtuell zur Verfügung, bitte <a href="mailto:pko@fossgis.de">per E-Mail</a> beim Programmkomitee anfragen. </p>
 
 	
 	
