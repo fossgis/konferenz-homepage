af9639f6aea664964fad3a749c5f26172e639e4a
Katja Haferkorn <katjahafi@gmx.de>
2021-06-09 16:49:03 +0200

    Link zu den Videos

--- 2021/programm/index.php
+++ 2021/programm/index.php
@@ -42,7 +42,7 @@
 	<p>Die FOSSGIS 2021 wird auch im gewohnten Livestream übertragen: <a href="https://streaming.media.ccc.de/fossgis2021/"> https://streaming.media.ccc.de/fossgis2021/</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink im Bestelllink.</p>
 
 	<h3>Vortragsaufzeichnungen</h3>
-	<p>Die Vorträge und Demos werden aufgezeichnet und im Anschluss der Konferenz auf <a href="https://media.ccc.de/" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2021</a> veröffentlicht.</p> 
+	<p>Die Vorträge und Demos werden aufgezeichnet und im Anschluss der Konferenz auf <a href="https://media.ccc.de/c/fossgis2021" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2021</a> veröffentlicht.</p> 
 
 	<h3>FOSSGIS-Konferenz per Android-APP</h3>
 	<p>Die Android-App mit Fahrplan und Sponsoreninfos steht <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">zum Download im Playstore</a> bereit. </p>
