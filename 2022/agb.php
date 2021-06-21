070614a6271d2d637e95683e666c86c9c0ac4488
Katja Haferkorn <katjahafi@gmx.de>
2021-06-21 10:27:44 +0200

    FOSSGIS-Konferenz 2022 Homepage erstellt

---
+++ 2022/agb.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "inc/meta.inc"; ?>
+
+	<title>FOSSGIS 2019 - AGB</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+                <h2>Allgemeine Geschäftsbedingungen FOSSGIS-Konferenz</h2>
+
+<p>Der FOSSGIS e.V. richtet als Veranstalter die FOSSGIS-Konferenz aus.
+Für die Konferenz werden die Tickets, Workshops, Sponsorenpakete und Ausstellerpakete über die Internetseite <a href="
+https://pretix.eu/fossgis/2021" target="_blank"> </a> verkauft. Für das Vertragsverhältnis gelten die nachfolgenden allgemeinen Geschäftsbedingungen:<br></p>
+
+<p>Für die Richtigkeit der vom FOSSGIS e.V. als Veranstalter auf der Internetseite <a href="https://pretix.eu/fossgis/2021" target="_blank"> </a> gemachten Angaben zur Veranstaltung wird keine Gewähr übernommen. </p>
+
+<h3>Konferenztickets</h3>
+<h3>Konferenztickets für Verbraucher</h3>
+<p>Für den Verkauf der Konferenz-Tickets und Workshops an Verbraucher besteht für diese ein gesetzliches Widerrufsrecht gemäß § 312g BGB. In diesem Fall gilt nachfolgendes Widerrufsrecht:<br></p>
+
+<p>Sie haben das Recht, innerhalb von vierzehn Tagen ohne Angabe von Gründen den Vertrag über den Kauf eines Konferenz-Tickets und Workshopteilnahme zu widerrufen. Zur Wahrung der Frist ist es ausreichend, wenn Sie die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der Widerrufsfrist an eine der folgenden Kontaktmöglichkeiten senden: <br>
+
+<ul class="list">
+			<li> Email: konferenz-orga@fossgis.de</li>
+			<li> Telefon: +49 (0)30 62 93 203 </li>
+</ul>
+
+<p>Aus der Erklärung muss der Entschluss zum Widerruf des Vertrags eindeutig hervorgehen. Der Widerruf muss keine Begründung enthalten.</p>
+
+<h3>Stornierungsmöglichkeiten für Tickets</h3>
+
+<p>Neben dem eben gesetzlichen Widerrufsrecht für Verbraucher, gelten die folgenden Stornierungsmöglichkeiten:<br>
+Eine kostenfreie Stornierung ist bis 3 Tage vor Konferenzbeginn möglich.
+Für die eine Stornierung senden Sie bitte eine E-Mail an: konferenz-orga@fossgis.de.</p>
+
+<h3>Ausstellerpaket</h3>
+
+<p>Die Kündigung oder Stornierung des Ausstellerpaketes durch den Aussteller ist, unabhängig von der Inanspruchnahme des bereitgestellten Stellplatzes, ausgeschlossen. Der Stellplatz wird dem Aussteller vom FOSSGIS e. V. oder dessen Vertretern vor Ort zugewiesen. Ein Anspruch auf einen bestimmten Stellplatz in den Konferenzräumlichkeiten besteht für den Aussteller nicht.</p>
+
+<p>Die vom Aussteller präsentierten Produkten, Software und Dienstleistungen haben sich an den Zielen der Förderung und Verbreitung freier Geographischer Informationssysteme (GIS) im Sinne von freier oder offener Software und Open Data zu orientieren.</p>
+
+<p>Der FOSSGIS e.V. ist zur fristlosen Sonderkündigung des Ausstellerpakets berechtigt, soweit der Aussteller gegen die Vereinszwecke des FOSSGIS e.V. (Förderung und Verbreitung freier Geographischer Informationssysteme (GIS) im Sinne von freier oder offener Software und Open Data) verstößt. Schadensersatzansprüche des Ausstellers sind insoweit ausgeschlossen.</p>
+
+<p>Der FOSSGIS e.V haftet nicht für durch einfache Fahrlässigkeit verursachte Schäden, es sei denn, es handelt sich um eine mit einfacher Fährlässigkeit begangene Verletzung einer wesentlichen Vertragspflicht oder um die Verletzung von Leben, Körper oder Gesundheit. Im Falle der mit einfacher Fahrlässigkeit verursachten
+Verletzung einer wesentlichen Vertragspflicht ist die Haftung des FOSSGIS e.V. beschränkt auf den vorhersehbaren, vertragstypischen Schaden.</p>
+
+<h3>Sponsorenpaket</h3>
+
+<p>Der FOSSGIS e. V. ist zur fristlosen Sonderkündigung des Sponsorenpakets berechtigt, soweit der Sponsor gegen die Vereinszwecke des FOSSGIS e.V. (Förderung und Verbreitung freier Geographischer Informationssysteme (GIS) im Sinne von freier oder offener Software und Open Data) verstößt. Schadensersatzansprüche des Sponsors sind insoweit ausgeschlossen.</p>
+
+<h3>Haftung</h3>
+
+<p>Der FOSSGIS e.V haftet nicht für durch einfache Fahrlässigkeit verursachte Schäden, es sei denn, es handelt sich um eine mit einfacher Fährlässigkeit begangene Verletzung einer wesentlichen Vertragspflicht oder um die Verletzung von Leben, Körper oder Gesundheit. Im Falle der mit einfacher Fahrlässigkeit verursachten Verletzung einer wesentlichen Vertragspflicht ist die Haftung des FOSSGIS e.V. beschränkt auf den vorhersehbaren, vertragstypischen Schaden.</p>
+
+
+<p>Die hier geregelten Haftungsausschlüsse bzw. Haftungsbeschränkungen gelten auch für die Haftung der Erfüllungs- und Verrichtungsgehilfen des FOSSGIS e.V.</p>
+
+<h3>Bildaufzeichnungen</h3>
+<p>Auf der FOSSGIS-Konferenz werden üblicherweise Video-, Fernseh- und Fotoaufnahmen angefertigt. Dabei kann es vorkommen, dass Besucher abgelichtet werden. Jede:r Besucher:in erklärt mit der Teilnahme an der FOSSGIS-Konferenz sein/ihr Einverständnis, dass entsprechend Aufnahmen von ihm/ihr als Besucher:in im Rahmen der Konferenz ohne Vergütungsanspruch angefertigt und veröffentlicht werden dürfen.</p>
+
+<p>Ein entsprechender Hinweis ist auch auf den Tickets enthalten. Personenbezogene Daten werden unter Beachtung der geltenden datenschutzrechtlichen Bestimmungen verarbeitet: <a href="https://pretix.eu/fossgis/2021/privacy/" target="_blank">siehe Datenschutzerklärung</a> </p>
+
+<h3>Streitbeilegung</h3>
+<p>Die EU-Kommission stellt für die außergerichtliche Streitschlichtung eine Plattform bereit. Sie gibt Verbrauchern die Möglichkeit, Rechtsstreitigkeiten aus Verträgen oder Streitigkeiten über das Bestehen solcher Verträge zunächst ohne Einschaltung eines Gerichts zu klären. Die Streitbeilegungsplattform kann unter dem Link
+<a href="http://ec.europa.eu/consumers/odr/" target="_blank">http://ec.europa.eu/consumers/odr/</a> erreicht werden.</p>
+
+<p>Der FOSSGIS e.V. hat die Möglichkeit der Teilnahme am außergerichtlichen Streitbeilegungsverfahren geprüft und ist zu dem Ergebnis gekommen, nicht an einem Streitbeilegungsverfahren vor einer
+Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+<h3>Gerichtsstand</h3>
+<p>Ist der Vertragspartner Kaufmann, so ist ausschließlicher (auch internationaler) Gerichtsstand Kirchzarten für alle sich aus dem Vertragsverhältnis unmittelbar oder mittelbar ergebenen Streitigkeiten.</p>
+
+<p>Dies gilt im Falle von grenzüberschreitenden Verträgen auch für Nichtkaufleute.</p>
+
+<h3>Anwendbares Recht</h3>
+<p>Es gilt ausschließlich das Recht der Bundesrepublik Deutschland unter Ausschluss des UN-Kaufrechts.</p>
+
+<p>Stand der AGB: 25. März 2021</p>
+
+	<?php include "inc/footer.inc"; ?>
+
+</body>
+
+</html>
---
+++ 2022/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+	<?php include "inc/meta.inc"; ?>
+<base href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+   	<title>FOSSGIS 2022</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+      <h1>Willkommen zur FOSSGIS Konferenz 2022</h1>
+
+            <p>Die FOSSGIS-Konferenz ist im D-A-CH-Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme sowie für die Themen Open Data und OpenStreetMap.</p>
+
+            <p>Die FOSSGIS-Konferenz 2022 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> mit Unterstützung <a href="https://www.uni-marburg.de/de/fb12">des Fachbereich Mathematik und Informatik</a> der <a href="https://www.uni-marburg.de/de">Universität Marburg</a> organisiert.</p>
+
+
+            <ul class="tiles">
+				<li class="tile bronze">
+                     <a href="https://www.fossgis.de/">
+                         <img src="./img/fossgis.png" alt="FOSSGIS e.V.">
+                     </a>
+                 </li>
+                 <li class="tile bronze">
+                     <a href="https://www.openstreetmap.de/">
+                         <img src="./img/osm.png" alt="OSM Community">
+                     </a>
+                 </li>                                   
+                 <li class="tile bronze">
+                     <a href="https://www.osgeo.org/">
+                         <img src="img/osgeo.png" width="180px" Alt="OSGeo">
+					 </a>
+				 </li>
+				<!-- <li class="tile bronze">
+					 <a href="http://www.ost.ch/index.php/">
+						 <img src="./img/ost_logo_de_rgb.png" alt="OST Campus Rapperswil" width="180px">
+					 </a>
+                 </li>     -->               
+            </ul>
+           
+
+            <h3>Was ist die FOSSGIS Konferenz?</h3>
+
+            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender und Entwickler zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>	
+
+			<!--<p>Aufgrund der derzeitigen Pandemiesituation wird die FOSSGIS ausschließlich als Online-Event stattfinden. Es wird die Möglichkeit geben den Campus Rapperswil virtuell zu erkunden, lassen Sie sich überraschen.</p>-->
+
+           <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+
+			<!--<h3>Sponsoren der FOSSGIS-Konferenz</h3>
+			<p> Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich. Werden Sie Sponsor.</p><br>-->
+
+			
+			<h4 class="highlight">   </h4>
+			<h3>Werden Sie Sponsor!</h3>
+           	<p>Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich. Informationen zum Sponsoring sind ab August 2021 hier verfügbar</p>
+			<!-- <p>Sie möchten die FOSSGIS-Konferenz als Sponsor unterstützen? Sie finden Informationen für Sponsoren und Aussteller in der <a href="data/Sponsoreninformationen_FOSSGIS-2021.pdf">Sponsoringbroschüre</a>. Falls Sie sich für ein Sponsoring entschieden haben, können Sie die <a href="data/FOSSGIS-Konferenz-Sponsoring-Vereinbarung_online_2021.pdf">Sponsoringvereinbarung hier</a> herunterladen.<br>-->
+			<p>Bei Fragen wenden Sie sich an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.</p>
+  
+  
+        <?php include('inc/footer.inc'); ?>
+ 
+</body>
+</html>
