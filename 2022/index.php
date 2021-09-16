c2ca9179b98f168bc56a256bf87c7c3581105b21
Katja Haferkorn <katjahafi@gmx.de>
2021-09-16 13:44:43 +0200

    minimale Änderungen

--- 2022/index.php
+++ 2022/index.php
@@ -59,7 +59,7 @@
 			
 			<h4 class="highlight">   </h4>
 			<h3>Werden Sie Sponsor!</h3>
-           	<p>Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich. Informationen zum Sponsoring sind ab August 2021 hier verfügbar</p>
+           	<p>Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich. Informationen zum Sponsoring sind in Kürze hier verfügbar</p>
 			<!-- <p>Sie möchten die FOSSGIS-Konferenz als Sponsor unterstützen? Sie finden Informationen für Sponsoren und Aussteller in der <a href="data/Sponsoreninformationen_FOSSGIS-2021.pdf">Sponsoringbroschüre</a>. Falls Sie sich für ein Sponsoring entschieden haben, können Sie die <a href="data/FOSSGIS-Konferenz-Sponsoring-Vereinbarung_online_2021.pdf">Sponsoringvereinbarung hier</a> herunterladen.<br>-->
 			<p>Bei Fragen wenden Sie sich an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.</p>
   
