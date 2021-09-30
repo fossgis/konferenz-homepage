26c1582c3eca07e6e8d40abcfffdd79c9b9c7602
Katja Haferkorn <katjahafi@gmx.de>
2021-09-30 11:38:04 +0200

    Anmeldungsinformationen

--- 2022/anmeldung/index.php
+++ 2022/anmeldung/index.php
@@ -5,7 +5,7 @@
 	<?php include "../inc/meta.inc"; ?>
 
 	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
-	<title>FOSSGIS 2021 - Anmeldung zur Konferenz</title>
+	<title>FOSSGIS 2022 - Anmeldung zur Konferenz</title>
 
 	<link rel="stylesheet" href="./css/normalize.css">
 	<link rel="stylesheet" href="./css/base.css">
@@ -22,10 +22,13 @@
 
 	<h2>Anmeldung zur Konferenz</h2>
 
-	<p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz 2021 zu werden. Die Konferenz lebt von Ihren Beitr&auml;gen und Ihrem Besuch. Wir freuen uns auf Sie! </p>
+	<p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz 2022 zu werden. Die Konferenz lebt von Ihren Beiträgen und Ihrem Besuch. Wir freuen uns auf Sie! </p>
 
-	<p>Die FOSSGIS-Konferenz findet vom 6.-9. Juni 2021 ausschließlich Online statt. Dazu nutzen wir die Online-Plattform Venueless. Der Zugang wird Ihnen über den Ticketbuchungslink am Vortag der Konferenz als Link zur Verfügung gestellt. Sie benötigen einen Zugang zum Internet sowie einen aktuellen Browser.<br>
-		Hier finden Sie Preise, Informationen zum Online-Event, Bildungsurlaub und wie Sie Helfer:in werden. </p>
+	<p>Die FOSSGIS-Konferenz findet vom 9. - 12. März 2022 an der Philipps Universität Marburg auf dem Campus im Lahntal im großen Hörsaalgebäude in der Biegenstraße 14 sowie online statt. 
+Das heißt die FOSSGIS wird als hybrides Event stattfinden, es wird Konferenztickets und Online-Konferenztickets geben.</p>
+	<p><b>Der Ticketverkauf startet in der ersten Januarwoche 2022.</b></p>
+		
+	<p>Im Folgenden finden Sie Informationen zur Teilnahme, Bildungsurlaub und wie Sie Helfer:in werden.</p>
 	
 	<!--<p class="highlight">
 		&darr; <a href="https://pretix.eu/fossgis/2021/">Direkt zur Anmeldung</a>
@@ -33,61 +36,99 @@
 
 
 
-	<h3>Preise FOSSGIS-Konferenz 2021</h3>
-	<ul>
-		<li>Online-Konferenzticket (Normalpreis): 80 € </li>
-		<li>Online-Konferenzticket (reduzierter Preis*): 15 € </li>
-		<li>Online-Konferenzticket für Community<sup>**</sup>: € 0</li>
-		<li>Online-Konferenzticket für Helfende<sup>**</sup>: 6 h helfen</li>
-		<li>Online-Workshop***: 120 € </li>
-	</ul>
-	<p><b>Im Konferenzticket enthalten:</b></p>
-	<ul>
-		<li>Online-Teilnahme (sie müssen nichts installieren, ein aktueller Browser reicht)</li>
-		<li>Chatmöglichkeiten mit den Teilnehmenden</li>
-		<li>Zugang zur <i>Netzwerkwelt</i> (dafür sind Kamera und Mikrofon hilfreich)</li>
-		<li>Tagungsband***</li>
-		<li>ordentliche Rechnung</li>
-	</ul>
+	#### Informationen zu den Preisen
+folgen in Kürze
 
-	<p>
-		<small>* gilt für: Schüler:in, Azubi, Freiwilligendienstleistende, Studierende, Rentner:innen</small><br>
-		<small>** Regelungen für das Communityticket oder Helferticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie aktive Helfer:innen der Konferenz für mindestens 6 Stunden.</small><br>
-		<small>*** wenn bestellt und solange verfügbar</small>
-	</p>
+<!--## Preise FOSSGIS-Konferenz 2022
+
+### Teilnahme in Marburg
+
+    Konferenzticket (Normalpreis): 220 €
+    Konferenzticket (reduzierter Preis*): 50 €
+    Konferenzticket für Community**: € 0
+    Konferenzticket für Helfende**: 6 h helfen
+    Workshop***: 120 €
+    
+*Im Konferenzticket enthalten:*
+
+* FOSSGIS-Konferenz-Teilnahme in Marburg
+* Zugang Online-Plattform
+* ordentliche Rechnung
+* Pausensnack
+* Tasche, Tagungsband***
+* T-Shirt***
+* Abendveranstaltung***
+    
+      * reduzierter Preis gilt für: Schüler:in, Azubi, Freiwilligendienstleistende, Studierende, Rentner:innen
+      ** Regelungen für das Communityticket oder Helferticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie aktive Helfer:innen der Konferenz für mindestens 6 Stunden.
+      *** wenn bestellt und solange verfügbar 
+    
+### Online-Teilnahme
+
+    Online-Konferenzticket (Normalpreis): 90 €
+    Online-Konferenzticket (reduzierter Preis*): 15 €
+    Online-Konferenzticket für Community**: € 0
+    Online-Konferenzticket für Helfende**: 6 h helfen
+    Online-Workshop***: 120 €
+    
+*Im Konferenzticket enthalten:*
+
+* Zugang zur Online-Plattform (sie müssen nichts installieren, ein aktueller Browser reicht)
+* ordentliche Rechnung
+* Chatmöglichkeiten mit den Teilnehmenden
+* Zugang zur Netzwerkwelt (dafür sind Kamera und Mikrofon hilfreich)
+* Tagungsband***
+ 
+
+      * reduzierter Preis gilt für: Schüler:in, Azubi, Freiwilligendienstleistende, Studierende, Rentner:innen
+      ** Regelungen für das Communityticket oder Helferticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie aktive Helfer:innen der Konferenz für mindestens 6 Stunden.
+      *** wenn bestellt und solange verfügbar-->
+
+	<p>Der Zugang zur Online-Plattform wird Ihnen über den Ticketbuchungslink als Link zur Verfügung gestellt. Für die Teilnahme benötigen Sie einen Zugang zum Internet sowie einen aktuellen Browser.</p>
+
+	<h3>Corona</h3>
+<p>Es ist geplant die Konferenz unter der 2G-Regel stattfinden zu lassen.<br>
+Das heißt diejenigen, die vor Ort teilnehmen, müssen beim Checkin einen entsprechenden Genesenen- oder Impfnachweis vorweisen. <br>
+Das Konferenzorganisationsteam bereitet zur Umsetzung der Hygienregeln ein ein entsprechndes Hygienekonzept vor.</p>
 
 	<h3 id="Workshop">Workshop buchen</h3>
-	<p>Das Programm bietet die Möglichkeit einen oder mehrere Workshops zu buchen. Für jeden Workshop sind 90 Minuten vorgesehen, der Preis beträgt 120 EUR. Einen Workshop können Sie im zweiten Schritt des Bestellvorgangs zum Konferenzticket buchen.<br> Sie nehmen mit Ihrem eigenen Gerät am Workshop teil, den Zugang erhalten Sie über Ihren Ticketbuchungslink. Der Workshop ist interaktiv und wird in einer Videokonferenz stattfinden. Im Ticketshop ist im jeweiligen Workshop ein Stichwort zur Durchführungsart angegeben, dieses gibt an, welche Vorbereitungen Ihrerseits getroffen werden können, um erfolgreich teilzunehmen. 
+	<p>Das Programm bietet die Möglichkeit einen oder mehrere Workshops zu buchen. Für jeden Workshop sind 90 Minuten vorgesehen, der Preis beträgt 120 EUR. Einen Workshop können Sie im zweiten Schritt des Bestellvorgangs zum Konferenzticket buchen.<br>
+Es wird Workshops vor Ort geben sowie auch Online-Workshops, die Durchführungsart sehen Sie im Programm sowie auch im Infotext zum jeweiligen Workshop.<br>
+Für die Workshops vor Ort stellt uns die Uni Marburg entsprechende Räume zur Verfügung.<br>
+Am Online-Workshop nehmen Sie mit Ihrem eigenen Gerät teil, den Zugang erhalten Sie über Ihren Ticketbuchungslink. Der Online-Workshop ist interaktiv und wird im Format Videokonferenz stattfinden. Im Ticketshop ist im jeweiligen Workshop ein Stichwort zur Durchführungsart* angegeben, dieses gibt an, welche Vorbereitungen Ihrerseits getroffen werden können, um erfolgreich teilzunehmen.</p>
 		<ul>
-		<li><i><b>Browserbasiert:</b></i> Sie benötigen lediglich einen aktuellen Internet-Browser.</li>
-		<li><i><b>OSGeoLive 14.0: </b></i> Im Workshop wird mit der aktuellen OSGeoLive gearbeitet, diese kann als Image von einem USB-Stick oder als Virtuelle Box auf Ihrem Computer laufen. Hier finden Sie eine <a href="http://live.osgeo.org/de/index.html"> Anleitung zur Installation der OSGeoLive.</a></li>
+		<li><i><b>Browserbasiert: </b></i> Sie benötigen lediglich einen aktuellen Internet-Browser.</li>
+		<li><i><b>OSGeoLive: </b></i> Im Workshop wird mit der aktuellen OSGeoLive gearbeitet, diese kann als Image von einem USB-Stick oder als Virtuelle Box auf Ihrem Computer laufen. Hier finden Sie eine <a href="http://live.osgeo.org/de/index.html"> Anleitung zur Installation der OSGeoLive.</a></li>
 		<li><i><b>Individuell: </b></i> Sie erhalten in der Woche vor der Konferenz eine Mail mit detaillierten Informationen zum Workshop sowie die Möglichkeit in einem Termin Ihre Technik zu checken.</li>
+		<li><i><b>Präsenz-Workshop: </b></i> findet in einem Workshopraum an der Uni Marburg statt.</li>
 		</ul>
-  	</p>
+
 
 	<h3>Bildungsurlaub</h3>
-	<p>Die Anerkennung der FOSSGIS-Konferenz als Bildungsurlaub ist beantragt, zunächst für die Bundesländer in Deutschland, wo die Beantragung keine zusätzlichen Kosten verursacht (BE, BB, HB, HH, NI, RP, SL, ST und SH). Weitere können bei Bedarf beantragt werden. Teilnehmer:innen, die dieses Angebot nutzen wollen, beantragen dies bei beim Arbeitgeber. Informationen zum Bildungsurlaub finden Sie unter <a href="https://www.bildungsurlaub.de/home.html"> www.Bildungsurlaub.de</a>.</p>
-	<h4>Folgende Bundesländer erkennen die FOSSGIS-Konferenz als Bildungsurlaub an:</h4>
+	<p>Die Anerkennung der FOSSGIS-Konferenz als Bildungsurlaub wird angestrebt. <!--ist beantragt, zunächst für die Bundesländer in Deutschland, wo die Beantragung keine zusätzlichen Kosten verursacht (BE, BB, HB, HH, NI, RP, SL, ST und SH). Weitere können bei Bedarf beantragt werden. Teilnehmer:innen, die dieses Angebot nutzen wollen, beantragen dies bei beim Arbeitgeber. --><br>
+	Folgende Bundesländer erkennen die FOSSGIS-Konferenz als Bildungsurlaub an:</p>
 	<ul>
-	<li>Berlin - <a href="data/BE_FOSSGIS-2021_Anerkennung.pdf">Download Anerkennung</a></li>
-	<li>Brandenburg - <a href="data/BB_FOSSGIS-2021_Anerkennung.pdf">Download Anerkennung</a></li>
 	<li>Rheinland-Pfalz - <a href="data/RP_FOSSGIS-2021_AnerkennungsBescheid_76484.pdf">Download Anerkennung</a></li>
 	</ul>
+	<p> Die Beantragung der FOSSGIS-Konferenz als Bildungsurlaub in weiteren Bundesländern ist in Planung.<br> Informationen zum Bildungsurlaub finden Sie unter <a href="https://www.bildungsurlaub.de/home.html"> www.Bildungsurlaub.de</a>.</p>
+	
 
 	
 	<h3>Netzwerkwelt</h3>
 
-	<p>Um optimales Netzwerken während der Veranstaltung zu ermöglichen, wird es eine virtuelle Variante des OST Campus Rapperswil geben, wir nennen es die <i>Netzwerkwelt</i>.<br>
-	   In der <i>Netzwerkwelt</i> können sich die Teilnehmenden in einer virtuellen Karte/Landschaft bewegen und einander begegnen. Zum Einstieg gibt man sich einen Namen und wählt einen Avatar aus. Nach der Landung im Eingangsbereich bewegen Sie sich mit Hilfe der Tastatur. Sie können die <i>Netzwerkwelt</i> erkunden (Informationsräume, Firmenausstellung betreten) und Sie sehen auch, ob weitere Teilnehmende unterwegs sind. Wenn Sie einer Person begegnen, öffnet sich ein Videochat, welcher sich mit einem Geräusch ankündigt, Sie können sich austauschen oder weglaufen - fast wie im echten Leben ;-).</p>
+<p>Um optimales Netzwerken während der Veranstaltung zu ermöglichen, wird es wieder eine <i>Netzwerkwelt</i> geben.<br>
+In der <i>Netzwerkwelt</i> können sich die Teilnehmenden in einer virtuellen Karte/Landschaft bewegen und einander begegnen. Zum Einstieg gibt man sich einen Namen und wählt einen Avatar aus. Nach der Landung im Eingangsbereich bewegen Sie sich mit Hilfe der Tastatur. Sie können die <i>Netzwerkwelt</i> erkunden und Sie sehen auch, ob weitere Teilnehmende unterwegs sind. Wenn Sie einer Person begegnen, öffnet sich ein Videochat, welcher sich mit einem Geräusch ankündigt, Sie können sich austauschen oder weglaufen - fast wie im echten Leben ;-).
+Erklärfilm Netzwerkwelt
 
 	<h3>Erklärfilm Netzwerkwelt</h3>
 	<p>Klicken Sie <a href="https://vortraege.fossgis.de/f/d575438dc2fb42c0a253/" target="_blank">hier</a>, um den Film anzusehen.</p>
 	<div>
 	<img src="./data/Foto_Erklaerfilm_Netzwerkwelt.png" alt="Erklärfilm Netzwerkwelt">
 
-	<h3>Helfer</h3>
-	<p>Freiwillige Helfer:innen sind eingeladen und willkommen vor der Konferenz bei der technischen Vorbereitung und während der Konferenz bei der technischen Betreuung oder als Sessionleitung zu unterstützen. Bei Interesse bitte im <a href="https://helfer.fossgis.de/" target="_blank">Helfersystem</a> registrieren und eine E-Mail an das <a href="mailto:helfer@fossgis.de"> Helferteam</a> senden, damit wir Sie kontaktieren können. Helfer:innen dürfen das kostenfreie "Online-Konferenzticket für Helfende" buchen.
-Bei Fragen empfehlen wir die <a href="https://pretix.eu/fossgis/2021/page/helfer/" target="_blank">Helfer-FAQ</a> zu lesen.</p>
+	<h3>Helfen</h3>
+	<p>Freiwillige Helfer:innen sind eingeladen und willkommen vor der Konferenz und/oder während der Konferenz zu unterstützen. Bei Interesse bitte die <a href="https://fossgis-konferenz.de/2022/helfen/">Website <i>Helfen</i></a> lesen, dort sind viele Details erklärt.</p>
+	<p>Helfer:innen dürfen das kostenfreie "Konferenzticket für Helfende" buchen.</p>
+	<p>Wenn Sie schon wissen, was Sie helfen möchten, melden Sie sich <a href="mailto:helfen@fossgis.de?subject=Interesse_Unterstützung_FOSSGIS-Konferenz">per E-Mail an helfen@fossgis.de</a> beim Helferteam.</p>
 
    	<!--<h3>Anmeldung</h3>
 	<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, können Sie das Ticketsystem auch unter <a href="https://pretix.eu/fossgis/2021/">diesem Link</a> direkt aufrufen.</p>
@@ -102,12 +143,13 @@ Bei Fragen empfehlen wir die <a href="https://pretix.eu/fossgis/2021/page/helfer
 		</noscript>-->
 			
 
-	<h3>Sie haben Fragen?</h3>
-	<p>Lesen Sie die <a href="https://pretix.eu/fossgis/2021/page/faq/">FAQ</a>, wenn Sie dort auf Ihre Frage keine Antwort finden, kontaktieren Sie das Konferenzorgateam via:
+	<h3>Sie haben Fragen oder wollen das Konferenzteam erreichen?</h3>
+
 	<ul>
-	<li>Matrixkanal: <a href="https://matrix.to/#/#FOSSGIS2021:matrix.org">#FOSSGIS2021:matrix.org </a></li>
-	<li>E-Mail an <a href="mailto:konferenz-orga@fossgis.de ">konferenz-orga@fossgis.de</a> </li>
-	<li>Fragen zum Programm an das <a href="mailto:PKO@fossgis.de">Programmkomitee</a> </li>
+	<li>Matrixkanal: <a href="https://matrix.to/#/!XajsOFLRhsznebfosT:matrix.org">#FOSSGIS2022:matrix.org </a></li>
+	<li>E-Mail an <a href="mailto:konferenz-orga@fossgis.de?subject=Konferenzorganisationsteam_erreichen">konferenz-orga@fossgis.de</a> </li>
+	<li>Anruf: +49 30-62932037</li>
+	<li>Fragen zum Programm an das <a href="mailto:PKO@fossgis.de?subject=Programmkomitee_erreichen">Programmkomitee</a> </li>
 	</ul>
 	</p>
 
