1af3eabaf193a115046bd7dc49b8e24c6e40c7cc
Jochen Topf <jochen@topf.org>
2021-10-05 16:54:39 +0200

    day?.php -> statisch

--- 2017/programm/day3.php
+++ 2017/programm/day3.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<?php require_once 'simple_html_dom.php'; ?>
 <html lang="de">
     <head>
         <title>FOSSGIS 2017 Programm</title>
@@ -15,14 +14,47 @@
 
     <body id="programm">
       <div id="wrap">
-        <?php include('../inc/head.inc'); ?>
+        <h1>
+    FOSSGIS-Konferenz 2017 <span class="venue">Passau</span><br>
+    <span class="date">22.&ndash;25. März 2017</span>
+</h1>
 
         <div id="content">
 
-          <?php
-	     include('../inc/nav.inc');
-	     include('prog_nav.inc');
-          ?>
+          <ul id="mainNav">
+    <li id="navHome">
+        <a href="/2017/">Startseite</a>
+    </li>
+    <li id="navProgramm">
+        <a href="/2017/programm">Programm</a>
+    </li>
+    <li id="navAnreise">
+        <a href="/2017/anreise">Anreise</a>
+    </li>
+    <li id="navEvents">
+        <a href="/2017/socialevents">Social Events</a>
+    </li>
+    <li id="navGallery">
+        <a href="/2017/gallery">Bilder</a>
+    </li>
+</ul>
+<ul id="progNav">
+    <li id="prog">
+        <a href="/2017/programm/">Übersicht</a>
+    </li>
+    <li id="prog">
+        <a href="/2017/programm/day1.php">Mittwoch</a>
+    </li>
+    <li id="prog">
+        <a href="/2017/programm/day2.php">Donnerstag</a>
+    </li>
+    <li id="prog">
+        <a href="/2017/programm/day3.php">Freitag</a>
+    </li>
+    <li id="prog">
+        <a href="/2017/programm/referenten.php">Referenten</a>
+    </li>
+</ul>
 
         <figure>
             <img src="../img/passau.jpg" width="880" height="241" alt="Passau">
@@ -30,19 +62,9 @@
           </figure>
 
           <h2>Freitag, 24. März</h2>
-<?php
-	        $html = file_get_html("https://frab.fossgis-konferenz.de/de/2017/public/schedule/3");
-	        $color = $html->find('div[id=colorkeys]', 0);
-                echo $color;
-                $table = $html->find('div[id=main-table]', 0);
-
-                $table = preg_replace("/de\/2017\/public\/events\/([0-9]+)/", '2017/programm/event\1.html', $table);
-                $table = preg_replace("/de\/2017\/public\/speakers\/([0-9]+)/", '2017/programm/speaker\1.html', $table);
-	        echo $table;
-	  ?>
-
+<div id='main-table'> <div id='conference-rooms_0'> <hr> <h2>AM HS 9, IM HS 13, AM Foyer, Workshop B, Workshop C</h2> <a name='0'></a> <table class='rooms-table'> <thead> <tr> <th class='cell-time'></th> <th>AM HS 9</th> <th>IM HS 13</th> <th>AM Foyer</th> <th>Workshop B</th> <th>Workshop C</th> </tr> </thead> <tbody> <tr class='cell-height1'> <td class='cell-time'> <p>09:30</p> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5156.html">Freie Satellitenbilder – ein Überblick</a></h2> </div> <div class='event-details'> <div class='subtitle'> wie man sich frei und neutral ein Bild der Erde machen kann </div> <div class='speakers'><a href="/2017/programm/speaker76.html">Christoph Hormann</a></div> </div> </div> </div> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-software'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5197.html">Open Source Routing Machine</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1600.html">Daniel J H</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5234.html">Freie Fernerkundungsdaten für alle – das Copernicus-Programm der EU</a></h2> </div> <div class='event-details'> <div class='subtitle'> Freie Sentinel-Daten mit freier Software </div> <div class='speakers'><a href="/2017/programm/speaker69.html">Christian Strobl</a></div> </div> </div> </div> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-software'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5222.html">Öffentlicher Verkehr in GraphHopper</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1614.html">Michael Zilske</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>10:15</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>11:00</p> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5184.html">Steigerung der Akzeptanz räumlicher Planung durch freiwillig gesammelte Geodaten</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1596.html">Stefan Küspert</a></div> </div> </div> </div> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5227.html">OSM-Daten verarbeiten auf der Kommandozeile mit Osmium</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1619.html">Jochen Topf</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td class='cell-height6' rowspan='6'> <div class='event track-workshop-bezahlt'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5151.html">GRASS-Plugin in QGIS</a></h2> </div> <div class='event-details'> <div class='subtitle'> Eine praktische Einführung </div> <div class='speakers'><a href="/2017/programm/speaker36.html">Otto Dassau</a></div> </div> </div> </div> </td> <td class='cell-height6' rowspan='6'> <div class='event track-workshop-bezahlt'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5190.html">Aufbau einer GDI mit freier Software</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker42.html">Astrid Emde</a>, <a href="/2017/programm/speaker1629.html">Frederik Häfker</a></div> </div> </div> </div> </td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5223.html">GeoPackages der freien Hamburger Geodaten</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1618.html">Johannes Kröger</a></div> </div> </div> </div> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5231.html">Vandalismus im OpenStreetMap Projekt</a></h2> </div> <div class='event-details'> <div class='subtitle'> Stand heute. Und morgen? </div> <div class='speakers'><a href="/2017/programm/speaker1620.html">Pascal Neis</a></div> </div> </div> </div> </td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>11:45</p> </td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-software'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5211.html">Mit Deep Learning raum-zeitliche Muster erkennen und voraussagen</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1572.html">Felix Kunde</a></div> </div> </div> </div> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5237.html">Zusammenführung und Vereinheitlichung von Eisenbahn-Streckennetzdaten</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker1601.html">Alexander Matheisen</a></div> </div> </div> </div> </td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>12:30</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>13:15</p> </td> <td class='cell-height1' rowspan='1'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5215.html">OSGeo Konferenzaufzeichnungen im TIB AV-Portal</a></h2> </div> <div class='event-details'> <div class='subtitle'> Wissenschaftliche Ressourcen dauerhaft bewahren und innovativ nutzen. </div> <div class='speakers'><a href="/2017/programm/speaker1610.html">Bastian Drees</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td class='cell-height2' rowspan='2'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5160.html">OSM Ehrenamt</a></h2> </div> <div class='event-details'> <div class='subtitle'> Freiwillige Arbeit jenseits des Daten-Mappings </div> <div class='speakers'><a href="/2017/programm/speaker26.html">Peter Barth</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>14:00</p> </td> <td class='cell-height2' rowspan='2'> <div class='event track-sonstiges'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5251.html">Abschlussveranstaltung</a></h2> </div> <div class='event-details'> <div class='subtitle'>  </div> <div class='speakers'><a href="/2017/programm/speaker94.html">Dr. Marco Lechner</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td class='cell-height4' rowspan='4'> <div class='event track-sonstiges'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5140.html">Sektempfang</a></h2> </div> <div class='event-details'> <div class='subtitle'> am FOSSGIS-Stand </div> <div class='speakers'><a href="/2017/programm/speaker1633.html">FOSSGIS</a></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>14:45</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>15:30</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td class='cell-height14' rowspan='14'> <div class='event track-freie-daten'> <div class='event-wrapper'> <div class='event-header'> <h2><a href="/2017/programm/event5246.html">Qualitätsoffensive OSM</a></h2> </div> <div class='event-details'> <div class='subtitle'> Gemeinsamer Mappingevent zum Erlernen und Vertiefen der OpenStreetMap-Kenntnisse </div> <div class='speakers'></div> </div> </div> </div> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>16:15</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>17:00</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>17:45</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> <p>18:30</p> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> <tr class='cell-height1'> <td class='cell-time'> </td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> </tbody> </table> </div> </div>
     </div>
-    <?php include('../inc/footer.inc'); ?>
+    <footer>Website by <a href="https://oliverroick.de/">Oliver Roick</a> &bull; <a href="https://www.fossgis.de/impressum/">Impressum</a> &bull; <a href="https://www.fossgis.de/datenschutzerklärung">Datenschutzerklärung</a></footer>
       </div>
     </body>
 </html>
