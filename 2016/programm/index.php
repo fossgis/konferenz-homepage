d691f041f58f8bd6aaad158730fbb512283b8fe6
Jochen Topf <jochen@topf.org>
2021-10-10 20:34:00 +0200

    Fix 2016 Programm-Overviews

--- 2016/programm/index.php
+++ 2016/programm/index.php
@@ -1,91 +1,88 @@
 <!DOCTYPE html>
 <html lang="de">
     <head>
-        <title>FOSSGIS 2018 Programm</title>
+        <title>FOSSGIS 2016 Programm</title>
         <meta name="description" content="FOSSGIS ist die Abk&uuml;rzung f&uuml;r Freie und Open Source Software f&uuml;r Geoinformationssysteme und ist die f&uuml;hrende Konferenz zu diesem Thema im deutschsprachigen Raum.">
-        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Bonn">
+        <meta name="keywords" content="FOSSGIS, Open Source, GIS, Konferenz, Geoinformatik, OpenStreetMap, OSM, Universität Salzburg, Hohensalzburg, Z_GIS">
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
         <link rel="stylesheet" type="text/css" href="/static/droidsans/droidsans.css" />
         <link href="../css/screen.css" type="text/css" rel="stylesheet" media="screen">
 	<link type="text/css" href="programm.css" media="screen,print" rel="Stylesheet"/>
 	<link type="text/css" href="extra.css" media="screen,print" rel="Stylesheet"/>
+
     </head>
-
     <body id="programm">
-      <div id="wrap">
-        <h1>
-    FOSSGIS-Konferenz 2018 <span class="venue">Bonn</span><br>
-    <span class="date">21.&ndash;24. März 2018</span>
-</h1>
+        <div id="wrap">
+            <h1>FOSSGIS-Konferenz 2016 <span class="venue">Salzburg</span><br><span class="date">04.&ndash;06. Juli 2016</span></h1>
 
-        <div id="content">
+            <div id="content">
 
-          <ul id="mainNav">
-    <li id="navHome"><a href="/2018/">Startseite</a></li>
-    <li id="navProgramm"><a href="/2018/programm">Programm</a></li>
-    <li id="navAnreise"><a href="/2018/anreise">Anreise</a></li>
-    <li id="navEvents"><a href="/2018/socialevents">Social Events</a></li>
-    <li id="navTeam"><a href="/2018/loc/loc.php">Das Team</a></li>
-    <li id="navGallery"><a href="/2018/gallery">Bilder</a></li>
+                <ul id="mainNav">
+    <li id="navHome"><a href="/2016/">Startseite</a></li>
+    <li id="navAnmeldung"><a href="/2016/anmeldung">Anmeldung</a></li>
+    <li id="navProgramm"><a href="/2016/programm">Programm</a></li>
+    <li id="navAnreise"><a href="/2016/anreise">Anreise + Unterkunft</a></li>
+    <li id="navEvents"><a href="/2016/socialevents">Social Events</a></li>
+    <li id="navGreen"><a href="/2016/green">Green</a></li>
 </ul>
 <ul id="progNav">
     <li id="prog">
-        <a href="/2018/programm/">Übersicht</a>
+        <a href="/2016/programm/">Übersicht</a>
     </li>
     <li id="prog">
-        <a href="/2018/programm/day1.php">Mittwoch</a>
+        <a href="/2016/programm/day1.php">Sonntag</a>
     </li>
     <li id="prog">
-        <a href="/2018/programm/day2.php">Donnerstag</a>
+        <a href="/2016/programm/day2.php">Montag</a>
     </li>
     <li id="prog">
-        <a href="/2018/programm/day3.php">Freitag</a>
+        <a href="/2016/programm/day3.php">Dienstag</a>
     </li>
     <li id="prog">
-        <a href="/2018/programm/day4.php">Samstag</a>
+        <a href="/2016/programm/day4.php">Mittwoch</a>
     </li>
     <li id="prog">
-        <a href="/2018/programm/referenten.php">Referenten</a>
+        <a href="/2016/programm/referenten.php">Referenten</a>
     </li>
 </ul>
 
-                <figure>
-                    <img src="../img/bonn.jpeg" width="880" height="241" alt="Bonn">
-                    <figcaption>Photo: Stadt Bonn</figcaption>
-                </figure>
-
-          <h2>
-	    Übersicht	  </h2>
-	  
-	<div class="app">Sie konnten selbst nicht dabei sein? Sie finden die Aufzeichnungen auf </br><a href="https://media.ccc.de/" traget="_blank"><img src="../img/media.ccc.de.png" alt="media.ccc.de" /></a></br> unter <a href="https://media.ccc.de/c/fossgis2018">https://media.ccc.de/c/fossgis2018</a>.</div>
-
-          <p>Programmübersicht:</p>
-          <div style="margin: 5px 85px 7px 160px">
-            <ul>
-              <li>
-                <a href="day1.php">Mittwoch, 21.03.2018</a>
-              </li>
-
-              <li>
-                <a href="day2.php">Donnerstag, 22.03.2018</a>
-              </li>
-
-              <li>
-                <a href="day3.php">Freitag, 23.03.2018</a>
-              </li>
-	          <li>
-                <a href="day4.php">OSM Samstag, 24.03.2018</a>
-              </li>
-	          <li>
-		        <a href="/2018/programm/referenten.php">Referenten</a>
-	          </li>
-            </ul>
-          </div>
-
-          
-	</div>
-	<footer>Website by <a href="https://oliverroick.de/">Oliver Roick</a> &bull; <a href="https://www.fossgis.de/impressum/">Impressum</a> &bull;  <a href="https://www.fossgis-konferenz.de/2018/agb.php">AGB</a> &bull; <a href="https://www.fossgis.de/datenschutzerklärung">Datenschutzerklärung</a></footer>
-      </div>
+                <h2>Programm zur Konferenz 2016</h2>
+                <p>Wählen Sie den gewünschten Konferenztag aus.</p>
+                <div style="margin: 5px 85px 7px 160px">
+                    <ul>
+                        <li>
+                            <a href="/2016/programm/day1.php">OSM Sonntag, 03.07.2016</a>
+                        </li>
+                        <li>
+                            <a href="/2016/programm/day2.php">Montag, 04.07.2016</a>
+                        </li>
+                        <li>
+                            <a href="/2016/programm/day3.php">Dienstag, 05.07.2016</a>
+                        </li>
+                        <li>
+                            <a href="/2016/programm/day4.php">Mittwoch, 06.07.2016</a>
+                        </li>
+                    </ul>
+                </div>
+				<p>Hier können Sie das Programm als PDF downloaden:</p>
+				<div style="margin: 5px 85px 7px 160px">
+                    <ul>
+                        <li>
+                            <a href="Programm_FOSSGIS-2016.pdf">FOSSGIS-Konferenz-Programm PDF</a>
+                        </li>
+                    </ul>
+                </div>
+				<p>Die Vorträge der FOSSGIS-Konferenz wurden aufgezeichnet, eine Übersicht finden Sie hier:</p>
+				<div style="margin: 5px 85px 7px 160px">
+                    <ul>
+                        <li>
+                            <a href="/2016/videos/">Liste der Videoaufzeichnungen</a>.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <footer>Website by <a href="https://oliverroick.de/">Oliver Roick</a> &bull; <a href="https://www.fossgis.de/impressum/">Impressum</a> &bull; <a href="https://www.fossgis.de/datenschutzerklärung">Datenschutzerklärung</a></footer>
+        </div>
     </body>
 </html>
