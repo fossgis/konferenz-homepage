37a6d57ccfc655b0fc453470e776f1a75436fa46
omYoga <92447781+omYoga@users.noreply.github.com>
2021-11-04 13:07:07 +0100

    Text Update

--- 2022/standort/index.php
+++ 2022/standort/index.php
@@ -29,23 +29,29 @@
 		<div id="map" class="map"></div>
 	</div>-->
 
-	<p> Die dUniversitÃ¤tsstadt Marburg befindet sich in unmittelbarer Nachbarschaft zur UniversitÃ¤tsstadt GieÃŸen, 
-	quasi etwa in der Mitte zwischen Frankfurt/M und Kassel. <br>Seiner besonderen beidseitigen Lage an der Lahn gelegen, 
-	setzt sich Marburg aus der Kernstadt sowie 18 Stadtteilen zusammen.</p>
-	<p> Marburg bietet vieles an SehenswÃ¼rdigkeiten, Kunst, Kultur und Natur.
-	Die drei interessanten Wahrzeichen von Marburg sind das Marburger Schloss, das Landgrafenschloss und die Elisabethkirche.
-	</p>
+  <p>
+   Die Konferenz findet in den Hörsälen und Seminarräumen rund um die Zentralverwaltung, Forschungszentrum Deutscher Sprachatlas und das Campus Firmanei statt. Der zentrale Empfang wird in    der H&ouml;rsaalgebäude Biegenstrasse 10 aufgebaut. Die Zugänge zu den Räumlichkeiten sind behindertengerecht gestaltet. Sollten Sie trotzdem Probleme beim Erreichen der Räume haben, zögern    Sie bitte nicht das Konferenz-Team anzusprechen.
+  </p>
 
 	
-	<h2>AusflugsmÃ¶glichkeiten & SehenswÃ¼rdigkeiten</h2>
-	<p>Ein beliebtes Ausflugsziel mit CafÃ© und Kultur ist die â€Spiegelslustâ€œ Turm.</p>
+	<h2>Ausflugsm&uuml;glichkeiten & Sehensw&uuml;rdigkeiten</h2>
+	  
+	<p> Die Universit&auml;tsstadt Marburg befindet sich in unmittelbarer Nachbarschaft zur Universit&auml;tsstadt GieÃŸen, 
+	quasi etwa in der Mitte zwischen Frankfurt/M und Kassel. <br>Seiner besonderen beidseitigen Lage an der Lahn gelegen, 
+	setzt sich Marburg aus der Kernstadt sowie 18 Stadtteilen zusammen.</p>
+	<p> Marburg bietet vieles an Sehensw&uuml;rdigkeiten, Kunst, Kultur und Natur.</p>
+	Die drei interessanten Wahrzeichen von Marburg sind das Marburger Schloss das Landgrafenschloss und die Elisabethkirche.</p>
+	 <img src="../B-01HSfront.jpg" alt="Girl in a jacket" width="500" height="600"> 
+	Für mehr Informationen stellt die Stadt eine umfangreiche und übersichtliche <a href="https://www.marburg.de/">Website</a> für Besucher , die   Ausflugsziele in der Umgebung vorstellt, Informationen über die Stadt    zusammenfasst und die Planung von der Unterkunft bis zur Stadtführung bündelt.
+	</p>
+	<p>Ein beliebtes Ausflugsziel mit CafÃ© und Kultur ist die Spiegelslust Turm.</p>
 	
 	<h2>Die Lokalen Nahverkehrsorganisationen </h2>
 	<p>Der Stadtverkehr in Marburg bittet 19 Buslinien, dazu kommen die Anruf-Sammel-Taxen  
 	<a href="https://www.stadtwerke-marburg.de/verkehr/bus-bahn/anruf-sammeltaxi-ast">l(AST)</a>
 	</p>
 	<p>
-	ErgÃ¤nzt wird dieses Verkehrsangebot durch weitere Linien des Busverkehrs des Regionalen Nahverkehrsverbandes 
+	Erg&auml;nzt wird dieses Verkehrsangebot durch weitere Linien des Busverkehrs des Regionalen Nahverkehrsverbandes 
 	Marburg-Biedenkopf und des Rhein-Main-Verkehrsverbundes.
 
 
