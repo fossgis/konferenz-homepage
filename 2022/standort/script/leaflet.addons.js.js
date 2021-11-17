59b89973b1dd49952d75c434660f36fe7e9b560b
Spaska Forteva <spaska.forteva@geo.uni-marburg.de>
2021-11-17 15:47:21 +0100

    umlaute korrigieren

--- 2022/standort/script/leaflet.addons.js.js
+++ 2022/standort/script/leaflet.addons.js.js
@@ -48,7 +48,7 @@
             position: "bottomleft",
             growSize: "90vh",
             growText: "???",
-            growTitle: "Karte vergrößern",
+            growTitle: "Karte vergr&ouml;&szlig;ern",
             shrinkSize: "40vh",
             shrinkText: "???",
             shrinkTitle: "Karte verkleinern"
