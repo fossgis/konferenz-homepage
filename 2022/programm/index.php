0cbe510b459c1515153f9b0e7f7e67865eb1ce35
Katja Haferkorn <katjahafi@gmx.de>
2021-11-23 09:58:36 +0100

    CV online

---
+++ 2022/programm/index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2022 - Programm</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="programm">
+	<?php include "../inc/header.inc"; ?>
+
+        <h2>Programm</h2>
+		<p>Der Call for Participation ist beendet. Dass Programmkomitee gestaltet im Laufe der nächsten Wochen aus den Einreichungen ein Konferenzprogramm. Dieses wird ab Anfang Januar hier veröffentlicht.</p>
+		<p>Hier ist der <a href="https://fossgis-konferenz.de/2022/callforpapers/">Call for Participation</a> zum nachlesen verlinkt.</p>
+
+	<!--	<a href="https://pretalx.com/fossgis2021/schedule/" target="_blank" rel="noopener nofollow">hier geht es zum Konferenzprogramm</a>
+	</p>
+
+	<!-- <p>Wählen Sie den gewünschten Konferenztag aus.</p>
+	<ul>
+		<li>
+			<a href="programm/sonntag.php" target="_blank" rel="noopener nofollow">OSM-Sonntag, 06.06.2021</a>
+                </li>
+		<li>
+			 href-old "https://pretalx.com/fossgis2021/schedule/" 
+			<a href="programm/montag.php">Montag, 07.06.2021</a>
+		</li>
+		<li>
+			<a href="programm/dienstag.php">Dienstag, 08.06.2021</a>
+		</li>
+		<li>
+			<a href="programm/mittwoch.php">Mittwoch, 09.06.2021</a>
+		</li>
+        </ul> 
+
+	<p>
+		<a href="https://pretalx.com/fossgis2021/speaker/" target="_blank" rel="noopener nofollow">Liste der Vortragenden</a>
+	</p>
+
+	<h3>Tagungsband</h3>
+	<p>Der Tagungsband zur Konferenz steht <a href="data/FOSSGIS_Tagungsband_2021.pdf">als PDF zum Download </a> zur Verfügung.
+
+	<!-- 	<h3>FOSSGIS-Konferenz im "gewohnten" Livestream</h3>
+	<p>Die FOSSGIS 2021 wird auch im gewohnten Livestream übertragen: <a href="https://streaming.media.ccc.de/fossgis2021/"> https://streaming.media.ccc.de/fossgis2021/</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink im Bestelllink.</p>
+
+	<h3>Vortragsaufzeichnungen</h3>
+	<p>Die Vorträge und Demos sind aufgezeichnet und veröffentlicht:</p>
+		<ul>
+			<li> <a href="https://media.ccc.de/c/fossgis2021" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2021</a></p> 
+			</li>
+			<li> <a href="https://av.tib.eu/series/1102/fossgis+konferenz+2021+online+event+06+09+juni+2021" target="_blank" rel="noopener nofollow">TIB</a></p> 
+			</li>
+			<li> <a href="https://www.youtube.com/results?search_query=FOSSGIS+2021" target="_blank" rel="noopener nofollow">YouTube</a></p> 
+			</li>
+		</ul>
+
+	<h3>FOSSGIS-Konferenz per Android-APP</h3>
+		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">zum Download im Playstore</a> bereit. </p>
+	<!--<p>Ansicht der App für Smartphone:	
+                    <a href="https://www.fossgis.de/">
+                      	<img src="./data/FOSSGIS-2021-Phones.png" class="img-fluid"  alt="APP">
+                    </a>
+                 </p>
+		<p>Ansicht der App für Tablet:	
+                    <a href="https://www.fossgis.de/">
+                         <img src="./data/FOSSGIS-2021-Tablet-Phone.jpg" class="img-fluid" alt="APP">
+					</a>
+                 </p>-->
+
+
+
+	<h3>Community Voting</h3>
+	<p>Bis zum 3.12.2021 ist es für die Community möglich über die Einreichungen abzustimmen. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.  <br>Vielen für's mitmachen!</p>
+	<p><b>Vorgehensweise:</b></p>
+	<ul>
+		<li>1. Den Link zur Öffentlichen Abstimmung klicken, um sich dafür anzumelden: <a href="https://pretalx.com/fossgis2022/p/voting/signup/"> https://pretalx.com/fossgis2022/p/voting/signup/</a></li>
+		<li>2. Sie erhalten eine E-Mail mit Ihrem persönlichen Link zur Öffentlichen Abstimmung</li>
+		<li>3. Einreichungen anschauen und bewerten- dies kann in Etappen erfolgen.</li>
+	</ul>
+
+
+
+
+
+		
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+
+</html>
