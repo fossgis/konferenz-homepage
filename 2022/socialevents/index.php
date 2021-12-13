6e3476809dd4b0379a63be87da88a15b531fbc8a
Katja Haferkorn <katjahafi@gmx.de>
2021-12-13 13:37:10 +0100

    Texte auf Konferenzhomepage an Online-Veranstaltung angepasst

---
+++ 2022/socialevents/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Rahmenprogramm - Social Events</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="socialEvents">
+
+	<?php include "../inc/header.inc"; ?>
+
+	<h2>Rahmenprogramm und Social Events</h2>
+
+	<h4>Inoffizieller Start</h4>
+
+	<p>Am Abend des 08. März laden wir Sie ein zum virtuellen "Warmup in der Netzwerkwelt". Nutzen Sie die Möglichkeit sich in der Onlineplattform umzuschauen und in der Netzwerkwelt andere Teilnehmende zu treffen. Bei Fragen finden Sie Hilfe am InfoDesk (Personen mit rotem Hut sind Helfer der FOSSGIS-Konferenz). Der Zugang zur Online-Plattform wird ab 07. März 2022 in Ihrem Ticketbestelllink zur Verfügung gestellt. </p>
+	
+	<h4>Abendveranstaltung </h4>
+	<p>Traditionell findet am ersten Abend der FOSSGIS-Konferenz die Abendveranstaltung statt. Auch wenn die FOSSGIS im Jahr 2022 ausschließlich online stattfindet, soll es für alle Teilnehmenden die Möglichkeit geben sich bei einer Abendveranstaltung zu treffen. </p>
+
+	<p>Schauen Sie gerne am 09. März von 20 -23 Uhr bei der Abendveranstaltung "Netzwerkweltdialoge" rein, vielleicht treffen Sie dort auch andere Teilnehmende.</p>
+
+	<!--<h4>Jeopardy-Quiz</h4>
+	<p>Am <a href="https://fossgis-konferenz.de/2021/sessions/DSXKQN.php">Montag</a> und <a href="https://fossgis-konferenz.de/2021/sessions/AWSQZ7.php">Dienstag</a> gibt es nach dem regulären Konferenzprogramm um 17:30 Uhr ein humorvolles Quiz in Form des FOSSGIS-Jeopardy. Zum generellen Ablauf sowie zur Anmeldung als Teilnehmerin oder Teilnehmer finden sich <a href="https://fossgis-konferenz.de/2021/sessions/DSXKQN.php">Details im Programm</a>.
+
+	<!-- <p>Live-Acts oder musikalische Beiträge sind im Partyraum der Netzwerkwelt möglich. Wer etwas beitragen möchte, meldet sich beim <a href="mailto:konferenz-orga@fossgis.de"> Konferenzorgateam</a>.</p>
+		
+	<h4>Diskussionspanel</h4>
+	<p>Am Dienstagabend wird ein Diskussionspanel stattfinden. Das Thema wird im Programm bekannt gegeben</p>-->
+
+	<h4>Gruppenfoto</h4>
+	<p>Auch in diesem Jahr wollen wir uns das Gruppenfoto nicht entgehen lassen und überlegen uns noch etwas Schönes dazu. 
+<!--laden ein am Dienstag, den 08. Juni um 15.30 Uhr beim Gruppenfoto in der Netzwerkwelt auf der Fotowiese auf dem virtuellen Campus dabei zu sein. </p>
+	
+	<p><img src="./socialevents/img/Fotowiese_Netzwerkwelt.png" class="img-fluid" alt="Fotowiese Netzwerkwelt">-->
+	</p>
+	
+	<h4>Anwendertreffen</h4>
+	<p>Es können Anwendertreffen, Sprints und BOFs selber organisiert werden. Räume stehen virtuell zur Verfügung, bitte <a href="mailto:pko@fossgis.de">per E-Mail</a> beim Programmkomitee anfragen, damit Beitrag im Programm veröffentlicht wird. </p>
+
+	<h4>OSM-Event</h4>
+
+	<p>Am Samstag, den 12. März findet von 10 bis 16 Uhr die beliebte OSM-Unconference als Online-Veranstaltung statt. Interessierte sind eingeladen daran teilzunehmen. Die Veranstaltung ist kostenfrei. Für das leibliche Wohl muss jeder selbst sorgen. Die Themensammlung erfolgt im <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2022/OSM-Samstag">OSM-Wiki</a>. Wir bitten um <a href="https://fossgis-konferenz.de/2021/anmeldung/">Anmeldung</a> (ab Anfang Januar möglich).</p>
+	
+	
+
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+</html>
