adec8d7061c3dde5ba330a6db1934db8e0afa394
Katja Haferkorn <katjahafi@gmx.de>
2021-12-21 15:03:54 +0100

    Unterseite Sponsor BGK

---
+++ 2022/sponsor/101_bkg.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2022 - Sponsor Bundesamt für Kartographie und Geodäsie (BKG)</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2022">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+			
+			<img src="./img/s/101_BKG_Logo_RGB.png" 
+				class="sponsor-logo" 
+				alt="Firmenlogo">
+			
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>Bundesamt für Kartographie und Geodäsie (BKG)</h3>
+
+					<p>Text folgt</p>
+				</div>
+				<div class="col-lg-3">
+					<div class="mb-4">
+						<h6 class="mb-0">Kontakt<br>
+							Webseite:<br>
+						<a href="https://www.bkg.bund.de">
+							https://www.bkg.bund.de</a><br>
+
+							E-Mail: <br>
+							<a href="mailto:VorzimmerPraesident@bkg.bund.de">VorzimmerPraesident@bkg.bund.de</a><br></h6>
+					</div>
+
+						
+				
+					<div class="social-icon">
+						<h6 class="font-weight-bold mb-0">Social</h6>
+						<ul>
+							<li>
+								<a href="https://twitter.com/BKG_Bund"><i class="fab fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com/company/bkg-bund"><i class="fab fa-linkedin"></i></a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/bkg.bund"><i class="fab fa-instagram"></i></a>
+							</li>
+							<li>
+								<a href="https://www.xing.com/companies/bundesamtf%C3%BCrkartographieundgeod%C3%A4sie"><i class="fab fa-xing"></i></a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/user/pressebkg"><i class="fab fa-youtube"></i></a>
+							</li>
+
+						</ul>
+					</div>
+				</div>	
+
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
