34e0e961b400358ebc18b772a522ffdac6517a47
Katja Haferkorn <katjahafi@gmx.de>
2022-02-22 11:15:13 +0100

    Jobbörse angelegt

---
+++ 2022/jobs.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "inc/meta.inc"; ?>
+
+	<title>FOSSGIS 2022 - Jobbörse</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+    <h3>Jobbörse im Rahmen der FOSSGIS-Konferenz 2022</h3>
+					<p>Die Jobbörse wird im Rahmen der FOSSGIS-Konferenz, die vom 09. - 12. März 2022 stattfindet, erstellt. Es erfolgt keine Fortschreibung</p>
+
+	<ul>
+						<li>
+							<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7372161" rel="noopener" target="_blank">
+						Software-Entwickler*in (m/w/d) in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid.
+
+
+							</a>
+							<br>(Firma WhereGroup)
+						</li>
+						<br>
+						<li>
+							<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7376027" rel="noopener" target="_blank">
+								GIS-Spezialist*in (m/w/d) in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid.
+
+
+							</a>
+							<br>(Firma WhereGroup)
+						</li>
+						<br>
+						
+						<li>
+							<a href="" rel="noopener" target="_blank">		Jobbezeichnung + Ort
+							</a>
+							<br>Firma
+						</li>
+						<br>
+						
+						<li>
+							<a href="" rel="noopener" target="_blank">		Jobbezeichnung + Ort
+							</a>
+							<br>Firma
+						</li>
+						<br>
+						
+						
+					</ul>
+
+
+<h3>Jobankündigung einreichen:</h3>
+						<p>Senden Sie eine E-Mail an <a href="mailto:konferenz-orga@fossgis.de">konferenz-orga@fossgis.de</a>. <p>
+
+
+
+
+	<?php include "inc/footer.inc"; ?>
+
+</body>
+
+</html>
