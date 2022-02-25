43f1060f098367fe9285aa3d1613b8f5f799c021
Katja Haferkorn <katjahafi@gmx.de>
2022-02-25 17:18:46 +0100

    Jobs hinzugefügt

--- 2022/jobs.php
+++ 2022/jobs.php
@@ -16,39 +16,55 @@
 	<?php include "inc/header.inc"; ?>
 
     <h3 id="Jobbörse" name="Jobbörse">Jobbörse im Rahmen der FOSSGIS-Konferenz 2022</h3>
-					<p>Die Jobbörse wird im Rahmen der FOSSGIS-Konferenz, die vom 09. - 12. März 2022 stattfindet, erstellt. Es erfolgt keine Fortschreibung</p>
+					<p>Die Jobbörse wird im Rahmen der FOSSGIS-Konferenz, die vom 09. - 12. März 2022 stattfindet, erstellt. Es erfolgt keine Fortschreibung nach der Konferenz.</p>
+	<h4>WhereGroup GmbH</h4>
+		<ul>
+			<li>
+			<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7372161" rel="noopener" target="_blank">
+			Software-Entwickler*in (m/w/d)</a> in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid</li>
+			<li>
+			<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7376027" rel="noopener" target="_blank">
+			GIS-Spezialist*in (m/w/d)</a> in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid</li>
+			</ul>		
 
-	<ul>
-						<li>
-							<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7372161" rel="noopener" target="_blank">
-						Software-Entwickler*in (m/w/d)</a> in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid							
-							<br>(Firma WhereGroup)
-						</li>
-						<br>
-						<li>
-							<a href="https://jobs.maxime-media.de/wheregroup-gmbh/7376027" rel="noopener" target="_blank">
-								GIS-Spezialist*in (m/w/d)</a> in Bonn/Berlin/Freiburg/Einsatzort Hamburg oder Hybrid
-							<br>(Firma WhereGroup)
-						</li>
-						<br>
-						
-						<li>
-							<a href="" rel="noopener" target="_blank">		Jobbezeichnung</a> + Ort
-							
-							<br>Firma
-						</li>
-						<br>
-						
-						<li>
-							<a href="" rel="noopener" target="_blank">		Jobbezeichnung</a> + Ort
-							
-							<br>Firma
-						</li>
-						<br>
-						
-						
-					</ul>
+			
+	<h4>komoot</h4>	
+		<ul>
+			<li>
+			<a href="https://www.komoot.de/jobs/EBFDAAA6FF/senior-android-developer" rel="noopener" target="_blank"> Senior Android Developer</a> remote</li>
+			<li>
+			<a href="https://www.komoot.de/jobs/DCC51EA91D/senior-web-developer-node-reactjs" rel="noopener" target="_blank">Senior Web Developer - Node/ReactJS</a> </li>
+			<li>
+			<a href="https://www.komoot.de/jobs/B5A205E15D/senior-ios-developer" rel="noopener" target="_blank">Senior iOS Developer</a></li>
+			<li>
+			<a href="https://www.komoot.de/jobs/74ACE8EE0E/senior-backend-engineer" rel="noopener" target="_blank">Senior Backend Engineer</a>
+		</ul>
 
+	<h4>VSB Neue Energien Osnabrück</h4>	
+		<ul>
+			<li>
+			<a href="https://www.vsb.energy/de/de/karriere/stellenangebote/GIS-Analyst-Geoinformatiker-mwd-de-j447.html " rel="noopener" target="_blank">GIS ANALYST / GEOINFORMATIKER (M/W/D) </a></li>
+			<li>
+			<a href="https://www.vsb.energy/de/de/karriere/stellenangebote/Geograf-Projektentwickler-mwd-de-j425.html" rel="noopener" target="_blank">GEOGRAF / PROJEKTENTWICKLER (M/W/D)</a></li>
+			<li>
+			<a href="https://www.vsb.energy/de/de/karriere/stellenangebote/Projektentwickler-Planung-Geografmwd-de-j482.html" rel="noopener" target="_blank">PROJEKTENTWICKLER PLANUNG / GEOGRAF (M/W/D)</a></li>
+			<li>
+			<a href="https://www.vsb.energy/de/de/karriere/stellenangebote/Projektingenieur-Bauingenieur-Geotechniker-mwd-de-j459.html" rel="noopener" target="_blank">PROJEKTINGENIEUR / BAUINGENIEUR / GEOTECHNIKER (M/W/D)</a></li>
+			<li>
+			<a href="https://www.vsb.energy/de/de/karriere/stellenangebote/Werkstudent-Projektentwicklung-mwd-de-j484.html" rel="noopener" target="_blank">WERKSTUDENT PROJEKTENTWICKLUNG (M/W/D) </a></li>	
+		</ul>
+
+
+<!--	<h4>Firma</h4>	
+		<ul>
+			<li>
+			<a href="" rel="noopener" target="_blank">		Jobbezeichnung</a> + Ort
+			</li>
+			<li>
+			<a href="" rel="noopener" target="_blank">		Jobbezeichnung</a> + Ort
+			</li>
+		</ul>-->
+<br>
 
 <h3>Jobankündigung einreichen:</h3>
 						<p>Senden Sie eine E-Mail an <a href="mailto:konferenz-orga@fossgis.de">konferenz-orga@fossgis.de</a>. <p>
