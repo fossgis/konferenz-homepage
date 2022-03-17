7fb38044a1080552b56a4e01e6c9e744e8afc230
Katja Haferkorn <katjahafi@gmx.de>
2022-03-17 18:46:58 +0100

    Webseite 2023 angelegt

---
+++ 2023/helfen/index.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2022 - Helfen bei der Konferenz</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2021/widget/v1.css">
+	<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+</head>
+
+
+
+<body id="helfen">
+
+	<?php include "../inc/header.inc"; ?>
+
+	<h2>Unterstützung der Konferenzorganisation</h2>
+
+	<p>Seit vielen Jahren findet die FOSSGIS-Konferenz im deutschsprachigen Raum an wechselnden Standorten statt.<br>
+Ohne die Unterstützung durch zahlreiche Mitorganisierende vor der Veranstaltung und Helfer:innen während der Veranstaltung wäre eine FOSSGIS-Konferenz nicht möglich.<br>
+	<p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen. Dies ermöglicht eine flexible Konferenzorganisation und vielfältiges Rahmenprogramm. Die Aufgaben werden auf mehrere Schultern verteilt.</p>
+
+	<p><b>Damit dies so bleiben kann, brauchen wir Deine Unterstützung!<sup>*</sup></b><br>
+		<span style="color:grey">* wer in der Community aktiv ist, wird geduzt :-)
+         </span></p>
+	
+	<p>Auf dieser Seite kommen hin und wieder neue Informationen hinzu, also gerne wiederkommen.</p>
+
+	<h3>Wo kannst Du bei der Konferenzorganisation unterstützen?</h3>
+	<ul>
+		<li><b>Programmkomitee</b><br>
+Call for Participation formulieren, Einreichungen reviewen, Programm gestalten, bei Umsetzung des Programms unterstützen.<br>
+2023: das Programmkomitee konstituiert sich im Sommer.</li>
+		<li><b>Presse- und Öffentlichkeitsarbeit</b><br>
+Pressetexte schreiben, Konferenz bekannter machen, raus aus der Blase, neue Menschen erreichen. Teilnehmende informieren.</li>
+		<li><b>Helfer:innenkoordination</b><br>
+Engelsystem, Überblick, Helfer:innen anwerben, Jobbeschreibungen, Job-Schichten koordinieren - genau das hier ^ gut zu organisieren ;-)</li>
+		<li><b>Technische Umsetzung</b><br>
+Unterstützung der technischen Umsetzung der Konferenz in Zusammenarbeit mit dem VOC, u.a. vorab aufgezeichnete Vorträge einsammeln und zur Verfügung stellen, Mails aus Pretalx versenden.</li>
+		<li><b>Workshoporganisation</b><br>
+Online oder Vor-Ort-Workshop, was braucht es für die gute Umsetzung, Kommunikation mit Workshopleiter:innen, Information der TN.</li>
+		<li><b>OSM-Event</b><br>
+Das OSM-Event organisieren (Unconference, Mappingevent...) und moderieren.</li>
+		<li><b>Programmheft</b><br>
+Programm als gedruckte Ausgabe im Format A6 für die TN.</li>
+		<li><b>Teilnehmer:innenbetreuung/ Welcome Desk</b><br>
+Im Arbeitspaket Teilnehmer:innenbetreuung stecken mehrere Aufgaben.
+Die Informationen zur Konferenz im Vorfeld so aufbereiten, dass es für die TN übersichtlich und nachvollziehbar ist (Website, TN-Mail).
+Welcome Desk organisieren ist in erster Linie eine Aufgabe des Lokal Teams.
+Einen Code of Conduct zu formulieren, steht schon lange auf der Agenda (Beispiel: https://2021.foss4g.org/about/coc.html).</li>
+		<li><b>Statistik und Teilnehmer:innenbefragung</b><br>
+Onlinebefragung + Auswertung, Befragung zu den Workshops auf den Weg bringen.</li>
+		<li><b>Hygienekonzept</b><br>
+Umsetzungskonzept für die Veranstaltung vor Ort sowie Informationen an Teilnehmende im Vorfeld der Konferenz (Website, TN-Mail).</li>
+		<li><b>Bildungsurlaub</b><br>
+Die Konferenz als Bildungsurlaub zu bewerben, kann es für viele Arbeitnehmer:innen attraktiver machen. In 2021 gab es drei positive Bescheide (BER, BB, RP, Zusage von RP gilt zwei Jahre). Es muss für alle Bundesländer ein Antrag gestellt werden. Im ersten Schritt die kostenfreien, bei entsprechendem Bedarf können auch die kostenpflichtigen beantragt werden.
+TN bekommen im Ticketsystem die Frage, ob sie Bildungsurlaub in Anspruch nehmen möchten, es wird nach Bundesland gefragt. Am Ende muss es gegenüber dem genehmigenden Bundesland abgerechnet werden.</li>
+	</ul>
+	<p>Im Wikihandbuch ist die Organisation der Konferenz einmal komplett beschrieben: <a href="https://www.fossgis.de/wiki/Handbuch">https://www.fossgis.de/wiki/Handbuch</a>
+
+
+
+	<p>Es gibt verschiedene Helfer:innenaufgaben, die während der Konferenz zu erledigen sind. Die Schichten werden über das <!--<a href="https://helfer.fossgis.de/">Helfersystem</a>-->Helfersystem gemanagt. Die Aufgaben tauchen hier auf dieser Seite auf, wenn sie für die nächste Veranstaltung formuliert sind.</p>
+
+	<!--
+
+
+<h3>Wo kannst Du bei der Durchführung der Konferenz unterstützen?</h3>
+	<p>Es gibt zwei Helfer:innenarten.</p>
+	
+	<ul>
+		<li><b>Freiwillige Helfer </b>(ab 6h helfen gibt es ein kostenfreies Helfer:innenticket)<br>
+Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen.</li>
+		<li><b>Bezahlte Helfer </b>(Aufwandsentschädigung über FOSSGIS e.V.)<br>
+Für Aufgaben, die während der Umsetzung unbedingt gemacht werden müssen, steht ein Budget zur Aufwandsentschädigung bereit.</li>
+	</ul><h4>Helfen vor Ort</h4>
+	<ul>
+		<li><b>Sessionleitung</b> (freiwillig)<br>
+Die Sessionleitung moderiert den jeweiligen Track, kündigt Vortrag und Vortragende an und gibt organisatorische Informationen an die Teilnehmenden. Sessionleitung ist vor Ort und moderiert aus dem Hörsaal.
+			<ul><i>Aufgaben:</i>
+				<li>Anmoderation des Tracks</li>
+				<li>mitlesen des Online-Chats während des Vortrags</li>
+				<li>relevante Fragen sammeln, dabei eventuell vorhandenes Voting der TN beachten</li>
+				<li>ausgewählte Fragen an Vortragende stellen.</li>
+				<li>(Matrix-Chat im Blick behalten )</li>
+				<i>technische Ausstattung für Online-Sessionleitung:</i> Laptop, Browser, Headset mit Mikro, Kamera, stabiles Internet<br>
+				<i>technische Aussattung Sessionleitung im Hörsaal:</i> Mikrophone, Zugang zur Onlineplattform Venueless
+			</ul>				
+		</li>
+
+		<li><b>Welcome Desk</b> (bezahlt + gerne freiwillig)<br>
+Der Welcome Desk ist die Servicestelle für alle Teilnehmer:innen. Beschreibung im Handbuch: https://www.fossgis.de/wiki/Handbuch#Ablauf_und_Organisation_des_Welcome_Desk
+Hier läuft der Ticket-Checkin, Taschen, T-Shirts und Tagungsbände werden herausgegeben.
+			<ul><i>Aufgaben:</i>
+				<li>Informationen für Teilnehmende</li>
+				<li>Checkin Teilnehmende - Ticket, Zertifikate sowie Ausgabe der Tasche, Namensschild, T-Shirt, Tagungsband</li>
+				<li>Prüfung Workshopbuchung</li>
+				<li>Verkauf von Tickets für Konferenz oder Workshop (Handkasse, Belege)</li>
+				<li>ggf. Verkauf von T-Shirts, Tagungsbänden, usw.</li>
+				<li>Garderobe</li>
+				<li>Desinfektion im Raum nach Hygienekonzept</li>
+			</ul>	
+		</li>
+		<li><b>Catering</b> (freiwillig oder bezahlt)<br>
+Die Pausenversorgung für die Konferenzteilnehmenden ist wichtig und wird von einem Caterer erledigt. Da Caterer erfahrungsgemäß zu wenig Personal dabei haben, stellt die Konferenz Helfer:innen beim Catering zur Verfügung.
+			<ul><i>Aufgaben:</i>
+				<li>Lieferungen entgegennehmen</li>
+				<li>Getränke vom Lager holen und ausreichend bereit halten</li>
+				<li>Tische vorbereiten und Angebot anrichten</li>
+				<li>falls nicht vom Caterer: Obst und Gemüse waschen und schnippeln</li>
+				<li>Geschirr wegräumen</li>
+				<li>Desinfektion im Raum nach Hygienekonzept</li>
+			</ul>	
+		</li>
+
+		<li><b>Hörsaalhelfer</b> (bezahlt)<br>
+In jedem Hörsaal gibt es eine verantwortliche Person.
+			<ul><i>Aufgaben:</i>
+				<li>Technik betreuen (im Vorfeld mit der Technik vertraut machen (Licht, Jalousien, Audio- und Videoanlage, Rechner)</li>
+				<li>morgens an jede Tür ein aktuelles Tagesprogramm hängen</li>
+				<li>Unterstützung der Vortragenden beim Laden ihrer Präsentationen auf den Präsentationsrechner</li>
+				<li>Kontrolle, ob Unterschrift zur Einverständniserklärung zur Aufzeichnung des Vortrages vorliegt. Wenn nicht, einholen</li>
+				<li>Versorgung der Vortragenden mit Wasser</li>
+				<li>Koordinierung der Platzierung der TN, damit nicht alle außen sitzen</li>
+				<li>Bei Fragen aus dem Publikum das Mikrofon herumtragen</li>
+				<li>Vortrag als PDF auf Laptop im Raum speichern</li>
+				<li>Desinfektion im Raum nach Hygienekonzept</li>
+				
+			</ul>	
+		</li>
+		<li><b>Workshophelfer</b> (bezahlt)<br>
+In jedem Workshopraum gibt es eine verantwortliche Person.
+			<ul><i>Aufgaben:</i>
+				<li>Technik betreuen (im Vorfeld mit der Technik vertraut machen (Licht, Jalousien, Audio- und Videoanlage, Rechner)</li>
+				<li>morgens an jede Tür ein aktuelles Tagesprogramm hängen</li>
+				<li>Versorgung der Workshopleiter mit Wasser</li>
+				<li>Kontrolle/Checkin Worksshop-Teilnehmende</li>
+				<li>Feedbackbögen einsammeln</li>
+				<li>Desinfektion im Raum nach Hygienekonzept</li>
+			</ul>
+	</ul>
+
+	<h4>Helfen die Konferenz Online umzusetzen</h4>
+	<ul>
+
+		<li><b>Videoschnitt</b> (freiwillig)<br>
+			Viele Vortragende stellen ihr Video vorab aufgezeichnet über einen Webserver zur Verfügung. 
+			<ul><i>Aufgaben:</i>
+				<li>Video öffnen und checken, was zu tun ist.</li>
+				<li>Prüfen, ob VortragsID und Vortragender und Vortragstitel zusammenpassen (siehe md-File).</li>
+				<li>prüfen, ob Ton, Bild und Folien da sind.</li>
+				<li>prüfen, ob Vortrag die angegebene Länge nicht überschreitet.</li>
+				<li>Video schneiden (Anfang und Ende passend machen, falls es nicht passt)  mit LosslessCut (https://www.mifi.no/losslesscut/) schneiden, im Dateiformat: matroshka speichern.</li>
+				<li>im beiliegenden md-File abhaken bzw. entsprechende Notizen hinterlegen.</li>
+				<li>wenn fertig, wird der Ordner nach "Fertig" verschoben.</li>
+				<li>wenn irgendetwas nicht passt, Nachricht schreiben.</li>
+			</ul>
+		
+				<li><b>Sessionleitung</b> (freiwillig)<br>
+Die Sessionleitung moderiert den jeweiligen Track, kündigt Vortrag und Vortragende an und gibt organisatorische Informationen an die Teilnehmenden. Sessionleitung ist im Videoraum mit den jeweils Vortragenden. Darüber hinaus übernimmt die Sessionleitung den technischen Checkin der Vortragenden.
+			<ul><i>Aufgaben:</i>
+				<li>technischer Checkin der Vortragenden (Mikro, Audio, Folien wenn Livevortrag) 30 Minuten vor Trackbeginn</li>
+				<li>Anmoderation des Tracks</li>
+				<li>mitlesen des Online-Chats während des Vortrags</li>
+				<li>relevante Fragen sammeln, dabei eventuell vorhandenes Voting der TN beachten</li>
+				<li>ausgewählte Fragen an Vortragende stellen.</li>
+				<li>(Matrix-Chat im Blick behalten )</li>
+				<i>technische Ausstattung für Online-Sessionleitung:</i> Laptop, Browser, Headset mit Mikro, Kamera, stabiles Internet<br>
+			</ul>				
+		</li>
+	<!--	<li><b>Bühnenkoordination </b>(freiwillig)<br>
+Schnittstelle zw. OBS-Regie, Sessionleitung und Vortragenden für Umsetzung des Tracks, kann auch von der Sessionleitung gemacht werden; 
+ .</li>
+			<ul><i>Aufgaben:</i>
+				<li>techniche Ausstattung: Laptop, 2 Bildschirme, stabiles Internet</li>
+				<li>Technikcheck der Vortragenden und Sessionleitung 30 min vor Trackbeginn mit</li>
+				<li>Mikrophonetest</li>
+				<li>Videoqualität</li>
+				<li>Bildschirm teilen Foliencheck</li>
+			</ul>
+	
+			<li><b>OBS-Regie </b><br>
+Die Regie arbeitet mit Open Broadcast Studio Software (OBS) und mixt den Livestream. Es gibt pro Bühne eine Regie. Die Vortragenden kommen in einen Jitsiraum, für den Beitrag betreten Sie einen Breakout Raum, von dort wird gestreamt.</li>
+
+			<ul><i>Aufgaben: </i>
+				<li>Livemix über OBS Studio</li>
+				<li>Nach Zeitplan die vorab aufgezeichneten Videos oder Livevortrag einspielen</li>
+				<li>Fragen und Antworten-Session live einspielen</li>
+				<li>Pausentrailer mit Infobeamer</li>
+			</ul>
+			<ul><i>technische Ausstattung: </i>
+				<li>Desktop-PC mit OBS</li>
+				<li>2 Bildschirme, gute Internetanbindung LAN; DSL 50down/10up wäre zu knapp</li>
+				<li>RasperyPI, Video Capture Karte </li>
+				<li>Chats mitlesen</li>
+			</ul>	
+
+		<li><b>Info Desk </b>(freiwillig)<br>
+Die Online Teilnehmenden brauchen Informationen oder Hilfestellung? Helfer:innen am Info Desk checken den Chat der Onlineplattform und stehen in der Netzwerkwelt für Fragen zur Verfügung.</li>
+			<ul><i>Aufgaben:</i>
+				<li>Informationen weitergeben</li>
+				<li>Chats mitlesen und Fragen beantworten</li>
+				<li>InfoDesk in der Netzwerkwelt</li>
+				<li>InfoDesk im Venueless</li>
+			</ul>	
+
+
+	</ul>-->
+
+
+
+
+	
+
+	<h3>Schritte, die zu tun sind, um Helfer:in zu werden</h3>
+		<ul>
+		<li>diese Seite lesen</li> 
+		<li>bei Fragen einfach fragen, z.B. <a href="mailto:helfer@fossgis.de"> per E-Mail an das Helferteam</a> oder anrufen unter: +49 30-62932037</li> 
+	<!--	<li>dem Telegramkanal <i>Helfer:innen_FOSSGIS2022</i> beitreten: <a href="https://t.me/joinchat/IChw-K8-pB0yMjEy">https://t.me/joinchat/FOSSGIS2022</a></li> 
+		<li>ab Nov/Dez. im Helfersystem registrieren: <a href="https://helfer.fossgis.de">https://helfer.fossgis.de</a></li> -->
+
+	<?php include('../inc/footer.inc'); ?>
+
+</body>
+
+</html>
---
+++ 2023/index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html>
+<head>
+	<?php include "inc/meta.inc"; ?>
+<base href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+   	<title>FOSSGIS 2023</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+      <h1>Willkommen zur FOSSGIS-Konferenz 2023</h1>
+
+            <p>Die FOSSGIS-Konferenz ist im D-A-CH-Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme sowie für die Themen Open Data und OpenStreetMap.</p>
+
+       <!--     <p>Die FOSSGIS-Konferenz 2022 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a> und der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> mit Unterstützung der <a href="https://www.uni-marburg.de/de">Philipps Universität Marburg</a> organisiert.</p>
+
+            <p>Mitarbeitende der Fachbereiche <a href="https://www.uni-marburg.de/de/fb12">Mathematik und Informatik</a>, <a href="https://www.uni-marburg.de/de/fb19"> Geographie</a>, der <a href="">HeFDI - Hessische Forschungsdateninfrastrukturen</a>, des <a href="https://www.uni-marburg.de/de/fb09/dsa">Forschungszentrums Deutscher Sprachatlas <a/> sowie des <a href="https://hlgl.hessen.de/">Hessischen Landesamtes für geschichtliche Landeskunde</a> beteiligen sich maßgeblich an der Organisation.</p>
+
+			<p>Die FOSSGIS 2022 wird ausschließlich als Online-Veranstaltung stattfinden. Hintergrund der Entscheidung ist die unklare Entscheidungslage hinsichtlich der Coronapandemie und die damit einhergehende Unsicherheit in der Planung.</p> 
+
+
+            <ul class="tiles">
+				<li class="tile bronze">
+                     <a href="https://www.fossgis-konferenz.de/2022/sponsor/fossgis-ev.php">
+                         <img src="./img/FOSSGIS_RGB.png" alt="FOSSGIS e.V.">
+                     </a>
+                 </li>
+
+                 <li class="tile bronze">
+                     <a href="https://www.openstreetmap.de/">
+                         <img src="./img/osm.png" alt="OSM Community">
+                     </a>
+                 </li>                                   
+                 <li class="tile bronze">
+                     <a href="https://www.osgeo.org/">
+                         <img src="img/osgeo.png" width="180px" Alt="OSGeo">
+					 </a>
+				 </li>
+				<li class="tile bronze">
+					 <a href="https://www.uni-marburg.de/">
+						 <img src="./img/PhUniMa_Logo_4c.jpg" alt="Uni Marburg" width="180px">
+					 </a>
+                 </li>           
+            </ul>-->
+           
+
+            <h3>Was ist die FOSSGIS Konferenz?</h3>
+
+            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme. Hier treffen sich Anwender:innen und Entwickler:innen zum gemeinsamen Austausch über Anwendungs- und Arbeitsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement, GeoProcessing, Datenbanken und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.</p>	
+
+           <p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p>
+			<p>Weitere Informationen folgen.</p>
+
+		<!--	<h3 id="Sponsoren" name="Sponsoren">Sponsoren der FOSSGIS-Konferenz</h3>
+			<p> Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich.</p><br>
+
+			<h4 class="highlight">Platinsponsoren</h4>
+				<ul class="tiles">
+					<li class="tile platin">
+						<a href="sponsor/001_wheregroup.php" target="_blank">
+						<img src="./img/s/001_WhereGroup-zentriert.jpg" alt="WhereGroup">
+						</a>
+					</li>
+					<li class="tile platin">
+						<a href="sponsor/002_camp2camp.php" target="_blank">
+						<img src="./img/s/002_camptocamp_logo.png" alt="camptocamp">
+						</a>
+					</li>
+				</ul>	
+
+			<h4 class="highlight">Goldsponsoren</h4>
+				<ul class="tiles">
+					<li class="tile gold">
+						<a href="sponsor/101_bkg.php" target="_blank">
+						<img src="./img/s/101_BKG_Logo_RGB.png" alt="BKG">
+						</a>
+					</li>	
+				</ul>
+
+			<h4 class="highlight">Silbersponsoren</h4>
+				<ul class="tiles">
+					<li class="tile silver">
+						<a href="sponsor/201_terrestris.php" target="_blank">
+						<img src="./img/s/201_terrestris.png" alt="terrestris">
+						</a>
+					</li>	
+				</ul>	
+
+			<h4 class="highlight">Bronzesponsoren</h4>
+				<ul class="tiles">
+					<li class="tile bronze">
+						<a href="sponsor/401_opencage.php" target="_blank">
+						<img src="./img/s/401_opencage-logo.png" alt="OpenCage">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/402_wagner-it.php" target="_blank">
+						<img src="./img/s/402_Wagner-IT.png" alt="Wagner-IT">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/403_gbd.php" target="_blank">
+						<img src="./img/s/403_GBD.png" alt="Geoinformatikbüro Dassau">
+						</a>
+					</li>	
+					<li class="tile bronze">
+						<a href="sponsor/404_gkg.php" target="_blank">
+						<img src="./img/s/404_gkg_logo.png" alt="gkg">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/405_bemastergis.php" target="_blank">
+						<img src="./img/s/405_beMasterGIS.png" alt="beMasterGIS">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/406_komoot.php" target="_blank">
+						<img src="./img/s/406_komoot.png" alt="komoot">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/407_mundialis.php" target="_blank">
+						<img src="./img/s/407_mundialis.png" alt="mundialis">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/408_gis-ops.php" target="_blank">
+						<img src="./img/s/408_gis_ops_logo.png" alt="GIS • OPS UG">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/409_GEOINFO.php" target="_blank">
+						<img src="./img/s/409_Geoinfo_Logo.png" alt="GEOINFO">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/410_52north.php" target="_blank">
+						<img src="./img/s/410_52North.png" alt="52°North Spatial Information Research GmbH">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/411_NTI_CWSM_GmbH.php" target="_blank">
+						<img src="./img/s/411_nti_cwsm.png" alt="NTI CWSM GmbH">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/412_sourcepole.php" target="_blank">
+						<img src="./img/s/412_sourcepole.png" alt="sourcepole">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/413_meggsimum.php" target="_blank">
+						<img src="./img/s/413_meggsimum-logo.svg" alt="meggsimum">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/414_latlon.php" target="_blank">
+						<img src="./img/s/414_logo_latlon_web.png" alt="latlon">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/415_geocockpit.php" target="_blank">
+						<img src="./img/s/415_YEY-Maps_Logo.png" alt="GeoCockpit UG">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/416_DBG.php" target="_blank">
+						<img src="./img/s/416_DBG.jpg" alt="d.b.g. Datenbankgesellschaft mbH">
+						</a>
+					</li>
+
+
+					
+				</ul>
+			<h4 class="highlight">Medienpartner</h4>
+				<ul class="tiles">
+					<li class="tile media">
+						<a href="https://live.osgeo.org/de/index.html" target="_blank">
+						<img src="./img/s/501_OSGeoLive_banner.png" alt="OSGeoLive">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://c3voc.de/" target="_blank">
+						<img src="./img/s/502_voc_square.png" alt="C3VOC">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://av.tib.eu/search?f=publisher%3Bhttp://av.tib.eu/resource/FOSSGIS_e.V." target="_blank">
+						<img src="./img/s/503_TIB.PNG" alt="TIB">
+						</a>
+					</li>
+					<!--<li class="tile media">
+						<a href="https://liberapay.com/johnjohndoe/" target="_blank">
+						<img src="./img/s/504_EventFahrplan.png" alt="FahrplanAPP">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://www.oreilly.de/" target="_blank">
+						<img src="./img/s/506_OReilly_logo.jpg" alt="O'Reilly">
+						</a>
+					</li>
+
+					<!--<li class="tile media">
+						<a href="https://ffmuc.net/" target="_blank">
+						<img src="./img/s/505_image_fffmuc.png" alt="ffmuc">
+						</a>
+					</li>
+
+					<li class="tile media">
+						<a href="https://workadventu.re/" target="_blank">
+						<img src="./img/s/507_TCM_WA.png" alt="Workadventure">
+						</a>
+					</li>
+				</ul>
+
+
+			
+			<h4 class="highlight">   </h4>
+			<h3>Werden Sie Sponsor!</h3>
+           	<p>Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich.</p>
+			<p>Sie möchten die FOSSGIS-Konferenz mit einem Sponsoring unterstützen? Sie finden Informationen für Sponsoring und Ausstellung in der <a href="https://files.fossgis.de/Konferenz/Sponsoreninformationen_FOSSGIS-2022.pdf">Sponsoringbroschüre</a>. Falls Sie sich für ein Sponsoring entschieden haben, können Sie die <a href="https://files.fossgis.de/Konferenz/FOSSGIS-Konferenz-Sponsoring-Vereinbarung_2022.pdf">Sponsoringvereinbarung hier</a> herunterladen.<br>
+			<p>Bei Fragen wenden Sie sich an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.</p>-->
+  
+  
+        <?php include('inc/footer.inc'); ?>
+ 
+</body>
+</html>
