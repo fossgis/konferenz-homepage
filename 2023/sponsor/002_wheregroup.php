4f8b0e9af22294628c63518a3d579e1a4836edf9
Katja Haferkorn <katjahafi@gmx.de>
2022-10-05 14:11:48 +0200

    WhereGroup Platinsponsor

---
+++ 2023/sponsor/002_wheregroup.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2023 - WhereGroup GmbH</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2023">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+
+				<div class="col-lg-3">
+					<div class="icon mt-3">
+						<img src="./img/s/002_WhereGroup.jpg"  class="sp-logo" title="wheregroup" alt="wheregroup">
+					</div></div>
+			
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>WhereGroup GmbH</h3>
+
+					<p>Die WhereGroup ist eines der größten Open-Source-Softwarehäuser der GEO-IT-Branche in Deutschland. Wir managen zahlreiche komplexe GIS-Projekte unterschiedlichster Art. Als mittelständisches Unternehmen mit über 40 Mitarbeiter*innen an vier Standorten arbeiten wir innovativ und haben dennoch die Bodenhaftung nicht verloren.</p>
+
+					<p>Wir bieten Ihnen kompetente Unterstützung in den Bereichen Geographische Informationssysteme (GIS), Web-GIS, Datenbanken, Standards, Interoperabilität und System-Integration. Angefangen bei der Beratung, Konzeption und Entwicklung bis hin zum Betrieb dynamischer Kartenanwendungen im Intra- und Internet.</p>
+
+					<p>Grundlage unseres Schaffens ist der Open-Source-Gedanke. Als Teil einer starken Community, mit der wir in engem Austausch stehen, engagieren wir uns aus Überzeugung im FOSSGIS e. V., im QGIS-DE e.V. und bei der OSGeo. Wir sind auf vollen Überzeugung auf Open-Source-Entwicklungen spezialisiert und integrieren professionelle freie Software nahtlos in proprietäre Systeme.</p>
+
+					<p>Wir implementieren Lösungen für den Mittelstand, die Industrie und auf allen Ebenen der öffentlichen Verwaltung, beraten und begleiten bei Planung, Migration und Einführung von raumbezogenen Informationssystemen.</p>
+
+					<p>Dabei reicht das Spektrum unserer Projekte von Desktop-Lösungen über Geoportalen und kartenbasierter Datenverwaltung bis hin zu hochverfügbaren Anwendungen für die freie Wirtschaft und die öffentliche Verwaltung.</p>
+
+					<p>Unser Schulungsinstitut, die FOSS Academy, bietet außerdem praxisorientierte Schulungen zum Thema „GIS mit Open-Source-Software“ an.</p>
+
+					<p>Mehr zur WhereGroup unter <a href="www.wheregroup.com">www.wheregroup.com</a> und <a href="www.foss-academy.com">www.foss-academy.com</a>.</p>
+
+
+
+				</div>
+			<div class="col-lg-3">
+				<div class="mb-4">
+					<h6 class="mb-0">Kontakt<br><br>
+						Webseite:<br>
+						<a href="https://wheregroup.com">www.wheregroup.com</a><br>
+						FOSS-Academy<br>
+						<a href="https://www.foss-academy.com">www.foss-academy.com</a><br><br>
+						E-Mail: <br>
+						<a href="mailto:info@wheregroup.de">info@wheregroup.de</a><br></h6>
+					</div>
+				
+					<div class="social-icon">
+						<h6 class="font-weight-bold mb-0">Social</h6>
+						<ul>
+							<li>
+								<a href="https://twitter.com/WhereGroup_com"><i class="fab fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="https://www.xing.com/pages/wheregroupgmbh-co-kg"><i class="fab fa-xing"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>	
+
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
