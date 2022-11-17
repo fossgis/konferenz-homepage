1e4967df3f5603cbd342643ff32a81a12e271875
Katja Haferkorn <katjahafi@gmx.de>
2022-11-17 18:20:23 +0100

    Seite Anmeldung erstellt

---
+++ 2023/anmeldung/index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2023 - Anmeldung zur Konferenz</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2021/widget/v1.css">
+	<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+</head>
+
+
+
+<body id="anmeldung">
+
+	<?php include "../inc/header.inc"; ?>
+
+
+<!--	<h2>Anmeldung zur Konferenz</h2>
+
+	<p>Die FOSSGIS-Konferenz 2022 hat vom 09.-12. März 2022 stattgefunden. Knapp 700 Teilnehmende waren dabei, davon ca. 50 beim OSM-Event.</p>-->
+
+		<h2>Anmeldung zur Konferenz</h2>
+
+	<p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz zu werden. Die Konferenz lebt von Ihren Beiträgen und Ihrem Besuch. Wir freuen uns auf Sie! </p>
+
+	<p>Vom 15.-18. März 2023 findet die FOSSGIS-Konferenz als Präsenzveranstaltung an der Humboldt-Universität zu Berlin im Erwin-Schrödinger-Zentrum in Berlin-Adlershof statt.</p>
+	<p>Im Folgenden finden Sie Informationen zur Teilnahme, zu den Workshops und wie Sie Helfer:in werden können.  
+Der Ticketverkauf startet in der ersten Januarwoche 2022.</p>
+		
+	<!--<p class="highlight">
+		&darr; <a href="https://pretix.eu/fossgis/2022/">Direkt zur Anmeldung</a>
+	</p>-->
+
+	<h3>Preise FOSSGIS-Konferenz 2023</h3>
+
+	<ul>
+		<li>Konferenzticket: 220 €</i>
+		<li>Konferenzticket - reduzierter Preis*: 60 €</i>
+<!--		<li>Onlineticket: 60 €</i>
+		<li>Onlineticket - reduzierter Preis*: 15 €</i>-->
+		<li>Konferenzticket - für Community**: € 0</i>
+		<li>Konferenzticket für Helfende**: 6 h helfen</i>
+		<li>Workshop***: 120 €</i>
+		<li>Mittagessen pro Tag***: 7 €</li>
+	</ul>
+		
+
+	<ul><p><b><i>Das Konferenzticket enthält:</i></b></p>
+	<ul>
+		<li>FOSSGIS-Konferenz-Teilnahme in Berlin</li>
+		<li>ordentliche Rechnung, unter dem jeweiligen Buchungslink abrufbar.</li>
+		<li>Pausensnack</li>
+		<li>Tasche, T-Shirt***</li>
+		<li>Tagungsband***</li>
+		<li>Abendveranstaltung***</li>
+	</ul>
+
+
+	<p><small>* reduzierter Preis gilt für: Schüler:in, Azubi, Freiwilligendienstleistende, Studierende, Rentner:in.</small><br>
+		<small>** Regelungen für das Communityticket oder Helferticket gelten für FOSSGIS-, OpenStreetMap- und OSGeo-Aktive sowie aktive Helfer:innen der Konferenz für mindestens 6 Stunden.</small><br>
+		<small>*** wenn bestellt und solange verfügbar.</small><br>
+		<small>Einige Tage vor der Konferenz erhalten Sie eine Mail mit wichtigen Informationen.</small>
+	</p>
+	
+
+	<b><i>Hygieneschutz</i></b>
+	<p>Ausschlaggebend für die Durchführung der Konferenz sind die entsprechend gültigen Hygienebestimmungen der HU-Berlin für Präsenzveranstaltungen. Grundlage ist der <a href="https://www.hu-berlin.de/de/pr/coronavirus-informationen/leitfaeden/rahmenhygieneplan.pdf/@@download/file/20220920_Hygieneplan_HU.pdf">Rahmenhygieneplan</a>. Änderungen sind vorbehalten. </p></ul>
+
+	<h3 id="Workshop" name="Workshop">Workshop buchen</h3>
+	<p>Das Programm bietet die Möglichkeit einen oder mehrere Workshops zu buchen. Für jeden Workshop sind 90 Minuten vorgesehen, der Preis beträgt 120 EUR. Einen Workshop können Sie im zweiten Schritt des Bestellvorgangs zum Konferenzticket hinzubuchen.<br>
+ Der Workshop ist interaktiv und wird in einem mit PCs ausgestattenen Raum im Geographischen Institut stattfinden. Die meisten Workshops arbeiten mit der aktuellen <a href="http://live.osgeo.org/de/index.html">OSGeoLive.</a>
+	
+<!--	<h3>Bildungsurlaub</h3>
+	<p>Die Anerkennung der FOSSGIS-Konferenz als Bildungsurlaub wurde angestrebt. Zunächst für die Bundesländer in Deutschland, wo die Beantragung keine zusätzlichen Kosten verursacht (BE, BB, HB, HH, NI, RP, SL, ST und SH). Weitere werden bei Bedarf beantragt. </p>
+	<p> Bildungsurlaub ist der gesetzlich verbriefte Anspruch auf Weiterbildung für Arbeitnehmer:innen im Rahmen der Arbeitszeit. Die Anerkennung einer Veranstaltung als Bildungsurlaub gibt Arbeitnehmer:innen das Recht, die Veranstaltung während der Arbeitszeit zu besuchen und sich dafür beurlauben zu lassen. Der Mehrwert besteht darin, dass der Arbeitnehmer:in pro Jahr bis zu 5 Tage Bildungsurlaub zustehen, unter Weiterzahlung des Gehalts.</p>
+
+	<p>Weitere Informationen zum Bildungsurlaub finden Sie unter <a href="https://fossgis-konferenz.de/2022/anmeldung/bildungsurlaub.php"> Anmeldung/Bildungsurlaub</a>.</p>-->
+
+	
+	<h3>Helfen</h3>
+	<p>Freiwillige Helfer:innen sind eingeladen und willkommen vor der Konferenz und/oder während der Konferenz zu unterstützen. Bei Interesse bitte die <a href="https://fossgis-konferenz.de/2023/helfen/">Website <i>Helfen</i></a> lesen, dort sind viele Details erklärt.</p>
+	<p>Helfer:innen dürfen das kostenfreie "Konferenzticket für Helfende" buchen.<br>
+	Wenn Sie Fragen zum helfen haben, wenden Sie sich per <a href="mailto:helfen@fossgis.de?subject=Interesse_Unterstützung_FOSSGIS-Konferenz">E-Mail</a> an das Helferteam.</p>
+
+ <!--  	<h3 id="Anmeldeformular" name="Anmeldeformular">Anmeldeformular</h3>
+	<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, können Sie das Ticketsystem auch unter <a href="https://pretix.eu/fossgis/2023/">diesem Link</a> direkt aufrufen.</p>
+	<div id="shop">
+		<pretix-widget event="https://pretix.eu/fossgis/2023/"></pretix-widget>
+		<noscript>
+  			 <div class="pretix-widget">
+       			 <div class="pretix-widget-info-message">
+            		JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2023/">hier</a>.
+        		</div>
+    		</div>
+		</noscript>-->
+			
+
+	<h3>Sie haben Fragen? Vielleicht finden Sie hier Antworten.</h3>
+
+	<ul>
+	<li> <a href="https://fossgis-konferenz.de/2023/faq.php">FAQ</a></li>
+<!--	<li> <a href="https://pretix.eu/fossgis/2023/page/informationen-fuer-teilnahme/">Informationen zur Teilnahme</a> </li>
+	<li> <a href="https://pretix.eu/fossgis/2023/page/informationen-zum-workshop/">Informationen zu den Workshops</a> </li>-->
+	
+
+	<li>Hier erreichen Sie das Konferenzteam</li>
+
+	<ul>
+	<li>Matrixkanal: <a href="https://matrix.to/#/!XajsOFLRhsznebfosT:matrix.org">#FOSSGIS2023:matrix.org </a></li>
+	<li>E-Mail an <a href="mailto:konferenz-orga@fossgis.de?subject=Konferenzorganisationsteam_erreichen">konferenz-orga@fossgis.de</a> </li>
+	<li>Anruf: +49 30-62932037</li>
+	<li>Fragen zum Programm an das <a href="mailto:PKO@fossgis.de?subject=Programmkomitee_erreichen">Programmkomitee</a> </li>
+	</ul></ul>
+
+	</p>
+
+	<?php include('../inc/footer.inc'); ?>
+
+</body>
+
+</html>
