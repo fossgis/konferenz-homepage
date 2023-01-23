4436c34cbc77ecb4d37825776ea4590f81a66c84
Katja Haferkorn <katjahafi@gmx.de>
2023-01-23 11:53:39 +0100

    Exkursion Stabi hinzugefügt.

--- 2023/socialevents/index.php
+++ 2023/socialevents/index.php
@@ -50,9 +50,13 @@ Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Eine Anmeldung ist erfor
 	<h4>Sektempfang am FOSSGIS-Stand</h4>
 	<p>Alle Mitglieder des FOSSGIS-Vereins, Freunde und Interessierte sind ab 15:30 Uhr herzlich zum Sektempfang zum Ausklang der FOSSGIS 2023 am FOSSGIS-Vereins-Stand eingeladen.</p>
 
-	<h4>Exkursionen im Rahmen der Konferenz</h4>
+<!--	<h4>Exkursionen im Rahmen der Konferenz</h4>
 	<p>Es gibt verschiedene Ideen für Exkursionen. Beispielsweise zur Kartensammlung der Staatsbibliothek oder Sehenswertes am Standort Adlershof oder eine Mappingaktion.<br>
-Weitere Informationen folgen.</p>
+Weitere Informationen folgen.</p>-->
+
+	<h4>Exkursion Kartensammlung Staatsbibliothek zu Berlin</h4>
+	<p>Am Freitag, den 17.03.2023 wird am späten Nachmittag eine Exkursion zur Kartensammlung der Staatsbibliothek zu Berlin, Haus Unter den Linden, angeboten. Wir bitten um Anmeldung über die Konferenzamneldung, da die Zahl der Teilnehmenden begrenzt ist. Gemeinsamer Start circa 16.30 Uhr vom ESZ, BVG-Tickets muss jeder selber kaufen.<br>
+Bei Bedarf gibt es am Samatg, den 18.03. noch eine Runde, einfach <a href="mailto:konferenz-orga@fossgis.de">melden</a> und Interesse bekunden.</p>
 
 	<h4 id="Mapathon" name="Mapathon">Mapathon mit Ärzte ohne Grenzen</h4>
 	<p>Ärzte ohne Grenzen lädt im Rahmen der FOSSGIS Konferenz 2023 zu einem Mapathon ein.
