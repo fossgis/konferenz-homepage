eb527bd05f4c4f9730717528d0e41593a45f1d8c
katjahafi <katjahafi@gmx.de>
2023-10-05 11:48:16 +0200

    CfP hinzugefügt

---
+++ 2024/callforpapers/index.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+    <?php include "../inc/meta.inc"; ?>
+
+    <base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+    <title>FOSSGIS 2024 - CfP</title>
+
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2024/widget/v1.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/print.css" media="print">
+    <script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+</head>
+
+<body id="call">
+
+    <?php include "../inc/header.inc"; ?>
+
+	<h2 ID="CfP" name="Call_for_Particiption">Call for Participation FOSSGIS 2024</h2>
+
+	<p>Die FOSSGIS-Konferenz ist die größte deutschsprachige Konferenz für Open Source Software für Geoinformationen und OpenStreetMap und wird vom 20.-23. März 2024 an der Technischen Universität Hamburg in Harburg stattfinden.   
+Konferenzveranstalter sind der gemeinnützige FOSSGIS e.V., die OpenStreetMap-Community in Kooperation mit dem Institut für Verkehrsplanung und Logistik der Technischen Universität Hamburg und der HafenCity Universität.</p>
+
+	<p>Freie quelloffene Software, Open Data und Open Science leisten einen wichtigen Beitrag zur Stärkung der Digitalen Souveränität. Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier Open Source Software (FOSS) für Geoinformationssysteme (GIS). Hier treffen sich Anwender:innen, Entwickler:innen und Interessierte zum gemeinsamen Austausch über GIS-Software, OpenStreetMap und neue Projekte und Themen mit Geodatenbezug.
+Mit Vorträgen, Workshops, Demo-Sessions, Anwendertreffen und Gelegenheiten zum Netzwerken und Austausch wird die Veranstaltung vorwiegend in Präsenz stattfinden. Die gleichzeitig stattfindende Firmen- und Projektausstellung ermöglicht einen direkten Kontakt zu Dienstleistern und Projekten.</p>
+
+	<p>Samstag, der 23. März ist dem "OSM-Event" gewidmet, einer Unkonferenz der deutschsprachigen OpenStreetMap-Community. Es finden Vorträge und Diskussionsrunden zu aktuellen Themen statt.</p>
+
+    <p>Der <a href="https://pretalx.com/fossgis2024/cfp">Call for Participation FOSSGIS 2024</a> ist bis zum 6. November 2023 geöffnet (keine Verlängerung).</p>
+
+    <h2>Themenbereiche</h2>
+
+	<p>Die FOSSGIS-Konferenz bedient ein weites Themenfeld, das unter anderem die folgenden Aspekte umfasst:</p>
+	<ul>
+    	<li>Grundlagen Open-Source-GIS und OpenStreetMap</li>
+    	<li>Neuigkeiten aus den Open-Source-Projekten</li>
+    	<li>OpenStreetMap</li>
+    	<li>Praxisberichte</li>
+    	<li>Daten, Datenbanken und Datenprozessierung</li>
+    	<li>Routing und Mobilität</li>
+    	<li>Barrierefreiheit und Inklusion</li>
+    	<li>Raum-zeitliche Analysen und Modelle</li>
+    	<li>Rasterdaten und Fernerkundung</li>
+    	<li>3D, Drohnen, LIDAR</li>
+    	<li>Kartographie und Visualisierung</li>
+    	<li>Offene Standards, z.B. INSPIRE, OGC</li>
+    	<li>Geodatenmanagement</li>
+    	<li>Open Data, Datenschutz und Lizenzen</li>
+    	<li>Ausbildung, Lehre, Open Science</li>
+     </ul>
+
+
+  	<h2>Einreichungstypen</h2>
+
+	<p>Das Konferenzprogramm beinhaltet unterschiedliche Formate.</p>
+	<ul>
+		<li><strong>Lightning Talk</strong><br>
+Ein Lightning Talk bietet Gelegenheit neue Software, neue Projekte oder neue Ideen in 5 Minuten zu präsentieren. Es ist möglich, noch kurz vor und während der Konferenz einen LT einzureichen, sofern Kapazitäten im Programm verfügbar sind.</li>
+
+		<li><strong>Vortrag</strong><br>
+Vorträge bilden den Hauptteil der Konferenz. Ein Vortrag ist 20 Minuten lang. Danach sind 5 Minuten Zeit für Fragen.</li>
+		
+		<li><strong> Vortrag mit Paper im Academic Track</strong><br> 
+Begutachtung im Peer-Review-Verfahren durch das Academic Track-Komitee mit dem Ziel einer zitierfähigen Veröffentlichung eines Full Papers (6-8 Seiten).    
+Der einzureichende Abstract sollte ca. 3.000 Zeichen umfassen.</li>
+
+		<li><strong>Demo-Session</strong><br>
+Die Demo-Session dient der Vorführung von Software oder Prozessen ("Live-Hacking") und ist 45 Minuten + 10 Minuten Diskussion lang.</li>
+
+		<li><strong>Workshop</strong><br>
+Workshops finden von Mittwoch bis Freitag parallel zu den Vorträgen statt. Die Teilnahme am 90-minütigen Workshop ist kostenpflichtig, die Zahl der Teilnehmenden ist begrenzt. Sollte der Workshop stark nachgefragt sein, sind mehrere Personen für die Betreuung vorgesehen.</li>
+
+		<li><strong>Anwendertreffen/BoF</strong><br>
+Anwendertreffen oder "Birds of a Feather"-Sessions (BoF- ) ermöglichen es Gleichgesinnten und -interessierten sich konzentriert und selbstorganisiert zu einem bestimmten Thema in einer 60-minütigen Session auszutauschen.</li>
+
+		<li><strong>Expert:innenfragestunde</strong><br>
+Ihre Einreichung passt nicht zu einem der Einreichungstypen? Dann setzen Sie sich bitte vor dem Ende der Einreichungsfrist mit dem Programmkomitee per E-Mail in Verbindung.</li>
+
+
+		<li><strong>Sonstiges</strong><br>
+Ihre Einreichung passt nicht zu einem der Einreichungstypen? Dann setzen Sie sich bitte vor dem Ende der Einreichungsfrist mit dem Programm-Komitee per <a href="mailto:programm@fossgis.de? subject=Frage%an%das%Programmkomitee"> per E-Mail</a> in Verbindung.</li>
+	</ul>
+
+
+	<h2>Bewertungskriterien</h2>
+
+	<p>Bei der Bewertung der Einreichungen finden folgende Kriterien Anwendung.</p>
+		<ul>
+			<li>Die Inhalte des Beitrags dienen der Wissensvermittlung und dem Erfahrungsaustausch bezüglich aktueller Themen.</li>
+
+			<li>Bei der Einreichung muss es um freie Software oder freie Daten gehen.</li>
+				<ul>
+				<li><a href="https://www.gnu.org/licenses/license-list.html">Software ist frei</a>, wenn ihre Lizenzbedingungen von der Free Software Foundation als frei anerkannt werden und der Quellcode öffentlich verfügbar ist.</li>
+				<li>Daten sind frei, wenn ihre Lizenzbedingungen der <a href="https://opendefinition.org/">Open Definition</a> entsprechen.</li>
+				<li>Der FOSSGIS e.V. unterstützt die Initiative <a href="https://publiccode.eu/de/">Public Money, Public Code</a>. Wir ermuntern alle Projekte und Organisationen, die mit öffentlichen Mitteln freie Software finanzieren, sich und ihre Projekte / Software vorzustellen und von ihren Erfahrungen zu berichten.</li>
+				<li>Selbstverständlich freuen wir uns über Beiträge, die die Migration von unfreier zu freier Software oder die Nutzung proprietärer Formate/Schnittstellen mit freier Software thematisieren.</li>
+				</ul>
+			<li>Vorträge sollten einen Neuigkeitswert bieten.</li>
+			<li>Die Einreichung wird nach inhaltlichen Kriterien bewertet. Daher ist für die Bewertung eine aussagekräftige Beschreibung (die etwas mehr Information als die Zusammenfassung enthält) wichtig.</li>
+			<li>Eine Einreichung muss von einer oder mehreren natürlichen Personen erfolgen, die den Vortrag / Workshop dann auch halten.</li>
+			<li>Beiträge von Sponsoren werden nicht bevorzugt behandelt, die Vorträge dienen nicht der reinen Firmenpräsentation.</li>
+			<li>Ausschließliche Werbebeiträge sind unerwünscht.</li>
+			<li>Es wird wieder ein Community-Voting stattfinden. Das Ergebnis wird vom Programmkomitee als Stimmungsbild aus der Community einbezogen.</li>
+
+			<h3>Zusätzlich für Academic Track</h3>
+			<p>Einreichungen - bzw. die Full Paper - im Academic Track werden zusätzlich nach gängigen wissenschaftlichen Kriterien bewertet.</p>
+			
+			<h3>Zusätzlich für Workshops:</h3>
+			<p>Wir bevorzugen Workshops zu aktuellen und nachgefragten Themen.<br>
+			Die geschätzte Nachfrage auf Basis der Vorjahre hat einen erheblichen Einfluss auf die Auswahl.<br>
+			Die Workshops finden ausschließlich vor Ort in Berlin statt.</li>
+	</p>
+
+	<h2>Grundsätze der Programmgestaltung</h2>
+
+	<p>Das Programmkomitee wünscht sich in jeder Hinsicht Diversität unter den Vortragenden. Neue Vortragende sind willkommen! Daher freuen wir uns besonders über Einreichungen von Personen, die noch nicht oder noch nicht oft vorgetragen haben. Sollten Sie unsicher sein, zögern Sie nicht das  Programm-Komitee zu kontaktieren (<a href="mailto:programm@fossgis.de">per E-Mail</a>, Telefon: +49 30-62932037)</p>
+
+	<p>Das Programmkomitee ist bestrebt ein ausgewogenes und informatives Programm anzubieten und achtet auf Ausgewogenheit bezüglich Vortragenden und Firmen.</p>
+
+	<p>Die Beiträge werden ausschließlich vor Ort präsentiert. Die Konferenzsprache ist Deutsch. In Ausnahmefällen können Beiträge auf Englisch erfolgen. Falls Themen doppelt eingereicht werden, bemüht sich das Programmkomitee, gemeinsam mit den Einreichenden eine Lösung zu finden.</p>
+
+	<p>Es wird angestrebt die Vorträge live zu streamen sowie die Formate Anwendertreffen/BoF und Expert:innenfragestunde hybrid zu gestalten.</p>
+
+<br>
+	<h2>Veröffentlichung</h2>
+
+	<p>Der Konferenzbeitrag umfasst die Kurzbeschreibung für das Programmheft, den Beitrag im Tagungsband, die Vortragsfolien sowie die Liveübertragung und Videoaufzeichnung des Vortrags. Die Konferenzbeiträge werden unter der Lizenz CC BY 4.0 veröffentlicht. Der Lizenztext findet sich unter <a href="https://creativecommons.org/licenses/by/4.0/legalcode.de"> https://creativecommons.org/licenses/by/4.0/legalcode.de.</a></p>
+
+	<p>Material, das nicht unter CC-BY 4.0 lizenziert werden kann, weil beispielsweise Fremdrechte bestehen, muss auf den Folien bzw. den Unterlagen kenntlich gemacht werden.</p>
+
+	<h2>Streaming und Aufzeichnung</h2>
+
+	<p>Die Vorträge der Konferenz werden vor Ort gehalten und voraussichtlich per Livestream übertragen. Aus diesem  Grund ist es erforderlich, dass alle Vortragenden dem Streaming und der Aufzeichnung ihrer Beiträge zustimmen.</p>
+
+<br>
+	<h2>Konferenzticket und Förderprogramm</h2>
+
+	<p>Workshopleitende und studierende Vortragende erhalten einen Gutscheincode für ein kostenfreies Konferenz-Ticket. Dies gilt für diejenigen Beitragenden, die bis zum Einreicheschluss als Autoren benannt sind.</p>
+
+	<p>Um möglichst vielen Personen die Teilnahme zu ermöglichen, hat der FOSSGIS e.V. ein Förderprogramm aus dem – auf Antrag – Teile der Anreise und/oder Übernachtung bezahlt werden können, siehe <a href="https://fossgis.de/f%c3%b6rderungen/reisekostenbeihilfe/"> Reisekostenbeihilfe des FOSSGIS e.V.</a>.</p>
+
+	<h2>Das Programmkomitee</h2>
+	<ul>
+		<li>Astrid Emde (GIS-Consultant und FOSSGIS-Begeisterte)</li>
+		<li>Berit Mohr (GIS Consultant und Open-Source Begeisterte)</li>
+		<li>Christian Strobl (Geographical Information Officer / StMUV Bayern, Lehrauftrag GIS / LMU München)</li>
+		<li>Christoph Aberle (Institut für Verkehrsplanung und Logistik an der TUHH)</li>
+		<li>Christopher Lorenz (Entwickler und Beitragender zu OpenStreetMap)</li>
+		<li>Daniela Cialfi (Institute for Complex Systems - National Research Council of Italy)</li>
+		<li>Jan Schmalfuß-Schwarz (Wissenschaftlicher Mitarbeiter am Lehrstuhl für Mensch-Computer-Interaktion der TU Dresden)</li>
+		<li>Jonas Frei </li>
+		<li>Katja Haferkorn (Koordinatorin FOSSGIS e.V. und Konferenzorganisation)</li>
+		<li>Maik Sydoruk (OSM-Beitragender)</li>
+		<li>Marc Jansen (GIS und Fernerkundung; ❤️ FOSS & Freie Daten)</li>
+		<li>Pirmin Kalberer (FOSSGIS-Vorstand, GIS-Entwickler & -Anbieter)</li>
+		<li>Ulrike Assmann (Kartographin, GIS und Hydrographie)</li>
+
+	<h3>Reviewer Academic Track</h3>
+		<li>Christian Strobl (Geographical Information Officer / StMUV Bayern, Lehrauftrag GIS / LMU München)
+		<li>Christoph Aberle (Institut für Verkehrsplanung und Logistik an der TUHH)
+		<li>Niklas Alt (Historiker, Digital / Spatial Humanities)
+		<li>Jan Schmalfuß-Schwarz (Wissenschaftlicher Mitarbeiter am Lehrstuhl für Mensch-Computer-Interaktion der TU Dresden)
+		<li>Torsten Wiebke (Forstwissenschaftler, Wissenschaftlicher Leiter Waldinventuren und Fernerkundung am Landesbetrieb Forst Brandenburg, Fan von Freier- und Open Source-(GIS)-Software)
+		<li>Peter Löwe (Bereichsleiter IT und eScience am WZB Berlin)
+		<li>Ralf Löwner (Hochschule Neubrandenburg, FB Landschaftswissenschaften und Geomatik)
+	</ul>
+
+	<h2>Termine und Fristen</h2>
+	<ul>
+		<li>vom 06. Oktober bis 06. November 2023: Call for Participation für alle Einreichungstypen, Abtract einreichen (Achtung: keine Verlängerung)</li>
+		<li>vom 10. bis 24. November 2023: Community-Voting</li>
+		<li>bis 06. Dezember 2023: Benachrichtigung der Vortragenden (Zusagen/Absagen)</li>
+		<li>bis 22. Dezember 2023: Einreichung Full Paper (6-8 Seiten) für zugelassene Academic Track-Beiträge</li>
+		<li>bis 06. Januar 2024: Rückmeldungen zu Academic Track-Einreichungen durch die Reviewer</li>
+		<li>Anfang Januar 2024: Programmveröffentlichung und Start des Ticketverkaufs</li>
+		<li>bis 31. Januar 2024: Beitrag für Tagungsband einreichen (inkl. verbesserte Academic Track-Einreichungen)</li>
+		<li>vom 20. bis 23. März 2024: FOSSGIS-Konferenz 2024 (inkl. OSM-Event am Samstag)</li>
+
+	</ul>
+
+   	<p>Das FOSSGIS 2024 Programmkomitee freut sich auf Ihre Einreichung und steht für Fragen per <a href="mailto:programm@fossgis.de"> E-Mail</a> zur Verfügung.</p>
+
+<p>Eine Einreichung zum <a href="https://pretalx.com/fossgis2024/cfp">Call for Participation FOSSGIS 2024</a> ist bis zum 06. November 2023 möglich (Achtung: keine Verlängerung).</p>
+
+
+    <?php include('../inc/footer.inc'); ?>
+
+</body>
+
+</html>
