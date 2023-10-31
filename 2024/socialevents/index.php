68cce88a52132d79682fa563493f0ac77f8d822e
katjahafi <katjahafi@gmx.de>
2023-10-31 14:48:34 +0100

    Seite Social Events angelegt

---
+++ 2024/socialevents/index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2024 - Rahmenprogramm - Social Events</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="socialEvents">
+
+	<?php include "../inc/header.inc"; ?>
+
+	<h2>Rahmenprogramm und Social Events</h2>
+	<p>Die FOSSGIS-Konferenz lebt vom Engagement und Beiträgen aus der Community. Das Rahmenprogramm zur FOSSGIS 2024 bietet neben dem regulären Vortrags- und Workshopprogramm eine interessante Bereicherung für unsere Teilnehmenden.</p> 
+
+	<h4 id="Anwendertreffen" name="Anwendertreffen">Anwendertreffen</h4>
+	<p>Es können Anwendertreffen, Sprints und BoF-Sessions im Rahmen der Konferenz selber organisiert werden. Räume stehen zur Verfügung, bitte bis 06.11.2023 einreichen oder per <a href="mailto:programm@fossgis.de?subject=Session fuer das Konferenzprogramm">per E-Mail</a> beim Programmkomitee anfragen, wenn es im Programm veröffentlicht werden soll. </p>
+
+	<h3>Dienstag, 19. März 2024</h3>
+	
+	<h4>Geochicas laden ein</h4>
+	<p>tba
+        </p>	
+
+	<h4 id="Inoffizieller Start" name="Inoffizieller Start">Inoffizieller Start</h4>
+
+	<p>Am Abend vor der FOSSGIS-Konferenz besteht die Möglichkeit bei einem gemeinsamen Abendessen alte Gesichter wiederzusehen und neue Bekanntschaften zu machen. Für den inoffiziellen Start sind am 19. März 2024 ab 18:30/19 Uhr alle schon angereisten Konferenzteilnehmenden willkommen. Jeder bezahlt seine Rechnung selbst.<br>
+	Location: tba </p>
+
+	<h3>Mittwoch, 20. März 2024</h3>
+
+	<h4>Abendveranstaltung </h4>
+	<p>Traditionell findet am ersten Abend der FOSSGIS-Konferenz die Abendveranstaltung statt, auch Social Event genannt. 
+Der Eintritt ist im FOSSGIS-Konferenz-Ticket enthalten. Eine Anmeldung ist erforderlich. <br>Ort: tba.</p>
+
+	<h3>Donnerstag, 21. März 2024</h3>
+
+	<h4 id="Gruppenfoto" name="Gruppenfoto">Gruppenfoto</h4>
+	<p>Auch in diesem Jahr wollen wir uns das Gruppenfoto nicht entgehen lassen und laden Sie ein am Donnerstag, den 21. März in der Nachmittagspause zum gemeinsamen Gruppenfoto am Haupteingang des Haus H ein.</p>
+
+<!--	<h4 id="Freiberufler" name="Freiberufler">Treffen der Freiberufler</h4>
+	<p>Am Donnerstag, den 16. März 2023 werden sich um 17.45 Uhr die Freiberufler aus dem FOSSGIS-Bereich im Seminarraum 1'306 zum gemeinsamen Austausch treffen.</p>-->
+	
+	<h4>FOSSGIS e.V. Mitgliederversammlung</h4>
+	<p>Am Donnerstag, den 21. März 2024, sind alle Mitglieder und Gäste ab 18:00 Uhr herzlich eingeladen an der Mitgliederversammlung teilzunehmen und sich zu beteiligen. Der FOSSGIS-Verein lädt ein zum Diskutieren, Kennenlernen, Abstimmen & Neuwahlen. Es gibt Getränke und es wird Pizza für alle bestellt. Der Verein freut sich über zahlreiches Erscheinen.</p>
+
+	<h3>Freitag, 22. März 2024</h3>
+	
+	<h4 id="Jeopardy-Quiz" name="Jeopardy-Quiz">Jeopardy-Quiz</h4>
+	<p>Am Freitag, den 21. März gibt es vor dem Abschluss des reguläres Konferenzprogramms das nunmehr legendäre und sehr humorvolle Quiz in Form des FOSSGIS-Jeopardy mit Hannes und tba. Informationen zum generellen Ablauf sowie zur Anmeldung als Teilnehmerin oder Teilnehmer finden Sie in Hannes's Blog <!-- <a href="https://hannes.enjoys.it/blog/fossgis-jeopardy-20243/">hier in Hannes's Blog</a>-->.	
+
+	<h4>Sektempfang am FOSSGIS-Stand</h4>
+	<p>Alle Mitglieder des FOSSGIS-Vereins, Freunde und Interessierte sind ab 16:00 Uhr herzlich zum Sektempfang zum Ausklang der FOSSGIS 2024 am FOSSGIS-Vereins-Stand eingeladen.</p>
+
+	<h4>Exkursionen im Rahmen der Konferenz</h4>
+	<p>Es gibt verschiedene Ideen für Exkursionen. Beispielsweise tba. <br>
+Weitere Informationen folgen.</p>-->
+
+<!--	<h4>OSM-Event am Freitagabend</h4>
+	<p>Für alle, die am Freitagabend, den 22. März 2024 noch in der Stadt sind und/oder am OSM-Event teilnehmen möchten, wird für 19/19.30 Uhr tba vorgeschlagen. Jeder bezahlt seine Rechnung selbst.</p>-->
+
+	<h3>Samstag, 23. März 2024</h3>
+	<h4 id="OSM-Event" name="OSM-Event">OSM-Event</h4>
+
+	<p>Am Samstag, den 23. März findet von 9.30 bis 17.30 Uhr die beliebte OSM-Unconference statt. Interessierte sind eingeladen daran teilzunehmen. Die Veranstaltung ist kostenfrei. Die Themensammlung erfolgt im OSM-Wiki. <!--<a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2023/OSM-Samstag" target="_blank">OSM-Wiki</a>. Wir bitten um Anmeldung über das <a href="https://fossgis-konferenz.de/2023/anmeldung/#Anmeldeformular"><b>FOSSGIS-Konferenz-Teilnehmeranmeldesystem.</b></a></p>-->
+	
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+</html>
