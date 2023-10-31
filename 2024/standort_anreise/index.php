06369403f9ada1db4a2e6ea11f8b9ec89518afde
katjahafi <katjahafi@gmx.de>
2023-10-31 14:09:16 +0100

    Entwurf Standort, Anreise und Unterkunft

---
+++ 2024/standort_anreise/index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2024 - Standort, Anreise und Übernachtung</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./standort_anreise/script/leaflet.css"/>
+
+	<script src="./standort_anreise/script/leaflet.js"></script>
+	<script src="./standort_anreise/script/leaflet.addons.js"></script>
+	<script src="./standort_anreise/data/buildings.js"></script>
+	<script src="./standort_anreise/data/hotels.js"></script>
+	<script src="./standort_anreise/script/map.js"></script>
+</head>
+
+<body id="standort">
+
+	<?php include "../inc/header.inc"; ?>
+	
+	<h1 ID="Standort" name="Standort">Standort, Anreise und Übernachtung</h1>
+
+	<!--<div class="map-container">
+	<div id="map" class="map"></div>
+	</div>-->
+
+	<p><b>Adresse:</b><br> Technische Universität Hamburg (TUHH)<br>
+Am Schwarzenberg-Campus 1<br>
+21073 Hamburg</p>
+		
+	<h2 ID="Campus" name="Campus">Konferenzort Campus TUHH</h2>
+<p>Die Konferenz findet auf dem Campus der TUHH in Hamburg-Harburg statt. Die Räumlichkeiten sind barrierefrei zugänglich. Für Fragen zum Erreichen der Räume, zögern Sie nicht das Konferenz-Team anzusprechen.</p>
+
+	<p>Im Foyer Haus H begrüßen wir Sie am Welcome Desk. Im Foyer und anliegenden Räumen ist Raum für ein Aufeinandertreffen der Teilnehmenden mit den Ausstellern, für eine Pausenversorgung wird gesorgt. Im Foyer besteht auch die Möglichkeit sich im "Himmel" als Konferenz-Helfer:in zu melden oder mit dem FOSSGIS e.V. am Vereinsstand ins Gespräch zu kommen.</p>
+
+	<p>Die <b>Vorträge</b> und <b>Anwendertreffen</b> finden im Haus H sowie Haus K (am Freitag Haus I) in den Hörsälen und Seminarräumen statt. Durch eine hohe Nachfrage kann es zeitweise dazu kommen, dass die Hörsäle bei Vorträgen voll ausgelastet sind. Für diesen Fall haben wir Räume organisiert, in denen die Vorträge im Livestream verfolgt werden können. Die Raumpläne finden Sie bald hier <!--<a href="./standort_anreise/raeume.php">hier</a>-->.
+	</p>
+
+	<p>Die <b>Workshops</b> werden in den Seminarräumen des Hauses D durchgeführt. Die Raumpläne finden Sie bald hier. <!--<a href="./standort_anreise/raeume.php">hier</a>.-->
+	</p>
+
+	<p>In den kleineren Pausen gibt es ein Catering im Erdgeschoss sowie 1. Obergeschoss im Haus H. Für das Mittagessen können voraussichtlich Essenmarken erworben werden.</p>
+
+
+	<h2 ID="Anreise" name="Anreise">Anreise </h2>
+	<h3> öffentlicher Nahverkehr</h3
+	<p>Der Campus ist recht gut an den öffentlichen Nahverkehr angeschlossen und in weniger als 30 min vom Hamburger Hauptbahnhof aus zu erreichen. Der Campus liegt zwischen den S-Bahnhöfen Rathaus Harburg und Heimfeld (S 3 und S 31) sowie auf den Buslinien 142 (Haltestelle Kasernenstraße (TU Hamburg) nördlich am Campus) und 14, 143, 146, 443, 543 (Haltestelle Eißendorfer Straße (TU Hamburg) südlich am Campus).
+	Der Bahnhof Harburg Rathaus liegt circa 7 Gehminuten in Richtung Westnordwest, der Bahnhof Heimfeld (TU Hamburg) ist circa 5 Gehminuten Richtung Ostsüdost vom Campus entfernt.</p> 
+	
+	<p>Der öffentliche Nahverkehr findet im <a href="https://geofox.hvv.de/web/de/connections?clear=true&language=de">Hamburger Verkehrsverbund (HVV)</a> statt und besteht aus Bussen, S-Bahn (DB), U-Bahn (Hochbahn) sowie den Hafenfähren, die ebenfalls im Nahverkehr ohne Aufpreis nutzbar sind. Der Ticketkauf erfolgt in der Regel per <a href="https://play.google.com/store/apps/details?id=de.eos.uptrade.android.fahrinfo.hamburg&hl=de&gl=US">HVV-App</a> oder <a href="https://www.bahn.de/service/mobile/db-navigator">DB Navigator</a>, im HVV Online-Shop, an den S- & U-Bahnhöfen, einigen Bushaltestellen oder auch direkt im Bus.<br>
+	Bis auf wenige Ausnahmen sind alle U- und S-Bahn Haltestellen barrierefrei und mit <a href="https://www.hvv.de/de/aufzuege">Aufzügen</a> ausgestattet. Alle Busse sind Niederflurbusse und verfügen über entsprechende ausklappbare Rampen.
+	</p>
+	
+<!--	<p>Die <b>Bushaltestelle Kasernenstraße (TU Hamburg)</b> befindet sich nördlich vom Konferenzort und wird von der Buslinie 142 bedient, welche über Bhf. Harburg/ (Bereich C) nach Kasernenstraße (TU Hamburg) fährt. <br>
+	  Die Haltestelle Eißendorfer Straße (TU Hamburg) befindet sich südlich vom Konferenzstandort und wird von den Buslinien 14, 143 und 146 angefahren.</p>-->
+	
+	
+	<h3 ID="Auto" name="Auto">Anreise mit dem Auto</h2>
+	
+	<p>Auch wenn auf dem Campus Parkplätze zur Verfügung stehen, raten wir Ihnen mit öffentlichen Verkehrsmitteln anzureisen. Zwischen Hamburg und Harburg kommt es häufig zu Staus und Verzögerungen.
+	
+
+	<h3>Taxi</h3>
+
+	<p>Sicherlich freuen sich die Hamburger Taxifahrenden über Fahraufträge.
+	<ul>
+		<li>Fahrten mit <a href="https://taxihamburg.de/">Taxi Hamburg</a> können Sie per <a href="https://taxihamburg.de/taxi-app/download-taxi-app"> Taxi-App</a> bestellen oder anrufen unter <b>040 66 66 66</b>.
+		</li>
+		<li> <a href="https://das-taxi.de/">Das Taxi in Hamburg</a> verweist auf <a href="https://www.taxi.eu/en/cities/hamburg/"> Taxi App for Europe</a> oder anrufen unter <b>040 22 11 22</b> anrufen und wirbt mit einem genossenschaftlichen und sozialen Geschäftsansatz.
+		</li>
+	</ul>
+	</p>
+
+	<h2 ID="Unterkunft" name="Unterkunft">Übernachtung und Unterkunft</h2>
+	<p>Aufgrund der Elbüberquerung und häufig auftretender Störungen (u.A. Kampfmittelbeseitigungen) empfehlen wir eine Unterkunft südlich der Elbe in Harburg.</p>
+
+	<p>Folgende Liste enthält Hotels in unmittelbarer Nähe zum Konferenzort.</p> 
+	
+	<ul>
+		<li><b>Privathotel Lindtner </b><br>
+		Web: <a href="http://www.Lindtner.com/" target="_blank">http://www.Lindtner.com/</a><br>
+		Adresse: Heimfelder Straße 123, 21075 Hamburg <br>
+		Telefon: +49 (0) 40 79 009 484<br>
+		E-Mail: <a href="mailto:Sandra.Grimme@lindtner.com">Sandra.Grimme@lindtner.com</a><br>
+		TUHH Rabatt: Stichwort "TUHH"<br><br>
+		</li>
+
+		<li><b>Hotel Panorama</b><br>
+		Web: <a href="http://www.panorama-harburg.de" target="_blank">www.panorama-harburg.de </a><br>
+		Adresse: Harburger Ring 8-10, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 76695-0<br>
+		E-Mail: <a href="mailto:info@panoramaharburg.de">info@panoramaharburg.de</a><br>
+		TUHH Rabatt: Stichwort "TUHH"<br><br>
+		</li>
+
+ 		<li><b>Kleines Hotel Heimfeld</b><br>
+		Web: <a href="https://www.kleines-hotel-heimfeld.de/" target="_blank">https://www.kleines-hotel-heimfeld.de/</a><br>
+		Adresse: Heimfelder Straße 91-93, 21075 Hamburg<br>
+		Telefon: +49 (0) 40 84307 109 <br>
+		E-Mail: <a href="mailto:info@kleines-hotel-heimfeld.de ">info@kleines-hotel-heimfeld.de </a><br>
+		TUHH Rabatt: Stichwort "TUHH"<br><br>
+		</li>
+
+		<li><b>B&B HOTEL Hamburg-Harburg</b><br>
+		Web: <a href="https://www.hotel-bb.com/de/hotel/hamburg-harburg?utm_source=gmb&utm_medium=referral&utm_content=hamburg&utm_campaign=yext" target="_blank">https://www.hotel-bb.com/de/hotel/hamburg-harburg?</a><br>
+		Adresse: Theodor-Yorck-Str. 1, 21079 Hamburg<br>
+		Telefon: +49 (0) 40 3208260<br>
+		E-Mail: <a href="mailto:hamburg-harburg@hotelbb.com">hamburg-harburg@hotelbb.com</a><br><br>
+		</li>
+
+		<li><b>Hotelschiff „Lydios“ im Harburger Binnenhafen Kanal 77</b><br>
+		Web: <a href="https://schlafenimhafen.de/" target="_blank">https://schlafenimhafen.de/</a><br>
+		Adresse: Lotsekai 8, 21098 Hamburg<br>
+		Telefon: +49 (0) 176 47399841 <br>
+		E-Mail: <a href="mailto:marcel@klovert.de">marcel@klovert.de</a><br><br>
+		</li>
+		
+		<li><b>PHNX Aparthotel Hamburg</b><br>
+		Web: <a href="https://www.phnx.me" target="_blank">www.phnx.me</a><br>
+		Adresse: Hannoversche Straße 88a, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 30954976<br>
+		E-Mail: <a href="mailto:hamburg@phnx.me">hamburg@phnx.me</a><br><br>
+		</li>
+
+		<li><b>Hotel Jeta</b><br>
+		Web: <a href="http://www.hotel-jeta.de/" target="_blank">http://www.hotel-jeta.de/</a><br>
+		Adresse: Großer Schippsee 31, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 76796130<br>
+		E-Mail: <a href="mailto:info@Hotel-Jeta.de">info@Hotel-Jeta.de</a><br><br>
+		</li>
+	
+		<li><b>Altstadthotel Harburg</b><br>
+		Web: <a href="https://www.altstadthotel-harburg.de/" target="_blank">https://www.altstadthotel-harburg.de/</a><br>
+		Adresse: Neue Straße 61, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 767316-0<br>
+		E-Mail: <a href="mailto:info@altstadthotel-harburg.de">info@altstadthotel-harburg.de</a><br><br>
+		</li>
+
+  		<li><b>Hotel Harburger Hof</b><br>
+		Web: <a href="https://www.hhhof.de/" target="_blank">https://www.hhhof.de/</a><br>
+		Adresse: Schloßmühlendamm 16, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 323107-0<br>
+		E-Mail: <a href="mailto:info@hhhof.de">info@hhhof.de</a><br><br>
+		</li>
+
+		<li><b>Hotel Süderelbe</b><br>
+		Web: <a href="http://hotel-suederelbe.de/#hotel" target="_blank">http://hotel-suederelbe.de/#hotel</a><br>
+		Adresse: Großer Schippsee 29, 21073 Hamburg<br>
+		Telefon: +49 (0) 40 7673640<br>
+		E-Mail: <a href="mailto:info@hotel-suederelbe.de">info@hotel-suederelbe.de</a><br><br>
+		</li>
+
+		<li><b>AHOI Hostel Hamburg-Harburg</b><br>
+		Web: <a href="https://ahoi-hostel.eu/" target="_blank">https://ahoi-hostel.eu/</a><br>
+		Adresse: Großmoordamm 1, 21079 Hamburg<br>
+		Telefon: +49 40 6921 1330 oder +49 170 313 4078<br>
+		E-Mail: <a href="mailto:info@ahoi-hostel.eu">info@ahoi-hostel.eu</a><br><br>
+		</li>
+
+			
+<!--			<li><b>Hotel</b><br>
+			Web: <br>
+			Adresse: <br>
+			Telefon: <br>
+			E-Mail: <br><br>
+			</li>-->
+		</ul>
+
+	<h2 ID="POI" name="POI"> Ausflugsmöglichkeiten & Sehenswürdigkeiten</h2>
+	<p>Die Hansestadt Hamburg ist geprägt vom Hafen und dem Handel, die Elbe ist die Lebensader der Stadt. Hamburg bezeichnet sich selbst als Grüne Metropole mit kurzen Wegen und bietet zahlreiche sehenswerte Ziele.</p> 
+	<p>Das Lokalteam Hamburg präsentiert eine Auswahl an Freizeitangeboten und Empfehlungen.</p>
+
+	<h3>Hafen</h3>
+	<p>Mit der U3 erreicht man die Hafenpromenade (U Landungsbrücken), sowie die Speicherstadt und Elbphilharmonie (U Baumwall). Durch den alten Elbtunnel (Eingang an den Landungsbrücken) erreicht man zu Fuß die andere Elbseite mit Blick auf die Stadt. Die Fährlinie 62 (im Nahverkehr enthalten) bietet eine ca. einstündige Rundfahrt durch den Hafen (Landungsbrücken → Finkenwerder und zurück). Weitere Hafenrundfahrten werden an den Landungsbrücken angeboten.</p>
+	
+	<h3>Museen</h3>
+	<p>In der Speicherstadt befinden sich viele Museen, wie z.B. das <a href="https://www.miniatur-wunderland.com/">Miniatur Wunderland </a>, das <a href="https://www.imm-hamburg.de/"> Internationale Maritime Museum</a> oder das <a href="https://speicherstadtmuseum.de/">Speicherstadtmuseum</a>. Weitere Angebote finden Sie online unter <a href="https://www.hamburg.de/museum-hamburg">https://www.hamburg.de/museum-hamburg</a>.</p>
+	
+	<h3>Erholung im Grünen</h3>
+	<p>Neben der Hafenpromenade bietet auch die Außenalster einen ca. 7,5km langen Rundweg zum Spazieren gehen und Joggen. In der Innenstadt zwischen Landungsbrücken und Dammtor befindet sich "Planten un Bloomen", eine große Parkanlage und "grüner Gürtel" Hamburgs. Der Stadtpark (U3 Borgweg) bietet ebenfalls Möglichkeiten für Aktivitäten im Grünen.<br>
+
+Von Harburg aus erreicht man das Waldgebiet "Schwarze Berge" mit den Bussen 142 (bis Forstweg), 143/443 (bis Majestätische Aussicht) oder per S-Bahn (Linie 3 und 31) bis Neuwiedenthal. Die Deiche im Osten Harburgs eignen sich ebenfalls gut zur Erholung und für Spaziergänge (z.B. mit der Linie 149 bis Neuländer Elbdeich 76).</p>
+
+	<h3>Gastronomie</h3>
+	<p>Das Schanzenviertel, Karolinenviertel, St. Pauli und St. Georg bieten eine Vielzahl von Restaurants und Kneipen, sowie Nachtclubs.</p>
+
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+</html>
---
+++ 2024/standort_anreise/raeume.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2024 - Räume am Konferenzstandort</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2024">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+			
+			
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>Räume am Konferenzstandort</h3>
+
+					<p><img src="" width="800" alt="Raumplan Campus"  style="margin-rigth: 600px" align="rigth" />
+					</p>
+
+					<p><img src="" width="800" alt="Raumplan Haus H"  style="margin-rigth: 600px" align="rigth" />
+					</p>
+					<h4>Übersicht Gebäude am Konferenzstandort
+					<p><img src="" width="800" alt="Gebäude"  style="margin-rigth: 600px" align="rigth" />
+					</p>
+				</div>
+				
+				</div>	
+
+			</div>
+		</div>
+
+	</section>
+
+	<p><a href="/2023">Zurück zur Startseite</a></p>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
