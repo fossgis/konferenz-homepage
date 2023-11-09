27a19a4971bf1f5b20ea3e77041ac259b504528c
katjahafi <katjahafi@gmx.de>
2023-11-09 10:53:40 +0100

    Programmseite angelegt

---
+++ 2024/programm/index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2024 - Programm</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<script type="text/javascript" src="https://pretalx.com/fossgis2023/schedule/widget/v2.en.js"></script>
+</head>
+
+<body id="programm">
+	<?php include "../inc/header.inc"; ?>
+
+<!--        <h2 id="Programm" name="Programm"class="Programm">Programm</h2>
+		<p>Das Programmkomitee hat aus den Einreichungen ein Konferenzprogramm gestaltet. Hier gelangen Sie zum Programm oder scrollen Sie nach unten: <a href="https://pretalx.com/fossgis2023/schedule/" target="_blank" rel="noopener nofollow">https://pretalx.com/fossgis2023/schedule/</a>.</p>
+	
+		<h3>Vortragende</h3>
+		<p>Die Liste der Vortragenden finden Sie hier: <a href="https://pretalx.com/fossgis2023/speaker/">https://pretalx.com/fossgis2023/speaker/</a>.</p>
+
+<!--		<h3>Programm-Highlights</h3>
+		<p>Neugierig, welche Beiträge beim Community-Voting, an dem sich 130 Teilnehmende beteiligt haben,  am besten abgeschnitten haben?<br> Hier finden Sie die Highlights: <a href="https://pretalx.com/fossgis2023/featured/">https://pretalx.com/fossgis2023/featured/</a>.</p> 
+
+    	<h3>Vortragsaufzeichnungen</h3>
+	<p>Die Vorträge und Demos sind aufgezeichnet und veröffentlicht:</p>
+		<ul>
+			<li> <a href="https://pretalx.com/fossgis2023/schedule/" target="_blank" rel="noopener nofollow">Konferenz-Programm</a></p> 
+			</li>
+			<li> <a href="https://media.ccc.de/c/fossgis2023" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2023</a></p> 
+			</li>
+<!--			<li> <a href="https://av.tib.eu/series/1220/fossgis+konferenz+2022+online+event+09+12+marz+2022" target="_blank" rel="noopener nofollow">TIB</a></p> 
+			</li>
+			<li> <a href="https://www.youtube.com/user/fossgis" target="_blank" rel="noopener nofollow">YouTube</a></p> 
+			</li>
+		</ul>
+
+	<h3 id="Tagungsband" name="Tagungsband"class="Tagungsband">Tagungsband</h3>
+	<p>Der Tagungsband zur Konferenz steht <a href="https://files.fossgis.de/Konferenz/fossgis_tagungsband_2023_digital.pdf">als PDF zum Download</a> zur Verfügung.
+
+		<h3>Programm-APP für Android</h3>
+		<p>Die Android-App für das Konferenzprogramm ist im <a href="https://t1p.de/mi1o">Playstore downloadbar</a>. </p> -->
+
+	<!--<h3>FOSSGIS-Konferenz im "gewohnten" Livestream</h3>
+	<p>Die FOSSGIS 2022 wird auch im gewohnten Livestream übertragen: <a href="https://media.ccc.de/"> https://media.ccc.de/c/fossgis2022</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink zur Online-Platform im Bestelllink.</p>-->
+
+	<!--<h3>FOSSGIS-Konferenz per Android-APP</h3>
+		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">zum Download im Playstore</a> bereit. </p>
+	<p>Ansicht der App für Smartphone:	
+                    <a href="https://www.fossgis.de/">
+                      	<img src="./data/FOSSGIS-2021-Phones.png" class="img-fluid"  alt="APP">
+                    </a>
+                 </p>
+		<p>Ansicht der App für Tablet:	
+                    <a href="https://www.fossgis.de/">
+                         <img src="./data/FOSSGIS-2021-Tablet-Phone.jpg" class="img-fluid" alt="APP">
+					</a>
+                 </p>
+
+
+
+		<h3 id="Programm23" name="Programm23"class="Programm23">Programm FOSSGIS 2023</h3>
+		<pretalx-schedule event-url="https://pretalx.com/fossgis2023/" locale="de-formal" format="grid" style="--pretalx-clr-primary: #EE7F00"></pretalx-schedule>
+<noscript>
+   <div class="pretalx-widget">
+        <div class="pretalx-widget-info-message">
+            JavaScript is disabled in your browser. To access our schedule without JavaScript,
+            please <a target="_blank" href="https://pretalx.com/fossgis2023/schedule/">click here</a>.
+        </div>
+    </div>
+</noscript>-->
+
+
+		<h3>Community Voting</h3>
+<!--		<p>An der Öffentlichen Abstimmung (Community-Voting) zu den Einreichungen haben sich 130 Teilnehmer:innen beteiligt. Vielen Dank dafür!</p>-->
+
+<!--		<p>Hier ist der <a href="https://fossgis-konferenz.de/2023/callforpapers/">Call for Participation</a> zum Nachlesen verlinkt.</p>-->
+
+		<p>Die Einreichungsfrist für Beiträge zum Programm ist vorbei. 
+		Hier ist der <a href="https://fossgis-konferenz.de/2024/callforpapers/">Call for Participation</a> zum Nachlesen verlinkt.</p>
+	<p>Bis zum 24.11.2023 ist es für die Community möglich über die Einreichungen abzustimmen. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.  <br>Vielen Dank fürs Mitmachen!</p>
+	<p><b>Vorgehensweise:</b></p>
+	<ul>
+		<li>1. Den Link zur Öffentlichen Abstimmung klicken, um sich dafür anzumelden: <a href="https://pretalx.com/fossgis2024/p/voting/signup/"> https://pretalx.com/fossgis2024/p/voting/signup/</a></li>
+		<li>2. Sie erhalten eine E-Mail mit Ihrem persönlichen Link zur Öffentlichen Abstimmung</li>
+		<li>3. Einreichungen anschauen und bewerten- dies kann in Etappen erfolgen.</li>
+	</ul>
+
+	<h3>Programm FOSSGIS 2024</h3>	
+	<p>Freuen Sie sich schon jetzt auf ein spannendes Programm, dieses wird im Dezember 2023 veröffentlicht.</p>
+
+		
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+
+</html>
