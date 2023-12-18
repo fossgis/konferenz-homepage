650ebff1e69777186d81114ba781a7c8bc574e99
katjahafi <katjahafi@gmx.de>
2023-12-18 12:06:39 +0100

    noirBIT Text angepasst.

--- 2024/sponsor/412_norBIT.php
+++ 2024/sponsor/412_norBIT.php
@@ -31,16 +31,7 @@
 				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
 					<h3>norBIT GmbH</h3>
 
-					<p>Wir füllen GIS mit Leben. Wir bieten nicht nur reine Softwarelösungen an. Wir unterstützen unsere Kunden auf dem gesamten Weg. Von der Analyse des Ist-Zustandes über die Beschaffung und Integration von Daten, die Einrichtung der Umgebung und der Datenerfassung bis zur Daten- und Softwarepflege bieten wir alles, um ein GIS in Allen Phasen mitzugestalten. Das machen wir bereits seit 30 Jahren erfolgreich.</p>
-
-<p>Zur Realisierung der Anforderungen und Abläufe nutzen wir verschiedene Open Source Software und ermöglichen dadurch eine flexibles Ausspielen der Stärken der einzelnen Komponenten. Von der zentralen PostgreSQL-Datenbank werden die Daten an die Grafikplattformen QGIS, BricsCAD und AutoCAD verteilt, um die unterschiedlichen Arbeitsplatzanforderungen zu bedienen. Die großzügige und dennoch einfache Verteilung und Bereitstellung der GIS-Daten wird durch den vielseitigen QGIS-Web-Client 2 erreicht.</p>
-
-<p>Vom mobilen Client bis zum umfangreich ausgestatteten Büroarbeitsplatz bieten wir eine umfangreiche Bereitstellung von GIS-Daten.</p>
-
-<p>Über die Nutzung und Weiterentwicklung des norGIS-ALKIS-Imports werden alle Plattformen mit den Basisdaten direkt aus einer Datenbank versorgt. Die QGIS-PostgreSQL-Integration, sowie die DWG-Basis bei den CAD-Oberflächen werden hierbei in vollem Umfang unterstützt, um die unterschiedlichsten Anforderungen bedienen zu können.</p>
-
-<p>Ergänzend bieten wir Fachschalen, die vom kommunalen Umfeld (Straßen, Grünflächen und Bäume) über die Abwasserentsorgung (Kanal) und Gewässerunterhaltung bis hin zur Versorgung (Gas, Wasser, Strom, und weitere) die Abläufe der Fachabteilungen unterstützen und erleichtern.
-					</p>
+					<p>Wir füllen GIS mit Leben. Zu unseren Softwarelösungen bieten wir Unterstützung auf dem gesamten Weg. Von der Datenanalyse über die Beschaffung und Datenintegration, die Einrichtung und Datenerfassung bis zur Daten- und Softwarepflege bieten wir alles, um ein GIS in allen Phasen mitzugestalten.</p>
 				</div>
 				<div class="col-lg-3">
 					<div class="mb-4">
