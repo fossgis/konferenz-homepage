d86f6ebdff8d137579fb4b20ca6f994277dce1b7
katjahafi <katjahafi@gmx.de>
2024-06-02 16:04:15 +0200

    KOnferenzwebseite Münster eingerichtet

---
+++ 2025/datenschutz.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "inc/meta.inc"; ?>
+
+	<title>FOSSGIS 2024 - Datenschutzerklärung FOSSGIS-Konferenz</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+        <h2>Datenschutzerklärung FOSSGIS-Konferenz</h2>
+
+<p> in Überarbeitung </p>
+<!--		<p>Der Schutz persönlicher Daten hat bei uns oberste Priorität. Wann immer möglich, setzen wir Datenschutz in Form von Datensparsamkeit um und erheben und speichern so wenige Daten wie möglich. Im Folgenden erläutern wir detailliert, welche Daten wir erheben und wer darauf Zugriff hat. Wir weisen darauf hin, dass die Datenübertragung im Internet immer Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich, auch wenn wir höchstmögliche Standards anstreben.</p>
+
+
+		<p>Die verantwortliche Stelle für die Datenverarbeitung der mit Pretix/FOSSGIS-Konferenz 2024 erfassten personenbezogenen Informationen ist:<br><br>
+   FOSSGIS e.V.<br>
+   Bundesallee 23<br>
+   10717 Berlin<br>
+   E-Mail: konferenz-orga@fossgis.de</p>
+
+		<p>Ein Datenschutzbeauftragter ist für den Verein nicht bestellt.</p>
+
+		<h3>Personenbezogenen Daten und deren Verarbeitung</h3>
+		<h4>Kategorien erfasster Personenbzogener bzw. personenbeziehbarer Informationen</h4>
+		<ol class="list">
+			<li>Stammdaten: Name, Adresse, E-Mailadresse;
+				<ul><li>a) Teilnehmer:innen:  ggf. Unternehmen,</li>
+					<li>b) Sponsoren, Ausstellende: Unternehmen, Telefonnummer</li>
+				</ul>
+			<li>Konferenzteilnehmer:innen-Konfektionsgröße, nur soweit ein T-Shirt bestellt wird</li>
+			<li>Konferenzteilnehmer:innen-Zusatzveranstaltung (nur soweit gebucht)</li>
+			<li>Konferenzteilnehmer:innen-OSM-Nickname</li>
+			<li>Konferenzteilnehmer:innen-Communityzugehörigkeit, Communitybeteiligung</li>
+			<li>Cookies: Beim Kauf eines Tickets setzen wir Cookies ein, um die Funktionalität der Bestellung zu gewährleisten und um sicherzustellen, welcher Warenkorb Ihnen gehört.</li>
+			<li>Server-Log Files: Wir speichern keine IP-Adressen, Browserdaten, Referer oder andere Metadaten.</li>
+			<li>Zahlungsinformationen Paypal: Soweit entsprechende Bezahlmethode gewählt wird. Die Bezahlungsmethode Paypal wird von der PayPal (Europe) S.à r.l. & Cie, S.C.A., 5. Etage, 22-24 Boulevard Royal, L-2449 Luxembourg abgewickelt. Weitere Informationen zum Datenschutz von Paypal finden Sie in der Datenschutzerklärung von Paypal.</li>
+			<li>Zahlungsinformationen Überweisung: IBAN und Zahlbetrag.</li>
+			<li>Gesundheitsbezogene Daten</li>
+
+		</ol>
+
+		<h4>Zweck der Verarbeitung der Informationen</h4>
+		<ol class="list">	
+			<li>Stammdaten alle: Vertragsabwicklung, Zahlungsabwicklung</li>	
+				<ul><li>a) Teilnehmer:innen: zusätzlich Feststellung der Teilnahmeberechtigung, Konferenznamensschlild (Vor- und Nachname) </li>	
+ 					<li>b) Sponsoren, Ausstellende: künftige Kontaktaufnahme nach Einwilligung</li>
+				</ul>	
+			<li>Konferenzteilnehmer:innen-Konfektionsgröße, soweit ein T-Shirt bestellt wird: Vertragsabwicklung, Zuteilung des bestellten T-Shirts</li>
+			<li>Konferenzteilnehmer:innen-Zusatzveranstaltung: Vertragsabwicklung, Feststellung der Teilnahmeberechtigung</li>
+			<li>Konferenzteilnehmer:innen-OSM-Nickname: Konferenznamensschild</li>
+			<li>Konferenzteilnehmer:innen-Communityzugehörigkeit, Communitybeteiligung: Beurteilung Gewährung von Teilnahmevergünstigungen</li>
+			<li>Cookies: Gewährleistung eines ordnungsgemäßen Bestellvorgangs</li>
+			<li>Zahlungsinformationen Paypal: Abwicklung des Bezahlvorgangs, Vertragsabwicklung</li>
+			<li>Zahlungsinformation Überweisung: Vertragsabwicklung.</li>
+			<li>Lebensmittelallergie: Weitergabe dieser Information in anonymisierter Form zur Berücksichtigung im Rahmen der Konferenzverpflegung. </li>
+		</ol>
+
+		<h4>Rechtsgrundlage der Verarbeitung</h4>
+		<ol class="list">
+  			<li>Konferenzteilnehmer:innenstammdaten, Sponsorenstammdaten, Ausstellendestammdaten: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+			<li>Konferenzteilnehmer:innen-Konfektionsgröße: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+			<li>Konferenzteilnehmer:innen-Zusatzveranstaltung: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+			<li>Konferenzteilnehmer:innen-OSM-Nickname: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+			<li>Konferenzteilnehmer:innen-Communityzugehörigkeit, Communitybeteiligung: berechtigte Interesse, Art. 6 Abs. 1 lit. f) DSGVO. Das berechtigte Interesse besteht in der OSM-Communityvernetzung durch den FOSSGIS e. V. als Local Chapter Germany und der Beurteilung einer Gewährung von Teilnahmevergünstigungen an der Konferenz.</li>	
+ 			<li>Cookies: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO i.V.m. § 25 Abs. 2 Nr. 2 TTDSG, berechtigte Interesse, Art. 6 Abs. 1 lit. f) DSGVO. Das berechtigt Interesse besteht in der ordnungsgemäßen Abwicklung eines Bestellvorgangs.</li>
+ 			<li>Zahlungsinformationen Paypal: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+ 			<li>Zahlungsinformationen Überweisung: Verarbeitung zur Erfüllung eines Vertrags, Art. 6 Abs. 1 lit. b) DSGVO</li>	
+ 			<li>Gesundheitsdaten: Einwilligung gem Art. 9 Abs.2 lit. a) DSGVO</li>
+		</ol>
+
+
+		<h4>Empfänger personenbezogener Informationen</h4>
+		<ol class="list">
+  			<li>Paypal: Die Bezahlungsmethode Paypal wird von der PayPal (Europe) S.à r.l. & Cie, S.C.A., 5. Etage, 22-24 Boulevard Royal, L-2449 Luxembourg abgewickelt.</li>
+		</ol>
+
+
+
+		<h4>Speicherdauer personenbezogener Informationen</h4>
+		<ol class="list">
+  			<li>Konferenzteilnehmer:innenstammdaten, Sponsorenstammdaten, Ausstellendestammdaten: Die Daten werden im Rahmen der gesetzlichen Vorgaben und Verjährungsregelungen gespeichert.</li>
+  			<li>Konferenzteilnehmer:innen-Konfektionsgröße:  Die Daten werden nach Veranstaltungsende gelöscht.</li>
+  			<li>Konferenzteilnehmer:innen-Zusatzveranstaltung: Die Daten werden aus der Datenbank nach Veranstaltungsende gelöscht.</li>
+  			<li>Konferenzteilnehmer:innen-OSM-Nickname: Die Daten werden aus der Datenbank nach Veranstaltungsende gelöscht.</li>
+  			<li>Konferenzteilnehmer:innen-Communityzugehörigkeit, Communitybeteiligung: Die Daten werden im Rahmen der gesetzlichen Vorgaben und Verjährungsregelungen gespeichert.</li>
+  			<li>Cookies: 2 Stunden</li>
+  			<li>Zahlungsinformationen Paypal: Löschung nach Übertragung an PayPal</li>
+  			<li>Zahlungsinformationen Überweisung: Die Daten werden im Rahmen der gesetzlichen Vorgaben und Verjährungsregelungen gespeichert.	</li>
+  			<li>Gesundheitsdaten: Die Informationen zu Lebensmittelallergien werden 2 Wochen nach Konferenzende gelöscht.</li>
+		</ol>
+
+
+		<h4>Pflicht zur Bereitstellung</h4>
+		<ol class="list">
+  			<li>Konferenzteilnehmer:innenstammdaten, Sponsorenstammdaten, Ausstellendestammdaten: Die Angabe der Daten ist notwendig für den Vertragsschluss. Werden die Daten durch den Betroffenen nicht angegeben, erfolgt kein Vertragsschluss, eine Teilnahme an der Konferenz ist dann nicht möglich.</li>
+  			<li>Konferenzteilnehmer:innen-Konfektionsgröße: Die Angabe der Daten ist im Rahmen des Vertragsschlusses optional.</li>
+  			<li>Konferenzteilnehmer:innen-Zusatzveranstaltung: Die Angabe der Daten ist im Rahmen des Vertragsschlusses optional. Eine Teilnahme an der Veranstaltung ist aber nur bei Vorliegen einer Anmeldung möglich.</li>
+  			<li>Konferenzteilnehmer:innen-Communityzugehörigkeit (Nickname), Communitybeteiligung: Die Angabe der Daten ist im Rahmen des Vertragsschlusses optional. Die Angabe ist notwendige Voraussetzung für die Beurteilung einer möglichen Gewährung einer Vergünstigung.</li>
+  			<li>Cookies: Das akzeptieren ist zur Abwicklung des Bestellvorgangs notwendig. Eine Konferenzteilnahme (Vertragsschluss) ist ohne die Akzeptanz der Cookies nicht möglich.</li>
+  			<li>Zahlungsinformationen Paypal: Die Angabe der Daten ist im Rahmen des Vertragsschlusses optional.</li>
+		</ol>
+
+
+		<h3>Ihre Rechte</h3>
+		<h4>Auskunft, Sperrung, Löschung</h4>
+		<p>Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung, Löschung und Widerspruch gegen die Verarbeitung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit an uns wenden.</p>
+		
+		<h4>Widerruf einer Einwilligung</h4>
+		<p> Sie haben das Recht auf jederzeitigen Widerruf einer erteilten Einwilligung (Artikel 7 DSGVO).</p>
+
+		<h4>Recht auf Datenübertragbarkeit</h4>
+		<p>Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an einen Dritten in einem gängigen, maschinenlesbaren Format aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.</p>
+
+		<h4>Beschwerderecht bei der zuständigen Aufsichtsbehörde</h4>
+		<p>Im Falle datenschutzrechtlicher Verstöße steht dem Betroffenen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.<br>Zuständige Aufsichtsbehörde in datenschutzrechtlichen Fragen für den FOSSGIS e.V.:<br><br>
+
+Berliner Beauftragte für Datenschutz und Informationsfreiheit<br>
+Friedrichstr. 219<br>
+10969 Berlin<br>
+Tel.: +49 (0)30 13889-0<br>
+E-Mail: mailbox@datenschutz-berlin.de<br>-->
+
+	<?php include "inc/footer.inc"; ?>
+
+</body>
+
+</html>
---
+++ 2025/index.php
@@ -0,0 +1,354 @@
+<!doctype html>
+<html>
+<head>
+	<?php include "inc/meta.inc"; ?>
+<base href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+   	<title>FOSSGIS 2024</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css" />
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+      <h1>Willkommen zur FOSSGIS-Konferenz 2025</h1>
+
+            <p>Die FOSSGIS-Konferenz ist im D-A-CH-Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme sowie für die Themen Open Data und OpenStreetMap.</p>
+
+            <p>Die FOSSGIS-Konferenz 2025 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> in Kooperation mit der <a href="https://www.">Universität Münster im Schloss Münster statt.</p>
+
+      <div>    <ul class="tiles">
+				<li class="tile silver">
+                     <a href="./sponsor/fossgis-ev.php">
+                         <img src="./img/FOSSGISev_ohneVerlauf.png" alt="FOSSGIS e.V."  align="center">
+                     </a>
+                 </li>
+
+                 <li class="tile silver">
+                     <a href="https://www.openstreetmap.de/">
+                         <img src="./img/osm.png" alt="OSM Community" align="center">
+                     </a>
+                 </li>                                   
+                 <li class="tile silver">
+                     <a href="https://www.osgeo.org/">
+                         <img src="img/osgeo.png" Alt="OSGeo" align="center">
+					 </a>
+				 </li>
+	 </ul></div>
+	 <div> <ul class="tiles">
+  		 <li class="tile silver">
+		     <a href="https://www.">
+			 <img src="./img/" alt="Uni Muenster" align="center">
+					 </a>
+                 </li>  
+	  
+            </ul> </div>
+           
+            <h3>Was ist die FOSSGIS Konferenz?</h3>
+
+            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme sowie Open Data. Hier treffen sich Anwender:innen und Entwickler:innen zum gemeinsamen Austausch über Anwendungs-, Arbeits- und Weiterentwicklungsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. Es geht um Webmapping, Geodateninfrastrukturen, Geodatenmanagement,  Geoprocessing, Geodatenanalysen und -modellierungen und Desktop-GIS und um freie Geodaten, wie OpenStreetMap.
+Themen wie Digitale Souveränität im Zusammenhang mit Open Source werden ebenso behandelt, wie Open Science und die FAIR Prinzipien.</p> 
+
+
+			<p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement  getragen, wobei neben den Teilnehmergebühren, die Spenden- und vor allem die Sponsoren- und Ausstellerstandeinnahmen eine wichtige finanzielle Einnahmequelle darstellen, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p> 
+			
+<!--	    <h3 id="Sponsoren" name="Sponsoren">Sponsoren der FOSSGIS-Konferenz</h3>
+			<p> Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich.</p><br>
+
+			<h4 id="Platin" name="Platin" class="highlight">Platinsponsoren</h4>
+			<ul class="tiles">
+			<li class="tile platin">
+			  <a href="sponsor/001_wheregroup.php" target="_blank">
+			  <img src="./img/s/001_WhereGroup.jpg" alt="WhereGroup">
+			  </a>
+			</li>
+			
+			
+			<li class="tile platin">
+			  <a href="sponsor/002_camp2camp.php" target="_blank">
+ 			  <img src="./img/s/002_camptocamp.png" alt="camptocamp">
+			  </a>
+			</li>
+							
+			<li class="tile platin">
+			  <a href="sponsor/003_dataport.php" target="_blank">
+			  <img src="./img/s/003_dataport.png" alt="Dataport">
+			  </a>
+			</li>
+			</ul>
+<!--					<li class="tile platin">
+						<a href="sponsor/003_hu-berlin.php" target="_blank">
+						<img src="./img/s/003_hu_siegel-kombi_rgb.png" alt="HU-Berlin">
+						</a>
+					</li>
+
+				</ul>	-->
+
+<!--			<h4 id="Gold" name="Gold" class="highlight">Goldsponsoren</h4>
+			<ul class="tiles">
+			<li class="tile gold">
+			<a href="sponsor/101_bkg.php" target="_blank">
+			<img src="./img/s/101_BKG_Logo_RGB.png" alt="BKG">
+			</a>
+			</li>	
+			<li class="tile gold">
+			<a href="sponsor/102_LGV.php" target="_blank">
+			<img src="./img/s/102_LGV.png" alt="LGV">
+			</a>
+			</li>	
+			<li class="tile gold">
+			<a href="sponsor/103_OPENGis_CH.php" target="_blank">
+			<img src="./img/s/103_Logo_QFieldCloud-by-OpenGIS_transparent.png" alt="opengis">
+			</a>
+			</li>	
+			<li class="tile gold">
+			<a href="sponsor/104_NRW-Mobidrom.php" target="_blank">
+			<img src="./img/s/104_Mobidrom_Logo.png" alt="NRW.Mobidrom">
+			</a>
+			</li>	
+			</ul>
+
+			<h4 id="Silber" name="Silber" class="highlight">Silbersponsoren</h4>
+			<ul class="tiles">
+			<li class="tile silver">
+			<a href="sponsor/201_HBT.php" target="_blank">
+			<img src="./img/s/201_HBT.png" alt="HBT">
+			</a>
+			</li>	
+			</ul>
+
+	<h4 id="Bronze" name="Bronze" class="highlight">Bronzesponsoren</h4>
+		<ul class="tiles">
+			<li class="tile bronze">
+			<a href="sponsor/401_opencage.php" target="_blank">
+			<img src="./img/s/401_opencage-logo.png" alt="OpenCage">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/402_terrestris.php" target="_blank">
+			<img src="./img/s/402_terrestris.png" alt="terrestris">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/403_mundialis.php" target="_blank">
+			<img src="./img/s/403_mundialis.png" alt="mundialis">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/404_52north.php" target="_blank">
+			<img src="./img/s/404_52North.png" alt="52°North Spatial Information Research GmbH">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/405_CONET-ISB.php" target="_blank">
+			<img src="./img/s/405_ISB_CONET.jpg" alt="CONET ISB GmbH">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/406_gbd.php" target="_blank">
+			<img src="./img/s/406_GBD.png" alt="Geoinformatikbüro Dassau">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/407_grit.php" target="_blank">
+			<img src="./img/s/407_grit.png" alt="grit GmbH ">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/410_geoSYS.php" target="_blank">
+			<img src="./img/s/410_geoSYS_logo.png" alt="geoSYS">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/408_latlon.php" target="_blank">
+			<img src="./img/s/408_logo_latlon_web.png" alt="latlon">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/409_sourcepole.php" target="_blank">
+			<img src="./img/s/409_sp_fossgis_2023.png" alt="sourcepole">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/411_komoot.php" target="_blank">
+			<img src="./img/s/411_komoot.png" alt="komoot">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/412_norBIT.php" target="_blank">
+			<img src="./img/s/412_norBIT.png" alt="norBIT">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/413_indiscale.php" target="_blank">
+			<img src="./img/s/413_indiscale.png" alt="indiscale">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/414_TomTom.php" target="_blank">
+			<img src="./img/s/414_TomTom.png" alt="TomTom North America Inc">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/415_gkg.php" target="_blank">
+			<img src="./img/s/415_gkg_logo.png" alt="gkg">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/416_geOps.php" target="_blank">
+			<img src="./img/s/416_geOps.png" alt="geOps AG">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/417_geofabrik.php" target="_blank">
+			<img src="./img/s/417_Geofabrik_logo4.png" alt="geofabrik">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/418_ioki.php" target="_blank">
+			<img src="./img/s/418_ioki.png" alt="ioki">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/419_lehmann.php" target="_blank">
+			<img src="./img/s/419_Ginger_L-P.png" alt="ioki">
+			</a>
+			</li>
+		</ul>
+	
+
+<!--
+					
+					<li class="tile bronze">
+						<a href="sponsor/410_GEOINFO.php" target="_blank">
+						<img src="./img/s/410_Geoinfo_Logo.png" alt="GEOINFO">
+						</a>
+					</li>
+					
+
+
+					<li class="tile bronze">
+						<a href="sponsor/406_wagner-it.php" target="_blank">
+						<img src="./img/s/406_Wagner-IT.png" alt="Wagner-IT">
+						</a>
+					</li>
+
+
+
+
+					<li class="tile bronze">
+						<a href="sponsor/413_DBG.php" target="_blank">
+						<img src="./img/s/413_DBG.jpg" alt="d.b.g. Datenbankgesellschaft mbH">
+						</a>
+					</li>
+
+					<li class="tile bronze">
+						<a href="sponsor/414_LandPlanOS.php" target="_blank">
+						<img src="./img/s/414_LandPlanOS.png" alt="LandPlan OS GmbH">
+						</a>
+					</li>
+
+					
+<!--				<li class="tile bronze">
+						<a href="sponsor/408_gis-ops.php" target="_blank">
+						<img src="./img/s/408_gis_ops_logo.png" alt="GIS • OPS UG">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/411_NTI_CWSM_GmbH.php" target="_blank">
+						<img src="./img/s/411_nti_cwsm.png" alt="NTI CWSM GmbH">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/413_meggsimum.php" target="_blank">
+						<img src="./img/s/413_meggsimum-logo.svg" alt="meggsimum">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/414_latlon.php" target="_blank">
+						<img src="./img/s/414_logo_latlon_web.png" alt="latlon">
+						</a>
+					</li>
+					<li class="tile bronze">
+						<a href="sponsor/415_geocockpit.php" target="_blank">
+						<img src="./img/s/415_YEY-Maps_Logo.png" alt="GeoCockpit UG">
+						</a>
+					</li>
+
+
+
+					
+				</ul>-->
+<!--			<h4 id="Medienpartner" name="Medienpartner"class="highlight">Medienpartner</h4>
+				<ul class="tiles">
+					<li class="tile media">
+						<a href="https://live.osgeo.org/de/index.html" target="_blank">
+						<img src="./img/s/501_OSGeoLive_banner.png" alt="OSGeoLive">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://c3voc.de/" target="_blank">
+						<img src="./img/s/502_voc_square.png" alt="C3VOC">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://av.tib.eu/search?f=publisher%3Bhttp://av.tib.eu/resource/FOSSGIS_e.V." target="_blank">
+						<img src="./img/s/503_TIB.PNG" alt="TIB">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://www.oreilly.de/" target="_blank">
+						<img src="./img/s/506_OReilly_logo.jpg" alt="O'Reilly">
+						</a>
+					</li>
+					<li class="tile media">
+					<a href="https://www.gogeogo.com/de" target="_blank">
+					<img src="./img/s/507_GGGlogo.svg" alt="GoGeoGo">
+             
+       
+					</a>
+					</li>
+					
+					</ul>
+			<h4 id="orangerStreifen" name="orangerStreifen" class="highlight"></h4>
+
+					<!--<li class="tile media">
+						<a href="https://liberapay.com/johnjohndoe/" target="_blank">
+						<img src="./img/s/504_EventFahrplan.png" alt="FahrplanAPP">
+						</a>
+					</li>-->
+
+
+<!--				</ul>
+
+			<h4 id="Eventpartner" name="Eventpartner"class="highlight">Eventpartner</h4>
+				<ul class="tiles">
+
+					<li class="tile media">
+						<a href="https://23.foss-backstage.de/" target="_blank">
+						<img src="./img/s/507_FOSSBack_logo_2023.png" alt="FOSSBackStage">
+						</a>
+					</li>-->
+				</ul>
+
+			</ul>
+
+<!--			<h3 id="Jobwand" >Jobbörse</h3>
+			<p>Es gibt wieder eine Jobbörse im Rahmen der Konferenz. Dafür stellen wir eine Pinnwand ins Foyer, dort können Stellenanzeigen angebracht werden. In Kooperation mit GoGeoGo gibt es vom 11.03.-01.04.2024 eine digitale Jobbörse. Beim Eintragen bitte die Auftragsnummer FOSSGIS24 eintragen, dann erscheint die Stellenanzeige in der <a href="https://www.gogeogo.com/de/company/fossgis2024" target="_blank"> FOSSGIS 2024 Jobbörse</a>.
+					</p>
+					
+			<h4 id="orangerStreifen" name="orangerStreifen" class="highlight"></h4>
+
+			
+			<h3 ID="Sponsoring" name="Sponsoring">Werden Sie Sponsor!</h3> 
+           	  	  <p>Sie möchten die FOSSGIS-Konferenz mit einem Sponsoring unterstützen? Sie finden Informationen für Sponsoring und Ausstellung in der <a href="https://files.fossgis.de/Konferenz/2024/Sponsoreninformationen_FOSSGIS-2024.pdf">Sponsoringbroschüre</a>. Falls Sie sich für ein Sponsoring entschieden haben, füllen Sie die <a href="https://files.fossgis.de/Konferenz/2024/FOSSGIS-Konferenz-Sponsoring-Vereinbarung_2024.pdf">Sponsoringvereinbarung</a> aus.</p>
+			<p>Bei Fragen wenden Sie sich an das <a href="mailto:konferenz-orga@fossgis.de">Konferenzorganisationsteam</a>.</p>  -->
+  
+        <?php include('inc/footer.inc'); ?>
+ 
+</body>
+</html>
