a3a09b918b65d55793d2b5feb4d6a26cd9356861
katjahafi <katjahafi@gmx.de>
2024-07-30 13:14:28 +0200

    Programmseite und helfen Seite erstellt

---
+++ 2025/programm/FahrplanAPP.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2024 - Fahrplan-APP</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2024">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+			
+		<div class="row">
+		<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+		<h3 id="APP" name="APP">Android-Fahrplan-App</h3>
+			<p>Die Fahrplan-App für das FOSSGIS2024 Programm ist für Android Smartphone und Tablet verfügbar und hier <a href="https://t1p.de/mi1o">im Playstore downloadbar</a>.</p>
+			<p> Hier ein paar Ansichten, probieren Sie es einfach selbst aus.</p>
+			
+			<p><img src="./programm/img/App_Beschreibung_Smartphone_1.png" class="img-fluid" style="margin-rigth: 250px" alt="APP"></p>
+			<p><img src="./programm/img/App_Beschreibung_Smartphone_2.png" class="img-fluid" style="margin-rigth: 250px" alt="APP"></p>	
+			<p>Die App funktioniert auch auf dem Tablet wunderbar und Sie haben alles in der Übersicht.</p>
+			<p><img src="./programm/img/App_Beschreibung_Tablet_01.png" class="img-fluid" style="margin-rigth: 250px" alt="APP"></p>
+			<p><img src="./programm/img/App_Beschreibung_Tablet_02.png" class="img-fluid" style="margin-rigth: 250px" alt="APP"></p>				
+			<p>Sie helfen auf der FOSSGIS und haben eine <a href="https://helfen.fossgis.de/">Helferschicht gebucht</a>? <br>
+			Dann integrieren Sie es in die App und behalten den Überblick.</p>
+			<p><img src="./programm/img/App_Beschreibung_Engelschicht.png" class="img-fluid" style="margin-rigth: 250px" alt="APP"></p></div>
+					
+		</div>	
+		</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
---
+++ 2025/programm/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2025 - Programm</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<script type="text/javascript" src="https://pretalx.com/fossgis2025/widgets/schedule.js"></script>
+</head>
+
+<body id="programm">
+	<?php include "../inc/header.inc"; ?>
+
+<!--        <h2 id="Programm" name="Programm"class="Programm">Programm</h2>
+	<p>Das Programmkomitee hat aus den Einreichungen ein Konferenzprogramm gestaltet. Hier gelangen Sie zum Programm oder scrollen Sie nach unten: <a href="https://pretalx.com/fossgis2024/schedule/" target="_blank" rel="noopener nofollow">https://pretalx.com/fossgis2024/schedule/</a>.</p>
+	<ul>
+	<li><p><b>Vortragende:</b> <a href="https://pretalx.com/fossgis2024/speaker/">https://pretalx.com/fossgis2024/speaker/</a></p></li>
+	<li><p><b>Highlights:</b> <a href="https://pretalx.com/fossgis2024/featured/">https://pretalx.com/fossgis2024/featured/</a></p></li>
+	<li><p><b>Community Voting:</b> über 300 Teilnehmende haben sich beteiligt, Vielen Dank!</li>
+	<li><p><b>Call for participation</b> zum Nachlesen: <a href="https://fossgis-konferenz.de/2024/callforpapers/">/2024/callforpapers/</a></p></li>
+	</ul>
+
+	
+   	<h3>Vortragsaufzeichnungen</h3>
+	<p>Die Vorträge und Demosessions sind aufgezeichnet und veröffentlicht:</p>
+		<ul>
+			<li> <a href="https://pretalx.com/fossgis2024/schedule/" target="_blank" rel="noopener nofollow">Konferenz-Programm</a></p>
+			</li>
+			<li> <a href="https://media.ccc.de/c/fossgis2024" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2024</a></p>
+			</li>
+<!--			<li> <a href="https://av.tib.eu/series/1220/fossgis+konferenz+2022+online+event+09+12+marz+2022" target="_blank" rel="noopener nofollow">TIB</a></p>
+			</li>
+			<li> <a href="https://www.youtube.com/user/fossgis" target="_blank" rel="noopener nofollow">YouTube</a></p>
+			</li>-->
+		</ul>
+
+<!--	<h3 id="Tagungsband" name="Tagungsband"class="Tagungsband">Tagungsband</h3>
+	<p>Der Tagungsband zur Konferenz steht <a href="https://files.fossgis.de/Konferenz/2024/fossgis_tagungsband_2024_digital.pdf">als PDF zum Download</a> zur Verfügung.
+
+		<h3>Programm-APP für Android</h3>
+		<p>Die Android-App für das Konferenzprogramm ist im <a href="https://t1p.de/mi1o">Playstore downloadbar</a>. </p>
+
+	<!--<h3>FOSSGIS-Konferenz im "gewohnten" Livestream</h3>
+	<p>Die FOSSGIS 2024 wird auch im gewohnten Livestream übertragen: <a href="https://media.ccc.de/"> https://media.ccc.de/c/fossgis2024</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink zur Online-Platform im Bestelllink.</p>-->
+
+<!--	<h3>FOSSGIS-Konferenz per Android-APP</h3>
+		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht zum Download <a href="https://f-droid.org/packages/info.metadude.android.fossgis.schedule">in F-Droid</a> und <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">im Playstore</a> und  bereit. <a href="./programm/FahrplanAPP.php">Hier</a> sind ein paar Vorschaubilder zu finden. </p>
+	
+		<h3 id="Programm24" name="Programm24"class="Programm24">Programm FOSSGIS 2024</h3>
+<pretalx-schedule event-url="https://pretalx.com/fossgis2024/" locale="de-formal" format="grid" style="--pretalx-clr-primary: #EE7F00"></pretalx-schedule>
+<noscript>
+   <div class="pretalx-widget">
+        <div class="pretalx-widget-info-message">
+            JavaScript is disabled in your browser. To access our schedule without JavaScript,
+            please <a target="_blank" href="https://pretalx.com/fossgis2024/schedule/">click here</a>.
+        </div>
+    </div>
+</noscript>-->
+
+
+	<h3>Programm FOSSGIS 2025</h3>	
+	<p>Freuen Sie sich schon jetzt auf ein spannendes Programm, dieses wird noch vor Weihnachten 2024 hier veröffentlicht.<br>
+	<p>Der Call for Participation startet spätestens am 05. Oktober 2024.</p>
+	
+
+<!--		<p>Die Einreichungsfrist für Beiträge zum Programm ist vorbei. 
+		Hier ist der <a href="https://fossgis-konferenz.de/2024/callforpapers/">Call for Participation</a> zum Nachlesen verlinkt.</p>
+	<p>Bis zum 24.11.2023 ist es für die Community möglich über die Einreichungen abzustimmen. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.  <br>Vielen Dank fürs Mitmachen!</p>
+	<p><b>Vorgehensweise:</b></p>
+	<ul>
+		<li>1. Den Link zur Öffentlichen Abstimmung klicken, um sich dafür anzumelden: <a href="https://pretalx.com/fossgis2024/p/voting/signup/"> https://pretalx.com/fossgis2024/p/voting/signup/</a></li>
+		<li>2. Sie erhalten eine E-Mail mit Ihrem persönlichen Link zur Öffentlichen Abstimmung</li>
+		<li>3. Einreichungen anschauen und bewerten- dies kann in Etappen erfolgen.</li>
+	</ul> -->
+
+		
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+
+</html>
