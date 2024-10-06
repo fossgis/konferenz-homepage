7b59be1537cff7d50895520dd83f87cf31a6d979
katjahafi <katjahafi@gmx.de>
2024-10-06 13:24:21 +0200

    AGB aktualisiert

--- 2025/agb.php
+++ 2025/agb.php
@@ -25,21 +25,22 @@ https://pretix.eu/fossgis/2025" target="_blank"> </a> verkauft. Für das Vertrag
 <h4>Konferenztickets für Verbraucher</h4>
 <p>Für den Verkauf der Konferenz-Tickets und Workshops an Verbraucher besteht für diese ein gesetzliches Widerrufsrecht gemäß § 312g BGB. In diesem Fall gilt nachfolgendes Widerrufsrecht:<br></p>
 
-<p>Sie haben das Recht, innerhalb von vierzehn Tagen ohne Angabe von Gründen den Vertrag über den Kauf eines Konferenz-Tickets und Workshopteilnahme zu widerrufen. Zur Wahrung der Frist ist es ausreichend, wenn Sie die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der Widerrufsfrist an eine der folgenden Kontaktmöglichkeiten senden: <br>
+<p>Sie haben das Recht, innerhalb von vierzehn Tagen nach Vertragsschluss ohne Angabe von Gründen den Vertrag über den Kauf eines Konferenz-Tickets und Workshopteilnahme zu widerrufen. Zur Wahrung der Frist ist es ausreichend, wenn Sie die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der Widerrufsfrist auf einem der folgenden Wege übermitteln: <br>
 
 <ul class="list">
 			<li> Email: konferenz-orga@fossgis.de</li>
 			<li> Telefon: +49 (0)30 62 93 2037 </li>
+			<li> Stornierungsmöglichkeit im Anmeldesystem </li>
 </ul>
 
 <p>Aus der Erklärung muss der Entschluss zum Widerruf des Vertrags eindeutig hervorgehen. Der Widerruf muss keine Begründung enthalten.</p>
 
-<h4>Stornierungsmöglichkeiten für Tickets</h4>
-<!--
-<p>Neben dem gesetzlichen Widerrufsrecht für Verbraucher, gelten die folgenden Stornierungsmöglichkeiten:<br>
-Eine kostenfreie Stornierung ist bis 3 Tage vor Konferenzbeginn möglich.
-Für die eine Stornierung senden Sie bitte eine E-Mail an: konferenz-orga@fossgis.de.</p>
-<p> in Überarbeitung</p>-->
+<h4>Stornierungsmöglichkeiten für Unternehmen</h4>
+
+<p>Für Unternehmen gelten die Widerrufsfristen analog zu denen für Verbraucher.</p>
+
+<h4>Vertragsschluss/ Sonderkündigung</h4>
+<p>Der FOSSGIS e.V. behält sich für Fälle von nicht oder nicht vollständig bezahlten Rechnungen von Konferenzteilnehmendne der vergangenen Jahre das Recht vor, den Vertragsschluss für die aktuelle Konferenz abzulehnen oder, soweit innerhalb der zahlungsfrist kein Zahlungseingang zu verzeichnen ist, mit Ablauf der Frist zu kündigen. Eine Zahlungserinnerung ist nicht erforderlich.
 
 <h3>Ausstellerpaket</h3>
 
@@ -63,7 +64,7 @@ Für die eine Stornierung senden Sie bitte eine E-Mail an: konferenz-orga@fossgi
 <h3>Bildaufzeichnungen</h3>
 <p>Auf der FOSSGIS-Konferenz werden üblicherweise Video-, Fernseh- und Fotoaufnahmen angefertigt. Dabei kann es vorkommen, dass Besucher abgelichtet werden. Jede:r Besucher:in erklärt mit der Teilnahme an der FOSSGIS-Konferenz sein/ihr Einverständnis, dass entsprechend Aufnahmen von ihm/ihr als Besucher:in im Rahmen der Konferenz ohne Vergütungsanspruch angefertigt und veröffentlicht werden dürfen.</p>
 
-<p>Ein entsprechender Hinweis ist auch auf den Tickets enthalten. Personenbezogene Daten werden unter Beachtung der geltenden datenschutzrechtlichen Bestimmungen verarbeitet: <a href="https://pretix.eu/fossgis/2022/privacy/" target="_blank">siehe Datenschutzerklärung</a> </p>
+<p>Ein entsprechender Hinweis ist auch auf den Tickets enthalten. Personenbezogene Daten werden unter Beachtung der geltenden datenschutzrechtlichen Bestimmungen verarbeitet: <a href="https://pretix.eu/fossgis/2025/privacy/" target="_blank">siehe Datenschutzerklärung</a> </p>
 
 <h3>Hausordnung</h3>
 <p>wird überarbeitet...Im Veranstaltungsgebäude gilt die <a href="https://www.">Hausordnung</a> der Universität Münster, diese ist von allen Teilnehmenden einzuhalten.</p>  
@@ -76,14 +77,17 @@ Für die eine Stornierung senden Sie bitte eine E-Mail an: konferenz-orga@fossgi
 Verbraucherschlichtungsstelle teilzunehmen.</p>
 
 <h3>Gerichtsstand</h3>
-<p>Ist der Vertragspartner Kaufmann, so ist ausschließlicher (auch internationaler) Gerichtsstand Berlin für alle sich aus dem Vertragsverhältnis unmittelbar oder mittelbar ergebenen Streitigkeiten.</p>
+<p>Ist die Vertragspartei der Gruppe der Kaufleute zugehörig oder eine juristische Person des öffentlichen Rechts, so ist ausschließlicher (auch internationaler) Gerichtsstand Berlin für alle sich aus dem Vertragsverhältnis unmittelbar oder mittelbar ergebenen Streitigkeiten.</p>
 
-<p>Dies gilt im Falle von grenzüberschreitenden Verträgen auch für Nichtkaufleute.</p>
+<p>Dies gilt im Falle von grenzüberschreitenden Verträgen auch für natürliche Personen.</p>
 
 <h3>Anwendbares Recht</h3>
 <p>Es gilt ausschließlich das Recht der Bundesrepublik Deutschland unter Ausschluss des UN-Kaufrechts.</p>
 
-<p>Stand der AGB: 02. Juni 2024</p>
+<!--Salvatorische Klausel
+Salvatorische Klausel sind weitestgehend mit § 306 BGB bzw. § 306a BGB unvereinbar (ERsetzungs- und Reduktionsklauseln)
+-->
+<p>Stand der AGB: 06. Oktober 2024</p>
 
 	<?php include "inc/footer.inc"; ?>
 
