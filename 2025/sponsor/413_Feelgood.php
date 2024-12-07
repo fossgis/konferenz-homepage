246474763d5bbd04af62c5a95043bb9f1771d851
katjahafi <katjahafi@gmx.de>
2024-12-07 15:31:51 +0100

    Feelgood Geosolutions GmbH

---
+++ 2025/sponsor/413_Feelgood.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2025 - Feelgood Geosolutions GmbH</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2025#Bronze">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+			
+			<img src="./img/s/413_FeelGood_logo.svg" 
+				class="sponsor-logo" 
+				alt="Firmenlogo">
+			
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3>Feelgood Geosolutions GmbH</h3>
+
+					<p>Firmenbeschreibung folgt in Kürze					</p>
+				</div>
+				<div class="col-lg-3">
+					<div class="mb-4">
+						<h6 class="mb-0">Kontakt<br>
+							Webseite:<br>
+						<a href="https://www.feelgoodgeosolutions.de/">
+							https://www.feelgoodgeosolutions.de/</a><br><br>
+
+							E-Mail: <br>
+							<a href="mailto:info@feelgood-gs.de">info@feelgood-gs.de</a><br></h6>
+					</div>
+
+										
+					<div class="social-icon">
+<!--						<h6 class="font-weight-bold mb-0">Social</h6>
+						<ul>
+							<li>
+								<a href="https://mastodon.online/" target="_blank"><i class="fab fa-mastodon"></i></a>
+							</li>
+<li>
+								<a href="https://twitter.com/"  target="_blank"><i class="fab fa-x-twitter"></i></a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com//" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+							</li>
+							<li>
+								<a href="https://github.com/" target="_blank"><i class="fab fa-github"></i></a>
+							</li>
+							<li>
+			<a href="https://www.xing.com/" target="_blank"><i class="fab fa-xing"></i></a>
+			</li>
+						</ul>
+					</div>-->
+				</div>	
+
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
