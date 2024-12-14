8bf823542e282a456dfd010ff29d68e449bdc72f
katjahafi <katjahafi@gmx.de>
2024-12-14 17:42:07 +0100

    Programm veröffentlicht

--- 2025/programm/index.php
+++ 2025/programm/index.php
@@ -16,17 +16,7 @@
 <body id="programm">
 	<?php include "../inc/header.inc"; ?>
 
-<!--        <h2 id="Programm" name="Programm"class="Programm">Programm</h2>
-	<p>Das Programmkomitee hat aus den Einreichungen ein Konferenzprogramm gestaltet. Hier gelangen Sie zum Programm oder scrollen Sie nach unten: <a href="https://pretalx.com/fossgis2024/schedule/" target="_blank" rel="noopener nofollow">https://pretalx.com/fossgis2024/schedule/</a>.</p>
-	<ul>
-	<li><p><b>Vortragende:</b> <a href="https://pretalx.com/fossgis2024/speaker/">https://pretalx.com/fossgis2024/speaker/</a></p></li>
-	<li><p><b>Highlights:</b> <a href="https://pretalx.com/fossgis2024/featured/">https://pretalx.com/fossgis2024/featured/</a></p></li>
-	<li><p><b>Community Voting:</b> über 300 Teilnehmende haben sich beteiligt, Vielen Dank!</li>
-	<li><p><b>Call for participation</b> zum Nachlesen: <a href="https://fossgis-konferenz.de/2024/callforpapers/">/2024/callforpapers/</a></p></li>
-	</ul>
-
-	
-   	<h3>Vortragsaufzeichnungen</h3>
+<!--    <h3>Vortragsaufzeichnungen</h3>
 	<p>Die Vorträge und Demosessions sind aufgezeichnet und veröffentlicht:</p>
 		<ul>
 			<li> <a href="https://pretalx.com/fossgis2024/schedule/" target="_blank" rel="noopener nofollow">Konferenz-Programm</a></p>
@@ -49,38 +39,55 @@
 	<p>Die FOSSGIS 2024 wird auch im gewohnten Livestream übertragen: <a href="https://media.ccc.de/"> https://media.ccc.de/c/fossgis2024</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink zur Online-Platform im Bestelllink.</p>-->
 
 <!--	<h3>FOSSGIS-Konferenz per Android-APP</h3>
-		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht zum Download <a href="https://f-droid.org/packages/info.metadude.android.fossgis.schedule">in F-Droid</a> und <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">im Playstore</a> und  bereit. <a href="./programm/FahrplanAPP.php">Hier</a> sind ein paar Vorschaubilder zu finden. </p>
+		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht zum Download <a href="https://f-droid.org/packages/info.metadude.android.fossgis.schedule">in F-Droid</a> und <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">im Playstore</a> und  bereit. <a href="./programm/FahrplanAPP.php">Hier</a> sind ein paar Vorschaubilder zu finden. </p>-->
+
+	<h3 id="Programm25" name="Programm25"class="Programm25">Programm FOSSGIS 2025</h3>
+	<p>Gesamtüberblick Konferenzprogramm FOSSGIS 2025 – sofort ins aktuelle Programm springen: <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">Link zu Pretalx</a>. 
+	<img src="./img/Zeitplan_FOSSGIS-Konferenz_2025.png" width="950px" alt="Programmübersicht" style="margin-rigth: 500px" align="rigth">
+	</p>
+<p>Die FOSSGIS 2025 beginnt am <b>Mittwochvormittag, den 26. März 2025</b> mit der Eröffnungsveranstaltung und wird in vier parallen Tracks bis <b>Freitag</b> fortgesetzt. Es finden Workshops und Communitysessions sowie ein spannendes Rahmenprogramm statt.
+	<p> Am <b>Samstag, den 29. März 2025</b> findet der OSM-Samstag sowie der Community-Sprint statt. Am OSM-Samstag werden aktuelle Themen rund um OpenStreetMap diskutiert. Den Community-Sprint planen wir in Anlehnung an die OSGeo-Codesprints bei FOSS4G-Konferenzen.</p> 
+	 
+	<ul>
+	<li><p><b>Konferenz-Programm:</b> <a href="https://pretalx.com/fossgis2025/schedule/" target="_blank" rel="noopener nofollow">https://pretalx.com/fossgis2025/schedule/</a></li>
+	<li><p><b>Vortragende:</b> <a href="https://pretalx.com/fossgis2025/speaker/">https://pretalx.com/fossgis2025/speaker/</a></p></li>
+	<li><p><b>Highlights:</b> <a href="https://pretalx.com/fossgis2025/featured/">https://pretalx.com/fossgis2025/featured/</a></p></li>
+	<li><p><b>Community Voting:</b> über 150 Teilnehmende haben sich beteiligt, Vielen Dank!</li>
+	<li><p><b>Call for participation</b> zum Nachlesen: <a href="https://fossgis-konferenz.de/2025/callforpapers/">/2025/callforpapers/</a></p></li>
+	</ul>
 	
-		<h3 id="Programm24" name="Programm24"class="Programm24">Programm FOSSGIS 2024</h3>
-<pretalx-schedule event-url="https://pretalx.com/fossgis2024/" locale="de-formal" format="grid" style="--pretalx-clr-primary: #EE7F00"></pretalx-schedule>
+	<h2 id="programmquer" name="programmquer"class="Programm">Konferenz-Programm im Querformat</h2>
+	
+<pretalx-schedule event-url="https://pretalx.com/fossgis2025/" locale="de-formal" format="grid" style="--pretalx-clr-primary: #EE7F00" alt="Widget"></pretalx-schedule>
+<noscript>
+   	<div class="pretalx-widget">
+        <div class="pretalx-widget-info-message">
+            JavaScript is disabled in your browser. To access our schedule without JavaScript,
+            please <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">click here</a>.
+        </div>
+    </div>
+</noscript>
+
+	<h2 id="programmlist" name="programmlist"class="Programm">Konferenz-Programm als Liste</h2>
+
+<pretalx-schedule event-url="https://pretalx.com/fossgis2025/" locale="de-formal" format="list" style="--pretalx-clr-primary: #EE7F00"></pretalx-schedule>
 <noscript>
    <div class="pretalx-widget">
         <div class="pretalx-widget-info-message">
             JavaScript is disabled in your browser. To access our schedule without JavaScript,
-            please <a target="_blank" href="https://pretalx.com/fossgis2024/schedule/">click here</a>.
+            please <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">click here</a>.
         </div>
     </div>
-</noscript>-->
+</noscript>
 
-
-	<h3>Programm FOSSGIS 2025</h3>	
-	<p>Die FOSSGIS 2025 beginnt am <b>Mittwochvormittag, den 26. März 2025</b> mit der Eröffnungsveranstaltung und wird in vier parallen Tracks bis <b>Freitag</b> fortgesetzt. Es finden Workshops und Communitysessions sowie ein spannendes Rahmenprogramm statt.
-	<p> Am <b>Samstag, den 29. März 2025</b> findet der OSM-Samstag sowie der Community-Sprint statt. Am OSM-Samstag werden aktuelle Themen rund um OpenStreetMap diskutiert. Den Community-Sprint planen wir in Anlehnung an die OSGeo-Codesprints bei FOSS4G-Konferenzen.</p> 
-
-	
-	<h3>Call for Participation (CfP)</h3>
-<!--	<p>Der <a href="https://pretalx.com/fossgis2025/cfp">Call for Participation</a> für die FOSSGIS 2025 ist veröffentlicht. Einreichungen sind bis zum 05.11.2024 möglich, es ist keine Verlängerung vorgesehen.</p>-->
+<!--	<h3>Call for Participation (CfP)</h3>
+	<p>Der <a href="https://pretalx.com/fossgis2025/cfp">Call for Participation</a> für die FOSSGIS 2025 ist veröffentlicht. Einreichungen sind bis zum 05.11.2024 möglich, es ist keine Verlängerung vorgesehen.</p> 
 	
 
 	<p>Die Einreichungsfrist für Beiträge zum Programm ist vorbei. 
 	Hier ist der <a href="https://fossgis-konferenz.de/2025/callforpapers/">Call for Participation</a> zum Nachlesen verlinkt.<br> Danke an alle, die beim Community Voting mitgemacht haben. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.</p>
-<p> Freuen Sie sich schon jetzt auf ein spannendes Programm, dieses wird noch vor Weihnachten 2024 hier veröffentlicht.</p>
-
-	<h3>Programmübersicht</h3>
+<p> Freuen Sie sich schon jetzt auf ein spannendes Programm, dieses wird noch vor Weihnachten 2024 hier veröffentlicht.</p>-->
 	
-	<p>Die Programmübersicht soll einen Überblick zum Programm der FOSSGIS 2025 geben. 
-	<img src="./img/Zeitplan_FOSSGIS-Konferenz_2025.png" width="900px" alt="Programmübersicht" style="margin-rigth: 500px" align="rigth">
-	</p>
 		
 	<?php include "../inc/footer.inc"; ?>
 
