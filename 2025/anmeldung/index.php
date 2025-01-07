ad8df6a2052fdb4e36f4344ab8c121f63bbc61ea
katjahafi <katjahafi@gmx.de>
2025-01-07 09:12:56 +0100

    Anmeldeformular

--- 2025/anmeldung/index.php
+++ 2025/anmeldung/index.php
@@ -10,8 +10,8 @@
 	<link rel="stylesheet" href="./css/normalize.css">
 	<link rel="stylesheet" href="./css/base.css">
 	<link rel="stylesheet" href="./css/print.css" media="print">
-<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2025/widget/v1.css">
-<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2025/widget/v1.css" crossorigin>
+<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async crossorigin></script>
 </head>
 
 
@@ -76,10 +76,6 @@
 		<small>** Wenn bestellt und solange verfügbar.</small><br>
 		<small>*** Das kostenfreie Ticket ist an Bedingungen geknüpft und wird als personalisiertes Ticket herausgegeben. Grundsätzlich gilt, wer etwas zur FOSSGIS-Konferenz beiträgt, kann ein kostenfreies Konferenzticket erhalten. Ein adäquater Beitrag ist z.B. ein Vortrag (LT zählt nicht), Workshop, mind. 10 Stunden <a href="https://fossgis-konferenz.de/2025/helfen/">helfen</a> oder nach den Richtlinien der <a href="https://blog.openstreetmap.org/2020/08/20/active-osm-contributor-membership-program/">OSMF</a>-OpenStreetMap Foundation aktiv mappen (42 Mappingtage in den letzten 365 Tage) bzw. aktiv zur Entwicklung von freier GIS-Softwareentwicklung beitragen.</small></p>
 
-<!--	<b><i>Hygieneschutz</i></b>
-	<p>Ausschlaggebend für die Durchführung der Konferenz sind die entsprechend gültigen Hygienebestimmungen der HU-Berlin für Präsenzveranstaltungen. Grundlage ist der <a href="...</a>. Änderungen sind vorbehalten. </p>-->
-</ul>
-
 	<h3 id="Workshop" name="Workshop">Workshop buchen</h3>
 	<p>Das Programm bietet die Möglichkeit einen oder mehrere Workshops zu buchen. Für jeden Workshop sind 90 Minuten vorgesehen, der Preis beträgt 130 €. Einen Workshop können Sie im zweiten Schritt des Bestellvorgangs zum Konferenzticket hinzubuchen.<br>
 Der Workshop ist interaktiv und wird in einem mit Laptops ausgestattenen Raum stattfinden. Die meisten Workshops arbeiten mit der aktuellen <a href="http://live.osgeo.org/de/index.html">OSGeoLive.</a> Es finden keine Onlineworkshops statt.
@@ -88,22 +84,21 @@ Der Workshop ist interaktiv und wird in einem mit Laptops ausgestattenen Raum st
 	<p>Freiwillige Helfer:innen sind eingeladen und willkommen während der Konferenz zu unterstützen. Bei Interesse bitte die Webseite <a href="https://fossgis-konferenz.de/2025/helfen/#HelfenvorOrt">Helfen</a> lesen, dort sind viele Details erklärt.<br>Helfer:innen registrieren sich ab Ende Dezember im Helfer:innensystem. Wer mind. 10 Stunden hilft, kann ein kostenfreies Konferenzticket erhalten. </p>
 
 	<h3 id="Anmeldeformular" name="Anmeldeformular">Anmeldeformular</h3>
-	<p>Das Anmeldeformular erscheint hier Anfang Januar 2025.</p>
-<!--	<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, können Sie das Ticketsystem auch unter <a href="https://pretix.eu/fossgis/2024/" target="_blank">diesem Link</a> direkt aufrufen.</p>
+	<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, können Sie das Ticketsystem auch unter <a href="https://pretix.eu/fossgis/2025/" target="_blank">diesem Link</a> direkt aufrufen.</p>
 <!--	<p>Die FOSSGIS ist ausgebucht. Wir können keine weiteren Tickets für die Konferenz vor Ort in Berlin-Adlershof anbieten.
 Nutzen Sie die Möglichkeit einer Onlineteilnahme und buchen Sie ein Onlineticket.
 Sie erhalten Zugang zum Livestream der Vorträge mit Chat der Teilnehmenden sowie Fragenfunktion und Anwendertreffen, die im Seminarraum und per Videokonferenz stattfinden werden.
-Vielleicht schalten wir auch die Netzwerkwelt an und bieten damit einen Onlinetreffpunkt für Teilnehmende.</p>
+Vielleicht schalten wir auch die Netzwerkwelt an und bieten damit einen Onlinetreffpunkt für Teilnehmende.</p>-->
 	
 <div id="shop">
-		<pretix-widget event="https://pretix.eu/fossgis/2024/"></pretix-widget>
+		<pretix-widget event="https://pretix.eu/fossgis/2025/" single-item-select="button"></pretix-widget>
 <noscript>
    <div class="pretix-widget">
         <div class="pretix-widget-info-message">
-            JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2024/">hier</a>.
+            JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticketshop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2025/">hier</a>.
         </div>
     </div>
-</noscript>-->
+</noscript>
 		
 	<h3 id="Bildungszeit" name="Bildungszeit">Bildungszeit</h3>
 	<p>Die Anerkennung der FOSSGIS-Konferenz als Bildungszeit wird angestrebt und in verschiedenen Bundesländern in Deutschland vom Orgateam beantragt. </p>
