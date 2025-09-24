8801776653a87d3156544d69e8eccc586e5bd3b7
katjahafi <katjahafi@gmx.de>
2025-09-24 18:04:34 +0200

    Standortinfos, Karte und Hotelkontingentlink

---
+++ 2026/standort/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2026 - Standort, Anreise und Übernachtung</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./standort/script/leaflet.css"/>
+
+	<script src="./standort/script/leaflet.js"></script>
+	<script src="./standort/script/leaflet.addons.js"></script>
+	<!--<script src="./standort/data/buildings.js"></script>
+	<script src="./standort/data/hotels.js"></script>-->
+	<script src="./standort/script/map1.js"></script>
+</head>
+
+<body id="standort">
+
+	<?php include "../inc/header.inc"; ?>
+	
+	<!--<h1 ID="Standort" name="Standort">Standort</h1>-->
+	<h1 ID="Standort" name="Standort">Standort, Infos zu Anreise und Übernachtung</h1>
+
+    <div class="map-container">
+		<div id="map" class="map"></div>
+	</div>
+
+	<h3>Konferenzort Zentrales Hörsaalgebäude (Mittwoch bis Freitag)</h3>
+
+        <p><strong>Adresse:</strong><br>Zentrales Hörsaalgebäude (ZHG)<br> Georg-August-Universität Göttingen<br>
+Pl. d. Göttinger Sieben 5<br>
+37073 Göttingen<br>
+Karte: <a href="https://openstreetmap.de/karte/?zoom=18&lat=51.54092&lon=9.93581&layers=B00TT" target="_blank">OSM</a>
+<br></p>
+
+	<p>Die Konferenz findet im Zentralen Hörsaalgebäude der Uni Göttingen (ZHG) statt. Die Räumlichkeiten sind weitestgehend barrierefrei zugänglich. Hier ist ein Blick in den <a href="https://lageplan.uni-goettingen.de/?ident=5257_1_EG_0.103" target="_blank">Lageplan der Uni Göttingen</a> möglich. Für Fragen zum Erreichen der Räume, zögern Sie nicht das Konferenz-Team anzusprechen.
+	</p>
+	
+	<p>Die Vortragsräume sinnd alle im ZHG sowie das Foyer, wo Welcome Desk, Firmen- und Projekteausstellung, Pausenversorgung und Posterausstellung platziert werden.</p>
+
+	<h3>Konferenzort Institut für Geographie (Samstag)</h3>
+
+        <p><strong>Adresse:</strong><br>Fakultät für Geowissenschaften und Geographie<br> Georg-August-Universität Göttingen<br>
+Goldschmidtstr. 3-5<br>
+37073 Göttingen<br>
+Karte: <a href="https://openstreetmap.de/karte/?zoom=19&lat=51.5563&lon=9.94759&layers=B00TT" target="_blank">OSM</a></p>
+
+ 	<p>Am Institut für Geographie findet der OSM-Samstag und Community Sprint statt. Eine Voranmeldung ist erforderlich.
+
+ <!--      <h3 ID="Anreise" name="Anreise">Anreise mit öffentlichen Verkehrsmitteln</h3>
+        
+        <p><a href="https://www.veranstaltungsticket-bahn.de:443/?event=25170&amp;language=de" style=" width: 300px; height: 40px; display: inline-block; position: relative; text-decoration: none; cursor: pointer; margin: 0; padding: 0; zoom: 1;" target="_blank" title="Klick Veranstaltungsticket Deutsche Bahn"><span style=" display: block; margin: 0; padding: 0; position: relative; line-height: 0; overflow: visible;"><img style=" width: 100%; margin: 0; padding: 0; display: block;" src="standort/img/Button_Veranstaltungsticket.png" alt=""/></span></a></p>
+        <p><br>Veranstaltungsticket Deutsche Bahn: https://www.veranstaltungsticket-bahn.de/?event=25170&language=de</p>
+
+        <p>Nutzen Sie den ÖPNV in Göttingen oder gehen Sie zu Fuß, die Wege sind kurz.
+
+               
+        <h4>Anreise mit dem Auto</h4>
+        <p>Von einer Anreise mit dem Auto wird aufgrund fehlender Parkmöglichkeiten auf dem Uni Campus abgeraten.</p>-->
+
+        <h3>Unterkünfte und Hotels in Göttingen</h3>
+        <p><b>Serviceangebot Göttingen Marketing GmbH:</b> 
+		<ul>
+<li><a href="https://tportal.tomas.travel/tagungen-goettingen/ukv/?doSearch=1&ukv_result_order=1&number_adult=1&rate=NDS00020140610763429&date_from=24.03.2026&date_to=29.03.2026&reset=1&globalReset=1" target="_blank" >Hotelkontingente für FOSSGIS 2026</a></li>
+		<li>Die Zimmer müssen bis 4 Wochen vor der Konferenz gebucht werden, ansonsten gehen die Angebote an die Hotels zurück.</li>
+		</ul>
+<!--		<p><b>Angebote Privatzimmer:</b> 
+		<ul>
+		<li><a href="standort/data/Privatzimmer_Goettingen_Stand_31-03-2025.pdf" target="_blank">als PDF</a></li>
+		</ul>-->
+
+		<h4 ID="Anreise" name="Anreise">weitere Informationen zur Anreise und Standort folgen</h4>
+        
+
+
+ <?php include "../inc/footer.inc"; ?>
+
+</body>
+</html>
---
+++ 2026/standort/script/map1.js
@@ -0,0 +1,121 @@
+(function () {
+	'use strict';
+
+	const posZHG = [51.54154, 9.93555];
+	const posHbf = [51.53659, 9.92584];
+    const posGeoinst = [51.55638, 9.94739];
+
+	const zoom = 16;
+	const htwOrange = '#ff9d15';
+	const views = {
+		'<b>ZHG Göttingen</b> (Mittwoch – Freitag)': posZHG,
+        '<b>Geoinstitut</b> (Samstag)':posGeoinst,
+		'Bahnhof Göttingen': posHbf,
+        
+	};
+	const tooltipTemplate =
+		'<b>{name}</b><br>' +
+		'{street} {housenumber}'; //<br>' +
+		// '<a href="{website}">{website}</a><br>' +
+		//'<img src="./standort_anreise/img/{src}" alt="Bild {name}" class="tooltip-img" width="240" height="157">';
+
+	const featureOptions = {
+		color: '#333333',
+		weight: 1,
+		opacity: 1.0,
+		fillColor: htwOrange,
+		fillOpacity: 1.0,
+		radius: 12,
+		className: 'svg-feature'
+	};
+
+	const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	});
+
+	var campusLabels = L.layerGroup();
+	var campusLayer = L.geoJSON(window.buildings, {
+		style: featureOptions,
+		onEachFeature: onEachFeature
+	});
+	var hotelsLayer = L.geoJSON(window.hotels, {
+		pointToLayer: function (feature, latlng) {
+			var props = feature.properties;
+			return L.circleMarker(latlng, {
+				color: '#333333',
+				weight: 1,
+				opacity: 1.0,
+				fillColor: '#3388ff',
+				fillOpacity: .5,
+				radius: 8
+			}).bindTooltip(props.name);
+		}
+	});
+
+  	var mapOptions = {
+		layers: [
+			baseLayer,
+			campusLayer,
+			campusLabels,
+			hotelsLayer
+		],
+		center: posZHG,
+		zoom: zoom,
+		minZoom: 5,
+		maxZoom: 18,
+		
+	};
+	var div, map;
+
+	document.addEventListener('DOMContentLoaded', init);
+
+	function init() {
+		var baseLayers = { 'OSM': baseLayer };
+		var overlays = { 'Hotels': hotelsLayer };
+		var options = { collapsed: false, hideSingleBase: true };
+		var mapContainerQuery = '.map-container';
+		var mapId = 'map';
+
+		// setup
+		div = document.getElementById(mapId);
+		map = L.map(div, mapOptions);
+
+		// controls
+		L.control.layers(baseLayers, overlays, options).addTo(map);
+		L.control.grow({ mapContainerQuery: mapContainerQuery }).addTo(map);
+		L.control.panTo({ views: views }).addTo(map);
+	}
+
+	function createLabel(center, label, priority) {
+		var icon = L.divIcon({
+			className: 'label label-priority-' + priority,
+			iconAnchor: [8, 9],
+			html: label
+		});
+		return L.marker(center, {
+			icon: icon
+		});
+	}
+
+	function onEachFeature(feature, layer) {
+		var props = feature.properties;
+		var content = tooltipTemplate
+			.replaceAll('{name}',props['name:de'])
+			.replaceAll('{website}', props['website'])
+			.replaceAll('{street}', props['addr:street'])
+			.replaceAll('{housenumber}', props['addr:housenumber'])
+			.replaceAll('{postcode}', props['postcode']);
+			//.replace('{purpose}', props.purpose)
+			//.replace('{src}', props.bild)
+			//.replace('{title}', name);
+
+		layer.bindTooltip(content, { offset: [-5, 0] });
+
+		if (props.priority < 2 && props.label.length) {
+			var label = props.short_name;
+			var priority = props.priority;
+			var marker = createLabel(props.label, label, priority);
+			campusLabels.addLayer(marker);
+		}
+	}
+})();
