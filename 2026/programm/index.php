92d35db9e888f836c6621143ade057688799aeac
katjahafi <katjahafi@gmx.de>
2025-11-10 18:35:11 +0100

    CV auf KonfHP

---
+++ 2026/programm/index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2026 - Programm</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<script type="text/javascript" src="https://pretalx.com/fossgis2025/widgets/schedule.js"></script>
+</head>
+
+<body id="programm">
+	<?php include "../inc/header.inc"; ?>
+
+<!--   <h2>Vortragsaufzeichnungen</h2>
+	<p>Die Vorträge und Demosessions der FOSSGIS 2025 sind aufgezeichnet und veröffentlicht:</p>
+		<ul>
+			<li> <a href="https://pretalx.com/fossgis2025/schedule/" target="_blank" rel="noopener nofollow">Konferenz-Programm</a></p>
+			</li>
+			<li> <a href="https://media.ccc.de/c/fossgis2025" target="_blank" rel="noopener nofollow">media.ccc.de/fossgis2025</a></p>
+			</li>
+			<li> <a href="https://av.tib.eu/series/1220/fossgis+konferenz+2022+online+event+09+12+marz+2022" target="_blank" rel="noopener nofollow">TIB</a></p>
+			</li>
+			<li> <a href="https://www.youtube.com/user/fossgis" target="_blank" rel="noopener nofollow">YouTube</a></p>
+			</li>
+		</ul>-->
+
+<!--	<h2 id="Tagungsband" name="Tagungsband"class="Tagungsband">Tagungsband</h2>
+	<p>Der Tagungsband zur Konferenz steht <a href="https://files.fossgis.de/Konferenz/2025/fossgis_tagungsband_2025_digital.pdf">als PDF zum Download</a> zur Verfügung.
+
+		
+
+	<!--<h3>FOSSGIS-Konferenz im "gewohnten" Livestream</h3>
+	<p>Die FOSSGIS 2024 wird auch im gewohnten Livestream übertragen: <a href="https://media.ccc.de/"> https://media.ccc.de/c/fossgis2024</a>. Hier stehen keine Möglichkeiten zur Interaktion mit anderen Teilnehmer:innen zur Verfügung. Wer ein Konferenzticket hat, findet den Zugangslink zur Online-Platform im Bestelllink.</p>-->
+
+<!--	<h3 id="Programm25" name="Programm25"class="Programm25">Programm FOSSGIS 2025</h3>
+	<p>Gesamtüberblick Konferenzprogramm FOSSGIS 2025 – sofort ins aktuelle Programm springen: <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">Link zu Pretalx</a>. 
+	<img src="./img/Zeitplan_FOSSGIS-Konferenz_2025.png" "overflow-x="auto" alt="Programmübersicht" align="rigth">
+	</p>
+<p>Die FOSSGIS 2025 beginnt am <b>Mittwochvormittag, den 26. März 2025</b> mit der Eröffnungsveranstaltung und wird in vier parallen Tracks bis <b>Freitag</b> fortgesetzt. Es finden Workshops und Communitysessions sowie ein spannendes Rahmenprogramm statt.
+	<p> Am <b>Samstag, den 29. März 2025</b> findet der <a href="https://wiki.openstreetmap.org/wiki/FOSSGIS_2025/OSM-Samstag" target="_blank">OSM-Samstag</a> sowie der Community-Sprint statt. Am OSM-Samstag werden aktuelle Themen rund um OpenStreetMap diskutiert. Den Community-Sprint planen wir in Anlehnung an die OSGeo-Codesprints bei FOSS4G-Konferenzen.</p> 
+	 
+	<ul>
+	<li><p><b>Konferenz-Programm:</b> <a href="https://pretalx.com/fossgis2025/schedule/" target="_blank" rel="noopener nofollow">https://pretalx.com/fossgis2025/schedule/</a></li>
+	<li><p><b>Vortragende:</b> <a href="https://pretalx.com/fossgis2025/speaker/">https://pretalx.com/fossgis2025/speaker/</a></p></li>
+	<li><p><b>Highlights:</b> <a href="https://pretalx.com/fossgis2025/featured/">https://pretalx.com/fossgis2025/featured/</a></p></li>
+	<li><p><b>Community Voting:</b> über 150 Teilnehmende haben sich beteiligt, Vielen Dank!</li>
+	<li><p><b>Call for participation</b> zum Nachlesen: <a href="https://fossgis-konferenz.de/2025/callforpapers/">/2025/callforpapers/</a></p></li>
+	</ul>
+
+	<h4>FOSSGIS-Konferenz per Android-APP</h4>
+		<p>Die Android-App mit Fahrplan und Sponsoreninfos steht zum Download in <a href="https://f-droid.org/packages/info.metadude.android.fossgis.schedule">F-Droid</a> und <a href="https://play.google.com/store/apps/details?id=info.metadude.android.fossgis.schedule">Playstore</a> bereit. 
+<!--<a href="./programm/FahrplanAPP.php">Hier</a> finden Sie Vorschaubilder sowie eine Anleitung die eigene Helferschicht zu integrieren.--> </p>
+
+<!--	<h4> Workshop nachbuchen</h4>
+	<p>Sie wollen einen Workshop nachbuchen? <br>
+		Schreiben Sie eine <a href="mailto:konferenz-orga@fossgis.de?subject=Workshop_nachbuchen"">E-Mail</a> mit folgenden Angaben: Bestellnummer Ticketbuchung; Name Teilnehmer:in; Email-Adresse Teilnehmer:in; welcher Workshop. Eine Gebühr von 130 EUR wird pro Workshop erhoben.</p>-->
+	
+	<h2 id="programmquer" name="programmquer"class="Programm">Konferenz-Programm</h2>
+	
+<!--
+<pretalx-schedule event-url="https://pretalx.com/fossgis2025/" locale="de-formal" format="grid" style="--pretalx-clr-primary: #EE7F00" alt="Widget"></pretalx-schedule>
+<noscript>
+   	<div class="pretalx-widget">
+        <div class="pretalx-widget-info-message">
+            JavaScript is disabled in your browser. To access our schedule without JavaScript,
+            please <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">click here</a>.
+        </div>
+    </div>
+</noscript>
+
+<!--	<h2 id="programmlist" name="programmlist"class="Programm">Konferenz-Programm als Liste</h2>
+
+<pretalx-schedule event-url="https://pretalx.com/fossgis2025/" locale="de-formal" format="list" style="--pretalx-clr-primary: #EE7F00"></pretalx-schedule>
+<noscript>
+   <div class="pretalx-widget">
+        <div class="pretalx-widget-info-message">
+            JavaScript is disabled in your browser. To access our schedule without JavaScript,
+            please <a target="_blank" href="https://pretalx.com/fossgis2025/schedule/">click here</a>.
+        </div>
+    </div>
+</noscript>-->
+
+
+	
+
+	<p>Die Einreichungsfrist für Beiträge zum Programm ist vorbei. 
+	Hier ist der <a href="https://fossgis-konferenz.de/2026/callforpapers/">Call for Participation</a> zum Nachlesen verlinkt.<br> 
+
+	<h3 id="CV" name="CV"class="Programm">Community Voting</h3>
+	<p>Das Community-Voting (Öffentliche  Abstimmung) ist bis zum 17.11.2025 möglich. Das Programmkomitee lädt die Community ein die Einreichungen mit zu bewerten. Das Ergebnis dient dem Programm-Komitee als Meinungsbild aus der Community und wird in die Bewertung einbezogen.</p>
+	<ul><b>Vorgehensweise:</b><br>
+	 1. Den Link zur Öffentlichen Abstimmung klicken, um sich dafür anzumelden: https://pretalx.com/fossgis2026/p/voting/signup/<br>
+	 2. Sie erhalten eine E-Mail mit Ihrem persönlichen Link zur Öffentlichen  Abstimmung<br>
+	 3. Einreichungen anschauen und bewerten - dies kann in Etappen erfolgen.<br>
+	</ul>
+
+<!--Danke an alle, die beim Community Voting mitgemacht haben. Das Ergebnis wird vom Programm-Komitee als Meinungsbild aus der Community in die Bewertung einbezogen.</p>-->
+
+<p> Freuen Sie sich schon jetzt auf ein spannendes Programm, dieses wird noch vor Weihnachten 2025 hier veröffentlicht.</p>
+		
+	<?php include "../inc/footer.inc"; ?>
+
+</body>
+
+</html>
