f874559377e29112747b96427c8beff71f3883a0
katjahafi <katjahafi@gmx.de>
2025-12-04 18:12:14 +0100

    Anmeldeseite erstellt

---
+++ 2026/anmeldung/bildungszeit.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2025 - Bildungsurlaub</title>
+
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/expo.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css">
+
+</head>
+
+<body id="expo">
+
+	<p><a href="/2025/anmeldung#Bildungszeit">Zurück zur Startseite</a></p>
+
+	<section class="section">
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-lg-9 mb-4 mb-lg-0 mt-3">
+					<h3 id="Bildungszeit" name="Bildungszeit">Was ist Bildungszeit?</h3>
+			<p>Bildungszeit, umgangssprachlich auch Bildungsurlaub ist der gesetzlich verbriefte Anspruch auf Weiterbildung für Arbeitnehmer:innen im Rahmen der Arbeitszeit. Mit der Bildungszeit fördert der Staat die Initiative von Arbeitnehmer:innen zum lebenslangen Lernen.</p>
+			<p>Weiterbildung ist ständig notwendig, da Wissen veraltet oder seine Gültigkeit oder Bedeutsamkeit verlieren kann, neues oder aufbauendes Wissen ist notwendig. Die digitalisierte Arbeitswelt fordert immer wieder neue technische, organisatorische oder kommunikative Kompetenzen.</p>
+			<p>Eine Anerkennung als Bildungszeit gibt Arbeitnehmenden das Recht, die Veranstaltung während der Arbeitszeit zu besuchen und sich dafür freistellen zu lassen.<br>
+Der Mehrwert besteht darin, dass der Arbeitnehmer:in pro Jahr bis zu 5 Tage Bildungszeit zustehen, unter Weiterzahlung des Gehalts.</p>
+			<p>Dass heißt, es ist nicht notwendig für eine Teilnahme an der FOSSGIS-Konferenz den Erholungsurlaub zu beanspruchen, sondern die Konferenz kann als Weiterbildungsveranstaltung genutzt werden.</p>
+					
+			<h3>Wie läuft die Beantragung beim Arbeitgeber?</h3>
+			<p>Es ist jeweils das Bundesland zuständig, wo sich der Arbeitsort befindet. Die genauen Regelungen zu jedem Bundesland sind in jeweiligen Landesgesetzen geregelt. Nähere Erläuterungen sind auf der Website <a href="https://www.bildungsurlaub.de/ "><i>Bildungsurlaub.de</i></a> zu finden.</p>
+
+			<p>Reichen Sie bei Ihrem Arbeitgeber, am besten dort, wo Sie auch Ihren Antrag auf Erholungsurlaub anmelden, folgende Unterlagen ein:
+			<ul>
+			<li>Formloses Schreiben: “Hiermit mache ich meinen Anspruch auf Freistellung für Bildungszeit vom 26.03.bis 29.03.2025 geltend. Beigefügt sind Ablaufplan, Anerkennungsbescheid und Anmeldebestätigung”.</li>
+			</ul></p>
+			<p>Den inhaltlichen Ablaufplan finden Sie ab Ende des Jahres im <a href="https://fossgis-konferenz.de/2025/programm/">Programm</a>. Für die notwendige Anmeldebestätigung nutzen Sie entweder die Bestell-Email oder Ihre Rechnung, die Sie bei Konferenzticketbuchung erhalten. Anerkennungsbescheid siehe jeweiliges Bundesland.</p>
+<p>Nach dem Bildungsurlaub: Sie laden sich Ihre Teilnahmebescheingung, welche wir Ihnen in Ihrer Bestellung zur Verfügung stellen herunter und geben diese bei Ihrem Arbeitgeber ab.</p>
+
+<p>Für einige Länder gibt es Vordrucke für die Beantragung und Abrechnung beim Arbeitgeber. Hierzu informieren Sie sich bitte unter <a href="https://www.bildungsurlaub.de/ ">https://www.bildungsurlaub.de/</a>.</p>
+
+				
+			<h3 id="Anerkennung" name="Anerkennung">Anerkennung</h3>
+<p>Folgende Bundesländer erkennen die FOSSGIS-Konferenz als Bildungsurlaub an:</p>
+
+			<ul>
+			<li><b>Rheinland-Pfalz</b> - <a href="data/2024-11-27_Anerkennung_Bildungsveranstaltung_RheinlandPfalz.pdf">Download Anerkennungsbescheid</a>, Ihre Teilnahmebestätigung ist nach der Konferenz in Ihrer Bestellung downloadbar.
+			</li>
+			<li><b>Niedersachsen</b> - <a href="data/Anerkennung_Bildungzeit_FOSSGIS2025_Niedersachsen.pdf">Download Anerkennungsbescheid, <a href="data/Teilnahmebestaetigung_Bildungszeit_Niedersachsen.pdf">Vorlage Teilnahmebestätigung</a>
+			</li>
+			<li><b>Hamburg</b> - <a href="data/2025_Bescheid_Anerkennung_Bildungsveranstaltung_Hamburg.pdf">Download Anerkennungsbescheid</a>, <a href="data/Anmeldebestätigung_Hamburg.pdf">Download Anmeldeformular</a></li>
+			<li><b>Sachsen-Anhalt</b> - <a href="data/2024-12-03_Bescheid_Anerkennung_Bildungsveranstaltung_Sachsen-Anhalt.pdf">Download Anerkennungsbescheid</a>, Ihre Teilnahmebestätigung ist nach der Konferenz in Ihrer Bestellung downloadbar.
+			</li>
+			<li><b>Thüringen</b> - <a href="data/2024-12-02_Aenderungsbescheid_Anerkennung_Bildungsveranstaltung_TH.pdf">Download Anerkennungsbescheid</a>, Ihre Teilnahmebestätigung ist nach der Konferenz in Ihrer Bestellung downloadbar.
+			</li>			
+			<li><b>Schleswig-Holstein</b> - <a href="data/Anerkennung_Bildungszeit_Schleswig-Holstein_2025-02-10_document1619672.pdf">Download Anerkennungsbescheid</a>
+			</li>	
+			<li> Mehr ist für dieses Jahr nicht zu bekommen.</li>
+			</ul>
+			<p>Nutzen Sie für Ihre Beantragung Ihrer Bildungsfreistellung beim Arbeitgeber den jeweils verlinkten Anerkennungsbescheid.</p>
+			
+<!--			<h3 id="Antrag" name="Antrag" >Antrag</h3>
+<p>Für folgende Bundesländer ist die Anerkennung der FOSSGIS-Konferenz als Bildungsurlaub beantragt:</p>
+			<ul>
+			<li><b>Schleswig-Holstein</b></li>
+			</ul>-->
+
+			<h3 id="Ablehnung" name="Ablehnung" >Ablehnung</h3>
+<p>Folgende Bundesländer haben die FOSSGIS-Konferenz leider nicht als Bildungszeit anerkannt, von einem erneuten Antrag wird abgesehen:</p>
+			<ul>
+			<li><b>Bremen</b> </li>	
+			<li><b>Saarland</b></li>
+			<li><b>Brandenburg</b></li>
+			<li><b>Berlin</b> </li>
+			<li><b>Hessen</b></li>
+			</ul>
+																
+			<h3 id="keinAntrag" name="keinAntrag" >kein Antrag</h3>
+			<p>Für folgende Bundesländer ist die Anerkennung der FOSSGIS-Konferenz als Bildungszeit zu teuer, zu kompliziert oder gar nicht möglich</p>
+			<ul>
+			<li><b>Baden-Württemberg</b> </li>
+			<li><b>Bayern</b></li>
+			<li><b>Mecklenburg-Vorpommern</b> </li>
+			<li><b>Nordrhein-Westfalen</b></li>	
+			<li><b>Sachsen</b></li>
+			</ul>
+
+				</div>
+				<div class="col-lg-3">
+					<div class="mb-4">
+
+						<h6 class="mb-0">Kontakt<br>
+							Webseite:<br>
+						<a href="https://fossgis-konferenz.de/2025/anmeldung">
+							/anmeldung</a><br>
+
+							E-Mail: <br>
+							<a href="mailto:konferenz-orga@fossgis.de">konferenz-orga@fossgis.de</a><br></h6>
+					</div>
+
+						
+				
+					
+				</div>	
+
+			</div>
+		</div>
+
+	</section>
+
+	<?php include('../inc/footer.inc'); ?>
+</body>
---
+++ 2026/anmeldung/index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2026 - Anmeldung zur Konferenz</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+<link rel="stylesheet" type="text/css" href="https://pretix.eu/fossgis/2025/widget/v1.css" crossorigin>
+<script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async crossorigin></script>
+</head>
+
+
+
+<body id="anmeldung">
+
+	<?php include "../inc/header.inc"; ?>
+
+
+	<h2>Anmeldung zur Konferenz</h2>
+
+	<p>Wir laden Sie ein, Teil der FOSSGIS-Konferenz zu werden. Die Konferenz lebt von Ihren Beiträgen und Ihrem Besuch. Wir freuen uns auf Sie! </p>
+
+	<p>Vom 24.-27. März 2026 findet die FOSSGIS-Konferenz als Präsenzveranstaltung im <a href="https://lageplan.uni-goettingen.de/?piz=5257&etage=EG">Zentralen Hörsaalgebäude</a> der Uni Göttinen statt. <br> Der OSM-Samstag und Community Sprint findet am 28.03.2026 im <a href="https://lageplan.uni-goettingen.de/?piz=2410&etage=EG">Geographischen Institut</a> statt.</p>
+	<p>Im Folgenden finden Sie Informationen zur Teilnahme, zu den Workshops, Bildungsurlaub sowie ab Anfang Januar das Anmeldeformular. Das Programm ist <a href="https://fossgis-konferenz.de/2026/programm/">hier</a> veröffentlicht.</p>
+		
+<!--	<p class="highlight">
+		&darr; <a href="https://pretix.eu/fossgis/2024/">Direkt zur Anmeldung</a>
+	</p>-->
+
+	<h3>Preise FOSSGIS-Konferenz 2026</h3>
+
+	<ul>
+		<li>Konferenzticket: 360 €</i>
+		<li>Konferenzticket - reduzierter Preis*: 130 €</i>
+		<li>Konferenzticket - Community (OpenStreetMap und FOSSGIS)***: 0 €</i>
+		<li>Konferenzticket - Beitragende***: 0 bis 360 €</i>
+		<li>Konferenzticket - Helfende***: 0 bis 360 €</i>
+		<li>Workshop 90 min**: 130 €</i>
+		<li>Workshop 180 min**: 180,00 €</i>
+		<li>Onlinekonferenzticket: 100 €</i>
+		<li>Onlinekonferenzticket - reduzierter Preis*: 30 €</i>
+		<li>Onlinekonferenzticket - Community (OpenStreetMap und FOSSGIS)***: 0 €</i>
+		<li>OSM-Samstag: kostenfrei</li>
+		<li>Community-Sprint am Samstag: kostenfrei</li>
+		  		
+	</ul>
+		
+	<ul><p><b><i>Das Konferenzticket enthält:</i></b></p>
+	<ul>
+		<li>FOSSGIS-Konferenz-Teilnahme in Göttingen</li>
+		<li>Pausensnack</li>
+		<li>Konferenztasche</li>
+		<li>Eine ordentliche Rechnung unter ihrem persönlichen <a href="https://fossgis-konferenz.de/2026/faq.php">Buchungslink</a>.</li>
+		<li>Onlinezugang zu Vorträgen, Anwendertreffen, Chat der Teilnehmenden und Fragenfunktion</li>
+		<li>Eine Mail am Donnerstag vor der Konferenz mit wichtigen Informationen.</li></ul>
+	
+	<p><b><i>Das Onlinekonferenzticket enthält:</i></b></p>
+	<ul>
+		<li>Online-Teilnahme mit Zugang zu Vorträgen, Anwendertreffen, Chat der Teilnehmenden und Fragenfunktion.</li>
+		<li>Eine ordentliche Rechnung unter ihrem persönlichen <a href="https://fossgis-konferenz.de/2026/faq.php">Buchungslink</a>.</li>
+		<li>Eine Mail am Donnerstag vor der Konferenz mit wichtigen Informationen.</li>
+	</ul>
+	<p><b><i>Zusatzprodukte:</i></b></p>
+	<ul>
+		<li>Teilnahme am Exkursionsangebot: Preis abhänging vom jeweiligen Exkursionsangebot</li>
+		<li>Abendveranstaltung**, Tagungsband**, T-Shirt** sind grundsätzlich inkludiert, eine <a href="https://fossgis.de/verein/spenden/">Spende</a> dafür ist gern gesehen.</li>
+		
+	</ul>
+
+	<p><small>* Reduzierter Preis gilt für: Schüler:in, Azubi, Freiwilligendienstleistende, Studierende, Rentner:in, Arbeitslose - Nachweis erforderlich.</small><br>
+		<small>** Wenn bestellt und solange verfügbar.</small><br>
+		<small>*** Das kostenfreie Ticket ist an Bedingungen geknüpft und wird als personalisiertes Ticket herausgegeben. Grundsätzlich gilt, wer etwas zur FOSSGIS-Konferenz beiträgt, kann ein kostenfreies Konferenzticket erhalten. Ein adäquater Beitrag ist z.B. ein Vortrag (LT zählt nicht), Workshop, mind. 10 Stunden <a href="https://fossgis-konferenz.de/2026/helfen/">helfen</a> oder nach den Richtlinien der <a href="https://blog.openstreetmap.org/2020/08/20/active-osm-contributor-membership-program/">OSMF</a>-OpenStreetMap Foundation aktiv mappen (42 Mappingtage in den letzten 365 Tage) bzw. aktiv zur Entwicklung von freier GIS-Softwareentwicklung beitragen.</small></p>
+
+	<h3 id="Workshop" name="Workshop">Workshop buchen</h3>
+	<p>Das Programm bietet die Möglichkeit einen oder mehrere Workshops zu buchen. Einen Workshop können Sie im zweiten Schritt des Bestellvorgangs zum Konferenzticket hinzubuchen.</p>
+	<ul><li>Am Dienstag, den 24.03.2026 finden 180-minütige Workshops statt, der Preis beträgt 180 € pro Workshop.</li>
+	<li>Von Mittwoch, 25.03. bis Freitag, 27.03.2026 finden 90-minütige Workshops statt, der Preis beträgt 130 € pro Workshop.</li> </ul>
+	<p>Der Workshop ist interaktiv und wird in einem mit Laptops ausgestattenen Raum im <a href="https://lageplan.uni-goettingen.de/?piz=5361&etage=1.OG">Verfügungsgebäude</a> unweit des Zentralen Hörsaalgebäudes stattfinden. Die meisten Workshops arbeiten mit der aktuellen <a href="http://live.osgeo.org/de/index.html">OSGeoLive.</a> Es finden keine Onlineworkshops statt.</p>
+	<h4> Workshop nachbuchen</h4>
+	<p>Sie haben schon ein Ticket und wollen einen Workshop nachbuchen? <br>
+		Schreiben Sie eine <a href="mailto:konferenz-orga@fossgis.de?subject=Workshop_nachbuchen">E-Mail</a> mit folgenden Angaben: Bestellnummer Ticketbuchung; Name Teilnehmer:in; Email-Adresse Teilnehmer:in; welcher Workshop. Eine Gebühr von 130 bzw. 180 € wird pro Workshop in Rechnung gestellt.</p>
+	
+	<h3>Helfen</h3>
+	<p>Freiwillige Helfer:innen sind eingeladen und willkommen während der Konferenz zu unterstützen. Bei Interesse bitte die Webseite <a href="https://fossgis-konferenz.de/2026/helfen/#HelfenvorOrt">Helfen</a> lesen, dort sind viele Details erklärt.<br>Helfer:innen registrieren sich ab Ende Dezember im Helfer:innensystem. Wer mind. 10 Stunden hilft, kann ein kostenfreies Konferenzticket erhalten und schreibt dazu eine <a href="mailto:konferenz-orga@fossgis.de?subject=Helfen_Helferticket">E-Mail</a>. </p>
+
+<!--	<h3 id="Anmeldeformular" name="Anmeldeformular">Anmeldeformular</h3>
+<!--	<p>Nutzen Sie gerne das Anmeldeformular direkt auf dieser Seite. Falls etwas nicht funktionieren sollte, können Sie das Ticketsystem auch unter <a href="https://pretix.eu/fossgis/2026/" target="_blank">diesem Link</a> direkt aufrufen.</p>-->
+<!--	<p>Die FOSSGIS ist ausgebucht. Wir können keine weiteren Tickets für die Konferenz vor Ort in Göttingen anbieten.
+Nutzen Sie die Möglichkeit einer Onlineteilnahme und buchen Sie ein Onlineticket.
+Sie erhalten Zugang zum Livestream der Vorträge mit Fragenfunktion und Chat der Teilnehmenden sowie Community Sessions, wie Anwendertreffen und Expert:innenfragestunde, die hybrid stattfinden werden.</p>
+	
+<div id="shop">
+		<pretix-widget event="https://pretix.eu/fossgis/2026/" single-item-select="button"></pretix-widget>
+<noscript>
+   <div class="pretix-widget">
+        <div class="pretix-widget-info-message">
+            JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticketshop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/fossgis/2026/">hier</a>.
+        </div>
+    </div>
+</noscript>-->
+		
+<!--	<h3 id="Bildungszeit" name="Bildungszeit">Bildungszeit</h3>
+	<p>Die Anerkennung der FOSSGIS-Konferenz als Bildungszeit wird angestrebt und in verschiedenen Bundesländern in Deutschland vom Orgateam beantragt. </p>
+	<p> Bildungszeit, umgangssprachlich auch Bildungsurlaub ist der gesetzlich verbriefte Anspruch auf Weiterbildung für Arbeitnehmer:innen im Rahmen der Arbeitszeit. Eine Anerkennung einer Veranstaltung als Bildungszeit gibt Arbeitnehmer:innen das Recht, die Veranstaltung während der Arbeitszeit zu besuchen. Arbeitnehmenden stehen bis zu 5 Tage Bildungszeit pro Jahr zu.</p>
+
+	<p>Weitere Informationen zum Bildungszeit und welches Bundesland die FOSSGIS-Konferenz 2026 als Bildungszeit anerkannt hat, finden Sie unter <a href="https://fossgis-konferenz.de/2026/anmeldung/bildungszeit.php">Anmeldung/Bildungszeit</a>.</p>-->
+	
+
+	<h3>Sie haben Fragen?</h3>
+	<p> Vielleicht finden Sie hier Antworten.</p>
+
+	<ul>
+	<li> <a href="https://fossgis-konferenz.de/2026/faq.php">FAQ</a></li>
+	<li>Hier erreichen Sie das Konferenzteam</li>
+
+	<ul>
+	<li>Matrixkanal: <a href="https://matrix.to/#/!XajsOFLRhsznebfosT:matrix.org?">#FOSSGIS2026:matrix.org </a></li>
+	<li>E-Mail an <a href="mailto:konferenz-orga@fossgis.de?subject=Konferenzorganisationsteam_erreichen">konferenz-orga@fossgis.de</a> </li>
+<!--	<li>Anruf: +49 30-62932037</li>-->
+	<li>Fragen zum Programm an das <a href="mailto:programm@fossgis.de?subject=Programmkomitee_erreichen">Programmkomitee</a> </li>
+	</ul></ul>
+
+	</p>
+
+	<?php include('../inc/footer.inc'); ?>
+
+</body>
+
+</html>
