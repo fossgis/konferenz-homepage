829e63e7ba01790bf2121de359e0fb5e5d069fed
katjahafi <katjahafi@gmx.de>
2026-05-26 15:58:07 +0200

    Startseite FOSSGIS 2027 erstellt

---
+++ 2027/index.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<html>
+<head>
+	<?php include "inc/meta.inc"; ?>
+<base href="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+   	<title>FOSSGIS 2027</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+	<link rel="stylesheet" href="./fontawesome/css/all.css" />
+</head>
+
+<body id="home">
+
+	<?php include "inc/header.inc"; ?>
+
+      <h1>Willkommen zur FOSSGIS-Konferenz 2027</h1>
+
+            <p>Die FOSSGIS-Konferenz ist im D-A-CH-Raum die führende Konferenz für Freie und Open Source Software für Geoinformationssysteme sowie für die Themen Open Data und OpenStreetMap.</p>
+
+            <p>Die FOSSGIS-Konferenz 2027 wird vom gemeinnützigen <a href="https://www.fossgis.de/">FOSSGIS e.V</a>, der <a href="https://www.openstreetmap.de/">OpenStreetMap Community</a> in Kooperation mit dem <a href="https://heigit.org/de/">HeiGIT</a> und der <a href="https://www.uni-heidelberg.de/de">Universität Heidelberg </a> organisiert und findet auf dem Unicampus Heidelberg statt.</p>
+
+      <div>    <ul class="tiles">
+				<li class="tile silver">
+                     <a href="./sponsor/fossgis-ev.php">
+                         <img src="./img/FOSSGISev_ohneVerlauf.png" alt="FOSSGIS e.V."  align="center">
+                     </a>
+                 </li>
+
+                 <li class="tile silver">
+                     <a href="https://www.openstreetmap.de/">
+                         <img src="./img/osm.png" alt="OSM Community" align="center">
+                     </a>
+                 </li>                                   
+                 <li class="tile silver">
+                     <a href="https://www.osgeo.org/">
+                         <img src="img/osgeo.png" Alt="OSGeo" align="center">
+					 </a>
+				 </li>
+	
+  		 <li class="tile platin">
+		     <a href="https://heigit.org/de/">
+			 <img src="https://heigit.org/wp-content/uploads/2024/07/HeiGIT_Logo_base.png" alt="HeiGIT" align="center">
+					 </a>
+                 </li>  
+                 <li class="tile platin">
+		     <a href="hhttps://www.uni-heidelberg.de/de">
+			 <img src="https://backend.uni-heidelberg.de/sites/default/files/site_logo/inverse/logo_3.svg" alt="Uni Heidelberg" align="center">
+					 </a>
+                 </li>  
+
+	  
+            </ul> </div>
+           
+            <h3>Was ist die FOSSGIS-Konferenz?</h3>
+
+            <p>Ziel der jährlich stattfindenden Konferenz ist die Verbreitung von Freier und Open Source Software für Geoinformationssysteme sowie Open Data. Hier treffen sich Anwender:innen, Entwickler:innen, Forscher:innen und Interessierte zum gemeinsamen Austausch über Anwendungs-, Arbeits- und Weiterentwicklungsmöglichkeiten sowie neuste Entwicklungen in diesem Bereich. 
+Die Themen Geodateninfrastrukturen, Geodatenmanagement,  Geoprocessing, Geodatenanalysen und -modellierungen, Desktop-GIS und freie Geodaten, wie OpenStreetMap werden genauso beleuchtet, wie es um Digitale Souveränität im Zusammenhang mit Open Source sowie Open Science und die FAIR Prinzipien geht.</p> 
+
+
+<!--			<p>Die Konferenz wird vorwiegend durch ehrenamtliches Engagement getragen. Die Teilnahmegebühren, Spenden- und Sponsoren- und Ausstellerstandeinnahmen stellen eine wichtige finanzielle Einnahmequelle dar, um die Kosten für die Konferenz zu decken und darüber hinaus Open Source Projekte zu fördern.</p> 
+			
+		<h3 id="Sponsoren" name="Sponsoren">Sponsoren der FOSSGIS-Konferenz</h3>
+			<p> Die FOSSGIS wird durch die freundliche Unterstützung der Sponsoren möglich.</p><br>
+
+    		<h4 id="Platin" name="Platin" class="highlight">Platinsponsoren</h4>
+			<ul class="tiles">
+			<li class="tile platin">
+			  <a href="sponsor/001_wheregroup.php" target="_blank">
+			  <img src="./img/s/001_WhereGroup.jpg" alt="WhereGroup">
+			  </a>
+			</li>
+			<li class="tile platin">
+			<a href="sponsor/002_OPENGis_CH.php" target="_blank">
+			<img src="./img/s/002_QFieldCloud-by-OpenGIS.png" alt="opengis">
+			</a>
+			</li>
+			<li class="tile platin">
+			  <a href="sponsor/003_dataport.php" target="_blank">
+			  <img src="./img/s/003_dataport.png" alt="Dataport">
+			  </a>
+			</li>
+			</ul>
+
+
+			<h4 id="Gold" name="Gold" class="highlight">Goldsponsoren</h4>
+			
+			<li class="tile gold">
+			<a href="sponsor/101_bkg.php" target="_blank">
+			<img src="./img/s/101_BKG_Logo_RGB.png" alt="BKG">
+			</a>
+			</li> 
+<!--		
+
+			<li class="tile gold">
+			<a href="sponsor/105_openSenseLab.php" target="_blank">
+			<img src="./img/s/105_openSenseLab-Logo.png" alt="openSenseLab">
+			</a>
+			</li>
+			</ul>
+
+			<h4 id="Silber" name="Silber" class="highlight">Silbersponsoren</h4>
+			<ul class="tiles">
+			<li class="tile silver">
+			<a href="sponsor/201_terrestris_mundialis.php" target="_blank">
+			<img src="./img/s/201_Logos_fusioniert_neu-02.png" alt="terrestris mundialis">
+			</a>
+			</li>
+			<li class="tile silver">
+			  <a href="sponsor/202_camp2camp.php" target="_blank">
+ 			  <img src="./img/s/202_camptocamp.png" alt="camptocamp">
+			  </a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/203_LGV.php" target="_blank">
+			<img src="./img/s/203_LGV_HH.png" alt="LGV">
+			</a>
+			</li>	
+			<li class="tile silver">
+			<a href="sponsor/204_sourcepole.php" target="_blank">
+			<img src="./img/s/204_sourcepole.png" alt="sourcepole">
+			</a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/205_lutraconsulting.php" target="_blank">
+			<img src="./img/s/205_MM_logo_HORIZ_COLOR.jpg" alt="Lutra Consulting ltd.">
+			</a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/206_geoSYS.php" target="_blank">
+			<img src="./img/s/206_geoSYS_logo.png" alt="geoSYS">
+			</a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/207_gbd.php" target="_blank">
+			<img src="./img/s/207_GBD.png" alt="Geoinformatikbüro Dassau">
+			</a>
+			</li>
+	<li class="tile silver">
+			<a href="sponsor/208_GIS-Consult.php" target="_blank">
+			<img src="./img/s/208_GIS-Consult_Logo.png" alt="GIS-Consult">
+			</a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/209_Feelgood.php" target="_blank">
+			<img src="./img/s/209_FeelGood_logo.png" alt="FeelGood">
+			</a>
+			</li>
+
+			</ul>
+<!--			<li class="tile silver">
+			<a href="sponsor/201_CONET-ISB.php" target="_blank">
+			<img src="./img/s/201_ISB_CONET.jpg" alt="CONET ISB GmbH">
+			</a>
+			</li>
+			<li class="tile silver">
+			<a href="sponsor/202_EFTAS.php" target="_blank">
+			<img src="./img/s/202_EFTAS.png" alt="EFTAS">
+			</a>
+			</li>
+	
+			
+	<h4 id="Bronze" name="Bronze" class="highlight">Bronzesponsoren</h4>
+		<ul class="tiles">
+			<li class="tile bronze">
+			<a href="sponsor/301_latlon.php" target="_blank">
+			<img src="./img/s/301_latlon_Logo_neu.png" alt="latlon">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/302_geofabrik.php" target="_blank">
+			<img src="./img/s/302_Geofabrik_logo4.png" alt="geofabrik">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/303_TomTom.php" target="_blank">
+			<img src="./img/s/303_TomTom.png" alt="TomTom">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/304_Adesso.php" target="_blank">
+			<img src="./img/s/304_adesso_basic_Color_CMYK.png" alt="Adesso SE">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/305_geOps.php" target="_blank">
+			<img src="./img/s/305_geOps.png" alt="geOps AG">
+			</a>
+			</li>
+
+			<li class="tile bronze">
+			<a href="sponsor/306_MDPI-AG.php" target="_blank">
+			<img src="./img/s/306_IJGI-geomatics.png" alt="MDPI AG">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/307_grit.php" target="_blank">
+			<img src="./img/s/307_grit.png" alt="grit GmbH ">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/308_dvz-mv.php" target="_blank">
+			<img src="./img/s/308_DVZ-MV.png" alt="DVZ-MV GmbH ">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/309_NRW-Mobidrom.php" target="_blank">
+			<img src="./img/s/309_Mobidrom.png" alt="NRW.Mobidrom">
+			</a>
+			</li>
+
+<!--  		
+			<li class="tile bronze">
+			<a href="sponsor/404_opencage.php" target="_blank">
+			<img src="./img/s/404_opencage.png" alt="OpenCage">
+			</a>
+			</li>
+
+			<li class="tile bronze">
+			<a href="sponsor/406_geofabrik.php" target="_blank">
+			<img src="./img/s/406_Geofabrik_logo4.png" alt="geofabrik">
+			</a>
+			</li>
+
+
+			<li class="tile bronze">
+			<a href="sponsor/409_52north.php" target="_blank">
+			<img src="./img/s/409_52North.png" alt="52°North Spatial Information Research GmbH">
+			</a>
+			</li>
+			<li class="tile bronze">
+			<a href="sponsor/410_komoot.php" target="_blank">
+			<img src="./img/s/410_komoot.png" alt="komoot">
+			</a>
+			</li>
+				
+	
+			<li class="tile bronze">
+			<a href="sponsor/414_geocockpit.php" target="_blank">
+			<img src="./img/s/414_YEY-Maps_Logo.png" alt="GeoCockpit UG">
+			</a>
+			</li>	
+			<li class="tile bronze">
+			<a href="sponsor/415_GISCAD.php" target="_blank">
+			<img src="./img/s/415_GISCAD_Technologies.svg" alt="GISCAD">
+			</a>
+			</li>			
+
+			<li class="tile bronze">
+			<a href="sponsor/417_gkg.php" target="_blank">
+			<img src="./img/s/417_gkg_logo.png" alt="gkg">
+			</a>
+			</li>
+</ul>
+
+			<h4 id="Medienpartner" name="Medienpartner"class="highlight">Medienpartner</h4>
+				<ul class="tiles">
+					<li class="tile media">
+						<a href="https://live.osgeo.org/de/index.html" target="_blank">
+						<img src="./img/s/501_OSGeoLive_banner.png" alt="OSGeoLive">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://c3voc.de/events/" target="_blank">
+						<img src="./img/s/502_voc_square.png" alt="C3VOC">
+						</a>
+					</li>
+					<li class="tile media">
+						<a href="https://av.tib.eu/search?f=publisher%3Bhttp://av.tib.eu/resource/FOSSGIS_e.V." target="_blank">
+						<img src="./img/s/503_TIB.PNG" alt="TIB">
+						</a>
+					</li>
+									<li class="tile media">
+						<a href="https://www.oreilly.de/" target="_blank">
+						<img src="./img/s/506_OReilly_logo.jpg" alt="O'Reilly">
+						</a>
+					</li>
+
+					<li class="tile media">
+					<a href="https://www.gogeogo.com/de" target="_blank">
+					<img src="./img/s/504_GGGlogo.svg" alt="GoGeoGo">
+					</a>
+					</li>
+
+					</ul>
+
+<!--			<h4 id="orangerStreifen" name="orangerStreifen" class="highlight"></h4>
+
+			<h3 id="Jobwand" >Jobbörse</h3>
+			<p>Es gibt wieder eine Jobbörse im Rahmen der Konferenz. Dafür stellen wir eine Pinnwand ins Foyer, dort können Stellenanzeigen angebracht werden. In Kooperation mit GoGeoGo gibt es vom 09.03.-09.04. eine digitale Jobbörse. Beim Eintragen bitte die Auftragsnummer <i>FOSSGIS2026</i> eintragen, dann erscheint die Stellenanzeige in der <a href="https://www.gogeogo.com/de/company/fossgis-2026" target="_blank"> FOSSGIS 2026 Jobbörse</a>.
+					</p>
+					
+			<h4 id="orangerStreifen" name="orangerStreifen" class="highlight"></h4>
+
+			
+			<h3 ID="Sponsoring" name="Sponsoring">Werden Sie Sponsor!</h3> 
+           	  	  <p>Sie möchten die FOSSGIS-Konferenz mit einem Sponsoring unterstützen? Sie finden Informationen zum Sponsoring und zur Firmenausstellung in der <a href="https://files.fossgis.de/Konferenz/2026/Sponsoreninformationen_FOSSGIS-2026.pdf" target="_blank" >Sponsoringbroschüre</a>. Für die Vereinbarung eines Sponsorings, senden Sie die ausgefüllte <a href="https://files.fossgis.de/Konferenz/2026/FOSSGIS-Konferenz-Sponsoring-Vereinbarung_2026.pdf" target="_blank">Sponsoringvereinbarung</a>.</p>
+			<p>Bei Fragen wenden Sie sich an das <a href="mailto:konferenz-orga@fossgis.de? subject=Konferenzorganisationsteam_erreichen">Konferenzorganisationsteam</a>.</p>
+-->
+  
+        <?php include('inc/footer.inc'); ?>
+ 
+</body>
+</html>
